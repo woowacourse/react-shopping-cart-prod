2cb876cf1a1505cf063ecc6c21034ca58f90c64c
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-05-29 01:31:09 +0900

    ♻️ : reselect를 활용한 체크 목록 종북 계산 방지

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,15 +1,23 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
 
 import * as cartAction from 'actions/cart/action';
 import * as cartThunk from 'actions/cart/thunk';
+
+const selectItems = (state) => state.items;
+const selectCheckedItemList = createSelector(selectItems, (cartItems) => {
+  console.log('계산 시도!');
+  return cartItems.filter(({ isChecked }) => isChecked === true);
+});
 
 function useCart() {
   const dispatch = useDispatch();
   const cartState = useSelector((state) => state.cart);
   const { items, listAsyncState, curdAsyncState } = cartState;
 
-  const checkedItemList = items.filter(({ isChecked }) => isChecked === true);
+  console.log('값 로드!', items);
+  const checkedItemList = selectCheckedItemList(cartState);
 
   useEffect(() => {
     dispatch(cartThunk.getList());
