425c397dd7a5ffd8c8aff0be3eaa42127e80f709
intae92 <kangit2013@gmail.com>
2022-05-31 22:06:47 +0900

    feat: ValidateText 컴포넌트 구현
    Co-authored-by: moonheekim0118 <hellomooneekim@gmail.com>

---
+++ src/components/Common/ValidateText/ValidateText.stories.js
@@ -0,0 +1,10 @@
+import ValidateText from 'components/Common/ValidateText/ValidateText';
+
+export default {
+  title: 'components/ValidateText',
+  component: ValidateText,
+};
+
+const Template = (args) => <ValidateText {...args} />;
+
+export const Example = Template.bind({});
