46ca56ce1f5ebc25cab25d1136c03ba8e73ccc58
JUDONGHYEOK <judonghyeok@gmail.com>
2022-05-31 21:02:13 +0900

    refactor: signUp Form style 수정

--- src/components/SignUpForm/style.js
+++ src/components/SignUpForm/style.js
@@ -1,14 +1,12 @@
 import styled from 'styled-components';
 
 export default styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   width: 440px;
   padding: 80px 50px;
   border: 1px solid ${({ theme }) => theme.gray_100};
 
   .title {
+    text-align: center;
     margin-bottom: 35px;
     font-size: 3.2rem;
     font-weight: 800;
