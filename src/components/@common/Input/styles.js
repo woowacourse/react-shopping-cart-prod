64c66292975d8325cb1dd757d6bd3d6d435e7869
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-05-31 17:19:25 +0900

    feat: Input 컴포넌트 구현
    Co-authored-by: JASUN LEE <liswktjs@gmail.com>

---
+++ src/components/@common/Input/styles.js
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+import { COLORS } from 'styles/theme';
+
+const Input = styled.input`
+  cursor: text;
+  width: ${(props) => props.width || '100%'};
+  height: ${(props) => props.height || '70px'};
+  border: ${(props) => props.border || 'none'};
+  margin: ${(props) => props.margin || '1rem 0'};
+  padding: ${(props) => props.padding || '15px;'};
+  font-size: ${(props) => props.size || '1.16rem'};
+  border-radius: 4px;
+  outline: none;
+  &:focus {
+    border-color: ${(props) => props.focusBorderColor || COLORS.MINT_200};
+  }
+`;
+
+export default Input;
