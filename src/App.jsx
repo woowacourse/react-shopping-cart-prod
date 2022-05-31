88798831f287970de157dca0929c5764f8650ec7
airman5573 <rpf5573@kakao.com>
2022-05-31 19:38:07 +0900

    refactor(component): react import없이 jsx를 사용한다

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "@shared/header/Header";
 import styles from "@/app.module";
 import Home from "@home/Home";
