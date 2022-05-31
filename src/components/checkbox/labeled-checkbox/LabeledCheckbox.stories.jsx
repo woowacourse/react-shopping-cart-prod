ea9728832a0737dd4ff6e68fb5b68e159b366696
airman5573 <rpf5573@kakao.com>
2022-05-31 19:39:36 +0900

    refactor(lint): lint를 적용한다

--- src/components/checkbox/labeled-checkbox/LabeledCheckbox.stories.jsx
+++ src/components/checkbox/labeled-checkbox/LabeledCheckbox.stories.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import LabeledCheckbox from "@shared/checkbox/labeled-checkbox/LabeledCheckbox";
 
 export default {
@@ -7,16 +7,17 @@ export default {
   argTypes: {},
 };
 
-const Template = (args) => <LabeledCheckbox {...args} />;
+function Template(args) {
+  return <LabeledCheckbox {...args} />;
+}
 
 export const Primary = Template.bind({});
 
 Primary.args = {
-  id: 'input-id',
-  label: '라벨입니다',
+  id: "input-id",
+  label: "라벨입니다",
 };
 
 Primary.argTypes = {
   id: { control: { disable: true } },
 };
-
