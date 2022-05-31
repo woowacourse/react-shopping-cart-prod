29301751cbabcb2c6a55b33430d699de07bf4511
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-05-31 21:20:46 +0900

    refactor: 로그인 페이지 스타일 컴포넌트 파일 분리
    Co-Authored-By: DomMorello <zuzudnf@gmail.com>

--- src/components/pages/LoginPage/index.jsx
+++ src/components/pages/LoginPage/index.jsx
@@ -1,37 +1,10 @@
-import styled from "styled-components";
-
 import { theme } from "style";
 
 import PageHeader from "components/common/PageHeader";
 import UserInput from "components/common/UserInput";
 import DefaultButton from "components/common/Button/DefaultButton";
 import UserForm from "components/common/UserForm";
-
-export const LoginPageContainer = styled.section`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: column;
-  gap: 12px;
-`;
-
-export const LoginInputContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 620px;
-  align-items: center;
-`;
-
-export const LoginButtonContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 12px;
-
-  padding-top: 32px;
-
-  border-top: 1px solid ${({ theme: { color } }) => color.gray03};
-`;
+import { LoginPageContainer, LoginButtonContainer } from "./styled";
 
 function LoginPage() {
   return (
