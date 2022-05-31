7c826e3913fb08f9ce808ee275c91b0f53e8cf0d
Yo Wook Kim <uk960214@gmail.com>
2022-05-31 16:38:49 +0900

    feat: 회원가입 페이지 UI 구현
    Co-authored-by: Woo seung <woose28@users.noreply.github.com>

--- src/components/common/Input/Input.jsx
+++ src/components/common/Input/Input.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import * as Styled from 'components/common/Input/Input.style';
 import { Button } from 'components/common';
 
-function Input({ labelText, type = 'text', buttonText, onButtonClick }) {
+function Input({ type = 'text', labelText, placeholder, buttonText, onButtonClick }) {
   return (
     <Styled.Label>
       {labelText}
       <Styled.InputWrapper>
         <Styled.Input
           type={type}
-          placeholder="example@woowacourse.com"
+          placeholder={placeholder}
           hasButton={buttonText && onButtonClick}
         />
         {buttonText && onButtonClick && (
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,34 +3,41 @@ import React from 'react';
 import { Form, Input, PageTemplate, PageTitle } from 'components/common';
 
 function Register() {
+  const inputDescriptionList = [
+    {
+      name: 'email',
+      type: 'email',
+      labelText: '이메일 주소',
+      placeholder: 'example@woowacourse.com',
+      buttonText: '중복 확인',
+      onButtonClick: () => console.log('중복확인 버튼 클릭'),
+    },
+    {
+      name: 'password',
+      type: 'password',
+      labelText: '비밀번호',
+      placeholder: '비밀번호를 입력해주세요',
+    },
+    {
+      name: 'password-confirm',
+      type: 'password',
+      labelText: '비밀번호 확인',
+      placeholder: '비밀번호를 다시 입력해주세요',
+    },
+    {
+      name: 'nickname',
+      type: 'text',
+      labelText: '닉네임',
+      placeholder: '블링',
+    },
+  ];
   return (
     <PageTemplate>
       <PageTitle>회원가입</PageTitle>
       <Form buttonText="회원 가입" onSubmit={() => console.log('제출!')}>
-        <Input
-          type={'email'}
-          labelText={'이메일 주소'}
-          buttonText={'중복 확인'}
-          onButtonClick={() => console.log('buttonClicked')}
-        />
-        <Input
-          type={'email'}
-          labelText={'이메일 주소'}
-          buttonText={'중복 확인'}
-          onButtonClick={() => console.log('buttonClicked')}
-        />
-        <Input
-          type={'email'}
-          labelText={'이메일 주소'}
-          buttonText={'중복 확인'}
-          onButtonClick={() => console.log('buttonClicked')}
-        />
-        <Input
-          type={'email'}
-          labelText={'이메일 주소'}
-          buttonText={'중복 확인'}
-          onButtonClick={() => console.log('buttonClicked')}
-        />
+        {inputDescriptionList.map((inputDescription) => (
+          <Input key={inputDescription.name} {...inputDescription} />
+        ))}
       </Form>
     </PageTemplate>
   );
