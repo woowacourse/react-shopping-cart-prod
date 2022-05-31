72ab33e69adbe60af81ded8385e822744510951b
gytks4 <gytks4@naver.com>
2022-05-31 22:03:28 +0900

    feat: 로그인 로직 구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,33 @@
+import { authClient } from 'apis';
 import AuthPage from 'components/common/AuthPage';
 import LabeledInput from 'components/common/LabeledInput';
 import useInput from 'hooks/useInput';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { PATH } from 'Routers';
 import styled from 'styled-components';
+import type { LoginResponse } from 'types/domain';
 
 const Login = () => {
   const [email, onChangeEmail] = useInput();
   const [password, onChangePassword] = useInput();
+  const navigate = useNavigate();
 
-  const onSubmitAuthForm = (event: React.FormEvent<HTMLFormElement>) => {
+  const onSubmitAuthForm = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    const signInResponse = await authClient.post<LoginResponse>('/login', {
+      loginId: email,
+      password,
+    });
+
+    if (signInResponse.status === 401) {
+      throw new Error('로그인에 실패하였습니다');
+    }
+    const { accessToken, name } = signInResponse.data;
+
+    localStorage.setItem('access-token', accessToken);
+    alert(`${name}님 로그인 되었습니다.`);
+    navigate(PATH.home);
   };
 
   return (
