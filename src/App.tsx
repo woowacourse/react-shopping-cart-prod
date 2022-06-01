c4772e3d6175e7208d8197195b0c35880682342c
이아현 <lah1203@naver.com>
2022-06-01 18:03:53 +0900

    feat: 헤더 레이아웃 생성
    - 로그인, 회원가입 제외, 헤더 보임
    Co-authored-by: al-bur <al-bur@users.noreply.github.com>
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import CartPage from 'pages/CartPage/CartPage';
 import EditUserInfoPage from 'pages/EditUserInfoPage/EditUserInfoPage';
 import GlobalStyle from 'styles/GlobalStyle';
-import Header from 'components/Layout/Header/Header';
+import HeaderLayout from 'components/Layout/HeaderLayout/HeaderLayout';
 import LoginPage from 'pages/LoginPage/LoginPage';
 import MainPage from 'pages/MainPage/MainPage';
 import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
@@ -18,14 +18,15 @@ function App() {
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <Header />
         <Routes>
-          <Route path={PATH.BASE} element={<MainPage />} />
-          <Route path={`${PATH.PRODUCT}/:id`} element={<ProductPage />} />
-          <Route path={PATH.CART} element={<CartPage />} />
+          <Route path={PATH.BASE} element={<HeaderLayout />}>
+            <Route path={PATH.BASE} element={<MainPage />} />
+            <Route path={`${PATH.PRODUCT}/:id`} element={<ProductPage />} />
+            <Route path={PATH.CART} element={<CartPage />} />
+            <Route path={PATH.EDIT_USER_INFO} element={<EditUserInfoPage />} />
+          </Route>
           <Route path={PATH.LOGIN} element={<LoginPage />} />
           <Route path={PATH.SIGNUP} element={<SignupPage />} />
-          <Route path={PATH.EDIT_USER_INFO} element={<EditUserInfoPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </ThemeProvider>
