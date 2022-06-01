8a7c7c93d498b8a62680623aaa11b67ad3fec0b8
이아현 <lah1203@naver.com>
2022-06-01 16:37:21 +0900

    style: 로그인 UI 수정
    Co-authored-by: al-bur <al-bur@users.noreply.github.com>
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,6 @@
 import CheckBox from 'components/@shared/CheckBox';
 import Link from 'components/@shared/Link';
+import PATH from 'constants/path';
 import { ReactComponent as ZzangguLogo } from 'assets/Zzanggu.svg';
 import styled from 'styled-components';
 import { useState } from 'react';
@@ -20,13 +21,17 @@ function LoginPage() {
       <StyledLoginContainer>
         <header>
           <StyledTitle>로그인</StyledTitle>
-          <ZzangguLogo width={200} height={200} />
+          <ZzangguLogo width={200} height={180} />
         </header>
         <StyledForm>
-          <label>아이디</label>
-          <input type="text" placeholder="아이디를 입력해주세요" />
-          <label>비밀번호</label>
-          <input type="password" placeholder="비밀번호를 입력해주세요" />
+          <label htmlFor="id">아이디</label>
+          <input id="id" type="text" placeholder="아이디를 입력해주세요" />
+          <label htmlFor="password">비밀번호</label>
+          <input
+            id="password"
+            type="password"
+            placeholder="비밀번호를 입력해주세요"
+          />
           <StyledLoginHelper>
             <StyledKeepLogin>
               <CheckBox
@@ -35,7 +40,7 @@ function LoginPage() {
                 onChange={toggleChecked}
                 marginBottom="0px"
               />
-              <label>로그인 상태 유지</label>
+              <label htmlFor="keep-login">로그인 상태 유지</label>
             </StyledKeepLogin>
             <StyledFindLoginInfo>
               <Link to="#">아이디 찾기</Link>
@@ -45,7 +50,7 @@ function LoginPage() {
           <StyledLoginButton type="submit">로그인</StyledLoginButton>
         </StyledForm>
         <StyledSignupLinkGuide>
-          아직 회원이 아니신가요? <Link to="#">회원가입</Link>
+          아직 회원이 아니신가요? <Link to={PATH.SIGNUP}>회원가입</Link>
         </StyledSignupLinkGuide>
       </StyledLoginContainer>
     </StyledPage>
@@ -56,8 +61,6 @@ const StyledPage = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-
-  height: 100vh;
 `;
 
 const StyledLoginContainer = styled.div`
@@ -67,7 +70,7 @@ const StyledLoginContainer = styled.div`
   box-sizing: border-box;
 
   width: 480px;
-  height: 600px;
+  margin: 60px 0;
 
   background: ${({ theme: { colors } }) => colors.white};
   border: 1px solid ${({ theme: { colors } }) => colors.lightGray};
@@ -77,7 +80,7 @@ const StyledLoginContainer = styled.div`
 const StyledTitle = styled.h1`
   text-align: center;
 
-  color: ${({ theme: { colors } }) => colors.pink};
+  color: ${({ theme: { colors } }) => colors.redPink};
 
   font-weight: 900;
   font-size: 24px;
@@ -91,6 +94,7 @@ const StyledForm = styled.form`
   width: 100%;
 
   > label {
+    margin-top: 10px;
     font-size: 14px;
   }
 
@@ -104,7 +108,7 @@ const StyledForm = styled.form`
 const StyledLoginHelper = styled.div`
   display: flex;
   justify-content: space-between;
-
+  margin-top: 4px;
   width: 100%;
 `;
 
@@ -133,13 +137,14 @@ const StyledFindLoginInfo = styled.div`
 `;
 
 const StyledLoginButton = styled.button`
-  background: ${({ theme: { colors } }) => colors.pink};
+  background: ${({ theme: { colors } }) => colors.redPink};
   color: ${({ theme: { colors } }) => colors.white};
   border-radius: 5px;
 
-  height: 32px;
+  height: 40px;
+  margin-top: 20px;
 
-  font-size: 14px;
+  font-size: 17px;
   font-weight: 900;
 `;
 
@@ -149,12 +154,12 @@ const StyledSignupLinkGuide = styled.div`
   gap: 5px;
 
   width: 100%;
-  margin-top: 10px;
+  margin-top: 18px;
 
   font-size: 14px;
 
   a {
-    color: ${({ theme: { colors } }) => colors.pink};
+    color: ${({ theme: { colors } }) => colors.redPink};
 
     :hover {
       font-weight: 900;
