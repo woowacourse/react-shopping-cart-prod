5198d9ea4398db6a5a92cabf04ffdb3f1008829c
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-06-01 20:11:45 +0900

    fix : 로그인 상태에 따른 헤더 적용
    Co-authored-by: JASUN LEE <liswktjs@gmail.com>

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -4,28 +4,43 @@ import { 아이콘_코드 } from 'constants/';
 import * as CommonStyled from 'components/@common/CommonStyle/styles';
 import * as Styled from './styles';
 
-const Header = () => (
-  <Styled.Container>
-    <CommonStyled.FlexWrapper>
-      <Styled.LeftMenuButton>전체 카테고리</Styled.LeftMenuButton>
-    </CommonStyled.FlexWrapper>
+const Header = () => {
+  const accessToken = localStorage.getItem('accessToken');
+  const isLogin = !!accessToken;
 
-    <Link to="/">
-      <Styled.Logo />
-    </Link>
+  return (
+    <Styled.Container>
+      <CommonStyled.FlexWrapper>
+        <Styled.LeftMenuButton>전체 카테고리</Styled.LeftMenuButton>
+      </CommonStyled.FlexWrapper>
 
-    <Styled.RightMenu>
-      <Link to="/cart">
-        <Styled.RightMenuButton icon={아이콘_코드.CART}>장바구니</Styled.RightMenuButton>
+      <Link to="/">
+        <Styled.Logo />
       </Link>
 
-      {/* TODO: 주문목록은 로그인 상태일때만 보여주기
-      <Styled.RightMenuButton icon={아이콘_코드.RECEIPT}>주문 목록</Styled.RightMenuButton> */}
-      <Link to="/login">
-        <Styled.RightMenuButton icon={아이콘_코드.USER}>로그인</Styled.RightMenuButton>
-      </Link>
-    </Styled.RightMenu>
-  </Styled.Container>
-);
+      <Styled.RightMenu>
+        <Link to="/cart">
+          <Styled.RightMenuButton icon={아이콘_코드.CART}>장바구니</Styled.RightMenuButton>
+        </Link>
+
+        {isLogin ? (
+          <>
+            <Styled.RightMenuButton icon={아이콘_코드.RECEIPT}>주문 목록</Styled.RightMenuButton>
+            <Link to="/identification">
+              <Styled.RightMenuButton icon={아이콘_코드.USER}>회원정보</Styled.RightMenuButton>
+            </Link>
+            <Link to="/logout">
+              <Styled.RightMenuButton icon={아이콘_코드.USER}>로그아웃</Styled.RightMenuButton>
+            </Link>
+          </>
+        ) : (
+          <Link to="/login">
+            <Styled.RightMenuButton icon={아이콘_코드.USER}>로그인</Styled.RightMenuButton>
+          </Link>
+        )}
+      </Styled.RightMenu>
+    </Styled.Container>
+  );
+};
 
 export default Header;
