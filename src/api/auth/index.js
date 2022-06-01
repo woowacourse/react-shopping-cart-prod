ce767f0b5043ce9e95e9e8191e81da045a1eeb84
intae92 <kangit2013@gmail.com>
2022-06-01 21:56:07 +0900

    feat: auth api 구현
    Co-authored-by: moonheekim0118 <hellomooneekim@gmail.com>

---
+++ src/api/auth/index.js
@@ -0,0 +1,99 @@
+import axios from 'axios';
+import { setCookie, getCookie } from 'utils/cookie';
+
+export const loginApi = async (paylod) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: `/api/auth`,
+      paylod,
+    });
+
+    const {
+      data: { accessToken },
+    } = res;
+
+    setCookie('userToken', accessToken);
+  } catch (err) {
+    console.log('err', err);
+  }
+};
+
+export const getUserApi = async () => {
+  try {
+    const userToken = getCookie('userToken');
+    const res = await axios({
+      method: 'GET',
+      url: `/api/members/me`,
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+      },
+    });
+
+    const { id, name, email } = res.data;
+
+    return { id, name, email };
+  } catch (err) {
+    console.log('err', err);
+  }
+};
+
+export const signUpApi = async (payload) => {
+  try {
+    await axios({
+      method: 'POST',
+      url: '/api/members',
+      payload,
+    });
+  } catch (err) {
+    console.log('err', err);
+  }
+};
+
+export const duplicateEmailApi = async (payload) => {
+  try {
+    await axios({
+      method: 'POST',
+      url: '/api/members/duplicate-email',
+      payload,
+    });
+  } catch (err) {
+    console.log('err', err);
+  }
+};
+
+export const updateNameApi = async (payload) => {
+  try {
+    await axios({
+      method: 'PUT',
+      url: '/api/members/me/name',
+      payload,
+    });
+  } catch (err) {
+    console.log('err', err);
+  }
+};
+
+export const updatePasswordApi = async (payload) => {
+  try {
+    await axios({
+      method: 'PUT',
+      url: '/api/members/me/password',
+      payload,
+    });
+  } catch (err) {
+    console.log('err', err);
+  }
+};
+
+export const unRegisterApi = async (payload) => {
+  try {
+    await axios({
+      method: 'DELETE',
+      url: '/api/members/me',
+      payload,
+    });
+  } catch (err) {
+    console.log('err', err);
+  }
+};
