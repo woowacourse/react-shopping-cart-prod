c99581035da2f6e41616da3f4173601d8ea58444
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-06-01 20:38:39 +0900

    feat: implement Modal component
    Co-authored-by: Marco <wonsss@users.noreply.github.com>

---
+++ src/components/@shared/Modal/index.tsx
@@ -0,0 +1,30 @@
+import CloseButton from 'components/CloseButton';
+import { PropsWithChildren } from 'react';
+import ReactDOM from 'react-dom';
+import Styled from './index.style';
+
+interface Props {
+  onCloseModal(): void;
+}
+
+const ModalOverlay = ({ children, onCloseModal }: PropsWithChildren<Props>) => {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Styled.Backdrop onClick={onCloseModal} />,
+        document.getElementById('backdrop-root'),
+      )}
+      {ReactDOM.createPortal(
+        <Styled.Modal>
+          <Styled.CloseButton>
+            <CloseButton handleClick={onCloseModal} />
+          </Styled.CloseButton>
+          {children}
+        </Styled.Modal>,
+        document.getElementById('overlay-root'),
+      )}
+    </>
+  );
+};
+
+export default ModalOverlay;
