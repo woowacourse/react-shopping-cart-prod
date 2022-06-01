81f723ae2895a1ba8659a7dfbedf26df957de4d1
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-06-01 14:14:58 +0900

    ✨ : 페이지에 따른 레이아웃 분기 처리
    Co-authored-by: kyoul <yunjin-kim@users.noreply.github.com>

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,21 +1,28 @@
-import { Routes, Route } from 'react-router-dom';
+/* eslint-disable react/jsx-wrap-multilines */
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 import CartListPage from 'pages/CartListPage';
 import NotFoundPage from 'pages/NotFoundPage';
 import ProductListPage from 'pages/ProductListPage';
-
-import Layout from 'components/Layout';
+import SignUpPage from 'pages/SignUpPage';
 
 import { PAGE_LIST } from 'constants/';
+
+import MembersLayout from 'layout/MembersLayout';
+import ShoppingLayout from 'layout/ShoppingLayout';
 
 function PageRoutes() {
   return (
     <Routes>
-      <Route element={<Layout />}>
+      <Route element={<ShoppingLayout />}>
         <Route index element={<ProductListPage />} />
         <Route path={PAGE_LIST.PRODUCT_LIST} element={<ProductListPage />} />
         <Route path={PAGE_LIST.CART_LIST} element={<CartListPage />} />
         <Route path="*" element={<NotFoundPage />} />
+      </Route>
+
+      <Route element={<MembersLayout />}>
+        <Route path={PAGE_LIST.SIGN_UP} element={<SignUpPage />} />
       </Route>
     </Routes>
   );
