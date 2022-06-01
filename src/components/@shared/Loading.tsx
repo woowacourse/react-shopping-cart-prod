92dbeda16f66bf133650c07fd55ff6de1c072263
이아현 <lah1203@naver.com>
2022-06-01 16:36:41 +0900

    style: 로딩 UI 수정
    Co-authored-by: al-bur <al-bur@users.noreply.github.com>
    Co-authored-by: Seungchan On <cks3066@users.noreply.github.com>

--- src/components/@shared/Loading.tsx
+++ src/components/@shared/Loading.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import styled from 'styled-components';
 
 function Loading() {
   return (
     <LoadingContainer>
       <img src="https://3.bp.blogspot.com/-uHU-f8xSLOw/WDZwzCUuBMI/AAAAAAAELW0/VfUc6uwqovsudmSZNGg2dCENctJchI9OACLcB/s1600/AS000620_13.gif?time=Wed%20Jun%2001%202022%2014:49:55%20GMT+0900%20(%ED%95%9C%EA%B5%AD%20%ED%91%9C%EC%A4%80%EC%8B%9C)" />
-      <p>Loading...</p>
+      <p>빙글빙글 돌아가는 짱구의 하루</p>
     </LoadingContainer>
   );
 }
