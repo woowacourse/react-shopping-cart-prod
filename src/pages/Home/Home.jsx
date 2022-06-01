21a430207acf50f62a2329a8a3dd41c6dc6ed6d6
Yo Wook Kim <uk960214@gmail.com>
2022-06-01 12:21:02 +0900

    refactor: 불필요한 코드 제거
    Co-authored-by: Woo seung <woose28@users.noreply.github.com>

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { PageTemplate, Pagination } from 'components/common';
+import { Pagination } from 'components/common';
 import { ProductList } from 'components/product';
 
 import * as Styled from 'pages/Home/Home.style';
