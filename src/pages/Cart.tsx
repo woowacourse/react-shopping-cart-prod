68dfb789297be1cbd3514e2b5d18f92165e78e5c
gytks4 <gytks4@naver.com>
2022-06-01 21:36:31 +0900

    feat: 비회원일 때 장바구니 페이지 접근 금지

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -3,9 +3,13 @@ import LayoutWithTitle from 'components/common/LayoutWithTitle';
 import Loading from 'components/common/Loading';
 import PaymentBox from 'components/common/PaymentBox';
 import RequestFail from 'components/common/RequestFail';
+import { useAppSelector } from 'hooks/useAppSelector';
 import useThunkFetch from 'hooks/useThunkFetch';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { getCartListRequest } from 'redux/cartList/thunk';
 import { getItemList } from 'redux/itemList/thunk';
+import { PATH } from 'Routers';
 import styled from 'styled-components';
 
 const Cart = () => {
@@ -19,6 +23,8 @@ const Cart = () => {
     error: error_getCartList,
     loading: loading_cartList,
   } = useThunkFetch(state => state.cartList, getCartListRequest());
+  const isLogin = useAppSelector(state => !!state.user.data);
+  const navigate = useNavigate();
 
   const itemListInCart = cartList.map(cartItem => {
     const itemInfo = itemList.find(item => item.id === cartItem.id);
@@ -31,6 +37,13 @@ const Cart = () => {
 
   const selectedItem = itemListInCart.filter(item => item.isSelected);
   const totalPrice = selectedItem.reduce((acc, item) => item.price * item.quantity + acc, 0);
+
+  useEffect(() => {
+    if (!isLogin) {
+      alert('잘못된 접근입니다.');
+      navigate(PATH.home);
+    }
+  }, [isLogin]);
 
   if (loading_itemList || loading_cartList === 'getCartList') return <Loading />;
   if (error_getItemList || error_getCartList) return <RequestFail />;
