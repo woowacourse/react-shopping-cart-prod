2a13f487cacad08a2c7fdcbcbf12d405a6173f22
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-06-02 18:29:25 +0900

    ✨ : 회원 정보 관리 및 로그아웃 페이지 라우팅 처리
    Co-authored-by: kyoul <yunjin-kim@users.noreply.github.com>

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,9 +2,11 @@ import { Routes, Route } from 'react-router-dom';
 
 import CartListPage from 'pages/CartListPage';
 import LoginPage from 'pages/LoginPage';
+import LogoutPage from 'pages/LogoutPage';
 import NotFoundPage from 'pages/NotFoundPage';
 import ProductListPage from 'pages/ProductListPage';
-import SignUpPage from 'pages/SignUpPage';
+import ProfilePage from 'pages/ProfilePage';
+import SignUpPage from 'pages/SignUpPage/index';
 
 import { PAGE_LIST } from 'constants/';
 
@@ -18,12 +20,14 @@ function PageRoutes() {
         <Route index element={<ProductListPage />} />
         <Route path={PAGE_LIST.PRODUCT_LIST} element={<ProductListPage />} />
         <Route path={PAGE_LIST.CART_LIST} element={<CartListPage />} />
+        <Route path={PAGE_LIST.PROFILE} element={<ProfilePage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
 
       <Route element={<MembersLayout />}>
         <Route path={PAGE_LIST.SIGN_UP} element={<SignUpPage />} />
         <Route path={PAGE_LIST.LOGIN} element={<LoginPage />} />
+        <Route path={PAGE_LIST.LOGOUT} element={<LogoutPage />} />
       </Route>
     </Routes>
   );
