db1b83b125358245540c5b4698fe17910e24e2d4
Daye Lee <508yeah@gmail.com>
2022-06-02 13:56:38 +0900

    chore: 회원 기능 api mocking
    Co-authored-by: Koy <inkyo.dev@gmail.com>

---
+++ src/mocks/handlers.js
@@ -0,0 +1,57 @@
+import { rest } from 'msw';
+import productList from './dummyData';
+
+export const handlers = [
+  rest.get(`${process.env.REACT_APP_API_URL}`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(productList));
+  }),
+
+  rest.post('/api/customers', (req, res, ctx) => {
+    const { userName, password } = req.body;
+    const customers = JSON.parse(localStorage.getItem('customers')) ?? [];
+
+    customers.push({ userName, password, accessToken: `accessToken_${userName}` });
+    localStorage.setItem('customers', JSON.stringify(customers));
+
+    return res(ctx.status(201));
+  }),
+
+  rest.post('/api/customers/duplication', (req, res, ctx) => {
+    const { userName } = req.body;
+    const customers = JSON.parse(localStorage.getItem('customers')) ?? [];
+    const isDuplicateUserName =
+      customers.find((customer) => customer.userName === userName) !== undefined;
+
+    return res(ctx.json(isDuplicateUserName));
+  }),
+
+  rest.put('/api/customers/me', (req, res, ctx) => {
+    const { authorization } = req.headers._headers;
+
+    if (!authorization) return res(ctx.status(400));
+
+    return res(ctx.status(200));
+  }),
+
+  rest.delete('/api/customers/me', (req, res, ctx) => {
+    const { authorization } = req.headers._headers;
+
+    if (!authorization) return res(ctx.status(400));
+
+    return res(ctx.status(204));
+  }),
+
+  rest.post('/api/login', (req, res, ctx) => {
+    const { userName, password } = req.body;
+    const customers = JSON.parse(localStorage.getItem('customers')) ?? [];
+    const loginUser = customers.find(
+      (customer) => customer.userName === userName && customer.password === password
+    );
+
+    if (!loginUser) return res(ctx.status(400));
+
+    loginUser.accessToken = `accessToken_${userName}`;
+
+    return res(ctx.status(200), ctx.json(`accessToken_${userName}`));
+  }),
+];
--- src/mocks/handlers.ts
+++
@@ -1,9 +0,0 @@
-// src/mocks/handlers.js
-import { rest } from 'msw';
-import productList from './dummyData';
-
-export const handlers = [
-  rest.get(`${process.env.REACT_APP_API_URL}`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(productList));
-  }),
-];
