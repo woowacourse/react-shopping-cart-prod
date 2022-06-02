dd88d5b7f94ff751f2c52e89781f31e24901cc51
gytks4 <gytks4@naver.com>
2022-06-02 09:55:42 +0900

    refactor: App에서 getUser Loading 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import Header from 'components/common/Header';
 import Loading from 'components/common/Loading';
 import { useAppDispatch } from 'hooks/useAppDispatch';
-import { useAppSelector } from 'hooks/useAppSelector';
 import { Suspense, useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { getUser } from 'redux/user/thunk';
 import Routers from 'Routers';
 import styled from 'styled-components';
-import { flexCenter } from 'styles/mixin';
 
 function App() {
   const dispatch = useAppDispatch();
-  const { loading } = useAppSelector(state => state.user);
 
   useEffect(() => {
     dispatch(getUser());
-  }, [dispatch]);
-
-  if (loading === 'getUser') return <Loading />;
+  }, []);
 
   return (
     <Suspense fallback={<Loading />}>
