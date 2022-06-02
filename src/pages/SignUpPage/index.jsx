835dc5857b85231cae16bb16d7f48cd232a50b97
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-06-02 18:27:04 +0900

    ✨ : 회원가입 페이지 기능 구현
    Co-authored-by: kyoul <yunjin-kim@users.noreply.github.com>

--- src/pages/SignUpPage/index.jsx
+++ src/pages/SignUpPage/index.jsx
@@ -1,23 +1,58 @@
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
+import useFormValidation from 'hooks/useFormValidation';
 
 import { Button, FlexContainer } from 'components/@common';
 import FieldSet from 'components/@common/FieldSet';
 import InputField from 'components/@common/InputField';
 
+import { requestSignUp } from 'api/members';
+import { REQUEST_STATUS } from 'constants/';
+import { getFormData } from 'lib/formUtils';
+import { userValidator } from 'lib/validateUtils';
+
 import * as S from './styles';
 
 function SignUpPage() {
+  const navigate = useNavigate();
+
+  const validationList = {
+    userId: ({ userId }) => userValidator.userId(userId),
+    password: ({ password }) => userValidator.password(password),
+    passwordConfirm: ({ password, passwordConfirm }) =>
+      userValidator.passwordConfirm(password, passwordConfirm),
+    nickname: ({ nickname }) => userValidator.nickname(nickname),
+  };
+
+  const { errorList, isAllPassed, validationForm } = useFormValidation(validationList);
+
+  const handleInputValidate = ({ target }) => {
+    if (target.tagName !== 'INPUT') return;
+
+    const formData = getFormData(target.form);
+
+    validationForm({ name: target.name, formData });
+  };
+
+  const handleSubmitSignUp = async (event) => {
+    event.preventDefault();
+    const formData = getFormData(event.target);
+
+    const response = await requestSignUp(formData);
+
+    if (response.status === REQUEST_STATUS.FAIL) return;
+    navigate('/login');
+  };
+
   return (
-    <S.Container onBlur={(event) => console.log(event)}>
+    <S.Container onBlur={handleInputValidate} onSubmit={handleSubmitSignUp}>
       <FieldSet labelText="이메일">
         <InputField
           name="userId"
-          status=""
           type="text"
-          message=""
+          status={errorList.userId ? 'danger' : 'default'}
+          message={errorList.userId}
           placeholder="로그인 시 사용할 이메일"
-          value=""
         />
       </FieldSet>
 
@@ -25,19 +60,17 @@ function SignUpPage() {
         <InputField
           name="password"
           type="password"
-          status=""
-          message=""
+          status={errorList.password ? 'danger' : 'default'}
+          message={errorList.password}
           placeholder="영문, 숫자, 특수문자 조합 최소 8자 최대 16자"
-          value=""
         />
 
         <InputField
-          name="password-confirm"
+          name="passwordConfirm"
           type="password"
-          status=""
-          message=""
+          status={errorList.passwordConfirm ? 'danger' : 'default'}
+          message={errorList.passwordConfirm}
           placeholder="비밀번호 재입력"
-          value=""
         />
       </FieldSet>
 
@@ -45,15 +78,14 @@ function SignUpPage() {
         <InputField
           name="nickname"
           type="text"
-          status=""
-          message=""
+          status={errorList.nickname ? 'danger' : 'default'}
+          message={errorList.nickname}
           placeholder="영문, 한글, 숫자로 최소 2자, 최대 10자"
-          value=""
         />
       </FieldSet>
 
       <FlexContainer gap={20}>
-        <Button type="button" status="primary">
+        <Button type="submit" status="primary" isDisabled={!isAllPassed}>
           회원가입
         </Button>
         <S.NonMemberText>
