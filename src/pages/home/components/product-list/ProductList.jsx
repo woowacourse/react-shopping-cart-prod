d726fb1c25d7274aba9aff03d9f3e7568d8a138e
airman5573 <rpf5573@kakao.com>
2022-06-02 17:40:19 +0900

    refactor(cleanup): 지원하지 않는 기능을 삭제한다

--- src/pages/home/components/product-list/ProductList.jsx
+++ src/pages/home/components/product-list/ProductList.jsx
@@ -2,16 +2,13 @@ import cn from "classnames";
 import { useSelector } from "react-redux";
 import ProductItem from "@home/components/product-item/ProductItem";
 import styles from "@home/components/product-list/product-list.module";
-import { Link } from "react-router-dom";
 
 function ProductList({ className }) {
   const productList = useSelector((state) => state.productList);
   return (
     <div className={cn(styles.productList, className)}>
       {productList.map((item) => (
-        <Link key={item.sku} to={`/product/${item.sku}`}>
-          <ProductItem {...item} />
-        </Link>
+        <ProductItem key={item.sku} {...item} />
       ))}
     </div>
   );
