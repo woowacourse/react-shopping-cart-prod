32a34504ca06efe13472c62552ef2a75be8a2eee
JUDONGHYEOK <judonghyeok@gmail.com>
2022-06-02 15:52:35 +0900

    feat: header 유저 상황에 따라 다른 렌더링
    - 비로그인시 : 로그인,회원가입,장바구니
    - 로그인시: 프로필, 장바구니,주문목록

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,14 @@
-import { PATH } from 'constants';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Wrapper from './style';
 
+import { PATH } from 'constants';
+
 const Header = () => {
+  const { accessToken } = useSelector((state) => state.user);
+
   return (
     <Wrapper>
       <Link to={PATH.HOME}>
@@ -17,10 +21,24 @@ const Header = () => {
         <Link to={PATH.CARTS}>
           <button>장바구니</button>
         </Link>
-        <Link to={PATH.PROFILE}>
-          <button>프로필</button>
-        </Link>
         <button>주문목록</button>
+        {accessToken && (
+          <>
+            <Link to={PATH.PROFILE}>
+              <button>프로필</button>
+            </Link>
+          </>
+        )}
+        {accessToken || (
+          <>
+            <Link to={PATH.LOGIN}>
+              <button>로그인</button>
+            </Link>
+            <Link to={PATH.SIGNUP}>
+              <button>회원가입</button>
+            </Link>
+          </>
+        )}
       </div>
     </Wrapper>
   );
