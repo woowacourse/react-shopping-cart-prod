3149f19decedaf797458f83f453331b81f1d1d49
Jeongmin Woo <greenblues1190@gmail.com>
2022-06-02 16:38:50 +0900

    feat: TermsStep 약관 동의 후 제출 기능 추가
    Co-authored-by: Kimsanglok <evertree6031@gmail.com>

--- src/pages/SignupPage/Steps/TermsStep/TermsStep.tsx
+++ src/pages/SignupPage/Steps/TermsStep/TermsStep.tsx
@@ -3,12 +3,31 @@ import DivideLine from '../../../../components/DivideLine/DivideLine';
 import Button from '../../../../components/Button/Button';
 import * as S from './TermsStep.styled';
 import { useOutletContext } from 'react-router-dom';
+import { useState } from 'react';
 
 function TermsStep() {
   const { goNextStep } = useOutletContext<{
     currentStepId: number;
     goNextStep: () => void;
   }>();
+  const [checkedFlags, setCheckedFlags] = useState<Record<string, boolean>>({
+    'term-of-service': false,
+    'term-of-personal-info': false,
+  });
+
+  const handleCheck =
+    (name: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      setCheckedFlags((prev) => ({ ...prev, [name]: e.target.checked }));
+    };
+
+  const handleCheckAll = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCheckedFlags({
+      'term-of-service': e.target.checked,
+      'term-of-personal-info': e.target.checked,
+    });
+  };
+
+  const isAllChecked = Object.values(checkedFlags).every((checked) => checked);
 
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
@@ -22,27 +41,39 @@ function TermsStep() {
       <S.Form onSubmit={handleSubmit}>
         <S.FormItem>
           <S.Label>
-            <Checkbox />
+            <Checkbox checked={isAllChecked} onChange={handleCheckAll} />
             모든 이용약관에 동의합니다.
           </S.Label>
         </S.FormItem>
         <DivideLine color="lightGray" thickness="thin" />
         <S.FormItem>
           <S.Label>
-            <Checkbox />
+            <Checkbox
+              name="term-of-service"
+              checked={checkedFlags['term-of-service']}
+              onChange={handleCheck('term-of-service')}
+              required
+            />
             (필수) 서비스 이용 약관
           </S.Label>
           <S.TermDetailButton>상세보기</S.TermDetailButton>
         </S.FormItem>
         <S.FormItem>
           <S.Label>
-            <Checkbox />
+            <Checkbox
+              name="term-of-personal-info"
+              checked={checkedFlags['term-of-personal-info']}
+              onChange={handleCheck('term-of-personal-info')}
+              required
+            />
             (필수) 개인 정보 취급 방침
           </S.Label>
           <S.TermDetailButton>상세보기</S.TermDetailButton>
         </S.FormItem>
         <S.SubmitButtonBox>
-          <Button type="submit">다음으로</Button>
+          <Button type="submit" disabled={!isAllChecked}>
+            다음으로
+          </Button>
         </S.SubmitButtonBox>
       </S.Form>
     </S.TermsBox>
