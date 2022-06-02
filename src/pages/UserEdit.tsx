a6147bda0487e80020adcb2687c8db0dddafca7e
gytks4 <gytks4@naver.com>
2022-06-02 09:07:58 +0900

    fix: 수정된 이름이 똑같을 때 모달 띄우기 않도록 구현
    Co-authored-by: Jungmin Hwang <hwangstar156@users.noreply.github.com>

--- src/pages/UserEdit.tsx
+++ src/pages/UserEdit.tsx
@@ -1,18 +1,29 @@
 import AuthPage from 'components/common/AuthPage';
 import LabeledInput from 'components/common/LabeledInput';
+import Snackbar, { MESSAGE } from 'components/common/Snackbar';
 import PasswordConfirmModal from 'components/UserEdit/PasswordConfirmModal';
 import { useAppDispatch } from 'hooks/useAppDispatch';
+import { useAppSelector } from 'hooks/useAppSelector';
 import useInput from 'hooks/useInput';
-import { useEffect, useReducer } from 'react';
+import useSnackBar from 'hooks/useSnackBar';
+import { useReducer } from 'react';
 import { getUser } from 'redux/user/thunk';
 
 const UserEdit = () => {
   const [name, onChangeName] = useInput();
   const [isShowModal, toggleModal] = useReducer(prev => !prev, false);
   const dispatch = useAppDispatch();
+  const prevName = useAppSelector(state => state.user.data?.name);
+  const { isOpenSnackbar, openSnackbar } = useSnackBar();
 
   const onSubmitAuthForm = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    if (name === prevName) {
+      openSnackbar();
+
+      return;
+    }
+
     dispatch(getUser());
     toggleModal();
   };
@@ -28,6 +39,7 @@ const UserEdit = () => {
         onChange={onChangeName}
       />
       {isShowModal && <PasswordConfirmModal closeModal={toggleModal} name={name} />}
+      {isOpenSnackbar && <Snackbar message={MESSAGE.editUser} />}
     </AuthPage>
   );
 };
