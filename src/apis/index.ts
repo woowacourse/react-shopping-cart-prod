e89672a55822a379ffde27c32d7d3f4fb86d7eda
Hwang Jung Min <hwangstar156@gmail.com>
2022-06-02 17:21:58 +0900

    chore: 로컬 실행을 위해 .env 의존성 삭제

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,14 +1,7 @@
 import axios from 'axios';
 
-const LOCAL_BASE_URL = process.env.REACT_APP_LOCAL_BASE_URL;
-const PRODUCT_BASE_URL = process.env.REACT_APP_PRODUCT_BASE_URL;
-const AUTH_LOCAL_BASE_URL = process.env.REACT_APP_AUTH_LOCAL_BASE_URL;
-const AUTH_PRODUCT_BASE_URL = process.env.REACT_APP_AUTH_PRODUCT_BASE_URL;
-
-export const BASE_URL = process.env.NODE_ENV === 'production' ? PRODUCT_BASE_URL : LOCAL_BASE_URL;
-
-export const AUTH_BASE_URL =
-  process.env.NODE_ENV === 'production' ? AUTH_PRODUCT_BASE_URL : AUTH_LOCAL_BASE_URL;
+export const BASE_URL = "http://localhost:4000";
+export const AUTH_BASE_URL = "http://localhost:4000";
 
 export const client = axios.create({
   baseURL: BASE_URL,
