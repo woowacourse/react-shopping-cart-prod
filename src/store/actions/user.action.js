a8b0d7773b0026b48255ee436c64c98a0a3a44ac
woose28 <dldntmdsla@naver.com>
2022-06-02 17:44:56 +0900

    feat: logout 시 header의 token을 제거하는 기능 구현

--- src/store/actions/user.action.js
+++ src/store/actions/user.action.js
@@ -1,5 +1,6 @@
 import { userActionType } from 'store/reducers/user.reducer';
 
+import customInstance from 'api/customInstance';
 import {
   sendDeleteUserRequest,
   sendLoginRequest,
@@ -7,6 +8,7 @@ import {
 } from 'api/user.api';
 
 import { ERROR_MESSAGES } from 'constants/messages';
+
 
 export const loginUserThunk = (loginData) => async (dispatch) => {
   dispatch({ type: userActionType.START });
@@ -24,7 +26,8 @@ export const loginUserThunk = (loginData) => async (dispatch) => {
 export const logoutUser = () => {
   window.sessionStorage.removeItem('nickname');
   window.sessionStorage.removeItem('token');
-
+  
+  delete customInstance.defaults.headers.common
   return {
     type: userActionType.LOGOUT,
   };
