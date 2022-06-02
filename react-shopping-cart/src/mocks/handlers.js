c6a36ab74298d5ea7f8f37c18c0d533baefea7dd
kwannee <yleh1126@naver.com>
2022-06-02 14:44:59 +0900

    feat: 로그인 기능 구현
    Co-authored-by: ronci <ronci@users.noreply.github.com>

--- react-shopping-cart/src/mocks/handlers.js
+++ react-shopping-cart/src/mocks/handlers.js
@@ -1,8 +1,37 @@
-import { products } from 'mocks/mockData';
+import { products, customers } from 'mocks/mockData';
 import { rest } from 'msw';
 
 export const handlers = [
   rest.get(`${process.env.REACT_APP_API_HOST}/product`, (req, res, ctx) =>
-    res(ctx.status(200), ctx.json([...products]))
+    res(ctx.status(200), ctx.json(products))
   ),
+  rest.post(`${process.env.REACT_APP_API_HOST}/customers/login`, (req, res, ctx) => {
+    const { email, password } = req.body;
+
+    const isExist = customers[email]?.password === password;
+
+    if (isExist) {
+      return res(ctx.status(200), ctx.json({ accessToken: 1 }));
+    }
+    return res(ctx.status(404), ctx.json({ message: '존재하지 않는 email/password입니다.' }));
+  }),
+  rest.post(`${process.env.REACT_APP_API_HOST}/customers/email`, (req, res, ctx) => {
+    const { email } = req.body;
+
+    const isExist = customers[email];
+
+    return res(ctx.status(200), ctx.json({ isValidEmail: isExist }));
+  }),
+  rest.post(`${process.env.REACT_APP_API_HOST}/customers`, (req, res, ctx) => {
+    const { email, password, name, phone, address } = req.body;
+
+    customers[email] = {
+      password,
+      name,
+      phone,
+      address,
+    };
+
+    return res(ctx.status(201), ctx.json({ email, name, phone, address }));
+  }),
 ];
--- react-shopping-cart/src/pages/Login/Login.page.jsx
+++ react-shopping-cart/src/pages/Login/Login.page.jsx
@@ -1,4 +1,6 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 import AuthContainer from 'components/@shared/AuthContainer/AuthContainer.component';
@@ -7,6 +9,10 @@ import FlexBox from 'components/@shared/FlexBox/FlexBox.component';
 import Input from 'components/@shared/Input/Input.component';
 import Logo from 'components/@shared/Logo/Logo.component';
 import TextBox from 'components/@shared/TextBox/TextBox.component';
+
+import { loginUser } from 'redux/actions/auth.action';
+
+import useFetch from 'hooks/useFetch';
 
 const CopyrightBox = styled(FlexBox).attrs({
   justifyContent: 'center',
@@ -21,17 +27,64 @@ const SignupLink = styled(Link)`
 `;
 
 function Login() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const {
+    fetchData: login,
+    data,
+    error,
+  } = useFetch({
+    url: '/customers/login',
+    method: 'post',
+    skip: true,
+  });
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const accessToken = data?.accessToken && data.accessToken;
+
+  const handleLogin = async e => {
+    e.preventDefault();
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    await login({ email, password });
+  };
+
+  const { accessToken: isLoggedIn } = useSelector(state => state.auth);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate('/');
+    }
+  }, []);
+
+  useEffect(() => {
+    if (accessToken) {
+      dispatch(loginUser(accessToken));
+      navigate('/');
+    }
+  }, [accessToken]);
+
   return (
     <AuthContainer>
       <FlexBox height="100%" direction="column" justifyContent="flex-end" gap="25px">
         <Link to="/">
           <Logo color="MINT_001" />
         </Link>
-        <FlexBox width="100%" direction="column" gap="20px" alignItems="flex-end">
-          <Input type="email" placeholder="이메일" />
-          <Input type="password" placeholder="비밀번호" />
+        <FlexBox
+          as="form"
+          onSubmit={handleLogin}
+          width="100%"
+          direction="column"
+          gap="20px"
+          alignItems="flex-end"
+        >
+          <Input ref={emailRef} type="email" placeholder="이메일" />
+          <Input ref={passwordRef} type="password" placeholder="비밀번호" />
+          {error && error}
           <SignupLink to="/signup">회원가입</SignupLink>
-          <Button width="100%" borderRadius="10px">
+          <Button width="100%" borderRadius="10px" onClick={handleLogin}>
             <TextBox color="WHITE_001">로그인</TextBox>
           </Button>
         </FlexBox>
