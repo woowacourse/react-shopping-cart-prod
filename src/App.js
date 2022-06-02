f7a927fa96811c8a7acfac93e8f080d48981fdde
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-06-02 17:46:44 +0900

    feat: import implemented pages
    Co-authored-by: Marco <wonsss@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,74 +1,65 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import styled, { ThemeProvider } from 'styled-components';
+// @ts-nocheck
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import { ProductListPage, ProductDetailPage, CartPage } from 'page';
-import { Header, CartIcon, GlobalStyles, theme } from 'components';
+import { GlobalStyles, theme } from 'components';
 
-import { BASE_URL, ROUTES } from 'constants';
+import { BASE_URL, ROUTES } from 'utils/constants';
+import LoginPage from 'page/LoginPage';
+import SignupPage from 'page/SignupPage';
+import AccountPage from 'page/AccountPage';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { getCookie } from 'utils/cookie';
+import store from 'store/store';
+import { doLogin } from 'actions/actionCreator';
+import Layout from 'components/Layout';
+import { useSelector } from 'react-redux';
+import Snackbar from 'components/Snackbar';
 
 function App() {
+  const { isVisible, message, status } = useSelector(state => state.snackbarReducer);
+
+  const getAccount = async () => {
+    try {
+      const accessToken = getCookie('accessToken');
+
+      if (!accessToken) return;
+
+      const response = await axios.get('/customers', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      store.dispatch(doLogin({ nickname: response.data.nickname }));
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getAccount();
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter basename={BASE_URL}>
-        <Header
-          left={
-            <Styled.HomeLink to={ROUTES.HOME}>
-              <CartIcon category="header" /> WOOWA SHOP
-            </Styled.HomeLink>
-          }
-          right={
-            <div>
-              <Styled.CartLink to={ROUTES.CART}>장바구니</Styled.CartLink>
-              <Styled.OrderLink to={ROUTES.HOME}>주문목록</Styled.OrderLink>
-            </div>
-          }
-        />
-
-        <Styled.Body>
-          <Routes>
+        <Routes>
+          <Route element={<Layout />}>
             <Route path={ROUTES.HOME} element={<ProductListPage />} />
             <Route path={ROUTES.DETAILS} element={<ProductDetailPage />} />
             <Route path={ROUTES.CART} element={<CartPage />} />
-          </Routes>
-        </Styled.Body>
-
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/account" element={<AccountPage />} />
+          </Route>
+        </Routes>
         <GlobalStyles />
       </BrowserRouter>
+
+      {isVisible && <Snackbar message={message} status={status} />}
     </ThemeProvider>
   );
 }
-
-const Styled = {
-  Body: styled.div`
-    display: flex;
-    justify-content: center;
-  `,
-
-  HomeLink: styled(Link)`
-    font-weight: 900;
-    font-size: 40px;
-    line-height: 58px;
-    text-decoration: none;
-    color: white;
-  `,
-
-  CartLink: styled(Link)`
-    font-weight: 500;
-    font-size: 24px;
-    line-height: 12px;
-    text-decoration: none;
-    color: white;
-    margin: 0 10px;
-  `,
-
-  OrderLink: styled(Link)`
-    font-weight: 500;
-    font-size: 24px;
-    line-height: 12px;
-    text-decoration: none;
-    color: white;
-    margin: 0 10px;
-  `,
-};
 
 export default App;
