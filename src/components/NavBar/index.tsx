2fe09c7af62a924cb0acaed888ef2b9a1b3d5996
InKyoJeong <viapolar@naver.com>
2022-06-03 17:31:10 +0900

    fix: 누락된 navigate 추가 - 로그아웃, 회원 정보 수정 성공시 홈으로 이동

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import routes from '../../routes';
 
 import { useDispatch, useSelector } from 'react-redux';
@@ -11,10 +11,12 @@ import Logo from '../../assets/Logo.png';
 function NavBar() {
   const { isLoggedIn } = useSelector((state: RootState) => state.customer);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const onClickLogout = () => {
     document.cookie = 'accessToken=';
     dispatch(logout());
+    navigate(routes.home);
   };
 
   return (
