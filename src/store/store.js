f06d3c7d5a0f4b95e15c4392e78de42df3380963
wonsss <bigdatajang@gmail.com>
2022-06-05 00:49:35 +0900

    feat: redux 미들웨어(composeWithDevTools, logger) 설치

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,9 @@
-import { legacy_createStore as createStore } from 'redux';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import rootReducer from 'store/rootReducer';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import logger from 'redux-logger';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 store.subscribe(() => store.getState());
 
 export default store;
