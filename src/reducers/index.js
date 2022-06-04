f8a13ae8b2e716d172b4ca19122e350626368d75
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-06-05 01:48:43 +0900

    ⚰️ : 사용하지 않는 코드 제거

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-
-import { cartReducer } from 'store/cart/reducer';
-import { productReducer } from 'store/product/reducer';
-
-import members from './members';
-
-export default combineReducers({ product: productReducer, members, cart: cartReducer });
