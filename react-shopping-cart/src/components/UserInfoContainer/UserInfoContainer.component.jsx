ca4469c3d2b4d525215c9dbd50d096ffdec7c377
kwannee <yleh1126@naver.com>
2022-06-06 21:21:35 +0900

    refactor: 유저 정보 중 phoneNumber, 서버 저장 키에 맞게 phone으로 수정
    Co-authored-by: ronci <ronci@users.noreply.github.com>

--- react-shopping-cart/src/components/UserInfoContainer/UserInfoContainer.component.jsx
+++ react-shopping-cart/src/components/UserInfoContainer/UserInfoContainer.component.jsx
@@ -6,9 +6,10 @@ import FlexBox from 'components/@shared/FlexBox/FlexBox.component';
 import InputBox from 'components/@shared/InputBox/InputBox.component';
 import TextBox from 'components/@shared/TextBox/TextBox.component';
 
-import { setAddress, setName, setPhoneNumber } from 'redux/actions/userInfo.action';
+import { setAddress, setName, setPhone } from 'redux/actions/userInfo.action';
 
 const UserInfoWrapper = styled(FlexBox).attrs({
+  as: 'form',
   id: 'userInfo',
   width: '100%',
   direction: 'column',
@@ -18,21 +19,44 @@ const UserInfoWrapper = styled(FlexBox).attrs({
   margin: 1rem 0;
 `;
 
+const PhoneInputBox = styled.div`
+  display: grid;
+  grid-template-columns: 4fr 1fr 7fr 1fr 7fr;
+  text-align: center;
+  align-items: center;
+`;
+
 function UserInfoContainer({ onClickNext }) {
   const dispatch = useDispatch();
-  const { name, phoneNumber, address } = useSelector(state => state.userInfo);
+  const { name, phone, address } = useSelector(state => state.userInfo);
   const isAllValid = !!(
     !name.error &&
-    !phoneNumber.error &&
+    !phone.error &&
     !address.error &&
     name.value.length &&
-    phoneNumber.first.length === 4 &&
-    phoneNumber.second.length === 4 &&
+    phone.first.length === 4 &&
+    phone.second.length === 4 &&
     address.value.length
   );
 
+  const handlePhoneInput = (e, place) => {
+    if (e.target.value.length > 4) {
+      return;
+    }
+    if (isNaN(e.nativeEvent.data)) {
+      return;
+    }
+
+    dispatch(setPhone(place, e.target.value));
+  };
+
+  const handleShowNextForm = e => {
+    e.preventDefault();
+    onClickNext();
+  };
+
   return (
-    <UserInfoWrapper>
+    <UserInfoWrapper onSubmit={handleShowNextForm}>
       <InputBox
         {...name}
         onChange={e => {
@@ -45,33 +69,26 @@ function UserInfoContainer({ onClickNext }) {
       />
       <FlexBox width="100%" direction="column" gap="5px">
         <label>전화번호</label>
-        <div
-          style={{
-            display: 'grid',
-            gridTemplateColumns: '4fr 1fr 7fr 1fr 7fr',
-            textAlign: 'center',
-            alignItems: 'center',
-          }}
-        >
+        <PhoneInputBox>
           <span>010</span>-
           <InputBox
-            value={phoneNumber.first}
-            onChange={e => dispatch(setPhoneNumber('first', e.target.value))}
+            value={phone.first}
+            onChange={e => {
+              handlePhoneInput(e, 'first');
+            }}
             type="tel"
-            maxLength="4"
             placeholder="0000"
           />
           -
           <InputBox
-            value={phoneNumber.second}
+            value={phone.second}
             onChange={e => {
-              dispatch(setPhoneNumber('second', e.target.value));
+              handlePhoneInput(e, 'second');
             }}
             type="tel"
-            maxLength="4"
             placeholder="0000"
           />
-        </div>
+        </PhoneInputBox>
       </FlexBox>
       <InputBox
         {...address}
@@ -82,7 +99,7 @@ function UserInfoContainer({ onClickNext }) {
         type="text"
         placeholder="주소"
       />
-      <FlexBox as="button" gap="5px" onClick={onClickNext} disabled={!isAllValid}>
+      <FlexBox as="button" gap="5px" onClick={handleShowNextForm} disabled={!isAllValid}>
         <TextBox color={isAllValid ? 'BLACK_001' : 'GRAY_001'} fontSize="small">
           다음
         </TextBox>
