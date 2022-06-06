ebd05325cf285597fe21834b206e2c1df34b6ca4
Marco <bigdatajang@gmail.com>
2022-06-06 22:15:46 +0900

    feat: thunk 설치 및 createPromiseThunk 유틸 작성

--- src/store/store.js
+++ src/store/store.js
@@ -2,8 +2,8 @@ import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import rootReducer from 'store/rootReducer';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import logger from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
-store.subscribe(() => store.getState());
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(ReduxThunk, logger)));
 
 export default store;
