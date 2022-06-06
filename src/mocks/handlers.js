803dcb179647f27db38c73fcfbfb2aff5ff54e1c
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-06-06 22:43:49 +0900

    feat: msw 모킹 로직 수정
    실제 서버와 비슷하게 처리 및 에러 상황 추가

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -72,35 +72,86 @@ export const cartsHandler = [
   }),
 ];
 
+let users = [];
 export const userHandler = [
   rest.get(
     `${BASE_SERVER_URL}${SERVER_PATH.CUSTOMER_LIST}/:customerId`,
     (req, res, ctx) => {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          id: 1,
-          email: "test@naver.com",
-          username: "test",
-        })
-      );
+      const { customerId } = req.params;
+      const selectedUser = users.find((user) => user.id === Number(customerId));
+      if (!selectedUser)
+        return res(
+          ctx.status(404),
+          ctx.json({ message: "존재하지 않는 회원입니다." })
+        );
+
+      return res(ctx.status(200), ctx.json(selectedUser));
     }
   ),
   rest.post(
     `${BASE_SERVER_URL}${SERVER_PATH.CUSTOMER_LIST}`,
     (req, res, ctx) => {
+      const { email, username, password } = req.body;
+      if (!email)
+        return res(
+          ctx.status(400),
+          ctx.json({ field: "email", message: "이메일이 존재하지 않습니다." })
+        );
+      if (!username)
+        return res(
+          ctx.status(400),
+          ctx.json({
+            field: "nickname",
+            message: "닉네임이 존재하지 않습니다.",
+          })
+        );
+      if (!password)
+        return res(
+          ctx.status(400),
+          ctx.json({
+            field: "password",
+            message: "비밀번호가 존재하지 않습니다.",
+          })
+        );
+
+      if (users.some((user) => user.email === email))
+        return res(
+          ctx.status(400),
+          ctx.json({
+            field: "email",
+            message: "이미 가입한 이메일입니다. ",
+          })
+        );
+
+      users = [
+        ...users,
+        { id: Math.floor(Math.random() * 10000), email, username, password },
+      ];
       return res(ctx.status(201));
     }
   ),
   rest.post(`${BASE_SERVER_URL}${SERVER_PATH.LOGIN}`, (req, res, ctx) => {
+    const { email: emailInfo, password } = req.body;
+    if (!emailInfo || !password)
+      return res(
+        ctx.status(404),
+        ctx.json({ message: "유효하지 않은 입력입니다." })
+      );
+
+    const selectedUser = users.find((user) => user.email === emailInfo);
+    if (!selectedUser)
+      return res(
+        ctx.status(404),
+        ctx.json({ message: "이메일 또는 비밀번호가 틀렸습니다." })
+      );
     return res(
       ctx.status(200),
       ctx.json({
         accessToken: "ddfsdfadsfdsafsad",
         customer: {
-          id: 1,
-          email: "test@naver.com",
-          username: "test",
+          id: selectedUser.id,
+          email: selectedUser.email,
+          username: selectedUser.username,
         },
       })
     );
@@ -108,18 +159,39 @@ export const userHandler = [
   rest.post(
     `${BASE_SERVER_URL}${SERVER_PATH.CUSTOMER_LIST}/:customerId`,
     (req, res, ctx) => {
-      return res(ctx.status(204));
+      const { customerId } = req.params;
+      const { password } = req.body;
+      if (users.some((user) => user.password === password)) {
+        users = users.filter((user) => user.id !== Number(customerId));
+        return res(ctx.status(204));
+      }
+      return res(
+        ctx.status(400),
+        ctx.json({ message: "비밀번호가 틀렸습니다." })
+      );
     }
   ),
   rest.put(
     `${BASE_SERVER_URL}${SERVER_PATH.CUSTOMER_LIST}/:customerId`,
     (req, res, ctx) => {
+      const { customerId } = req.params;
+      const { username } = req.body;
+
+      if (!username)
+        return res(
+          ctx.status(400),
+          ctx.json({ message: "수정에 실패했습니다." })
+        );
+
+      const selectedUser = users.find((user) => user.id === Number(customerId));
+      selectedUser.username = username;
+
       return res(
         ctx.status(200),
         ctx.json({
-          id: 1,
-          email: "test@naver.com",
-          username: Math.round(Math.random() * 100).toString(),
+          id: Number(customerId),
+          email: selectedUser.email,
+          username: selectedUser.username,
         })
       );
     }
