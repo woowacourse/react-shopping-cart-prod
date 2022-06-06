c6bd976664664ea8dc34d640393dcebb49f08c13
InKyoJeong <viapolar@naver.com>
2022-06-06 21:59:28 +0900

    feat: 로그인 thunk 추가

--- src/redux/modules/user.ts
+++ src/redux/modules/user.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import { AppDispatch, RootState } from 'redux/store';
-import { getCookie } from 'utils';
+import { getCookie, setCookie } from 'utils';
 
 export type UserState = {
-  userName: string | null;
   loading: boolean;
   error: Error | null;
+  userName: string | null;
   isLoggedIn: boolean;
 };
 
@@ -13,22 +13,23 @@ export type Action =
   | ReturnType<typeof loadUserRequest>
   | ReturnType<typeof loadUserSuccess>
   | ReturnType<typeof loadUserFailure>
-  | ReturnType<typeof login>
-  | ReturnType<typeof logout>;
+  | ReturnType<typeof loginRequest>
+  | ReturnType<typeof loginSuccess>
+  | ReturnType<typeof loginFailure>;
 
 const initialState: UserState = {
-  userName: null,
   loading: false,
   error: null,
-  isLoggedIn: false,
+  userName: null,
+  isLoggedIn: !!getCookie('accessToken'),
 };
 
 const LOAD_USER_REQUEST = 'user/LOAD_REQUEST' as const;
 const LOAD_USER_SUCCESS = 'user/LOAD_SUCCESS' as const;
 const LOAD_USER_FAILURE = 'user/LOAD_FAILURE' as const;
-
-const LOGIN = 'user/LOGIN' as const;
-const LOGOUT = 'user/LOGOUT' as const;
+const LOGIN_REQUEST = 'user/LOGIN_REQUEST' as const;
+const LOGIN_SUCCESS = 'user/LOGIN_SUCCESS' as const;
+const LOGIN_FAILURE = 'user/LOGIN_FAILURE' as const;
 
 const loadUserRequest = () => ({ type: LOAD_USER_REQUEST });
 const loadUserSuccess = (userName: string) => ({
@@ -39,16 +40,24 @@ const loadUserFailure = (error: Error) => ({
   type: LOAD_USER_FAILURE,
   payload: { error },
 });
-
-const login = () => ({ type: LOGIN });
-const logout = () => ({ type: LOGOUT });
+const loginRequest = () => ({ type: LOGIN_REQUEST });
+const loginSuccess = () => ({ type: LOGIN_SUCCESS });
+const loginFailure = (error: Error) => ({
+  type: LOGIN_FAILURE,
+  payload: { error },
+});
 
 export const loadUserAPI = (): any => async (dispatch: AppDispatch) => {
+  const token = getCookie('accessToken');
+  if (!token) {
+    return;
+  }
+
   dispatch(loadUserRequest());
   try {
     const { data: userName } = await axios.get('/api/customers/me', {
       headers: {
-        Authorization: `Bearer ${getCookie('accessToken')}`,
+        Authorization: `Bearer ${token}`,
       },
     });
     dispatch(loadUserSuccess(userName));
@@ -59,10 +68,25 @@ export const loadUserAPI = (): any => async (dispatch: AppDispatch) => {
   }
 };
 
+export const loginAPI =
+  (userName: string, password: string): any =>
+  async (dispatch: AppDispatch) => {
+    dispatch(loginRequest());
+    try {
+      const { data } = await axios.post('/api/login', { userName, password });
+      setCookie('accessToken', data);
+      dispatch(loginSuccess());
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        dispatch(loginFailure(error));
+      }
+    }
+  };
+
 const userReducer = (state = initialState, action: Action) => {
   switch (action.type) {
     case LOAD_USER_REQUEST: {
-      return { ...state, loading: true };
+      return { ...state, loading: true, error: null };
     }
     case LOAD_USER_SUCCESS: {
       const { userName } = action.payload;
@@ -74,11 +98,16 @@ const userReducer = (state = initialState, action: Action) => {
 
       return { ...state, loading: false, error };
     }
-    case LOGIN: {
-      return { ...state, isLoggedIn: true };
+    case LOGIN_REQUEST: {
+      return { ...state, loading: true, error: null };
     }
-    case LOGOUT: {
-      return { ...state, isLoggedIn: false };
+    case LOGIN_SUCCESS: {
+      return { ...state, loading: false, isLoggedIn: true };
+    }
+    case LOGIN_FAILURE: {
+      const { error } = action.payload;
+
+      return { ...state, loading: false, error, isLoggedIn: false };
     }
     default:
       return state;
@@ -86,7 +115,5 @@ const userReducer = (state = initialState, action: Action) => {
 };
 
 export const selectUserState = (state: RootState) => state.user;
-
-export { login, logout };
 
 export default userReducer;
