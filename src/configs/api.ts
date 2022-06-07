83ec576a7fecdc6169dd1447264f274746f94606
cks3066 <cks3066@naver.com>
2022-06-07 16:57:31 +0900

    refactor: axios interceptors 적용

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,6 +1,11 @@
 import Axios from 'axios';
+import { getAccessToken } from 'utils/auth';
 
 const axios = Axios.create({
+  baseURL: 'http://localhost:8080/api',
+});
+
+const axiosWithToken = Axios.create({
   baseURL: 'http://localhost:8080/api',
 });
 
@@ -8,4 +13,21 @@ const productAxios = Axios.create({
   baseURL: 'https://heroku-shopping-cart-lv2.herokuapp.com',
 });
 
-export { axios, productAxios };
+axiosWithToken.interceptors.request.use(
+  (config) => {
+    const accessToken = getAccessToken();
+
+    if (!!accessToken) {
+      config.headers = {
+        Authorization: `Bearer ${accessToken}`,
+      };
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export { axios, axiosWithToken, productAxios };
