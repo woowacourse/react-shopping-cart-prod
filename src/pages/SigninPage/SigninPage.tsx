cfa60b0e4e565f9b111ebdd5ac72e9ed5e6989eb
Jeongmin Woo <greenblues1190@gmail.com>
2022-06-07 17:18:13 +0900

    refactor: SigninPage에 useForm 적용

--- src/pages/SigninPage/SigninPage.tsx
+++ src/pages/SigninPage/SigninPage.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import useForm from '../../hooks/useForm';
 import Button from '../../components/Button/Button';
 import Input from '../../components/Input/Input';
-import * as S from './SigninPage.styled';
-import axios from 'axios';
 import PlainLink from '../../components/PlainLink/PlainLink';
-import { SigninResponseBody } from '../../types';
 import { SERVER_URL } from '../../configs/api';
+import { SigninResponseBody } from '../../types';
+import * as S from './SigninPage.styled';
 
 function SigninPage() {
   const navigate = useNavigate();
+  const { isSubmitting, registerForm, registerInput } = useForm();
+
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
     const payload = Object.fromEntries(formData.entries());
 
@@ -28,7 +30,7 @@ function SigninPage() {
       navigate('/');
     } catch (e) {
       if (axios.isAxiosError(e)) {
-        alert('유효하지 않은 이메일 형식입니다.');
+        alert('아이디, 비밀번호를 다시 확인해주세요.');
       } else {
         alert(e);
       }
@@ -38,23 +40,29 @@ function SigninPage() {
   return (
     <S.PageBox>
       <S.Title>로그인</S.Title>
-      <S.Form onSubmit={handleSubmit}>
+      <S.Form {...registerForm({ onSubmit: handleSubmit })}>
         <S.Section>
           <Input
             type="email"
-            name="email"
-            placeholder="이메일을 입력해주세요."
-            required
+            {...registerInput('email', {
+              placeholder: '이메일을 입력해주세요.',
+              maxLength: 50,
+              required: true,
+            })}
           />
           <Input
             type="password"
-            name="password"
-            placeholder="비밀번호를 입력해주세요."
-            required
+            {...registerInput('password', {
+              placeholder: '비밀번호를 입력해주세요.',
+              maxLength: 20,
+              required: true,
+            })}
           />
         </S.Section>
         <S.Section>
-          <Button type="submit">로그인</Button>
+          <Button type="submit" disabled={isSubmitting}>
+            로그인
+          </Button>
           <PlainLink to="/signup/1">
             <Button type="button" color="white">
               회원가입
