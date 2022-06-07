a0da6276d9e3a2f5db601c832406476ad37525f0
woose28 <dldntmdsla@naver.com>
2022-06-07 22:04:01 +0900

    refactor: 과도한 memoization 제거
    - CartProductCard 내부의 memoization 제거

--- src/components/cart/CartProductCard/CartProductCard.jsx
+++ src/components/cart/CartProductCard/CartProductCard.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from 'react';
+import React from 'react';
 
 import useCart from 'hooks/useCart';
 
@@ -11,8 +11,8 @@ import { WARNING_MESSAGES } from 'constants/messages';
 import { Position } from 'styles/GlobalStyles';
 import { color } from 'styles/Theme.js';
 
-
-const NotConfirmDecrement = (quantity, stock) => !window.confirm(WARNING_MESSAGES.DECREASE_TO_STOCK(quantity, stock));
+const NotConfirmDecrement = (quantity, stock) =>
+  !window.confirm(WARNING_MESSAGES.DECREASE_TO_STOCK(quantity, stock));
 
 function CartProductCard({
   product: { id: productId, name, price, imageURL, stock },
@@ -29,44 +29,38 @@ function CartProductCard({
   const isSoldOut = stock === 0;
   const isMoreThanStock = quantity >= stock;
   const counterValue = Math.min(stock, quantity);
-  const checked = useMemo(() => isChecked(productId), [isChecked]);
+  const checked = isChecked(productId);
 
-  const handleQuantityIncrement = useCallback(
-    () => {
-      if (isMoreThanStock) {
-        alert(WARNING_MESSAGES.MAX_QUANTITY);
+  const handleQuantityIncrement = () => {
+    if (isMoreThanStock) {
+      alert(WARNING_MESSAGES.MAX_QUANTITY);
+      return;
+    }
+
+    incrementCartProduct(productId, quantity);
+  };
+
+  const handleQuantityDecrement = () => {
+    let currentQuantity = quantity;
+
+    if (quantity > stock) {
+      if (NotConfirmDecrement(quantity, stock)) {
         return;
       }
+      currentQuantity = stock;
+    }
 
-      incrementCartProduct(productId, quantity)
-    },
-    [quantity],
-  );
+    decrementCartProduct(productId, currentQuantity);
+  };
 
-  const handleQuantityDecrement = useCallback(
-    () => {
-      let currentQuantity = quantity;
+  const handleProductDelete = () => deleteProduct([productId]);
 
-      if (quantity > stock) {
-        if (NotConfirmDecrement(quantity, stock)) {
-          return;
-        }
-        currentQuantity = stock
-      }
+  const handleCheckBoxClick = () => toggleCheck(productId);
 
-      decrementCartProduct(productId, currentQuantity)
-    },
-    [quantity],
-  );
-
-  const handleProductDelete = useCallback(() => deleteProduct([productId]), []);
-
-  const handleCheckBoxClick = useCallback(() => toggleCheck(productId), []);
-  
   return (
     <S.Container>
       <CheckBox checked={checked} disabled={isSoldOut} onClick={handleCheckBoxClick} />
-      <Image src={imageURL} width="150px" backgroundColor={color.WHITE}/>
+      <Image src={imageURL} width="150px" backgroundColor={color.WHITE} />
       <S.Description>
         <Position position="absolute" top="0" right="0">
           <S.Button type="button" onClick={handleProductDelete}>
@@ -81,13 +75,11 @@ function CartProductCard({
           onDecrement={handleQuantityDecrement}
         />
         <S.StatusMessage>
-          {
-            isSoldOut ?
-            '품절된 상품입니다.' : 
-            isMoreThanStock ? 
-            '구매 가능한 최대 수량입니다.' :
-            ''
-          }
+          {isSoldOut
+            ? '품절된 상품입니다.'
+            : isMoreThanStock
+            ? '구매 가능한 최대 수량입니다.'
+            : ''}
         </S.StatusMessage>
         <S.Price>{isSoldOut ? 0 : price * quantity}원</S.Price>
       </S.Description>
