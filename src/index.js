9a44d42262adf042e424495b7f67764e6fe4b9f2
DomMorello <zuzudnf@gmail.com>
2022-06-07 16:11:25 +0900

    fix: 초기 라우팅 수정

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { worker } from "mocks/browser";
 
 import App from "./components/App";
 
 import GlobalStyle from "./GlobalStyle";
 
-if (process.env.NODE_ENV === "development") {
-  const { worker } = require("./mocks/browser");
-  worker.start();
-}
+worker.start({
+  serviceWorker: {
+    url: "/react-shopping-cart/mockServiceWorker.js",
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="react-shopping-cart">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <GlobalStyle />
       <App />
     </BrowserRouter>
