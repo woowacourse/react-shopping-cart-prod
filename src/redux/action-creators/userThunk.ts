2efb8a7ae2220f21123d2b82e268bf9781ee27ad
kamwoo <rkadndud06@naver.com>
2022-06-07 22:37:28 +0900

    refactor: 유저 관련 async thunk, reducer, action 정리

--- src/redux/action-creators/userThunk.ts
+++ src/redux/action-creators/userThunk.ts
@@ -1,33 +1,28 @@
 import type { Dispatch } from 'redux';
-import { UserAction, UserActionType } from 'redux/actions/user';
+import { userAction, UserAction } from 'redux/actions/user';
 import axios from 'axios';
 import { LOCAL_BASE_URL } from 'apis';
 import { SignUpInfo, SignInInfo, EditPasswordInfo } from 'types/domain';
-import {
-  getLocalStorageToken,
-  KEYS,
-  setLocalStorageCartList,
-  setLocalStorageToken,
-} from 'utils/localStorage';
+import { getLocalStorageToken, setLocalStorageToken } from 'utils/localStorage';
 
 export const signUp = (signUpInfo: SignUpInfo) => async (dispatch: Dispatch<UserAction>) => {
-  dispatch({ type: UserActionType.POST_SIGN_UP_START });
+  dispatch(userAction.postSignUp.pending());
 
   try {
-    const response = await axios({
+    await axios({
       method: 'post',
       url: `${LOCAL_BASE_URL}/users`,
       data: signUpInfo,
     });
 
-    dispatch({ type: UserActionType.POST_SIGN_UP_SUCCESS });
-  } catch (e) {
-    dispatch({ type: UserActionType.POST_SIGN_UP_FAILURE, payload: e.message });
+    dispatch(userAction.postSignUp.success());
+  } catch (error) {
+    dispatch(userAction.postSignUp.failure(error.message));
   }
 };
 
 export const signIn = (signInInfo: SignInInfo) => async (dispatch: Dispatch<UserAction>) => {
-  dispatch({ type: UserActionType.POST_SIGN_IN_START });
+  dispatch(userAction.postSignIn.pending());
 
   try {
     const response = await axios({
@@ -37,21 +32,20 @@ export const signIn = (signInInfo: SignInInfo) => async (dispatch: Dispatch<User
     });
 
     setLocalStorageToken(response.data.token);
-
-    dispatch({ type: UserActionType.POST_SIGN_IN_SUCCESS, payload: response.data });
-  } catch (e) {
-    dispatch({ type: UserActionType.POST_SIGN_IN_FAILURE, payload: e.message });
-    alert(e.response.data.errorMessage);
+    dispatch(userAction.postSignIn.success(response.data));
+  } catch (error) {
+    dispatch(userAction.postSignIn.failure(error.message));
+    alert(error.response.data.errorMessage);
   }
 };
 
 export const editPassword =
   (editPasswordInfo: EditPasswordInfo) => async (dispatch: Dispatch<UserAction>) => {
-    dispatch({ type: UserActionType.PATCH_NEW_PASSWORD_START });
+    dispatch(userAction.patchNewPassword.pending());
     const token = getLocalStorageToken();
 
     try {
-      const response = await axios({
+      await axios({
         method: 'patch',
         headers: {
           'Content-Type': 'application/json',
@@ -61,19 +55,20 @@ export const editPassword =
         data: editPasswordInfo,
       });
 
-      dispatch({ type: UserActionType.PATCH_NEW_PASSWORD_SUCCESS });
-    } catch (e) {
-      dispatch({ type: UserActionType.PATCH_NEW_PASSWORD_FAILURE, payload: e.message });
-      alert(e.response.data.errorMessage);
+      dispatch(userAction.patchNewPassword.success());
+    } catch (error) {
+      dispatch(userAction.patchNewPassword.failure(error.message));
+      console.log(error);
+      alert(error.response.data.errorMessage);
     }
   };
 
 export const resign = (password: string) => async (dispatch: Dispatch<UserAction>) => {
-  dispatch({ type: UserActionType.DELETE_USER_START });
+  dispatch(userAction.deleteUser.pending());
   const token = getLocalStorageToken();
 
   try {
-    const response = await axios({
+    await axios({
       method: 'delete',
       headers: {
         'Content-Type': 'application/json',
@@ -83,15 +78,15 @@ export const resign = (password: string) => async (dispatch: Dispatch<UserAction
       data: password,
     });
 
-    dispatch({ type: UserActionType.DELETE_USER_SUCCESS });
-  } catch (e) {
-    dispatch({ type: UserActionType.DELETE_USER_FAILURE, payload: e.message });
-    alert(e.response.data.errorMessage);
+    dispatch(userAction.deleteUser.success());
+  } catch (error) {
+    dispatch(userAction.deleteUser.failure(error.message));
+    alert(error.response.data.errorMessage);
   }
 };
 
 export const autoSignIn = () => async (dispatch: Dispatch<UserAction>) => {
-  dispatch({ type: UserActionType.AUTO_SIGN_IN_START });
+  dispatch(userAction.autoSignIn.pending());
   const token = getLocalStorageToken();
 
   try {
@@ -105,30 +100,9 @@ export const autoSignIn = () => async (dispatch: Dispatch<UserAction>) => {
     });
 
     setLocalStorageToken(response.data.token);
-
-    dispatch({ type: UserActionType.AUTO_SIGN_IN_SUCCESS, payload: response.data });
-  } catch (e) {
-    dispatch({ type: UserActionType.AUTO_SIGN_IN_FAILURE, payload: e.message });
-    alert(e.response.data.errorMessage);
+    dispatch(userAction.autoSignIn.success(response.data));
+  } catch (error) {
+    dispatch(userAction.autoSignIn.failure(error.message));
+    alert(error.response.data.errorMessage);
   }
 };
-
-/* TODO - 회원 이름 수정 api 명세 작성 후 연동
-export const editProfile = (editInfo: UserInfo) => async (dispatch: Dispatch<UserAction>) => {
-  dispatch({ type: UserActionType.PATCH_USER_INFO_START });
-
-  try {
-    const response = await axios({
-      method: 'patch',
-      url: `${LOCAL_BASE_URL}/users/me`,
-      data: editInfo,
-    });
-
-    localStorage.setItem('token', response.data.token);
-
-    dispatch({ type: UserActionType.PATCH_USER_INFO_SUCCESS, payload: response.data });
-  } catch (e) {
-    dispatch({ type: UserActionType.PATCH_USER_INFO_FAILURE, payload: e.message });
-    alert(e.response.data.errorMessage);
-  }
-};*/
