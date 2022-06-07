640d822144733eeac5cf77b1cbceb8e0449d4980
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-06-07 22:25:08 +0900

    feat: add getting cart msw handler

---
+++ src/page/CartPage/mock.ts
@@ -0,0 +1,31 @@
+// @ts-nocheck
+import { rest } from 'msw';
+import { users, cart } from 'mocks';
+import CustomError from 'utils/CustomError';
+
+const getCartHandler = rest.get('/cart', (req, res, ctx) => {
+  try {
+    const { authorization } = req.headers.headers;
+
+    const token = authorization.replace('Bearer ', '');
+    const accessToken = JSON.parse(!!token && !token.includes('undefined') ? token : null);
+
+    // [ERROR] 유효한 토큰이 아닌 경우
+    if (!accessToken || !users.some(user => user.id === accessToken.id)) {
+      throw new CustomError(1003, '유효하지 않은 토큰입니다.', 401);
+    }
+
+    // 전체카트조회 성공
+    return res(ctx.status(200), ctx.json(cart));
+  } catch (error) {
+    return res(
+      ctx.status(error.statusCode),
+      ctx.json({
+        code: error.code,
+        message: error.message,
+      }),
+    );
+  }
+});
+
+export default getCartHandler;
