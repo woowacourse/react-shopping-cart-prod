529a669eaf9bbef93be66039d6dc5c3876334b29
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-06-07 17:36:09 +0900

    feat: add details for logout

--- src/mocks/handlers/logoutHandler.ts
+++ src/mocks/handlers/logoutHandler.ts
@@ -1,21 +1,29 @@
 // @ts-nocheck
 import { rest } from 'msw';
 import { users } from 'mocks';
+import CustomError from 'utils/CustomError';
 
 const logoutHandler = rest.post('/auth/logout', (req, res, ctx) => {
   try {
-    const accessToken = JSON.parse(req.headers.headers.authorization.replace('Bearer ', ''));
-    // 유효한 토큰이 아닌 경우
-    if (!users.some(user => user.id === accessToken.id)) throw new Error();
+    const { authorization } = req.headers.headers;
+
+    const token = authorization.replace('Bearer ', '');
+    const accessToken = JSON.parse(!token && !token.includes('undefined') ? token : null);
+
+    // [ERROR] 유효한 토큰이 아닌 경우
+    if (!accessToken || !users.some(user => user.id === accessToken.id)) {
+      throw new CustomError(1003, '유효하지 않은 토큰입니다.', 401);
+    }
 
     // 로그아웃 성공
     return res(ctx.status(204));
   } catch (error) {
     // 로그아웃 실패
     return res(
-      ctx.status(401),
+      ctx.status(error.statusCode),
       ctx.json({
-        message: 'No authorization',
+        code: error.code,
+        message: error.message,
       }),
     );
   }
