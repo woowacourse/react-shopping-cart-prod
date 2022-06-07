a5890e7b96ba4c6701e59c065f91332fc0a8887a
Jeongmin Woo <greenblues1190@gmail.com>
2022-06-07 21:15:51 +0900

    refactor: reduce에서 filter로 수정
    Refs: https://github.com/woowacourse/react-shopping-cart-prod/pull/37#discussion_r889563394

--- src/pages/MainPage/useProductList.ts
+++ src/pages/MainPage/useProductList.ts
@@ -18,30 +18,18 @@ const useProductList = () => {
     productList: productsState.productList,
     error: productsState.error,
   }));
+  const avaliableList = productList.filter((product) => product.stock > 0);
+  const outOfStockList = productList.filter((product) => product.stock <= 0);
 
   useEffect(() => {
     dispatch(actions.getProductList());
   }, [dispatch]);
 
-  const sortedProductList = useMemo(() => {
-    const { availableList, outOfStockList } = productList.reduce(
-      (next, product) => {
-        const isAvailable = product.stock > 0;
-
-        (isAvailable ? next.availableList : next.outOfStockList).push(product);
-
-        return next;
-      },
-      {
-        availableList: [] as Array<Product>,
-        outOfStockList: [] as Array<Product>,
-      }
-    );
-
-    return [...availableList, ...outOfStockList];
-  }, [productList]);
-
-  return { isLoading, productList: sortedProductList, error };
+  return {
+    isLoading,
+    productList: [...avaliableList, ...outOfStockList],
+    error,
+  };
 };
 
 export default useProductList;
