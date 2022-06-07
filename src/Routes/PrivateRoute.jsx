1f4f83b867a57f3dd6e36469f8e7221b8d6fb32b
JUDONGHYEOK <judonghyeok@gmail.com>
2022-06-07 20:21:18 +0900

    fix: accessToken에 따라 routing시 snackbar 무한호출 해결

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
 
@@ -6,16 +7,23 @@ import { onMessage } from 'reducers/snackbar';
 import { SNACKBAR_MESSAGE, PATH } from 'constants';
 
 const PrivateRoute = ({ children, path = PATH.LOGIN, showMessage = true }) => {
-  const { accessToken } = useSelector((state) => state.user);
+  const accessToken = useSelector((state) => state.user.accessToken);
   const location = useLocation();
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const showRealMessage = async () => {
+      await dispatch(onMessage(SNACKBAR_MESSAGE.noAuth()));
+    };
+    accessToken || (showMessage && showRealMessage());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   if (accessToken) {
     return children;
   }
 
-  showMessage && dispatch(onMessage(SNACKBAR_MESSAGE.noAuth()));
-  return <Navigate to={path} state={{ from: location }} replace />;
+  return <Navigate to={path} state={{ from: location }} replace={true} />;
 };
 
 export default PrivateRoute;
