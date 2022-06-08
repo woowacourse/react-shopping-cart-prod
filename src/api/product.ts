160ac240ad77d6d92801c25099ddcd0530f70a9c
junzzi <jjh01027717502@gmail.com>
2022-06-08 14:26:32 +0900

    deploy: test https

--- src/api/product.ts
+++ src/api/product.ts
@@ -1,6 +1,7 @@
 import { caching } from '@/api/cache';
 import { API_URL } from '@/api/constants';
 import { ProductType } from '@/domain/product';
+import https from 'https';
 import axios from 'axios';
 export const cache = {};
 const productAPI = axios.create({
@@ -11,7 +12,11 @@ export const getProductList = () => {
   const cacheKey = `${API_URL}/products`;
 
   return caching((): Promise<any> => {
-    return productAPI.get('');
+    return productAPI.get('', {
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false, //허가되지 않은 인증을 reject하지 않겠다!
+      }),
+    });
   }, cacheKey);
 };
 
