9d4801adb679a948d56069e684758ec41f6af894
Yo Wook Kim <uk960214@gmail.com>
2022-06-08 15:19:34 +0900

    fix: API endpoint 수정

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL;
+export const API_URL = ['http://15.164.170.161:8080', 'http://43.200.3.154:8080/'];
 
 export const PRODUCT_LIST_PAGE_LIMIT = 12;
 
@@ -10,8 +10,8 @@ export const API_ENDPOINT = {
   MEMBERS: '/api/members',
   EMAIL_CHECK: '/api/members/email-check',
   AUTH: {
-    ME: '/api/members/auth/me',
-    PASSWORD: '/api/members/auth/password',
-    PASSWORD_CHECK: '/api/members/auth/password-check',
+    ME: '/api/members/me',
+    PASSWORD: '/api/members/password',
+    PASSWORD_CHECK: '/api/members/password-check',
   },
 };
