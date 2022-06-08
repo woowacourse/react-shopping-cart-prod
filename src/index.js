43fdc32e79a1a2cd89007e54201d4a189d3da005
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-06-08 22:54:12 +0900

    chore: add server url

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import reportWebVitals from 'reportWebVitals';
-import axios from 'axios';
 import App from 'App';
 import store from 'store/store';
 
 // 이프
-// const SERVER_URL1 = 'http://ec2-13-125-41-7.ap-northeast-2.compute.amazonaws.com:8080/';
+const SERVER_URL1 = 'http://ec2-3-39-234-109.ap-northeast-2.compute.amazonaws.com:8080/';
 
 // 더즈
-// const SERVER_URL2 = 'http://15.164.211.129:8080/';
+const SERVER_URL2 = 'http://15.164.211.129:8080/';
 
 // 토르
-// const SERVER_URL3 = 'http://ec2-15-164-232-166.ap-northeast-2.compute.amazonaws.com:8080/';
+const SERVER_URL3 = 'http://ec2-15-164-232-166.ap-northeast-2.compute.amazonaws.com/';
 
 // 찬
-// const SERVER_URL4 = 'http://ec2-3-34-130-116.ap-northeast-2.compute.amazonaws.com:8080/';
+const SERVER_URL4 = 'http://ec2-3-34-130-116.ap-northeast-2.compute.amazonaws.com/';
 
 if (process.env.NODE_ENV === 'development') {
   axios.defaults.baseURL = 'http://localhost:3000';
 } else if (process.env.NODE_ENV === 'production') {
-  axios.defaults.baseURL = 'http://ec2-13-125-41-7.ap-northeast-2.compute.amazonaws.com:8080/';
+  axios.defaults.baseURL = SERVER_URL1;
 }
 
 async function main() {
