6e440619c562ada2a40db493c3bbaae239bd168c
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-06-08 20:00:25 +0900

    ♻️ : Checkbox 컴포넌트 비제어 컴포넌트에서도 사용 가능하도록 개선

--- src/components/@common/Checkbox/index.jsx
+++ src/components/@common/Checkbox/index.jsx
@@ -1,24 +1,43 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 import * as S from './styles';
 
-function Checkbox({ size, checked, onChange, children }) {
+function Checkbox({ name, size, checked, onChange, children }) {
+  const [isChecked, setChecked] = useState(false);
+
+  const onClickCheckbox = () => {
+    if (checked !== null) {
+      return;
+    }
+
+    setChecked(!isChecked);
+  };
+
   return (
-    <S.Container size={size} checked={checked}>
-      <S.Check type="checkbox" checked={checked} onChange={onChange} />
+    <S.Container size={size} checked={checked || isChecked}>
+      <S.Check
+        type="checkbox"
+        name={name}
+        checked={checked || isChecked}
+        onClick={onClickCheckbox}
+        onChange={onChange}
+      />
       {children && <S.Text>{children}</S.Text>}
     </S.Container>
   );
 }
 
 Checkbox.defaultProps = {
-  checked: false,
+  checked: null,
   size: 'medium',
+  onChange: () => {},
 };
 
 Checkbox.propTypes = {
-  checked: PropTypes.bool,
+  checked: PropTypes.oneOf([PropTypes.bool, null]),
   size: PropTypes.oneOfType([PropTypes.oneOf(['large', 'medium', 'small']), PropTypes.number]),
+  onChange: PropTypes.func,
 };
 
 export default Checkbox;
