ccb35ad07e9ea29a6dd5c5a361ddba83674568ea
JUDONGHYEOK <judonghyeok@gmail.com>
2022-06-08 19:55:44 +0900

    build: 배포를 위해 browserRouter basename 설정

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -25,7 +25,7 @@ const Router = () => {
   const { show, message } = useSelector((state) => state.snackbar);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header />
       <Body>
         <Routes>
