e96d8b68672501f9d72a4535d1d44f8d6dcaf009
sangminsong <ssmin940606@gmail.com>
2022-06-08 15:12:30 +0900

    chore: redux logger 설정

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from 'redux/store';
 
-if (process.env.NODE_ENV === 'development') {
-  initMock();
-}
+// if (process.env.NODE_ENV === 'development') {
+//   initMock();
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
