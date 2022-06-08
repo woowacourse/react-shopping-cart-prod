096b04f79642f3019f3d5ed03e06a3f46a6339cb
moonheekim0118 <hellomooneekim@gmail.com>
2022-06-08 21:25:37 +0900

    fix: 중복된 이메일 메시지 보여주도록 수정

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -2,13 +2,9 @@ import apiClient from 'api';
 import { getCookie } from 'utils/cookie';
 
 export const duplicateEmailApi = async (email) => {
-  try {
-    await apiClient.get(`/api/members/duplicate-email?email=${email}`, {
-      email,
-    });
-  } catch (err) {
-    console.log('err', err);
-  }
+  await apiClient.get(`/api/members/duplicate-email?email=${email}`, {
+    email,
+  });
 };
 
 export const getAuthorizedHeaders = () => {
