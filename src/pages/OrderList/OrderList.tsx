7cc09eab45bdecc3f8f7ccd29a92a64461fa9c1e
kkojae <jaejeung3210@gmail.com>
2022-06-09 02:39:15 +0900

    feat; OrderList component 생성

--- src/pages/OrderList/OrderList.tsx
+++ src/pages/OrderList/OrderList.tsx
@@ -1,12 +1,47 @@
-import ErrorContainer from '../../components/common/ErrorContainer/ErrorContainer';
+import { getAllOrderList } from '@/api/orderList';
+import { withLogin } from '@/components/helper/withLogin';
+import OrderInformation from '@/components/order/OrderInformation/OrderInformation';
+import useResponsive from '@/hooks/useResponsive';
+import { useEffect, useState } from 'react';
 import PageTemplate from '../../components/common/PageTemplate/PageTemplate';
+import * as Styled from './OrderList.style';
 
 function OrderList() {
+  const [orderList, setOrderList] = useState<any>([]);
+  const responsive = useResponsive();
+
+  useEffect(() => {
+    const apiCall = async () => {
+      const {
+        data: { orders },
+      } = await getAllOrderList();
+
+      setOrderList(orders);
+    };
+
+    apiCall();
+  }, []);
+
   return (
     <PageTemplate>
-      <ErrorContainer>🚧 아직 준비 중인 서비스입니다. 🚧 </ErrorContainer>
+      <Styled.Container>
+        <Styled.Title>주문목록</Styled.Title>
+        {orderList.length !== 0 ? (
+          orderList.map(order => (
+            <div key={order.id}>
+              <OrderInformation
+                orderList={order.orderDetails}
+                responsive={responsive}
+                orderId={order.id}
+              />
+            </div>
+          ))
+        ) : (
+          <></>
+        )}
+      </Styled.Container>
     </PageTemplate>
   );
 }
 
-export default OrderList;
+export default withLogin(OrderList, true);
