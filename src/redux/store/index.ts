62b3c95666fe3646fc151aa331ee6f1215d22491
cks3066 <cks3066@naver.com>
2022-06-08 15:06:46 +0900

    feat: logger 적용

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,12 +1,12 @@
 import { applyMiddleware, createStore } from 'redux';
-
+import logger from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
 import { persistStore } from 'redux-persist';
 import rootReducer from 'redux/reducers';
 
 export type RootState = ReturnType<typeof store.getState>;
 
-const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk, logger));
 
 const persistor = persistStore(store);
 
