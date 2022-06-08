4180c123d67d92e669bfb99f5b78fc9e7628db43
junzzi <jjh01027717502@gmail.com>
2022-06-08 16:57:34 +0900

    msw: order mock API 개발, handler off

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,5 +1,7 @@
 import { cartHanlders } from '@/mocks/handlers/cart';
+import { customerHanlders } from '@/mocks/handlers/customer';
+import { orderHanlders } from '@/mocks/handlers/order';
 import { productHanlders } from '@/mocks/handlers/product';
 import { setupWorker } from 'msw';
 
-export const worker = setupWorker(...cartHanlders);
+export const worker = setupWorker();
--- src/mocks/handlers/product.js
+++ src/mocks/handlers/product.js
@@ -4,24 +4,9 @@ import { rest } from 'msw';
 
 export const productHanlders = [
   rest.get(`${API_URL}/products`, (req, res, ctx) => {
-    const {
-      url: { searchParams },
-    } = req;
-    const page = Number(searchParams.get('_page')) || 1;
-    const limit = Number(searchParams.get('_limit')) || 10;
-
-    if (page === 1 || page === 0) {
-      return res(
-        ctx.set('x-total-count', `${productList.length}`),
-        ctx.json(productList.slice(0, limit)),
-      );
-    }
-
-    return res(
-      ctx.set('x-total-count', `${productList.length}`),
-      ctx.json(productList.slice(limit)),
-    );
+    return res(ctx.json({ products: productList }));
   }),
+
   rest.get(`${API_URL}/products/:productId`, (req, res, ctx) => {
     const {
       params: { productId },
@@ -29,6 +14,6 @@ export const productHanlders = [
 
     const product = productList.find(product => product.id === Number(productId));
 
-    return res(ctx.json(product));
+    return res(ctx.json({ product }));
   }),
 ];
