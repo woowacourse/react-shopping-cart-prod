478656d41ef55239907d24b7acfc05ce0a38313e
kwannee <yleh1126@naver.com>
2022-06-09 01:50:33 +0900

    feat: 로그인 유저 , 비로그인 유저만 접근할 수 있는 path 설정
    Co-authored-by: ronci <ronci@users.noreply.github.com>

--- react-shopping-cart/src/index.jsx
+++ react-shopping-cart/src/index.jsx
@@ -1,26 +1,13 @@
+import App from 'App';
 import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { createStore } from 'redux';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
-import { ThemeProvider } from 'styled-components';
-
-import ErrorPage from 'pages/ErrorPage/ErrorPage.page';
-import Login from 'pages/Login/Login.page';
-import ModifyUserInfo from 'pages/ModifyUserInfo/ModifyUserInfo';
-import ProductList from 'pages/ProductList/ProductList.page';
-import ShoppingCartList from 'pages/ShoppingCartList/ShoppingCartList.page';
-import SignUp from 'pages/SignUp/SignUp.page';
-
-import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
 
 import rootReducer from 'redux/reducers';
-
-import GlobalStyle from 'styles/GlobalStyle';
-import theme from 'styles/theme';
 
 import { BASE_URL } from 'constants/api';
 
@@ -37,27 +24,13 @@ if (process.env.NODE_ENV === 'development') {
     },
   });
 }
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <GlobalStyle />
-        <BrowserRouter>
-          <ThemeProvider theme={theme}>
-            <ErrorBoundary fallback={<ErrorPage>오류가 발생했습니다</ErrorPage>}>
-              <Routes>
-                <Route path="/" element={<ProductList />} />
-                <Route path="/cart" element={<ShoppingCartList />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/user/modify" element={<ModifyUserInfo />} />
-                <Route path="/*" element={<ErrorPage>존재하지 않는 페이지입니다</ErrorPage>} />
-              </Routes>
-            </ErrorBoundary>
-          </ThemeProvider>
-        </BrowserRouter>
+        <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>
