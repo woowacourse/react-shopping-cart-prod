28dba353c7c00eb0d9cdeeaa69261482a7f8a1ac
jswith <typingjs@gmail.com>
2022-06-09 06:20:04 +0900

    style: url selector 스타일 수정

--- src/components/common/url-selector/UrlSelector.styled.js
+++ src/components/common/url-selector/UrlSelector.styled.js
@@ -6,12 +6,28 @@ const StyledUrlSelectorContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  width: 180px;
   left: 30px;
   top: 100px;
   border: 2px solid black;
+  background-color: #ccc;
+  padding: 10px;
+  border: none;
+  border-radius: 10px;
 
   button {
     font-size: 24px;
+    width: 100%;
+  }
+
+  button:hover {
+    background-color: white;
+    transform: scale(1.2);
+  }
+
+  div {
+    padding: 10px;
+    margin-bottom: 10px;
   }
 
   @media (max-width: 1600px) {
