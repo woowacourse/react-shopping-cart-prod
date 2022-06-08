0371b1baf1c74122acf3fbc7ad32a6a8728a9acd
kwannee <yleh1126@naver.com>
2022-06-09 00:00:52 +0900

    refactor: 상품 목록 불러오던 로직 커스텀 훅으로 분리
    Co-authored-by: ronci <ronci@users.noreply.github.com>

--- react-shopping-cart/src/components/ProductListContainer/ProductListContainer.component.jsx
+++ react-shopping-cart/src/components/ProductListContainer/ProductListContainer.component.jsx
@@ -3,15 +3,15 @@ import Error from 'components/@shared/Error/Error.component';
 import ProductListBox from 'components/ProductListBox/ProductListBox.component';
 import ProductListItem from 'components/ProductListItem/ProductListItem.component';
 
-function ProductListContainer({ data, loadProducts }) {
+function ProductListContainer({ products, loadProducts }) {
   return (
     <main>
       <h1 hidden>상품 리스트</h1>
-      {Array.isArray(data) && data.length === 0 ? (
+      {Array.isArray(products) && products.length === 0 ? (
         <Error>상품이 존재하지 않습니다</Error>
       ) : (
         <ProductListBox as="ul">
-          {data.map(itemInfo => (
+          {products.map(itemInfo => (
             <li key={itemInfo.id}>
               <ProductListItem {...itemInfo} loadProducts={loadProducts} />
             </li>
--- react-shopping-cart/src/pages/ProductList/ProductList.page.jsx
+++ react-shopping-cart/src/pages/ProductList/ProductList.page.jsx
@@ -1,5 +1,3 @@
-import { useSelector } from 'react-redux';
-
 import PageContainer from 'components/@shared/PageContainer/PageContainer.component';
 
 import Header from 'components/Header/Header.component';
@@ -7,7 +5,7 @@ import ProductListBox from 'components/ProductListBox/ProductListBox.component';
 import ProductListContainer from 'components/ProductListContainer/ProductListContainer.component';
 import SkeletonItem from 'components/SkeletonItem/SkeletonItem.component';
 
-import useFetch from 'hooks/useFetch';
+import useLoadProducts from 'hooks/useLoadProducts';
 
 import { API_URL_PATH } from 'constants/api';
 
@@ -22,16 +20,7 @@ function LoadingSection() {
 }
 
 function ProductList() {
-  const { accessToken } = useSelector(state => state.auth);
-  const headers = accessToken && { Authorization: `Bearer ${accessToken}` };
-  const {
-    data,
-    isLoading,
-    fetchData: loadProducts,
-  } = useFetch({
-    url: API_URL_PATH.PRODUCTS,
-    headers,
-  });
+  const { products, isLoading, loadProducts } = useLoadProducts();
 
   return (
     <>
@@ -40,7 +29,7 @@ function ProductList() {
         {isLoading ? (
           <LoadingSection />
         ) : (
-          <ProductListContainer data={data.products} loadProducts={loadProducts} />
+          <ProductListContainer products={products} loadProducts={loadProducts} />
         )}
       </PageContainer>
     </>
