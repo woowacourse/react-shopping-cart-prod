69f894ed9dec469a007b54c937e484177bd97ea9
(yunjin) <sion11290805@gmail.com>
2022-06-08 21:09:13 +0900

    refactor: api 명세에 맞게 요청을 변경한다.

--- src/api/cart.js
+++ src/api/cart.js
@@ -1,52 +1,82 @@
 import { request } from 'lib/requestUtils';
 
 const requestGetCartList = () =>
-  request('/cart', {
-    method: 'GET',
-  });
+  request(
+    '/auth/customer/cartItems',
+    {
+      method: 'GET',
+    },
+    {
+      isAccessTokenUsed: true,
+    },
+  );
 
 const requestAddCart = ({ id, image, name, price, quantity, isChecked }) =>
-  request('/cart', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
+  request(
+    '/auth/customer/cartItems',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        product: id,
+        image,
+        name,
+        price,
+        quantity,
+        isChecked,
+      }),
     },
-    body: JSON.stringify({
-      product: id,
-      image,
-      name,
-      price,
-      quantity,
-      isChecked,
-    }),
-  });
+    {
+      isAccessTokenUsed: true,
+    },
+  );
 
 const requestUpdateCartItem = async (id, content) => {
-  const response = await request(`/cart/${id}`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
+  const response = await request(
+    `/auth/customer/cartItems/${id}`,
+    {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        ...content,
+      }),
     },
-    body: JSON.stringify({
-      ...content,
-    }),
-  });
+    {
+      isAccessTokenUsed: true,
+    },
+  );
 
   return response;
 };
 
 const requestRemoveCartItem = async (id) => {
-  const response = await request(`/cart/${id}`, {
-    method: 'DELETE',
-  });
+  const response = await request(
+    `/auth/customer/cartItems/${id}`,
+    {
+      method: 'DELETE',
+    },
+    {
+      isAccessTokenUsed: true,
+    },
+  );
 
   return response;
 };
 
 const requestRemoveCartItemList = async (idList) => {
-  const response = await request(`/cart/${idList.join(',')}`, {
-    method: 'DELETE',
-  });
+  const response = await request(
+    `/auth/customer/cartItems/${idList.join(',')}`,
+    {
+      method: 'DELETE',
+    },
+    {
+      isAccessTokenUsed: true,
+    },
+  );
 
   return response;
 };
