17431fcf29fadd63fc7987d57d8b22a329878a85
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-06-09 04:40:22 +0900

    refactor: change server url

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import reportWebVitals from 'reportWebVitals';
 import App from 'App';
-import store from 'store/store';
+import store from 'store';
 
 // 이프
 const SERVER_URL1 = 'http://ec2-3-39-234-109.ap-northeast-2.compute.amazonaws.com:8080/';
@@ -13,10 +13,10 @@ const SERVER_URL1 = 'http://ec2-3-39-234-109.ap-northeast-2.compute.amazonaws.co
 const SERVER_URL2 = 'http://15.164.211.129:8080/';
 
 // 토르
-const SERVER_URL3 = 'http://ec2-15-164-232-166.ap-northeast-2.compute.amazonaws.com/';
+const SERVER_URL3 = 'http://ec2-15-164-232-166.ap-northeast-2.compute.amazonaws.com:8080/';
 
 // 찬
-const SERVER_URL4 = 'http://ec2-3-34-130-116.ap-northeast-2.compute.amazonaws.com/';
+const SERVER_URL4 = 'http://ec2-3-34-130-116.ap-northeast-2.compute.amazonaws.com:8080/';
 
 if (process.env.NODE_ENV === 'development') {
   axios.defaults.baseURL = 'http://localhost:3000';
