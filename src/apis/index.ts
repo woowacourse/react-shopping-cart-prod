eda995abe198007c230e8d5ff06a7861417583e2
Hwang Jung Min <hwangstar156@gmail.com>
2022-06-09 15:03:50 +0900

    fix: AUTH_BASE_URL -> BASE_URL로 통합

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,14 +1,9 @@
 import axios from 'axios';
 
 export const BASE_URL = process.env.REACT_APP_PRODUCT_BASE_URL;
-export const AUTH_BASE_URL = process.env.REACT_APP_AUTH_PRODUCT_BASE_URL;
 
 export const client = axios.create({
   baseURL: BASE_URL,
-});
-
-export const authClient = axios.create({
-  baseURL: AUTH_BASE_URL,
   headers: {
     'Access-Control-Allow-Origin': '*',
   },
