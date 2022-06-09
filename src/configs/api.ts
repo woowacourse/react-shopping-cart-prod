2fbcf046c67660091a05f8f3680711b1036e9b1d
이아현 <lah1203@naver.com>
2022-06-09 17:29:15 +0900

    chore: api 링크

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,7 +1,18 @@
 import Axios from 'axios';
 
 const axios = Axios.create({
-  baseURL: 'http://localhost:8080/api',
+  // 매트
+  baseURL:
+    'http://ec2-15-164-94-21.ap-northeast-2.compute.amazonaws.com:8080/api',
+  // 야호
+  // baseURL:
+  //   'http://ec2-13-125-121-5.ap-northeast-2.compute.amazonaws.com:8080/api',
+  // 차리
+  // baseURL:
+  //   'http://ec2-13-125-246-80.ap-northeast-2.compute.amazonaws.com:8080/api',
+  // 슬로
+  // baseURL:
+  //   'http://ec2-15-164-49-241.ap-northeast-2.compute.amazonaws.com:8080/api',
 });
 
 export default axios;
