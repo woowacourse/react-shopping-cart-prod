de6b2c0c0674cf80b7f321462af472baf354c3d5
Daye Lee <508yeah@gmail.com>
2022-06-09 15:02:11 +0900

    feat: 로그인 상태에 따라 상품 목록 request 분기

--- src/redux/modules/product/productThunk.ts
+++ src/redux/modules/product/productThunk.ts
@@ -2,12 +2,20 @@ import { AppDispatch } from '@/redux/store';
 import { loadProducts, loadProductsFailure, loadProductsSuccess } from './productAction';
 
 import axios from 'axios';
+import { getCookie } from '@/utils';
 
 const loadProductsAPI = (): any => async (dispatch: AppDispatch) => {
   dispatch(loadProducts());
 
   try {
-    const { data: productList } = await axios.get(`${process.env.REACT_APP_API_URL}/api/products`);
+    const token = getCookie('accessToken');
+    const { data: productList } = token
+      ? await axios.get(`${process.env.REACT_APP_API_URL}/api/products`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+      : await axios.get(`${process.env.REACT_APP_API_URL}/api/products`);
 
     dispatch(loadProductsSuccess(productList));
   } catch (error: unknown) {
