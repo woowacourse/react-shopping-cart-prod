d04e40a5157638aa472682e25082ef797a346a77
Hwang Jung Min <hwangstar156@gmail.com>
2022-06-09 15:25:53 +0900

    feat: msw 코드 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -7,17 +7,6 @@ import theme from 'styles/theme';
 
 import App from './App';
 
-if (process.env.NODE_ENV === 'development') {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const { worker } = require('./mocks/browser');
-
-  worker.start({
-    serviceWorker: {
-      url: '/react-shopping-cart/mockServiceWorker.js',
-    },
-  });
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
