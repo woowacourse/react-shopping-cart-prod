d033bb03846cf81e7fd006aa492108556a28983d
Jeongmin Woo <greenblues1190@gmail.com>
2022-06-09 16:26:07 +0900

    refactor: 백엔드 api url 추가

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,8 +1,8 @@
 const BE_PAIR_SERVER_URL = {
-  SKULL_SUDAL:
-    'http://ec2-3-37-87-144.ap-northeast-2.compute.amazonaws.com:8080',
+  SKULL: 'http://3.34.97.220:8080',
+  SUDAL: 'http://3.36.70.224:8080',
   HOODY: 'http://ec2-3-37-87-144.ap-northeast-2.compute.amazonaws.com:8080',
   BAEKARA: 'http://ec2-13-209-4-69.ap-northeast-2.compute.amazonaws.com:8080',
 };
 
-export const SERVER_URL = BE_PAIR_SERVER_URL.BAEKARA;
+export const SERVER_URL = BE_PAIR_SERVER_URL.HOODY;
