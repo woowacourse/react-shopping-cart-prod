9d8832e53f1f311109af612d7f45e8ce470b2ffd
Marco <bigdatajang@gmail.com>
2022-06-09 11:49:29 +0900

    heroku push 서버 선택 페이지'

--- src/apis/apiClient.js
+++ src/apis/apiClient.js
@@ -1,19 +1,10 @@
 import axios from 'axios';
-// import { selectedApiName } from 'page/SeverSelectPage';
 import { getCookie } from 'utils/cookie';
 
-// const AUTH_API_URL = {
-//   이프: 'http://ec2-3-39-234-109.ap-northeast-2.compute.amazonaws.com:8080',
-//   찬: 'http://ec2-3-34-130-116.ap-northeast-2.compute.amazonaws.com:8080',
-//   더즈: 'http://15.164.211.129:8080',
-//   토르: 'http://ec2-15-164-232-166.ap-northeast-2.compute.amazonaws.com:8080',
-// };
-
 export const isApiTest = true;
-// export let selectedApiName = '찬';
 
 if (isApiTest) {
-  console.log('API로 연결하였습니다.');
+  console.log('백엔드 크루의 API로 연결하였습니다.');
 } else {
   console.log('실제 Api로 연결하지 않고 MSW로 동작합니다.');
 }
@@ -21,8 +12,7 @@ if (isApiTest) {
 const accessToken = getCookie('accessToken');
 
 const apiClient = axios.create({
-  // baseURL: process.env.NODE_ENV === ENV.DEVELOPMENT && AUTH_API_URL.이프,
-  // baseURL: isApiTest && AUTH_API_URL[selectedApiName],
+  baseURL: isApiTest && 'http://ec2-3-39-234-109.ap-northeast-2.compute.amazonaws.com:8080',
   headers: { Authorization: `Bearer ${accessToken}`, withCredentials: true },
 });
 
