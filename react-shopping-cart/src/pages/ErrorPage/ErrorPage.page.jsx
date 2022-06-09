c3c3c2f985e9eb455765a8e42f869c6eaa0b7f14
kwannee <yleh1126@naver.com>
2022-06-09 17:18:04 +0900

    feat: 에러 페이지 홈으로 이동할 수 있는 링크 추가

--- react-shopping-cart/src/pages/ErrorPage/ErrorPage.page.jsx
+++ react-shopping-cart/src/pages/ErrorPage/ErrorPage.page.jsx
@@ -1,12 +1,26 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
 import Error from 'components/@shared/Error/Error.component';
+import FlexBox from 'components/@shared/FlexBox/FlexBox.component';
 import PageContainer from 'components/@shared/PageContainer/PageContainer.component';
 
 function ErrorPage({ children }) {
   return (
     <PageContainer>
-      <Error>{children}</Error>
+      <FlexBox direction="column" gap="50px" justifyContent="center">
+        <Error>{children}</Error>
+        <HomeLink to="/">홈으로</HomeLink>
+      </FlexBox>
     </PageContainer>
   );
 }
 
 export default ErrorPage;
+
+const HomeLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  font-weight: 900;
+  font-size: 5em;
+`;
