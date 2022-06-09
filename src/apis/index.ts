b2ada2fceaf451f8d5685dbed64acb76e20eb888
Hwang Jung Min <hwangstar156@gmail.com>
2022-06-09 16:54:08 +0900

    feat: api 주소들 주석으로 명세

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -2,6 +2,11 @@ import axios from 'axios';
 
 export const BASE_URL = 'http://13.124.173.149:8080';
 
+// 아서 http://13.124.173.149:8080
+// 승팡 http://3.37.123.166:8080
+// 페퍼 http://13.124.187.203:8080
+// 앤지 http://3.39.195.12:8080
+
 export const client = axios.create({
   baseURL: BASE_URL,
   headers: {
