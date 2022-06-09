905c79fed567feed79751b933f7ebff368ba390c
gytks4 <gytks4@naver.com>
2022-06-09 09:09:36 +0900

    chore: msw 실행로직 주석 처리

--- src/index.tsx
+++ src/index.tsx
@@ -7,16 +7,16 @@ import theme from 'styles/theme';
 
 import App from './App';
 
-if (process.env.NODE_ENV === 'development') {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const { worker } = require('./mocks/browser');
+// if (process.env.NODE_ENV === 'development') {
+//   // eslint-disable-next-line @typescript-eslint/no-var-requires
+//   const { worker } = require('./mocks/browser');
 
-  worker.start({
-    serviceWorker: {
-      url: '/react-shopping-cart/mockServiceWorker.js',
-    },
-  });
-}
+//   worker.start({
+//     serviceWorker: {
+//       url: '/react-shopping-cart/mockServiceWorker.js',
+//     },
+//   });
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
