6cc2342196e6a173636b8875314ce4ad8c55d6fa
이아현 <lah1203@naver.com>
2022-06-09 15:14:23 +0900

    refactor: global style도 순서 정리

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -7,27 +7,34 @@ const GlobalStyle = createGlobalStyle<{ theme: typeof theme }>`
   ${reset}
 
   body {
+    background: ${({ theme: { colors } }) => colors.sakuraPink};
+
     font-family: 'Gowun Dodum', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     font-size: 16px;
-    background: ${({ theme: { colors } }) => colors.sakuraPink};
+  }
+
+  input {
+    font-family: 'Gowun Dodum', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    :disabled {
+      background: ${({ theme: { colors } }) => colors.shadow};
+    }
   }
 
   button {
     border: none;
-    cursor: pointer;
-    padding: 0;
-  }
 
-  input, button {
+    padding: 0;
+
     font-family: 'Gowun Dodum', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-  }
 
-  input:disabled {
-    background: ${({ theme: { colors } }) => colors.shadow};
+    cursor: pointer;
   }
 `;
 
