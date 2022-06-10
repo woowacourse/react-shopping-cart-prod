967f459a6ffd8ad6b0b1e05176131060755f6028
InKyoJeong <viapolar@naver.com>
2022-06-10 23:27:48 +0900

    chore: 백엔드 url 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const { REACT_APP_API_URL } = process.env;
+const BACK_URL_ELLIE = 'http://ec2-54-180-82-199.ap-northeast-2.compute.amazonaws.com:8080';
 
 const apiClient = axios.create({
-  baseURL: REACT_APP_API_URL,
+  baseURL: BACK_URL_ELLIE,
 });
 
 export default apiClient;
