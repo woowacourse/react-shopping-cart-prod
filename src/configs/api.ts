f929b12f7296273318b8caa529eab7136581b2c6
sangminsong <ssmin940606@gmail.com>
2022-06-12 18:12:22 +0900

    chore: 기본 백엔드 API 변경

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,8 +1,14 @@
 import Axios from 'axios';
 
+
 const axios = Axios.create({
   baseURL:
-    'http://ec2-13-125-246-80.ap-northeast-2.compute.amazonaws.com:8080/api',
+    'http://ec2-15-164-94-21.ap-northeast-2.compute.amazonaws.com:8080/api',
 });
 
 export default axios;
+
+// - `매트`: http://ec2-15-164-94-21.ap-northeast-2.compute.amazonaws.com:8080/
+// - `야호`: http://ec2-13-125-121-5.ap-northeast-2.compute.amazonaws.com:8080/
+// - `차리`: http://ec2-13-125-246-80.ap-northeast-2.compute.amazonaws.com:8080/
+// - `슬로`: http://ec2-15-164-49-241.ap-northeast-2.compute.amazonaws.com:8080/
