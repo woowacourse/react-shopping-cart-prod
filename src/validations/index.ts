11fc96862e4b82cd27ace9ad48b95a060fceea1a
Daye Lee <508yeah@gmail.com>
2022-06-12 12:33:00 +0900

    refactor: 아이디 및 비밀번호 에러 메시지 상수화

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,8 +1,10 @@
+import { ERROR_MESSAGES } from '@/constants';
+
 const validateUserName = (id: string) => {
   const reg = /^[a-z0-9\_]{5,20}$/;
 
   if (!id.match(reg)) {
-    throw new Error('아이디는 영문 소문자/숫자/언더바(_)만 사용할 수 있습니다. (5~20자)');
+    throw new Error(ERROR_MESSAGES.SIGNUP.USER_NAME_RULE);
   }
 };
 
@@ -10,9 +12,7 @@ const validatePassword = (password: string) => {
   const reg = '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*\\-_])[A-Za-z\\d!@#$%^&*\\-_]{8,20}';
 
   if (!password.match(reg)) {
-    throw new Error(
-      '비밀번호는 영문 대문자/소문자/숫자/특수문자(!, @, #, $, %, ^, &, *, -, _)를 각 1자 이상 포함해야 합니다. (8~20자)'
-    );
+    throw new Error(ERROR_MESSAGES.SIGNUP.PASSWORD_RULE);
   }
 };
 
