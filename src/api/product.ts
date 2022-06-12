839a498d4ba2c1a791fc6368572e98e2756044b0
kkojae <jaejeung3210@gmail.com>
2022-06-12 22:58:54 +0900

    refactor: product API authorizedFetcher로 wrapping 하기

--- src/api/product.ts
+++ src/api/product.ts
@@ -2,6 +2,7 @@ import { caching } from '@/api/cache';
 import { PRODUCT_API_URL } from '@/api/constants';
 import { ProductType } from '@/domain/product';
 import axios from 'axios';
+import { authorizedFetcher } from './authorizedFetcher';
 
 const productAPI = axios.create({
   baseURL: PRODUCT_API_URL.TO_PRODUCTS,
@@ -13,7 +14,10 @@ export const getProductList = () => {
   const cacheKey = `${PRODUCT_API_URL.TO_PRODUCTS}`;
 
   return caching(async (): Promise<any> => {
-    const response = await productAPI.get('');
+    const response = await authorizedFetcher({
+      requestMethod: productAPI.get,
+      endPoint: '/',
+    });
 
     return {
       data: {
@@ -28,7 +32,10 @@ export const getProduct = id => {
   const cacheKey = `${PRODUCT_API_URL.TO_PRODUCTS}/${id}`;
 
   return caching(async (): Promise<{ data: ProductType }> => {
-    const response = await productAPI.get(`/${id}`, {});
+    const response = await authorizedFetcher({
+      requestMethod: productAPI.get,
+      endPoint: `/${id}`,
+    });
 
     return { data: response.data.product };
   }, cacheKey);
