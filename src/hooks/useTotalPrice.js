2ea1a21e10c3c35934bde650ca9d529546a5bb49
Marco <bigdatajang@gmail.com>
2022-06-15 17:39:13 +0900

    refactor: useTotalPrice 훅에서 불필요한 인증 관련 로직 삭제
    https://github.com/woowacourse/react-shopping-cart-prod/pull/74#discussion_r896777711

--- src/hooks/useTotalPrice.js
+++ src/hooks/useTotalPrice.js
@@ -1,15 +1,10 @@
 // @ts-nocheck
 import { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import useSnackbar from 'hooks/useSnackbar';
-import { MESSAGE, ROUTES } from 'utils/constants';
 
 const useTotalPrice = () => {
-  const { isLoading, isAuthenticated } = useSelector(state => state.authReducer);
   const { shoppingCart, order } = useSelector(state => state.cartReducer);
-  const [renderSnackbar] = useSnackbar();
-  const navigate = useNavigate();
+
   const [totalPrice, setTotalPrice] = useState(0);
 
   const calculateTotalPrice = useCallback(() => {
@@ -25,12 +20,8 @@ const useTotalPrice = () => {
   }, [shoppingCart, order]);
 
   useEffect(() => {
-    if (!isLoading && !isAuthenticated) {
-      renderSnackbar(MESSAGE.NO_AUTHORIZATION, 'FAILED');
-      navigate(ROUTES.LOGIN);
-    }
     setTotalPrice(calculateTotalPrice());
-  }, [calculateTotalPrice, isAuthenticated, isLoading, navigate, renderSnackbar]);
+  }, [calculateTotalPrice]);
 
   return { totalPrice };
 };
