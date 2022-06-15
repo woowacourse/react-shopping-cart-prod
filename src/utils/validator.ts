5c0c064c895d32314fcbc755bf7b4b8b478164b1
Marco <bigdatajang@gmail.com>
2022-06-15 18:04:04 +0900

    refactor: validator.ts 반환 타입 등 보완

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -8,7 +8,7 @@ type UserType = {
   password: string;
 };
 
-export const validateEmail = (email: string) => {
+export const validateEmail = (email: string): void => {
   if (!email.includes('@') || !email.includes('.', email.indexOf('@'))) {
     throw new CustomError(2101, MESSAGE.INVALID_EMAIL_FORMAT);
   }
@@ -18,7 +18,7 @@ export const validateEmail = (email: string) => {
   }
 };
 
-export const validatePassword = (password: string) => {
+export const validatePassword = (password: string): void => {
   const passwordRule = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{10,}$/;
 
   if (!passwordRule.test(password)) {
@@ -26,37 +26,37 @@ export const validatePassword = (password: string) => {
   }
 };
 
-export const validateNickname = (nickname: string) => {
+export const validateNickname = (nickname: string): void => {
   if (nickname.length < 2 || nickname.length > 10) {
     throw new CustomError(2102, MESSAGE.INVALID_NICKNAME_FORMAT);
   }
 };
 
-export const checkPasswordSame = (users: UserType[], userId: string, password: string) => {
+export const checkPasswordSame = (users: UserType[], userId: string, password: string): void => {
   if (users.find(user => user.id === userId).password !== password) {
     throw new CustomError(2201, ERROR[2201]);
   }
 };
 
-export const checkDuplicatedEmail = (users: UserType[], email: string) => {
+export const checkDuplicatedEmail = (users: UserType[], email: string): void => {
   if (users.some(user => user.email === email)) {
     throw new CustomError(2001, ERROR[2001]);
   }
 };
 
-export const validateToken = (users, accessToken) => {
+export const validateToken = (users: UserType[], accessToken: { id: string }): void => {
   if (!users.some(user => user.id === accessToken.id)) {
     throw new CustomError(1003, ERROR[1003]);
   }
 };
 
-export const checkUserPassword = (user, password) => {
+export const checkUserPassword = (user: UserType, password: string): void => {
   if (user.password !== password) {
     throw new CustomError(2201, ERROR[2201]);
   }
 };
 
-export const checkEmailExist = (users, email) => {
+export const checkEmailExist = (users: UserType[], email: string): void => {
   if (!users.find(user => user.email === email)) {
     throw new CustomError(2201, ERROR[2201]);
   }
