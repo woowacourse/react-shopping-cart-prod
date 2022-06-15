ee050e25d4124a73f1258149207b090b0b866d82
DomMorello <zuzudnf@gmail.com>
2022-06-15 17:07:36 +0900

    refactor: 두 번 렌더링 방지하기 위해 strictmode 제거

--- src/index.js
+++ src/index.js
@@ -33,15 +33,14 @@ export const store = createStore(
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <GlobalStyle />
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <GlobalStyle />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
 
 // main();
