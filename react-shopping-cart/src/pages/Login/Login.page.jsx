00f4644425ded1f85f41305011c93eb145d1598d
2019015414 <2019015414@hanyang.ac.kr>
2022-06-15 23:18:27 +0900

    feat: 로그인 에러메세지 색상 추가

--- react-shopping-cart/src/pages/Login/Login.page.jsx
+++ react-shopping-cart/src/pages/Login/Login.page.jsx
@@ -53,7 +53,11 @@ function Login() {
         >
           <Input ref={emailRef} type="email" placeholder="이메일" />
           <Input ref={passwordRef} type="password" placeholder="비밀번호" />
-          {error && '이메일이나 비밀번호를 확인해 주세요.'}
+          {error && (
+            <TextBox fontSize="small" color="RED_001">
+              이메일이나 비밀번호를 확인해 주세요.
+            </TextBox>
+          )}
           <SignupLink to="/signup">회원가입</SignupLink>
           <Button width="100%" borderRadius="10px" onClick={handleLogin}>
             <TextBox color="WHITE_001">로그인</TextBox>
