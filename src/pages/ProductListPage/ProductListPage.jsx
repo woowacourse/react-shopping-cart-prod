eae2edf39d5a93fe95ae660c63b7a943ecae6b6d
soyi47 <soyi.dev@gmail.com>
2022-06-16 12:33:26 +0900

    refactor: ProductList 컴포넌트 삭제
    ProductList 컴포넌트의 로직을 ProductListPage로 이동

--- src/pages/ProductListPage/ProductListPage.jsx
+++ src/pages/ProductListPage/ProductListPage.jsx
@@ -1,9 +1,29 @@
-import ProductList from "./components/ProductList";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import cn from "classnames";
+
+import { getProductList } from "@redux/reducers/product-list-reducer/productListThunks";
+
+import styles from "./ProductListPage.module";
+import ProductItem from "./components/ProductItem";
 
 function ProductListPage() {
+  const dispatch = useDispatch();
+  const { isLoading, isError, productList } = useSelector((state) => ({
+    productList: state.productList.data,
+    ...state.productList.query.getProductList,
+  }));
+
+  useEffect(() => {
+    dispatch(getProductList());
+  }, [dispatch]);
+
   return (
     <div className="wrapper">
-      <ProductList />
+      <div className={cn(styles.productList)}>
+        {productList &&
+          productList.map((item) => <ProductItem key={item.id} {...item} />)}
+      </div>
     </div>
   );
 }
