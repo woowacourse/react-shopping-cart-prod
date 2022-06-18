6c2caa1ba6ba812e4c0393398654b483aab85e4f
soyi47 <soyi.dev@gmail.com>
2022-06-18 11:18:49 +0900

    refactor: AuthGuard 페이지 이동 로직 수정
    - AuthGuard 컴포넌트에서 페이지 이동 시 react-router의 useNavigate 사용하도록 변경

--- src/components/AuthGuard.jsx
+++ src/components/AuthGuard.jsx
@@ -1,24 +1,24 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
+import { useNavigate } from "react-router-dom";
 import { USER_ACCESS_POLICY } from "../constants";
 
 function AuthGuard({
   policy = USER_ACCESS_POLICY.ONLY_LOGGED_IN_USER,
   children,
 }) {
+  const navigate = useNavigate();
   const isLoggedIn = useSelector((state) => state.user.data.isLoggedIn);
 
   useEffect(() => {
     if (policy === USER_ACCESS_POLICY.ONLY_LOGGED_IN_USER && !isLoggedIn) {
-      alert("접근할 수 없습니다!");
-      window.location.href = "/login";
+      navigate("/login", { replace: true });
     }
     if (policy === USER_ACCESS_POLICY.ONLY_LOGGED_OUT_USER && isLoggedIn) {
-      alert("접근할 수 없습니다!");
-      window.location.href = "/";
+      navigate("/", { replace: true });
     }
-  }, [isLoggedIn, policy]);
+  }, [isLoggedIn, navigate, policy]);
 
   if (policy === USER_ACCESS_POLICY.ONLY_LOGGED_IN_USER && !isLoggedIn) {
     return <div>로그인 후 이용해 주세요</div>;
