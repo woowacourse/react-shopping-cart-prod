a4f34228a002c5519e080861880656131c506f3b
kamwoo <rkadndud06@naver.com>
2022-06-18 14:54:52 +0900

    feat: env BASE_URL 추가

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,15 +1,12 @@
+//   ALIN: 'https://ec2-52-79-235-135.ap-northeast-2.compute.amazonaws.com:8080',
+//   GREEN: 'https://ec2-52-79-233-208.ap-northeast-2.compute.amazonaws.com:8080',
+//   LENUN: 'https://ec2-54-180-79-12.ap-northeast-2.compute.amazonaws.com:8080',
+//   ALPHA: 'https://ec2-52-79-235-137.ap-northeast-2.compute.amazonaws.com:8080',
+//   COOKIE: 'https://ec2-3-35-210-168.ap-northeast-2.compute.amazonaws.com:8080',
+
 export const LOCAL_BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
     : 'https://sisyphe-shopping-cart-server.herokuapp.com';
 
-export const BACK_URL = {
-  LOCAL: LOCAL_BASE_URL,
-  ALIN: 'https://ec2-52-79-235-135.ap-northeast-2.compute.amazonaws.com:8080',
-  GREEN: 'https://ec2-52-79-233-208.ap-northeast-2.compute.amazonaws.com:8080',
-  LENUN: 'https://ec2-54-180-79-12.ap-northeast-2.compute.amazonaws.com:8080',
-  ALPHA: 'https://ec2-52-79-235-137.ap-northeast-2.compute.amazonaws.com:8080',
-  COOKIE: 'https://ec2-3-35-210-168.ap-northeast-2.compute.amazonaws.com:8080',
-};
-
-export const BASE_URL = BACK_URL.COOKIE;
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
