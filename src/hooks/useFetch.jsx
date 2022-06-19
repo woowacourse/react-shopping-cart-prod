4907ba4bdb43eb0a35cd41e2de8ecbf1afef237f
soyi47 <soyi.dev@gmail.com>
2022-06-19 10:47:56 +0900

    refactor: useFetch의 response 상태 추가
    data 상태와 response 상태를 따로 가지고 있도록 하여, 데이터 값이 없는 응답에도 대응할 수 있도록 수정

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -3,48 +3,62 @@ import LocalStorage from "@utils/LocalStorage";
 import ApiError from "@redux/utils/ApiError";
 import { FETCH_STATUS, REQUEST_METHOD } from "../constants";
 
-const fetchData = async (method, requestUrl = "", requestData = {}) => {
+const getResponseWithFetch = async (
+  method,
+  requestUrl = "",
+  requestData = {}
+) => {
   const accessToken = LocalStorage.getItem("accessToken");
 
-  const fetchInitOption = { method };
+  const fetchOption = { method };
   if (accessToken) {
-    fetchInitOption.headers = { Authorization: `Bearer ${accessToken}` };
+    fetchOption.headers = { Authorization: `Bearer ${accessToken}` };
   }
   if (method !== REQUEST_METHOD.GET) {
-    fetchInitOption.body = JSON.stringify(requestData);
+    fetchOption.body = JSON.stringify(requestData);
   }
 
-  const response = await fetch(requestUrl, fetchInitOption);
+  const response = await fetch(requestUrl, fetchOption);
 
   if (!response.ok) {
     const { errorCode, message } = await response.json();
     throw new ApiError(errorCode, message);
   }
 
-  const data = await response.json();
-  if (!data) throw new Error("No Data");
-
-  return data;
+  return response;
 };
 
 export const useFetch = (
   method,
   requestUrl = "",
   initialData = {},
-  requestData = {}
+  requestData = {},
+  responseDataExist = true
 ) => {
   const [status, setStatus] = useState(FETCH_STATUS.PENDING);
+  const [response, setResponse] = useState(null);
   const [data, setData] = useState(initialData);
   const [error, setError] = useState(null);
 
   const fetch = async () => {
     setStatus(FETCH_STATUS.PENDING);
+    setResponse(null);
     setData(initialData);
     setError(null);
 
     try {
-      const fetchedData = await fetchData(method, requestUrl, requestData);
-      setData(fetchedData);
+      const response = await getResponseWithFetch(
+        method,
+        requestUrl,
+        requestData
+      );
+      setResponse(response);
+
+      if (responseDataExist) {
+        const responseData = await response.json();
+        if (!responseData) throw new Error("No Data");
+        setData(responseData);
+      }
     } catch (err) {
       setError(err);
       setStatus(FETCH_STATUS.FAIL);
@@ -53,5 +67,5 @@ export const useFetch = (
     setStatus(FETCH_STATUS.SUCCESS);
   };
 
-  return { fetch, data, status, error };
+  return { fetch, response, data, status, error };
 };
