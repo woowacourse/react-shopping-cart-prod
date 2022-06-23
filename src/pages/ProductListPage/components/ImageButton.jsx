be3891190297af34254bc264b49fdbda902ffc6b
soyi47 <soyi.dev@gmail.com>
2022-06-24 00:20:39 +0900

    refactor: 사용하지 않는 scss 파일 삭제

--- src/pages/ProductListPage/components/ImageButton.jsx
+++ src/pages/ProductListPage/components/ImageButton.jsx
@@ -1,13 +1,8 @@
 import cn from "classnames";
-import styles from "./ImageButton.module";
 
 function ImageButton({ children, onClick, className }) {
   return (
-    <button
-      className={cn(styles.imageButton, className)}
-      type="button"
-      onClick={onClick}
-    >
+    <button className={cn(className)} type="button" onClick={onClick}>
       {children}
     </button>
   );
