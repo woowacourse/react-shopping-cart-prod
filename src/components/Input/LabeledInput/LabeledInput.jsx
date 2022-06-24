ef3817d183d793afbea3786d6a47abe7c489a6ce
soyi47 <soyi.dev@gmail.com>
2022-06-24 12:23:14 +0900

    refactor: LabeledInput 내부 Input 컴포넌트 위치 이동

--- src/components/Input/LabeledInput/LabeledInput.jsx
+++ src/components/Input/LabeledInput/LabeledInput.jsx
@@ -8,8 +8,9 @@ const LabeledInput = React.forwardRef(
   ({ id, label, className, feedback, ...rest }, ref) => {
     return (
       <div className={cn(styles.labeledInput, className)}>
-        <label htmlFor={id}>{label}</label>
-        <Input id={id} {...rest} ref={ref} />
+        <label>
+          {label} <Input {...rest} ref={ref} />
+        </label>
         {feedback && <div className={styles.feedback}>{feedback}</div>}
       </div>
     );
