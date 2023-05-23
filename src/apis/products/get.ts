9fe2e23b5af6fde40a189a8eb3c87bcda1b9af39
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-23 17:34:11 +0900

    refactor(fetcher): fetch 래핑함수 시그니처 변경으로 인한 수정

--- src/apis/products/get.ts
+++ src/apis/products/get.ts
@@ -4,7 +4,8 @@ import { Product } from 'types/product';
 const GET_URL = '/products';
 
 export const getProducts = async (): Promise<Product[]> => {
-  const products = await fetcher<Product[]>(GET_URL);
+  const data = await fetcher<Product[]>(GET_URL);
+  const products = data.data ?? [];
 
-  return products as Product[];
+  return products;
 };
