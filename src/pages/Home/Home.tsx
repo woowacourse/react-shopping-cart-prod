aac999ad483c0b1cc88f2570ca02614deee99e5c
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-05-23 20:25:15 +0900

    fix: 에러 바운더리가 fallback한 이후에 복구는 현상 수정
    Co-authored-by: Nangkyeong Lim <57815133+nangkyeonglim@users.noreply.github.com>

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import { ErrorBoundary } from "react-error-boundary";
 import Modal from "../../components/Modal";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { modalContentState, modalOpenState } from "../../recoil/modalAtoms.tsx";
+import { serverState } from "../../recoil/serverAtom.ts";
 
 function Home() {
   const [isModalOpen, setModalOpen] = useRecoilState(modalOpenState);
@@ -11,8 +12,11 @@ function Home() {
     setModalOpen(false);
   };
 
+  const server = useRecoilValue(serverState);
+
   return (
     <ErrorBoundary
+      key={server}
       fallback={<div>상품 목록을 불러오는 도중 문제가 발생했습니다.</div>}
     >
       <ProductList />
