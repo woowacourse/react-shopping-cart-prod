881e33580b00d3bed889e45a7929f573c7e96e9f
d0dam <smallkdb@gmail.com>
2023-05-24 18:02:44 +0900

    refactor: 장바구니 아이디 저장 상태 atom을 서버마다 저장하도록 변경
    Co-authored-by: 정호진 <hozzijeong@users.noreply.github.com>

--- src/recoil/atom.ts
+++ src/recoil/atom.ts
@@ -1,29 +1,27 @@
-import { atom, selector } from 'recoil';
+import { atom, atomFamily } from 'recoil';
 import type { CartItem, ToastState } from '../types';
 
-export const $CheckedCartIdList = atom<number[]>({
-  key: 'CheckStateList',
-  default: [],
+const getCartIds = async (url: string) => {
+  const response = await fetch(`${url}/cart-items`, {
+    headers: {
+      Authorization: `Basic ${btoa('a@a.com:1234')}`,
+    },
+  });
+
+  if (!response.ok) return [];
+  const data = (await response.json()) as CartItem[];
+
+  return data.map(item => item.id);
+};
+
+export const $CheckedCartIdList = atomFamily<number[], string>({
+  key: 'CheckedCartIdList',
+  default: async url => await getCartIds(url),
 });
 
-export const $CartIdList = atom<number[]>({
+export const $CartIdList = atomFamily<number[], string>({
   key: 'CartIdList',
-  default: selector({
-    key: 'CatIdListSelector',
-    get: async ({ get }) => {
-      const currentUrl = get($CurrentServerUrl);
-      const response = await fetch(`${currentUrl}/cart-items`, {
-        headers: {
-          Authorization: `Basic ${btoa('a@a.com:1234')}`,
-        },
-      });
-
-      if (!response.ok) return [];
-      const data = (await response.json()) as CartItem[];
-
-      return data.map(item => item.id);
-    },
-  }),
+  default: async url => await getCartIds(url),
 });
 
 export const $ToastStateList = atom<ToastState[]>({
