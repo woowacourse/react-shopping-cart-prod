7c81e020fdca81263f2e5d971e8f67e2ffdb76ad
bassyu <yrkdgus@gmail.com>
2023-05-24 17:43:06 +0900

    feat(Toast): 토스트 컴포넌트 추가
    - css를 위해 포탈 사용
    
    Co-authored-by: 이세민 <semnil5202@users.noreply.github.com>

---
+++ src/components/common/Toast.tsx
@@ -0,0 +1,68 @@
+import type { ToastInfoType } from '../../types';
+
+import ReactDOM from 'react-dom';
+import styled, { keyframes } from 'styled-components';
+import { useRecoilValue } from 'recoil';
+
+import { toastInfoState } from '../../recoil/state';
+
+export default function Toast() {
+  const { show, message, type } = useRecoilValue(toastInfoState);
+
+  const root = document.querySelector('#root') as HTMLElement;
+  return ReactDOM.createPortal(show && <Wrapper type={type}>{message}</Wrapper>, root);
+}
+
+const toastAnim = keyframes`
+0% {
+  transform: translate(-50%, 20px);
+  opacity: 0;
+}
+
+8% {
+  transform: translate(-50%, 0);
+  opacity: 1;
+}
+
+80%{
+  transform: translate(-50%, 0);
+  opacity: 1;
+}
+
+95% {
+  transform: translate(-50%, 20px);
+  opacity: 0;
+}
+
+100% {
+  transform: translate(-50%, 20px);
+  opacity: 0;
+}
+`;
+
+const Wrapper = styled.div<{ type: ToastInfoType['type'] }>`
+  animation: ${toastAnim} 1.6s linear;
+  position: absolute;
+  left: 50%;
+  bottom: 64px;
+  transform: translateX(-50%);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 0 16px;
+  height: 42px;
+  border-radius: 4px;
+
+  background-color: ${({ type }) =>
+    ({
+      info: 'rgb(4, 192, 158)',
+      warning: 'rgb(245, 213, 96)',
+      error: 'rgb(222,96,96)',
+    }[type])};
+
+  font-size: 14px;
+  font-weight: 600;
+  color: white;
+`;
