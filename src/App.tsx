2a6d99bf02d0d769c13380656af5df24289d6a80
d0dam <smallkdb@gmail.com>
2023-05-24 18:01:52 +0900

    refactor: Suspense 추가
    Co-authored-by: 정호진 <hozzijeong@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { RecoilRoot } from 'recoil';
 import Router from './routes/Router';
+import { Suspense } from 'react';
+import LoadingView from './components/Common/LoadingView';
 
 function App() {
   return (
     <RecoilRoot>
-      <Router />
+      <Suspense fallback={<LoadingView />}>
+        <Router />
+      </Suspense>
     </RecoilRoot>
   );
 }
