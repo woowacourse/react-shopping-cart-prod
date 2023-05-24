bfa69c28efd27f6a08f01b32243a93543cba40b0
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-24 21:34:37 +0900

    test: 장바구니 도메인 로직 테스트 추가

---
+++ src/views/CartItemList/utils/cart.ts
@@ -0,0 +1,7 @@
+export const createCartItem = ({ cartId, product }) => {};
+
+export const addItemToCart = ({ cart, cartId, product }) => {};
+
+export const updateCartItemQuantity = ({ cart, cartId, quantity }) => {};
+
+export const removeCartItem = ({ cart, cartId }) => {};
