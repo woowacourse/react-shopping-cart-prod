3c172cd88d959a529e25a3835a82c58f51c694c9
d0dam <smallkdb@gmail.com>
2023-05-24 14:53:37 +0900

    refactor: fetch 요청 시 서버 경로 추가
    Co-authored-by: 정호진 <hozzijeong@users.noreply.github.com>

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,6 +1,6 @@
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { MESSAGE } from '../constants';
-import { $CartIdList, $CheckedCartIdList } from '../recoil/atom';
+import { $CartIdList, $CheckedCartIdList, $CurrentServerUrl } from '../recoil/atom';
 import useGetQuery from './useGetQuery';
 import useMutation from './useMutation';
 import useToast from './useToast';
@@ -10,13 +10,24 @@ const useCart = () => {
   const Toast = useToast();
   const [cartIdList, setCartIdList] = useRecoilState($CartIdList);
   const setCheckedCartIdList = useSetRecoilState($CheckedCartIdList);
-  const { data: cartItemStateList, refreshQuery, loading } = useGetQuery<CartItem[]>('/cart-items');
+  const currentServerUrl = useRecoilValue($CurrentServerUrl);
+
+  const {
+    data: cartItemStateList,
+    refreshQuery,
+    loading,
+  } = useGetQuery<CartItem[]>(`${currentServerUrl}/cart-items`, {
+    Authorization: `Basic ${btoa('a@a.com:1234')}`,
+  });
+
   const addCartQuery = useMutation<Record<string, number>, CartItem>({
     onSuccess: data => {
       const regex = /[^0-9]/g;
       const cartId = data?.headers.get('Location')?.replace(regex, '');
+
       setCartIdList(prev => [...prev, Number(cartId)]);
       setCheckedCartIdList(prev => [...prev, Number(cartId)]);
+
       Toast.success(MESSAGE.ADD_CART_SUCCESSFUL);
       refreshQuery();
     },
@@ -24,12 +35,15 @@ const useCart = () => {
       Toast.error(MESSAGE.ADD_CART_FAILED);
     },
   });
+
   const deleteCartQuery = useMutation<Record<string, number>, CartItem>({
     onSuccess: data => {
-      const regex = /[^0-9]/g;
-      const cartId = data?.headers.get('Location')?.replace(regex, '');
+      const regex = /(\d+)$/;
+      const cartId = data?.fetchInformation.url.match(regex)?.at(1);
+
       setCartIdList(prev => prev.filter(item => item !== Number(cartId)));
       setCheckedCartIdList(prev => prev.filter(item => item !== Number(cartId)));
+
       Toast.success(MESSAGE.DELETE_CART_SUCCESSFUL);
       refreshQuery();
     },
@@ -37,6 +51,7 @@ const useCart = () => {
       Toast.error(MESSAGE.DELETE_CART_FAILED);
     },
   });
+
   const mutateQuantityQuery = useMutation<Record<string, number>, CartItem>({
     onSuccess: () => {
       refreshQuery();
@@ -48,28 +63,47 @@ const useCart = () => {
 
   const mutateQuantity = async (cartId: number, quantity: number) => {
     await mutateQuantityQuery({
-      url: `/cart-items/${cartId}`,
+      url: `${currentServerUrl}/cart-items/${cartId}`,
       method: 'PATCH',
       bodyData: { quantity },
+      headers: {
+        Authorization: `Basic ${btoa('a@a.com:1234')}`,
+        'Content-Type': 'application/json',
+      },
     });
   };
 
   const deleteCartItem = async (cartId: number) => {
     await deleteCartQuery({
-      url: `/cart-items/${cartId}`,
+      url: `${currentServerUrl}/cart-items/${cartId}`,
       method: 'DELETE',
+      headers: {
+        Authorization: `Basic ${btoa('a@a.com:1234')}`,
+      },
     });
   };
 
   const addCartItem = async (productId: number) => {
     await addCartQuery({
-      url: `/cart-items`,
+      url: `${currentServerUrl}/cart-items`,
       method: 'POST',
       bodyData: { productId },
+      headers: {
+        Authorization: `Basic ${btoa('a@a.com:1234')}`,
+        'Content-Type': 'application/json',
+      },
     });
   };
 
-  return { cartIdList, cartItemStateList, mutateQuantity, deleteCartItem, addCartItem, refreshQuery, loading };
+  return {
+    cartIdList,
+    cartItemStateList,
+    mutateQuantity,
+    deleteCartItem,
+    addCartItem,
+    refreshQuery,
+    loading,
+  };
 };
 
 export default useCart;
