07c017c516961db351d95d7c67fa8abcc0fda864
ashleysyheo <ashleysyheo@gmail.com>
2023-05-24 20:45:38 +0900

    chore: 안 사용하는 장바구니 api 관련 함수 정리

--- src/api/cartAPI.ts
+++ src/api/cartAPI.ts
@@ -46,44 +46,4 @@ const cartAPI = async (baseUrl: string) => {
   return { getCartList, postCartItem, patchCartItem, deleteCartItem };
 };
 
-const getCartList = async (): Promise<CartItemData[]> => {
-  return await fetchAPI(API_ENDPOINT.CART_ITEMS, {
-    method: 'GET',
-    headers: { ...CART_FETCH_OPTION_HEADERS },
-  });
-};
-
-const postCartItem = async (productId: number): Promise<CartItemData[]> => {
-  const data = {
-    productId,
-  };
-  const jsonData = JSON.stringify(data);
-
-  return await fetchAPI(API_ENDPOINT.CART_ITEMS, {
-    method: 'POST',
-    headers: { ...CART_FETCH_OPTION_HEADERS },
-    body: jsonData,
-  });
-};
-
-const patchCartItem = async (cartItemId: number, quantity: number): Promise<CartItemData[]> => {
-  const data = {
-    quantity,
-  };
-  const jsonData = JSON.stringify(data);
-
-  return await fetchAPI(`${API_ENDPOINT.CART_ITEMS}/${cartItemId}`, {
-    method: 'PATCH',
-    headers: { ...CART_FETCH_OPTION_HEADERS },
-    body: jsonData,
-  });
-};
-
-const deleteCartItem = async (cartItemId: number): Promise<Response> => {
-  return await fetchAPI(`${API_ENDPOINT.CART_ITEMS}/${cartItemId}`, {
-    method: 'DELETE',
-    headers: { Authorization: CART_FETCH_OPTION_HEADERS.Authorization },
-  });
-};
-
-export { cartAPI, getCartList, postCartItem, patchCartItem, deleteCartItem };
+export { cartAPI };
