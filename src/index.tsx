d84cb78f55acf7a1bd14cdba25c713cca3e4b5d0
ashleysyheo <ashleysyheo@gmail.com>
2023-05-24 17:50:07 +0900

    chore: index.tsx msw 관련 코드 주석처리

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,23 @@ import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
 
-import { worker } from './mocks/worker';
+// import { worker } from './mocks/worker';
 import AppRouter from './router/routes';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 
-(async () => {
-  if (window.location.pathname === '/react-shopping-cart') {
-    window.location.pathname += '/';
-    return;
-  }
+// (async () => {
+//   if (window.location.pathname === '/react-shopping-cart-prod') {
+//     window.location.pathname += '/';
+//     return;
+//   }
 
-  await worker.start({
-    serviceWorker: {
-      url: '/react-shopping-cart/mockServiceWorker.js',
-    },
-  });
-})();
+//   await worker.start({
+//     serviceWorker: {
+//       url: '/react-shopping-cart-prod/mockServiceWorker.js',
+//     },
+//   });
+// })();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
