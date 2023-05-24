a2c2f49d2176f73e09b296917892d03689958251
d0dam <smallkdb@gmail.com>
2023-05-24 14:47:02 +0900

    refactor: msw 제거
    Co-authored-by: 정호진 <hozzijeong@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
-import worker from './mocks/browser';
-
-if (process.env.NODE_ENV === 'development') {
-  worker.start();
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
