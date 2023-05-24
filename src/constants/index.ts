746c175747bafef0cd77ee7c147cb98477a16e2a
Suyoung <jsy8492@gmail.com>
2023-05-24 17:31:25 +0900

    refactor: Basic Authorization 데이터 상수화

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,7 @@ export const CART_LOCAL_STORAGE_KEY = 'cart';
 
 export const PRODUCTS_BASE_URL = '/products';
 export const CART_BASE_URL = '/cart-items';
+
+const username = 'a@a.com';
+const password = '1234';
+export const base64 = btoa(username + ':' + password);
