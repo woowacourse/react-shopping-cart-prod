624126f718901fcf5dd9ca81e784b791ee02a698
woo-jk <201702042@o.cnu.ac.kr>
2023-05-24 15:41:00 +0900

    feat: 서버별 url 상수 및 유저 토큰 상수 작성

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,6 +1,17 @@
-export const BASE_URL = `/api`;
+import { ServerName } from '../types/server';
 
-export const PRODUCTS_BASE_URL = `${BASE_URL}/products`;
-export const CART_ITEMS_BASE_URL = `${BASE_URL}/cart-items`;
+export const BASE_URL: Record<ServerName, string | undefined> = {
+  누누: process.env.REACT_APP_NUNU_SERVER_URL,
+  체인저: process.env.REACT_APP_CHANGER_SERVER_URL,
+  필립: process.env.REACT_APP_PHILIP_SERVER_URL,
+};
+
+export const PRODUCTS_PATH_NAME = `/products`;
+export const CART_ITEMS_PATH_NAME = `/cart-items`;
 
 export const CART_LIST_KEY = 'cart_list';
+
+const TEMP_ID = 'a@a.com';
+const TEMP_PW = '1234';
+
+export const USER_AUTH_TOKEN = btoa(`${TEMP_ID}:${TEMP_PW}`);
---
+++ src/types/server.ts
@@ -0,0 +1,1 @@
+export type ServerName = '누누' | '체인저' | '필립';
