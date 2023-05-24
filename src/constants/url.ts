e673db0c2d40e5697ed45544d3e380f668fbde7a
regularPark <srk99279@naver.com>
2023-05-25 07:49:41 +0900

    fix: 서버 url 수정
    Co-authored-by: 고니_정우시 <jeongwusi@gmail.com>

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,7 +1,7 @@
 export const serverUrlObj: Record<string, string> = {
   주노: 'http://3.37.86.118:8080',
-  헤나: 'http://54.180.102.84:8080',
-  메리: 'http://13.209.69.80:8080',
+  메리: 'http://54.180.102.84:8080',
+  헤나: 'http://13.209.69.80:8080',
 };
 
 export const PRODUCT_BASE_URL = '/products';
