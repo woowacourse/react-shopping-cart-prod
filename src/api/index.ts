bdcf5b94f7c41111d8589e10037f5715429e681c
bassyu <yrkdgus@gmail.com>
2023-05-24 14:42:24 +0900

    feat(api): 헤더에 Authorization 추가
    Co-authored-by: 이세민 <semnil5202@users.noreply.github.com>

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,17 @@
 import type { CartType, ProductType, ServerNameType } from '../types';
 
-import { BASE_URL_MAP } from '../constants';
+import { BASE_URL_MAP, USER_ID, USER_PASSWORD } from '../constants';
+
+const Authorization = `Basic ${btoa(`${USER_ID}:${USER_PASSWORD}`)}`;
 
 const get =
   <T>(path: string) =>
   async (serverName: ServerNameType) => {
-    const response = await fetch(`${BASE_URL_MAP[serverName]}/${path}`);
+    const response = await fetch(`${BASE_URL_MAP[serverName]}/${path}`, {
+      headers: {
+        Authorization,
+      },
+    });
     if (!response.ok) throw new Error(`${path} GET error`);
 
     const data: T = await response.json();
@@ -21,7 +27,12 @@ export const postCartItem = async (serverName: ServerNameType, productId: number
   const url = `${BASE_URL_MAP[serverName]}/cart-items`;
   const body = { productId };
 
-  const response = await fetch(url, { method: 'POST', body: JSON.stringify(body) });
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: { Authorization, 'content-Type': 'application/json' },
+    body: JSON.stringify(body),
+  });
+
   if (!response.ok) throw new Error(`${url} POST Error`);
 };
 
@@ -33,13 +44,19 @@ export const patchCartItemQuantity = async (
   const url = `${BASE_URL_MAP[serverName]}/cart-items/${cartItemId}`;
   const body = { quantity };
 
-  const response = await fetch(url, { method: 'PATCH', body: JSON.stringify(body) });
+  const response = await fetch(url, {
+    method: 'PATCH',
+    headers: { Authorization, 'content-Type': 'application/json' },
+    body: JSON.stringify(body),
+  });
+
   if (!response.ok) throw new Error(`${url} PATCH Error`);
 };
 
 export const deleteCartItem = async (serverName: ServerNameType, cartItemId: number) => {
   const url = `${BASE_URL_MAP[serverName]}/cart-items/${cartItemId}`;
 
-  const response = await fetch(url, { method: 'DELETE' });
+  const response = await fetch(url, { method: 'DELETE', headers: { Authorization } });
+
   if (!response.ok) throw new Error(`${url} FETCH Error`);
 };
