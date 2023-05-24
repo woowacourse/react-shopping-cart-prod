80e11575c81046f3022be66fb6bde358adede4b3
d0dam <smallkdb@gmail.com>
2023-05-24 14:52:40 +0900

    feat: 현재 서버 주소 atom 추가, CartIdList 기본값 변경
    Co-authored-by: 정호진 <hozzijeong@users.noreply.github.com>

--- src/recoil/atom.ts
+++ src/recoil/atom.ts
@@ -1,19 +1,37 @@
-import { atom } from 'recoil';
-import { LOCAL_STORAGE_KEY } from '../constants';
-import { getLocalStorage } from '../utils/localStorage';
+import { atom, selector } from 'recoil';
 import type { CartItem, ToastState } from '../types';
 
 export const $CheckedCartIdList = atom<number[]>({
   key: 'CheckStateList',
-  default: getLocalStorage<CartItem[]>(LOCAL_STORAGE_KEY.CART_ITEM, []).map(item => item.id),
+  default: [],
 });
 
 export const $CartIdList = atom<number[]>({
   key: 'CartIdList',
-  default: getLocalStorage<CartItem[]>(LOCAL_STORAGE_KEY.CART_ITEM, []).map(item => item.id),
+  default: selector({
+    key: 'CatIdListSelector',
+    get: async ({ get }) => {
+      const currentUrl = get($CurrentServerUrl);
+      const response = await fetch(`${currentUrl}/cart-items`, {
+        headers: {
+          Authorization: `Basic ${btoa('a@a.com:1234')}`,
+        },
+      });
+
+      if (!response.ok) return [];
+      const data = (await response.json()) as CartItem[];
+
+      return data.map(item => item.id);
+    },
+  }),
 });
 
 export const $ToastStateList = atom<ToastState[]>({
   key: 'ToastMessageList',
   default: [],
+});
+
+export const $CurrentServerUrl = atom<string>({
+  key: 'CurrentServerUrl',
+  default: process.env.REACT_APP_SERVER_BASE_URL_LOGI,
 });
