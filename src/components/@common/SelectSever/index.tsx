74553f293d43c2e09e576c84c5465b6ff91295d4
TaeeunKim <xodms0309@naver.com>
2023-05-25 17:22:43 +0900

    fix: 서버를 바꿔도 select box가 변하지 않는 오류 해결

--- src/components/@common/SelectSever/index.tsx
+++ src/components/@common/SelectSever/index.tsx
@@ -1,22 +1,28 @@
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { serverAtom } from 'recoil/server';
 import { SERVERS } from 'utils/constants';
 import { ServerName } from 'types';
 import * as S from './SelectServer.styles';
 
 const SelectServer = () => {
-  const setServer = useSetRecoilState(serverAtom);
+  const [server, setServer] = useRecoilState(serverAtom);
 
   const onChangeServer: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
-    const server = e.target.value as ServerName;
-    setServer(SERVERS[server]);
+    const selectedServer = e.target.value as ServerName;
+    setServer(SERVERS[selectedServer]);
   };
 
   return (
     <S.SelectBox onChange={onChangeServer}>
-      <option value="여우">여우</option>
-      <option value="제이">제이</option>
-      <option value="루쿠">루쿠</option>
+      <option value="여우" selected={server === SERVERS['여우']}>
+        여우
+      </option>
+      <option value="제이" selected={server === SERVERS['제이']}>
+        제이
+      </option>
+      <option value="루쿠" selected={server === SERVERS['루쿠']}>
+        루쿠
+      </option>
     </S.SelectBox>
   );
 };
