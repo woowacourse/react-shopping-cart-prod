0c215407f0348030b7acbcfab3b3252581cfaeb5
solo5star <solo_5@naver.com>
2023-05-25 02:41:06 +0000

    refactor:  파일 삭제
    Co-authored-by: NaveOWO <pianowell96@gmail.com>

--- src/api/index.ts
+++
@@ -1,8 +0,0 @@
-import Client from './Client';
-import type { ShoppingCartRestAPI } from './rest/ShoppingCartRestAPI';
-
-export const client = new Client<ShoppingCartRestAPI>({
-  baseUrl: import.meta.env.BASE_URL,
-});
-
-export const path = client.path.bind(client);
