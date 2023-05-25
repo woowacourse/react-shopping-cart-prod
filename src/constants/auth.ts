fa99fedacecb870446793eed656341950ced1b82
Suyoung <jsy8492@gmail.com>
2023-05-25 15:08:32 +0900

    refactor: 상수를 각기 별도의 모듈로 관리하도록 분리

---
+++ src/constants/auth.ts
@@ -0,0 +1,3 @@
+const username = 'a@a.com';
+const password = '1234';
+export const base64 = btoa(username + ':' + password);
--- src/constants/index.ts
+++
@@ -1,8 +0,0 @@
-export const CART_LOCAL_STORAGE_KEY = 'cart';
-
-export const PRODUCTS_BASE_URL = '/products';
-export const CART_BASE_URL = '/cart-items';
-
-const username = 'a@a.com';
-const password = '1234';
-export const base64 = btoa(username + ':' + password);
