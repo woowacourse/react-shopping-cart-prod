5d46264a3b99d49a082152388067985194792923
d0dam <smallkdb@gmail.com>
2023-05-25 13:38:32 +0900

    refactor: 드롭다운 클릭 이벤트 수정
    Co-authored-by: 정호진 <hozzijeong@users.noreply.github.com>

--- src/components/Common/DropDown/index.tsx
+++ src/components/Common/DropDown/index.tsx
@@ -1,30 +1,45 @@
-import { useState } from 'react';
+/* eslint-disable react/no-array-index-key */
+import { useRef, useState } from 'react';
+import useOnClickOutside from '../../../hooks/useOnClickOutside';
 import styles from './index.module.scss';
 
 interface DropDownProps {
   options: string[];
   selectedListHandler: (currentTarget: HTMLLIElement) => void;
+  currentOptionIndex: number;
 }
 
-function DropDown({ options, selectedListHandler }: DropDownProps) {
-  const [currentValue, setCurrentValue] = useState(options[0]);
-  const [showOptions, setShowOptions] = useState(false);
+function DropDown({ options, selectedListHandler, currentOptionIndex }: DropDownProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [currentValue, setCurrentValue] = useState(options[currentOptionIndex]);
+  const [showOptions, setShowOptions] = useState<boolean>(false);
+
+  const show = () => setShowOptions(true);
+  const close = () => setShowOptions(false);
 
   const handleOnChangeSelectValue: React.MouseEventHandler<HTMLLIElement> = event => {
+    event.stopPropagation();
     const { innerText } = event.currentTarget;
     selectedListHandler(event.currentTarget);
     setCurrentValue(innerText);
+    close();
   };
 
-  const optionLists = options.map((data, idx) => (
-    <li key={idx} className={styles.option} onClick={handleOnChangeSelectValue}>
-      {data}
-    </li>
-  ));
+  const optionLists =
+    showOptions &&
+    options.map((data, idx) => (
+      <li key={idx} className={styles.option} onClick={handleOnChangeSelectValue}>
+        {data}
+      </li>
+    ));
+
+  useOnClickOutside<HTMLDivElement>(ref, close);
 
   return (
-    <div className={styles['select-box']} onClick={() => setShowOptions(prev => !prev)}>
-      <label className={styles.label}>{currentValue}</label>
+    <div role="button" tabIndex={0} className={styles['select-box']} onClick={show} ref={ref}>
+      <label htmlFor="option" className={styles.label}>
+        {currentValue}
+      </label>
       <ul className={`${styles.selectOption} ${showOptions ? styles.show : styles.hide}`}>{optionLists}</ul>
     </div>
   );
