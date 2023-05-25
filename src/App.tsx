ff8ac3aba8de7210ae3ef26190be5075c22990a9
d0dam <smallkdb@gmail.com>
2023-05-25 16:30:03 +0900

    refactor: LoadingView Suspense 위치 변경
    Co-authored-by: 정호진 <hozzijeong@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import { RecoilRoot } from 'recoil';
 import Router from './routes/Router';
-import { Suspense } from 'react';
-import LoadingView from './components/Common/LoadingView';
 
 function App() {
   return (
     <RecoilRoot>
-      <Suspense fallback={<LoadingView />}>
-        <Router />
-      </Suspense>
+      <Router />
     </RecoilRoot>
   );
 }
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,13 +1,17 @@
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import LoadingView from '../components/Common/LoadingView';
 import ToastList from '../components/Common/Toast/ToastList';
 import Header from '../components/Header';
 
 function Root() {
   return (
     <>
-      <Header />
+      <Suspense fallback={<LoadingView />}>
+        <Header />
+        <Outlet />
+      </Suspense>
       <ToastList />
-      <Outlet />
     </>
   );
 }
