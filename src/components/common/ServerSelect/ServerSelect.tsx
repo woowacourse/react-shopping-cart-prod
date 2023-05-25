6534977247ea65cf970bad5d25d649e13b6b503b
ashleysyheo <ashleysyheo@gmail.com>
2023-05-25 17:03:49 +0900

    refactor: ServerSelect 컴포넌트에서 에러처리 추가

--- src/components/common/ServerSelect/ServerSelect.tsx
+++ src/components/common/ServerSelect/ServerSelect.tsx
@@ -1,20 +1,43 @@
-import { ChangeEvent } from 'react';
-import { useRecoilState } from 'recoil';
+import { ChangeEvent, useState } from 'react';
+import { useRecoilCallback, useRecoilValue } from 'recoil';
 
+import HTTPError from '../../../api/HTTPError';
+import { getCartAPI } from '../../../api/cartAPI';
 import { API_BASE_URL_LIST } from '../../../constants/api';
+import { cartListState } from '../../../store/cart';
 import { currentServerState } from '../../../store/server';
-import { saveToLocalStorage } from '../../../utils/localStorage';
 import Select from '../Select/Select';
 
-const ServerSelect = () => {
-  const [currentServer, setCurrentServer] = useRecoilState(currentServerState);
+interface ServerSelectProps {
+  onChange?: () => void;
+}
 
-  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    setCurrentServer(event.target.value);
-    saveToLocalStorage<string>('API_BASE_URL', event.target.value);
-  };
+const ServerSelect = ({ onChange }: ServerSelectProps) => {
+  const currentServer = useRecoilValue(currentServerState);
+  const [error, setError] = useState<Error | HTTPError | null>(null);
 
-  return <Select value={currentServer} options={API_BASE_URL_LIST} onChange={handleChange} />;
+  if (error !== null) throw error;
+
+  const handleServerChange = useRecoilCallback(
+    ({ set }) =>
+      async (event: ChangeEvent<HTMLSelectElement>) => {
+        set(currentServerState, event.target.value);
+        const cartAPI = getCartAPI(event.target.value);
+
+        try {
+          const cartList = await cartAPI.getCartList();
+          set(cartListState, cartList);
+          onChange?.();
+        } catch (error) {
+          if (error instanceof Error) {
+            setError(error);
+          }
+        }
+      },
+    []
+  );
+
+  return <Select value={currentServer} options={API_BASE_URL_LIST} onChange={handleServerChange} />;
 };
 
 export default ServerSelect;
