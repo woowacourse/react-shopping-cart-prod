e5e30930969c0dcd504d6778395cd98599590566
solo5star <solo_5@naver.com>
2023-05-25 02:43:42 +0000

    refactor: 컴포넌트 분리
    Co-authored-by: NaveOWO <pianowell96@gmail.com>

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,12 +1,13 @@
-import { useRecoilValue } from 'recoil';
 import { styled } from 'styled-components';
 import CartEmptyPlaceholder from '../components/CartEmptyPlaceholder';
 import CartItemListItem from '../components/CartItemListItem';
 import CartOrder from '../components/CartOrder';
 import Checkbox from '../components/common/Checkbox';
+import AwaitRecoilState from '../components/utils/AwaitRecoilState';
 import useCartActions from '../hooks/useCartActions';
 import useCartOrder from '../hooks/useCartOrder';
 import cartItemsState from '../recoil/atoms/cartItemsState';
+import type { CartItem } from '../type';
 
 const Header = styled.header`
   padding-bottom: 32px;
@@ -73,13 +74,19 @@ const DeleteSelectedButton = styled.button`
   font-size: 16px;
 `;
 
-const CartPage = () => {
-  const cartItems = useRecoilValue(cartItemsState);
-  const { deleteCartItems } = useCartActions();
-  const { selectedCount, allSelected, selectForOrder, toggleForOrder, unselectAllForOrder } =
-    useCartOrder();
+type CartPageContentProps = {
+  cartItems: CartItem[];
+};
 
-  const handleEnableAll = () => {
+const CartPageContent = (props: CartPageContentProps) => {
+  const { cartItems } = props;
+  const selectedCount = cartItems.filter((cartItem) => !cartItem.unselectedForOrder).length;
+  const allSelected = selectedCount === cartItems.length;
+
+  const { deleteCartItems } = useCartActions();
+  const { selectForOrder, toggleForOrder, unselectAllForOrder } = useCartOrder();
+
+  const handleEnableAll = (cartItems: CartItem[]) => () => {
     if (allSelected) {
       unselectAllForOrder();
       return;
@@ -87,49 +94,61 @@ const CartPage = () => {
     cartItems.forEach((cartItem) => selectForOrder(cartItem.product.id));
   };
 
-  const handleDeleteSelected = () => {
+  const handleDeleteSelected = (cartItems: CartItem[]) => () => {
     unselectAllForOrder();
     deleteCartItems(cartItems.map((cartItem) => cartItem.product.id));
   };
 
   return (
+    <CartLayout>
+      <CartItemListSection>
+        <CartItemListCaption>배송 상품 ({cartItems.length}개)</CartItemListCaption>
+        <CartItemList>
+          {cartItems.map((cartItem) => (
+            <CartItemListItemContainer>
+              <Checkbox
+                value={!cartItem.unselectedForOrder}
+                onChange={() => toggleForOrder(cartItem.product.id)}
+              />
+              <CartItemListItem
+                key={cartItem.product.id}
+                product={cartItem.product}
+                quantity={cartItem.quantity}
+              />
+            </CartItemListItemContainer>
+          ))}
+        </CartItemList>
+
+        <CartItemListController>
+          <Checkbox value={allSelected} onChange={handleEnableAll(cartItems)} />
+          <CartItemSelected>
+            전체선택 ({selectedCount}/{cartItems.length}개)
+          </CartItemSelected>
+          <DeleteSelectedButton onClick={handleDeleteSelected(cartItems)}>
+            선택삭제
+          </DeleteSelectedButton>
+        </CartItemListController>
+      </CartItemListSection>
+
+      <CartOrder isCartEmpty={selectedCount === 0} />
+    </CartLayout>
+  );
+};
+
+const CartPage = () => {
+  return (
     <>
       <Header>장바구니</Header>
 
-      {cartItems.length === 0 ? (
-        <CartEmptyPlaceholder />
-      ) : (
-        <CartLayout>
-          <CartItemListSection>
-            <CartItemListCaption>배송 상품 ({cartItems.length}개)</CartItemListCaption>
-            <CartItemList>
-              {cartItems.map((cartItem) => (
-                <CartItemListItemContainer>
-                  <Checkbox
-                    value={!cartItem.unselectedForOrder}
-                    onChange={() => toggleForOrder(cartItem.product.id)}
-                  />
-                  <CartItemListItem
-                    key={cartItem.product.id}
-                    product={cartItem.product}
-                    quantity={cartItem.quantity}
-                  />
-                </CartItemListItemContainer>
-              ))}
-            </CartItemList>
-
-            <CartItemListController>
-              <Checkbox value={allSelected} onChange={handleEnableAll} />
-              <CartItemSelected>
-                전체선택 ({selectedCount}/{cartItems.length}개)
-              </CartItemSelected>
-              <DeleteSelectedButton onClick={handleDeleteSelected}>선택삭제</DeleteSelectedButton>
-            </CartItemListController>
-          </CartItemListSection>
-
-          <CartOrder isCartEmpty={selectedCount === 0} />
-        </CartLayout>
-      )}
+      <AwaitRecoilState state={cartItemsState}>
+        {(cartItems) =>
+          cartItems.length === 0 ? (
+            <CartEmptyPlaceholder />
+          ) : (
+            <CartPageContent cartItems={cartItems} />
+          )
+        }
+      </AwaitRecoilState>
     </>
   );
 };
