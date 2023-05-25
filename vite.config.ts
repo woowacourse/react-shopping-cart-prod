51cefa619e4467694fdebad5d03487e1fe80ce81
solo5star <solo_5@naver.com>
2023-05-25 02:39:21 +0000

    chore: 프록시 제거
    Co-authored-by: NaveOWO <pianowell96@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
-import servers from './src/servers';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -12,18 +11,18 @@ export default defineConfig(({ mode }) => {
     server: {
       host: '0.0.0.0',
       port: 3000,
-      proxy: Object.fromEntries(
-        servers.map((server) => {
-          return [
-            server.baseUrl,
-            {
-              target: server.origin,
-              changeOrigin: true,
-              rewrite: (path) => path.replace(/^\/api-\d+/, ''),
-            },
-          ];
-        }),
-      ),
+      // proxy: Object.fromEntries(
+      //   servers.map((server) => {
+      //     return [
+      //       server.baseUrl,
+      //       {
+      //         target: server.origin,
+      //         changeOrigin: true,
+      //         rewrite: (path) => path.replace(/^\/api-\d+/, ''),
+      //       },
+      //     ];
+      //   }),
+      // ),
     },
   };
 });
