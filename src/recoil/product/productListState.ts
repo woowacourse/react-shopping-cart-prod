a8ed44f8e6dbd1b99c91dcca7ae7ec77eb074d23
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-25 17:09:52 +0900

    feat: 서버 변경 시 상품 목록 변경 기능 구현

--- src/recoil/product/productListState.ts
+++ src/recoil/product/productListState.ts
@@ -1,3 +1,6 @@
+import { PRODUCT_PATH } from '@constants/urlConstants';
+import serverState, { SERVER } from '@recoil/server/serverState';
+import { createApiRequests } from '@utils/createApiRequests';
 import { getProductsFetched } from '@views/ProductItemList/remote/fetchProductList';
 import { atom, selector, useRecoilValue } from 'recoil';
 import { ProductItemType } from 'types/ProductType';
@@ -6,8 +9,13 @@ export const productListState = atom<ProductItemType[]>({
   key: 'productListState',
   default: selector({
     key: 'productListState/selector',
-    get: async () => {
-      const productList: ProductItemType[] = await getProductsFetched();
+    get: async ({ get }) => {
+      const server = get(serverState);
+
+      const productList: ProductItemType[] = await createApiRequests(SERVER[server])(
+        PRODUCT_PATH
+      ).GET();
+
       return productList;
     },
   }),
