7914ad341e4706e43f3a66ae4f1f5feae12c404f
hae-on <solbi2004@naver.com>
2023-05-25 14:56:02 +0900

    chore: localhost 삭제, id 카트id 받아오도록 수정, hostName으로 api fetch
    Co-authored-by: jero_kang <inyeong-kang@users.noreply.github.com>

--- src/hooks/useCartProducts.ts
+++ src/hooks/useCartProducts.ts
@@ -1,35 +1,43 @@
 import { useEffect } from 'react';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
-import type { Product } from '../types/product';
 import { cartProductAtom } from '../recoil/cartProductData';
-import { deleteProduct, findTargetProduct } from '../domain/cartProductHandler';
-import { deleteCartProduct, postCartProduct } from '../apis/cartProducts';
+import { findTargetProduct } from '../domain/cartProductHandler';
+import { api } from '../apis/cartProducts';
 import useProductQuantity from './useProductQuantity';
-import { setStoredCartProducts } from '../utils/localStorage';
+import { hostNameAtom } from '../recoil/hostData';
+import type { Product } from '../types/product';
 
 const useCartProducts = (product: Product) => {
   const { id } = product;
+  const hostName = useRecoilValue(hostNameAtom);
   const [cartProducts, setCartProducts] = useRecoilState(cartProductAtom);
   const { addCount, subtractCount } = useProductQuantity(id);
-
-  const addProduct = () => {
-    const updatedCartProducts = [...cartProducts, { id, quantity: 1, product }];
-
-    setCartProducts(updatedCartProducts);
-    setStoredCartProducts(updatedCartProducts);
-    postCartProduct(id);
-  };
-
-  const removeProduct = () => {
-    const updatedCartProducts = deleteProduct(cartProducts, id);
-
-    setCartProducts((prev) => deleteProduct(prev, id));
-    setStoredCartProducts(updatedCartProducts);
-    deleteCartProduct(id);
-  };
-
   const target = findTargetProduct(cartProducts, id);
+
+  const addProduct = async () => {
+    const cartItemId = await api(hostName).then((apiInstance) => {
+      return apiInstance.postCartProduct(product.id);
+    });
+
+    if (cartItemId)
+      setCartProducts([
+        ...cartProducts,
+        { id: Number(cartItemId), quantity: 1, product },
+      ]);
+  };
+
+  const removeProduct = async () => {
+    if (target) {
+      api(hostName).then((apiInstance) => {
+        return apiInstance.deleteCartProduct(target.id);
+      });
+
+      setCartProducts(
+        cartProducts.filter((cartProduct) => cartProduct.id !== target.id)
+      );
+    }
+  };
 
   useEffect(() => {
     if (!target) return;
