54b8d38979c78e28bac62b6470cf390b5d5bacaa
woo-jk <201702042@o.cnu.ac.kr>
2023-05-25 17:00:22 +0900

    fix: 서버 변경 시 체크 리스트 변경 안되는 문제 수정

--- src/hooks/useCheckedCartList.ts
+++ src/hooks/useCheckedCartList.ts
@@ -1,10 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useCartService from './useCartService';
+import { useRecoilValue } from 'recoil';
+import cartLoadingState from '../globalState/atoms/cartLoadingState';
 
 const useCheckedCartList = () => {
   const { cartList } = useCartService();
   const cartIdList = cartList.map((cartItem) => cartItem.id);
   const [checkedCartList, setCheckedCartList] = useState<string[]>(cartIdList);
+
+  const isCartLoading = useRecoilValue(cartLoadingState);
 
   const addCheckedItem = (id: string) => {
     setCheckedCartList((prevList) => [...prevList, id]);
@@ -29,6 +33,14 @@ const useCheckedCartList = () => {
   const isAllChecked = () => {
     return cartList.length === checkedCartList.length;
   };
+
+  useEffect(() => {
+    if (!isCartLoading) {
+      const cartIdList = cartList.map((cartItem) => cartItem.id);
+
+      setCheckedCartList(cartIdList);
+    }
+  }, [isCartLoading]);
 
   return {
     checkedCartList,
