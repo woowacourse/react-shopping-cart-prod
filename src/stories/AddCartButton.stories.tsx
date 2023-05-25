2d03a4f9e4bde0b14034ad0977989da7af3fa17c
d0dam <smallkdb@gmail.com>
2023-05-25 16:26:31 +0900

    refactor: 인자값 변경에 따른 수정
    Co-authored-by: 정호진 <hozzijeong@users.noreply.github.com>

--- src/stories/AddCartButton.stories.tsx
+++ src/stories/AddCartButton.stories.tsx
@@ -12,7 +12,12 @@ export default meta;
 
 export const Default: Story = {
   args: {
-    productId: 1,
+    product: {
+      id: 2,
+      name: 'PET보틀-밀크티(370ml)',
+      price: 73400,
+      imageUrl: 'https://cdn-mart.baemin.com/sellergoods/main/ac90cb6d-70ad-4271-a25e-03e4db9a9960.jpg?h=300&w=300',
+    },
   },
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
