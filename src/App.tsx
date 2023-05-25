c9c3437b8d65fbfcfb15346332a1dd61a3915575
hae-on <solbi2004@naver.com>
2023-05-25 14:36:42 +0900

    chore: loading suspense
    Co-authored-by: jero_kang <inyeong-kang@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
+import { Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import ProductsListPage from './pages/ProductsListPage';
 import CartProductsListPage from './pages/CartProductsListPage';
 import NotFoundPage from './pages/NotFoundPage';
+import Message from './components/Common/Message';
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path='/' element={<ProductsListPage />} />
+        <Route
+          path='/'
+          element={
+            <Suspense fallback={<Message type='loading' />}>
+              <ProductsListPage />
+            </Suspense>
+          }
+        />
         <Route path='/cart' element={<CartProductsListPage />} />
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
