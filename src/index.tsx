e4b241f47bf0ce74b8b0c195cbe95807dc6b8f1e
hae-on <solbi2004@naver.com>
2023-05-25 14:37:27 +0900

    chore: msw worker 삭제
    Co-authored-by: jero_kang <inyeong-kang@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import App from './App';
+import { RecoilRoot } from 'recoil';
 
 import GlobalStyle from './GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
-import { worker } from './mocks/browsers';
-import { RecoilRoot } from 'recoil';
+import App from './App';
 
 const main = async () => {
   if (window.location.pathname === '/react-shopping-cart') {
     window.location.pathname = '/react-shopping-cart/';
     return;
   }
-
-  await worker.start({
-    serviceWorker: {
-      url: '/react-shopping-cart/mockServiceWorker.js',
-    },
-  });
 };
 
 const root = ReactDOM.createRoot(
