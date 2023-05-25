22bf9fabfed862faf96f87d1c523b420fc1b1587
n0eyes <doll4586633@naver.com>
2023-05-25 16:43:11 +0900

    test: fetchQuery test

--- src/apis/api.test.ts
+++ src/apis/api.test.ts
@@ -1,9 +1,18 @@
-import { fetchQuery, FetchQueryInstance } from './api';
+import { fetchQuery } from './api';
+import { FetchQueryInstance, Method } from './api.type';
 
 const BASE = 'http://localhost:3000';
 const PATH = '/test';
+
 const RESOLVED_VALUE = { test: true };
-const REQUEST_BODY = { body: true };
+const FETCH_CONFIG = { data: { quantity: 3 } };
+const FETCH_RESPONSE = { data: RESOLVED_VALUE };
+
+const createFetchConfig = (method: Method) =>
+  Object.assign(FETCH_CONFIG, {
+    baseURL: BASE,
+    method,
+  });
 
 describe('fetchQuery', () => {
   let fetcher: FetchQueryInstance;
@@ -17,23 +26,20 @@ describe('fetchQuery', () => {
   test('get', async () => {
     const data = await fetcher.get(PATH);
 
-    expect(data).toEqual(RESOLVED_VALUE);
+    expect(data).toEqual(FETCH_RESPONSE);
     expect(fetch).toHaveBeenCalledWith(new URL(PATH, BASE), {
       baseURL: BASE,
-      method: 'get',
+      method: 'GET',
     });
   });
 
   test('post', async () => {
-    const data = await fetcher.post(PATH, {
-      body: REQUEST_BODY,
-    });
+    const data = await fetcher.post(PATH, createFetchConfig('POST'));
 
-    expect(data).toEqual(RESOLVED_VALUE);
-    expect(fetch).toHaveBeenCalledWith(new URL(PATH, BASE), {
-      baseURL: BASE,
-      body: JSON.stringify(REQUEST_BODY),
-      method: 'post',
-    });
+    expect(data).toEqual(FETCH_RESPONSE);
+    expect(fetch).toHaveBeenCalledWith(
+      new URL(PATH, BASE),
+      createFetchConfig('POST')
+    );
   });
 });
