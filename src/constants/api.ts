e30fd31b3a69b3f904b299031240f1f664961c05
ashleysyheo <ashleysyheo@gmail.com>
2023-05-25 15:09:18 +0900

    feat: 장바구니 api 관련 에러 메세지 상수 추가

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -59,6 +59,24 @@ const HTTP_ERROR_MESSAGE = {
   },
 } as const;
 
+const CART_API_ERROR_MESSAGE = {
+  ADD: {
+    [HTTP_STATUS_CODE.BAD_REQUEST]: '해당하는 상품이 없습니다.',
+    [HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR]:
+      '일시적인 오류로 상품을 장바구니 추가하는 데 실패했습니다. 새로고침 후 시도해 주세요.',
+  },
+  UPDATE: {
+    [HTTP_STATUS_CODE.BAD_REQUEST]: '해당하는 상품이 없습니다.',
+    [HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR]:
+      '일시적인 오류로 수량 변경을 할 수 없습니다. 새로고침 후 시도해 주세요.',
+  },
+  DELETE: {
+    [HTTP_STATUS_CODE.BAD_REQUEST]: '해당하는 상품이 없습니다.',
+    [HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR]:
+      '일시적인 오류로 삭제를 할 수 없습니다. 새로고침 후 시도해 주세요.',
+  },
+} as const;
+
 export {
   BASE64,
   API_BASE_URL_LIST,
@@ -68,4 +86,5 @@ export {
   CART_FETCH_OPTION_HEADERS,
   HTTP_STATUS_CODE,
   HTTP_ERROR_MESSAGE,
+  CART_API_ERROR_MESSAGE,
 };
