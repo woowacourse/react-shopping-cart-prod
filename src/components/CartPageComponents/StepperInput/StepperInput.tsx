ee5ff73bdd5c9599011adb1abfb8c577e096a4ae
sh981013s <sh981013s@gmail.com>
2023-05-25 17:25:40 +0900

    feat: stepper input request & response logic 매핑

--- src/components/CartPageComponents/StepperInput/StepperInput.tsx
+++ src/components/CartPageComponents/StepperInput/StepperInput.tsx
@@ -1,58 +1,29 @@
-import { ChangeEvent, useEffect, useState } from 'react';
 import * as Styled from './StepperInput.styles.tsx';
 import { Item } from '../../../types/CartList.ts';
-import usePostUpdateCart from '../../../hooks/requests/usePostUpdateCart.ts';
-import useCart from '../../../hooks/useCart.ts';
+import useCartItemOperations from '../../../hooks/useCartItemOperations.ts';
 
 type StepperInputProps = {
   initialValue: number;
   cartItem: Item;
+  refetchCartList: ({}) => void;
 };
 
-const StepperInput = ({ initialValue, cartItem }: StepperInputProps) => {
-  const [value, setValue] = useState(initialValue.toString());
-  const { updateCart } = useCart();
-  const { optimisticUpdate } = usePostUpdateCart(cartItem);
-
-  const handleIncrement = () => {
-    setValue((prevValue) => {
-      const parsedValue = parseInt(prevValue);
-      return isNaN(parsedValue) ? '1' : String(parsedValue + 1);
-    });
-  };
-
-  const handleDecrement = () => {
-    setValue((prevValue) => {
-      const parsedValue = parseInt(prevValue);
-      return isNaN(parsedValue) ? '1' : String(Math.max(parsedValue - 1, 1));
-    });
-  };
-
-  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const inputValue = event.target.value;
-
-    setValue(inputValue.replace(/\D/, ''));
-  };
-
-  useEffect(() => {
-    const targetValue = parseInt(value, 10);
-    const updatedCartItem = { ...cartItem, quantity: targetValue };
-    optimisticUpdate(updatedCartItem, { itemId: cartItem.id, quantity: targetValue });
-    updateCart(updatedCartItem);
-  }, [value]);
-
-  const handleInputBlur = () => {
-    if (value === '') {
-      setValue('1');
-    }
-  };
+const StepperInput = ({ cartItem, refetchCartList }: StepperInputProps) => {
+  const { handleStepperIncreaseButton, handleStepperDecreaseButton } = useCartItemOperations({
+    cartItemNumber: cartItem.id,
+    id: cartItem.product.id,
+    name: cartItem.product.name,
+    price: cartItem.product.price,
+    imageUrl: cartItem.product.imageUrl,
+    refetchCartList,
+  });
 
   return (
     <Styled.StepperInputWrapper>
-      <Styled.Input type='text' value={value} onChange={handleInputChange} onBlur={handleInputBlur} />
+      <Styled.Input type='text' value={cartItem.quantity} />
       <Styled.ButtonWrapper>
-        <Styled.Button onClick={handleIncrement}>+</Styled.Button>
-        <Styled.Button onClick={handleDecrement}>-</Styled.Button>
+        <Styled.Button onClick={() => handleStepperIncreaseButton(cartItem.quantity)}>+</Styled.Button>
+        <Styled.Button onClick={() => handleStepperDecreaseButton(cartItem.quantity)}>-</Styled.Button>
       </Styled.ButtonWrapper>
     </Styled.StepperInputWrapper>
   );
