3dd65f4b08545e6c6b1ca8abd3216cfb812590a0
ashleysyheo <ashleysyheo@gmail.com>
2023-05-25 17:02:17 +0900

    style: Select 컴포넌트 스타일 수정

--- src/components/common/Select/Select.styles.ts
+++ src/components/common/Select/Select.styles.ts
@@ -2,14 +2,15 @@ import styled from 'styled-components';
 
 const SelectLabel = styled.label`
   position: relative;
-  padding-right: 6px;
+  width: 150px;
+  padding-right: 10px;
   background-color: ${({ theme }) => theme.color.white};
   border: 1px solid ${({ theme }) => theme.color.gray3};
   border-radius: ${({ theme }) => theme.borderRadius.small};
 `;
 
 const Select = styled.select`
-  width: 150px;
+  width: 100%;
   padding: 12px;
   background-color: ${({ theme }) => theme.color.white};
   font-size: 14px;
