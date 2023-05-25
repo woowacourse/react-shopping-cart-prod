957e57938378a85b3e91179cdde6cf3cad3d05e1
regularPark <srk99279@naver.com>
2023-05-25 11:31:39 +0900

    refactor: `useFetchData`내 인증, api 객체 내부 수정
    Co-authored-by: 고니_정우시 <jeongwusi@gmail.com>

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -1,14 +1,38 @@
 import { Dispatch, SetStateAction, useState } from 'react';
 import { fetchApi } from '../api/fetchApi';
+import { CART_BASE_URL } from '../constants/url';
+import { useRecoilValue } from 'recoil';
+import { serverState } from '../store/ServerState';
 
 type SetDataType<T> = Dispatch<SetStateAction<T>>;
 
 export const useFetchData = <T>(setData?: SetDataType<T>) => {
   const [isLoading, setIsLoading] = useState(true);
 
-  const fetchData = async (url: string, options: RequestInit) => {
+  const serverUrl = useRecoilValue(serverState);
+
+  const email = 'a@a.com';
+  const password = '1234';
+  const base64 = btoa(`${email}:${password}`);
+
+  const fetchData = async (url: string, options: RequestInit, baseUrl?: string) => {
     try {
       const data = await fetchApi(url, options);
+
+      if (options.method !== 'GET') {
+        if (!baseUrl) return;
+
+        const refetchData = await fetchApi(`${serverUrl}${baseUrl}`, {
+          method: 'GET',
+          headers: {
+            Authorization: `basic ${base64}`,
+          },
+        });
+
+        if (setData) setData(refetchData);
+        return;
+      }
+
       if (setData) setData(data);
     } catch (error) {
       console.error(error);
@@ -19,27 +43,56 @@ export const useFetchData = <T>(setData?: SetDataType<T>) => {
 
   const api = {
     get: (url: string) => {
-      fetchData(url, {
-        method: 'GET',
-      });
+      url.includes(CART_BASE_URL)
+        ? fetchData(url, {
+            method: 'GET',
+            headers: {
+              Authorization: `basic ${base64}`,
+            },
+          })
+        : fetchData(url, {
+            method: 'GET',
+          });
     },
-    post: <T>(url: string, body: T) => {
-      fetchData(url, {
-        method: 'POST',
-        body: JSON.stringify(body),
-      });
+    post: <T>(url: string, body: T, baseUrl: string) => {
+      fetchData(
+        url,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': `application/json`,
+            Authorization: `basic ${base64}`,
+          },
+          body: JSON.stringify(body),
+        },
+        baseUrl,
+      );
     },
-    patch: <T>(url: string, body: T) => {
-      fetchData(url, {
-        method: 'PATCH',
-        body: JSON.stringify(body),
-      });
+    patch: <T>(url: string, body: T, baseUrl: string) => {
+      fetchData(
+        url,
+        {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `basic ${base64}`,
+          },
+          body: JSON.stringify(body),
+        },
+        baseUrl,
+      );
     },
-    delete: <T>(url: string, body: T) => {
-      fetchData(url, {
-        method: 'DELETE',
-        body: JSON.stringify(body),
-      });
+    delete: (url: string, baseUrl: string) => {
+      fetchData(
+        url,
+        {
+          method: 'DELETE',
+          headers: {
+            Authorization: `basic ${base64}`,
+          },
+        },
+        baseUrl,
+      );
     },
   };
 
