73482be909dce27cf4a210370e3d8f3b3a84c3be
sh981013s <sh981013s@gmail.com>
2023-05-26 03:07:22 +0900

    chore: 서버이름 이모지 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,10 +8,13 @@ import { useResetRecoilState, useSetRecoilState } from 'recoil';
 import { serverAtom } from '../../stores/serverStore.ts';
 import { ServerNames } from '../../types/request.ts';
 import { cartListAtom } from '../../stores/cartListStore.ts';
+import toastMessages from '../../constants/toastMessages.ts';
+import { useToast } from '../../hooks/useToast.ts';
 
 const Header = () => {
   const setServerName = useSetRecoilState(serverAtom);
   const resetCartList = useResetRecoilState(cartListAtom);
+  const showToast = useToast();
 
   const navigate = useNavigate();
   const handleLogoClick = () => {
@@ -28,6 +31,7 @@ const Header = () => {
     resetCartList();
 
     setServerName(name);
+    showToast(toastMessages.serverChanged);
   };
 
   return (
@@ -37,9 +41,9 @@ const Header = () => {
           <Styled.LogoImage src={Logo} alt='SHOP' />
         </Styled.LogoButton>
         <Styled.ServerSelectBox onChange={onChangeServerNameHandler}>
-          <option value='ROY'>로이</option>
-          <option value='SPLIT'>스플릿</option>
-          <option value='IRAE'>이레</option>
+          <option value='ROY'>로이 👨🏻‍🦰</option>
+          <option value='SPLIT'>스플릿 👨🏻‍🦱</option>
+          <option value='IRAE'>이레 👧🏻</option>
         </Styled.ServerSelectBox>
         <Styled.ShoppingCartButton onClick={handleShoppingCartButtonClick}>
           <Styled.ShoppingCartButtonText>장바구니</Styled.ShoppingCartButtonText>
