a96357a166eb23cce251c906aca4ab3f1b82f8a2
Dain Lee <feb.dain@gmail.com>
2023-05-25 16:47:56 +0900

    test: Storybook `ProductItem` 컴포넌트 수량 변경 안 되는 오류 개선

--- src/stories/components/ProductItem.stories.tsx
+++ src/stories/components/ProductItem.stories.tsx
@@ -1,24 +1,22 @@
-import { Meta, StoryObj } from '@storybook/react';
+import { Meta } from '@storybook/react';
+import { useEffect } from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import ProductItemComponent from '../../components/main/ProductItem';
+import { PRODUCT_LIST_URL } from '../../constants/url';
+import { useFetchData } from '../../hooks/useFetchData';
+import { productListState, serverState } from '../../recoil';
+import { Product } from '../../types';
 
 const meta = {
   component: ProductItemComponent,
   title: 'Components/Main/ProductItem',
   tags: ['autodocs'],
-} satisfies Meta<typeof ProductItemComponent>;
-
-export default meta;
-
-type Story = StoryObj<typeof ProductItemComponent>;
-
-export const ProductItem: Story = {
   args: {
     id: 1,
     imageUrl: `${process.env.PUBLIC_URL}/assets/product1.svg`,
     name: 'PET보틀-정사각(420ml)',
     price: 43400,
   },
-
   argTypes: {
     id: {
       control: {
@@ -48,4 +46,20 @@ export const ProductItem: Story = {
       description: '상품의 가격을 설정할 수 있습니다.',
     },
   },
+} satisfies Meta<typeof ProductItemComponent>;
+
+export default meta;
+
+export const ProductItem = (args: Product) => {
+  const { api } = useFetchData();
+  const setProductList = useSetRecoilState(productListState);
+  const server = useRecoilValue(serverState);
+  useEffect(() => {
+    api.get(`${server}${PRODUCT_LIST_URL}`).then((data) => {
+      setProductList(data);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [server]);
+
+  return <ProductItemComponent {...args} />;
 };
