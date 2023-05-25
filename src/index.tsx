7fc1299e773feaa55391a8c5fa6eb523a4160945
Dain Lee <feb.dain@gmail.com>
2023-05-25 17:47:45 +0900

    chore: MSW 비활성화

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,22 @@ import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import App from './App';
 import { GlobalStyle } from './GlobalStyle';
-import { worker } from './mock/browser';
 
-const main = async () => {
-  if (window.location.pathname === '/react-shopping-cart-prod') {
-    window.location.pathname = '/react-shopping-cart-prod/';
-    return;
-  }
+/* 스토리북을 위한 MSW 비활성화 */
+// import { worker } from './mock/browser';
 
-  await worker.start({
-    serviceWorker: {
-      url: '/react-shopping-cart-prod/mockServiceWorker.js',
-    },
-  });
-};
+// const main = async () => {
+//   if (window.location.pathname === '/react-shopping-cart-prod') {
+//     window.location.pathname = '/react-shopping-cart-prod/';
+//     return;
+//   }
+
+//   await worker.start({
+//     serviceWorker: {
+//       url: '/react-shopping-cart-prod/mockServiceWorker.js',
+//     },
+//   });
+// };
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -28,4 +30,4 @@ root.render(
   </React.StrictMode>
 );
 
-main();
+// main();
