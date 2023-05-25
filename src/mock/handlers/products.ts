6a4b65c598b4ef43983f88093ed1ae418f8add6f
Dain Lee <feb.dain@gmail.com>
2023-05-25 16:55:42 +0900

    chore: url 변경

--- src/mock/handlers/products.ts
+++ src/mock/handlers/products.ts
@@ -1,15 +1,15 @@
 import { rest } from 'msw';
-import { PRODUCT_LIST_URL } from '../../constants/url';
+import { PRODUCT_LIST_URL, SERVERS } from '../../constants/url';
 import productList from '../productList.json';
 
 export const productsHandlers = [
   // 상품 목록 조회
-  rest.get(PRODUCT_LIST_URL, (_, res, ctx) => {
+  rest.get(`${SERVERS.준팍}${PRODUCT_LIST_URL}`, (_, res, ctx) => {
     return res(ctx.delay(1000), ctx.status(200), ctx.json(productList));
   }),
 
   // 상품 조회
-  rest.get(`${PRODUCT_LIST_URL}/:id`, (req, res, ctx) => {
+  rest.get(`${SERVERS.준팍}${PRODUCT_LIST_URL}/:id`, (req, res, ctx) => {
     const productId = Number(req.params.id);
     const product = productList.find((product) => product.id === productId);
 
