5633a4e69cbca820d18f14a48637e6c36ca4d62a
Leejin-Yang <ejin1996@gmail.com>
2023-05-25 15:16:49 +0900

    build: 홈페이지 변경 및 workflow 브랜치 수정

--- src/index.tsx
+++ src/index.tsx
@@ -10,14 +10,14 @@ import theme from './styles/theme';
 import { RecoilRoot } from 'recoil';
 
 const main = async () => {
-  if (window.location.pathname === '/react-shopping-cart') {
-    window.location.pathname = '/react-shopping-cart/';
+  if (window.location.pathname === '/react-shopping-cart-prod') {
+    window.location.pathname = '/react-shopping-cart-prod/';
     return;
   }
 
   await worker.start({
     serviceWorker: {
-      url: '/react-shopping-cart/mockServiceWorker.js',
+      url: '/react-shopping-cart-prod/mockServiceWorker.js',
     },
   });
 };
