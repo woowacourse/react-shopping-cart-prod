621c4563f84fc72cb60204d0cad730f34ac86dd5
Dain Lee <feb.dain@gmail.com>
2023-05-25 17:38:03 +0900

    refactor: 필요 없는 코드 제거

--- src/index.tsx
+++ src/index.tsx
@@ -18,9 +18,7 @@ const main = async () => {
   });
 };
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <RecoilRoot>
--- src/stories/pages/CartPage.stories.tsx
+++ src/stories/pages/CartPage.stories.tsx
@@ -1,9 +1,5 @@
 import { Meta } from '@storybook/react';
-import { useEffect } from 'react';
-import { KEY_CART } from '../../constants';
 import CartPage from '../../pages/CartPage';
-import { setDataInLocalStorage } from '../../utils/getAndSetDataInLocalStorage';
-
 const meta = {
   component: CartPage,
   title: 'Pages/CartPage',
@@ -12,23 +8,6 @@ const meta = {
 export default meta;
 
 export const Cart = () => {
-  useEffect(() => {
-    const cart = [
-      {
-        id: 1,
-        quantity: 1,
-        product: {
-          id: 1,
-          name: 'PET보틀-정사각(420ml)',
-          price: 43400,
-          imageUrl: '/assets/product1.svg',
-        },
-      },
-    ];
-
-    setDataInLocalStorage(KEY_CART, cart);
-  }, []);
-
   return (
     <div style={{ width: '1270px' }}>
       <CartPage />
