33b91a5d9c81f2ab21bd61bf81c7f039093b0fe6
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-05-25 17:24:39 +0900

    refactor: createBrowserRouter로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
-import PageLayout from './pages/PageLayout';
-import ProductsListPage from './pages/ProductsListPage';
-import CartPage from './pages/CartPage';
-import NotFoundPage from './pages/NotFoundPage';
+import Header from "./components/Common/Header";
+import Toast from "./components/Common/Toast";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route element={<PageLayout />}>
-          <Route path='/' element={<ProductsListPage />} />
-          <Route path='cart' element={<CartPage />} />
-        </Route>
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Header />
+      <Outlet />
+      <Toast />
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import { RecoilRoot } from "recoil";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import theme from "./styles/theme";
+import GlobalStyle from "./styles";
 
-import App from './App';
+import App from "./App";
+import NotFoundPage from "./pages/NotFoundPage";
+import ProductsListPage from "./pages/ProductsListPage";
+import CartPage from "./pages/CartPage";
 
-import { worker } from './mocks/browser';
-import GlobalStyle from './styles';
-import theme from './styles/theme';
-import { RecoilRoot } from 'recoil';
+import { worker } from "./mocks/browser";
 
 const main = async () => {
-  if (window.location.pathname === '/react-shopping-cart') {
-    window.location.pathname = '/react-shopping-cart/';
+  if (window.location.pathname === "/react-shopping-cart-prod") {
+    window.location.pathname = "/react-shopping-cart-prod/";
     return;
   }
 
   await worker.start({
     serviceWorker: {
-      url: '/react-shopping-cart/mockServiceWorker.js',
+      url: "/react-shopping-cart-prod/mockServiceWorker.js",
     },
   });
 };
 
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <NotFoundPage />,
+      children: [
+        { path: "", element: <ProductsListPage /> },
+        { path: "/cart", element: <CartPage /> },
+      ],
+    },
+  ],
+  {
+    basename: process.env.PUBLIC_URL,
+  }
+);
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <RecoilRoot>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <App />
+        <RouterProvider router={router} />
       </ThemeProvider>
     </RecoilRoot>
   </React.StrictMode>
