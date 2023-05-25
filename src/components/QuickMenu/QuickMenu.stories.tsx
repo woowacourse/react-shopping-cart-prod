67b427de0fdef9d7a4ad01f58f639c65da78f48d
nlom0218 <nlom0218@naver.com>
2023-05-25 17:35:47 +0900

    build: 스토리북 배포

--- src/components/QuickMenu/QuickMenu.stories.tsx
+++ src/components/QuickMenu/QuickMenu.stories.tsx
@@ -1,10 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 import QuickMenu from '.';
 
 const meta: Meta<typeof QuickMenu> = {
   title: 'QuickMenu',
   component: QuickMenu,
+  decorators: [
+    (storyFn) => (
+      <RecoilRoot>
+        <BrowserRouter>{storyFn()}</BrowserRouter>
+      </RecoilRoot>
+    ),
+  ],
 };
 
 export default meta;
