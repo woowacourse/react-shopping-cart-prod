095e565f2ef532e0690eda466281efbae2416998
yeopto <gunyeop530@gmail.com>
2023-05-26 22:34:39 +0900

    refactor: Router NotFound 삭제

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,12 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
 import Cart from "../pages/Cart";
 import Main from "../pages/Main";
-import NotFound from "../pages/NotFound";
 
 export const ROUTER_PATH = {
   Main: "/",
   Cart: "/Cart",
-  NotFound: "/*",
 };
 
 export const PageRouterProvider = () => {
@@ -20,7 +18,6 @@ export const PageRouterProvider = () => {
       path: ROUTER_PATH.Cart,
       element: <Cart />,
     },
-    { path: ROUTER_PATH.NotFound, element: <NotFound /> },
   ]);
   return <RouterProvider router={router} />;
 };
