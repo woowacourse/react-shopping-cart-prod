97781dd70dd9d1643574cd63052fe48657b77a0d
2yunseong <dbsdltjd3701@naver.com>
2023-05-26 19:03:17 +0900

    feat: OrderList 컴포넌트 구현

--- src/components/OrderPage/OrderList.tsx
+++ src/components/OrderPage/OrderList.tsx
@@ -1,10 +1,18 @@
 import { useRecoilValue } from 'recoil';
 import { ordersSelector } from '../../atoms/orders';
+import * as S from './OrderList.styles';
+import Order from './Order';
 
 const OrderList = () => {
-  const orders = useRecoilValue(ordersSelector);
+  const { orders } = useRecoilValue(ordersSelector);
 
-  return <div></div>;
+  return (
+    <S.List>
+      {orders.map((order) => (
+        <Order orderId={order.orderId} orderItems={order.orderItems} />
+      ))}
+    </S.List>
+  );
 };
 
 export default OrderList;
