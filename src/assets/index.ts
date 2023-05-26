4f9f71ce1a0468e05a672dc84b88486bbdd61f51
ashleysyheo <ashleysyheo@gmail.com>
2023-05-27 01:31:17 +0900

    feat: > 아이콘 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,4 +2,5 @@ export { ReactComponent as AddIcon } from './add-icon.svg';
 export { ReactComponent as CheckedIcon } from './checked-icon.svg';
 export { ReactComponent as CloseIcon } from './close-icon.svg';
 export { ReactComponent as MinusIcon } from './minus-icon.svg';
+export { ReactComponent as RightChevronIcon } from './right-icon.svg';
 export { ReactComponent as UncheckedIcon } from './unchecked-icon.svg';
