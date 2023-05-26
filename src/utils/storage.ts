4aef548bb161657912a1d30b713d3de66cb279a7
Dahye <06robin11@gmail.com>
2023-05-26 22:39:51 +0900

    docs:요구사항정리

--- src/utils/storage.ts
+++
@@ -1,6 +0,0 @@
-export const getLocalStorageData = <T>(key: string): T =>
-  JSON.parse(localStorage.getItem(key) ?? '[]');
-
-export const setLocalStorageData = <T>(key: string, data: T) => {
-  localStorage.setItem(key, JSON.stringify(data));
-};
