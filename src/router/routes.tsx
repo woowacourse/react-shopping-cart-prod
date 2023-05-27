ff22ccc997aaa38acfa3cf4f6717cbbf5272408e
ashleysyheo <ashleysyheo@gmail.com>
2023-05-28 03:08:17 +0900

    feat: createBrowserRouter에 주문 관련 path 추가

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -4,6 +4,9 @@ import App from '../App';
 import { PATH } from '../constants/path';
 import CartPage from '../pages/CartPage/CartPage';
 import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
+import OrderDetailPage from '../pages/OrderDetailPage/OrderDetailPage';
+import OrderPage from '../pages/OrderPage/OrderPage';
+import OrderSuccessPage from '../pages/OrderSuccessPage/OrderSuccessPage';
 import ProductListPage from '../pages/ProductListPage/ProductListPage';
 
 const router = createBrowserRouter(
@@ -20,6 +23,18 @@ const router = createBrowserRouter(
         {
           path: PATH.CARTS,
           element: <CartPage />,
+        },
+        {
+          path: PATH.ORDER,
+          element: <OrderPage />,
+        },
+        {
+          path: PATH.ORDER_DETAIL,
+          element: <OrderDetailPage />,
+        },
+        {
+          path: PATH.ORDER_SUCCESS,
+          element: <OrderSuccessPage />,
         },
       ],
     },
