afb0a8af03292ca1ef46dd920d2b333552b9063d
chsua <csj1919@naver.com>
2023-05-27 18:23:18 +0900

    feat: msw로 불러온 쿠폰 목록을 select option에 보여주기

--- src/components/CouponSelector.tsx
+++ src/components/CouponSelector.tsx
@@ -1,19 +1,34 @@
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { couponListState } from "recoil/coupon";
 import { serverSelectState } from "recoil/server";
 import { styled } from "styled-components";
 
 const CouponSelector = () => {
+  const couponList = useRecoilValue(couponListState);
+
   //   const [couponList, setcouponList] = ;
+  const defaultText =
+    couponList.length === 0
+      ? "사용할 쿠폰이 없습니다."
+      : "사용할 쿠폰을 골라주세요.";
 
   return (
     <Container>
       적용 쿠폰
       <Select>
-        <Option>1.</Option>
-        <Option>2.</Option>
-        <Option>3.</Option>
-        <Option>4.</Option>
-        <Option>5.</Option>
+        <Option>{defaultText}</Option>
+        {couponList.map((coupon) => {
+          if (coupon.selected) return;
+
+          const { couponId, name, discount } = coupon;
+          const unit = discount.type === "rate" ? "%" : "원";
+
+          return (
+            <Option
+              key={couponId}
+            >{`${name}(-${discount.amount}${unit})`}</Option>
+          );
+        })}
       </Select>
     </Container>
   );
