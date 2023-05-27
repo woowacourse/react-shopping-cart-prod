3636b60f4cf2902975364a5e2846ad7d8d69e239
ashleysyheo <ashleysyheo@gmail.com>
2023-05-28 03:00:21 +0900

    feat: 날짜 formatter 유틸 함수 생성

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -2,4 +2,8 @@ const priceFormatter = (price: number) => {
   return price.toLocaleString();
 };
 
-export { priceFormatter };
+const dateFormatter = (createdAt: Date) => {
+  return new Date(createdAt).toLocaleDateString('sv').replaceAll('-', '.');
+};
+
+export { priceFormatter, dateFormatter };
