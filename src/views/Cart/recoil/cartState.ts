977d2ca2affa9ef186752ce3cd17699fdc391e3c
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-27 10:09:17 +0900

    feat: cartState 아톰을 사용하는 hook 추가

--- src/views/Cart/recoil/cartState.ts
+++ src/views/Cart/recoil/cartState.ts
@@ -4,20 +4,28 @@ import {
   useRecoilRefresher_UNSTABLE,
   useRecoilState,
   useRecoilValue,
+  useResetRecoilState,
 } from 'recoil';
 import type { CartItemType } from '../../../types/ProductType';
 
 import { useCallback, useMemo } from 'react';
-import fetchCartItems from '@views/Cart/remote/fetchCartItem';
-import serverState, { SERVER } from '@recoil/server/serverState';
-import { createApiRequests } from '@utils/createApiRequests';
+
+import serverUrlState from '@recoil/server/serverUrlState';
+
+import generateFetchCart from '../remote/generateFetchCart';
+import credentialState from '@recoil/server/credentialState';
+import useFetchCart from '../hooks/useFetchCart';
 import { CART_PATH } from '@constants/urlConstants';
 
-export const CartItemQuery = selector({
-  key: 'cartListWithInfoState/default',
+export const cartQuery = selector({
+  key: 'cartState/default',
   get: async ({ get }) => {
-    const server = get(serverState);
-    const cartProducts: CartItemType[] = await createApiRequests(SERVER[server])(CART_PATH).GET();
+    const serverUrl = get(serverUrlState);
+    const credential = get(credentialState);
+    const fetchCart = generateFetchCart({ resource: `${serverUrl}/${CART_PATH}`, credential });
+
+    const response = await fetchCart.GET();
+    const cartProducts: CartItemType[] = await response.json();
 
     return cartProducts.map((cartProduct) => {
       cartProduct.checked = true;
@@ -27,18 +35,82 @@ export const CartItemQuery = selector({
 });
 
 const cartState = atom<CartItemType[]>({
-  key: 'cartListWithInfoState',
-  default: CartItemQuery,
+  key: 'cartState',
+  default: cartQuery,
 });
 
 export default cartState;
 
-export const useRefreshCartList = () => useRecoilRefresher_UNSTABLE(cartState);
+export const useRefreshCart = () => useRecoilRefresher_UNSTABLE(cartState);
 
-export const useCartList = () => useRecoilValue(cartState);
+export const useCartItem = (productId: number) => {
+  const [cart, setCart] = useRecoilState(cartState);
+  const cartItem = cart.find((cartItem) => cartItem.product.id === productId);
+  const fetchCart = useFetchCart();
+
+  const quantity = cartItem?.quantity ?? 0;
+  const cartItemId = cartItem?.id;
+
+  const addCartItem = async (productId: number) => {
+    await fetchCart.POST(productId);
+    const response = await fetchCart.GET();
+    const newCart: CartItemType[] = await response.json();
+    const checkedCart = newCart.map((cartItem) => {
+      cartItem.checked = true;
+      return cartItem;
+    });
+
+    setCart(checkedCart);
+  };
+
+  const updateCartItemQuantity = (cartId: number, quantity: number) => {
+    if (!cart.some((cartItem) => cartItem.id === cartId)) {
+      console.error(
+        '수량 변경하였지만 recoil에서 관리하는 cartState에서 cartItem의 id를 찾을 수 없습니다. '
+      );
+
+      return;
+    }
+
+    // DELETE
+    if (quantity === 0) {
+      setCart(cart.filter((cartItem) => cartItem.id !== cartId));
+      fetchCart.DELETE(cartId);
+      return;
+    }
+
+    // UPDATE(PATCH)
+    setCart(
+      cart.map((cartItem) => {
+        if (cartItem.id === cartId) {
+          return {
+            ...cartItem,
+            quantity,
+          };
+        }
+        return cartItem;
+      })
+    );
+    fetchCart.PATCH(cartId, quantity);
+  };
+
+  return {
+    cartItemId,
+    quantity,
+    updateCartItemQuantity,
+    addCartItem,
+  };
+};
+
+export const useCheckedCartItem = () => {};
+
+export const useResetCart = () => useResetRecoilState(cartState);
+
+export const useCartReadOnly = () => useRecoilValue(cartState);
 
 export const useCheckCart = () => {
   const [cart, setCart] = useRecoilState(cartState);
+  const fetchCart = useFetchCart();
 
   const toggleMap = useMemo<{ [id: number]: boolean }>(() => {
     return cart.reduce((acc, { product, checked }) => {
@@ -72,7 +144,7 @@ export const useCheckCart = () => {
     cart
       .filter((item) => item.checked === true)
       .forEach((item) => {
-        fetchCartItems.delete(item.id);
+        fetchCart.DELETE(item.id);
       });
   };
 
