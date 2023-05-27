f956c8f2a4685ad4f2427e717765686c577aaa13
ukkodeveloper <ukkodeveloper@gmail.com>
2023-05-27 10:08:21 +0900

    remove: 사용하지 않는 테스트 삭제

--- src/__test__/setupTests.ts
+++
@@ -1,9 +0,0 @@
-import { setupServer } from 'msw/node';
-
-export const server = setupServer();
-
-beforeAll(() => server.listen());
-
-afterEach(() => server.resetHandlers());
-
-afterAll(() => server.close());
