c99867c7122cca420219143780a33f5ff2e401d1
gilpop8663 <wolfye@naver.com>
2023-05-28 02:46:32 +0900

    chore: eslint import 순서 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,26 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-import path from "path";
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-shopping-cart/",
+  base: '/react-shopping-cart/',
   resolve: {
     alias: {
-      "@common": path.resolve(__dirname, "src/common"),
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@constants": path.resolve(__dirname, "src/constants"),
-      "@layout": path.resolve(__dirname, "src/layout"),
-      "@mocks": path.resolve(__dirname, "src/mocks"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@recoil": path.resolve(__dirname, "src/recoil"),
-      "@router": path.resolve(__dirname, "src/router"),
-      "@styles": path.resolve(__dirname, "src/styles"),
-      "@utils": path.resolve(__dirname, "src/utils"),
-      "@views": path.resolve(__dirname, "src/views"),
-      "@hooks": path.resolve(__dirname, "src/hooks"),
-      "@type": path.resolve(__dirname, "src/types"),
+      '@router': path.resolve(__dirname, 'src/router'),
+      '@recoil': path.resolve(__dirname, 'src/recoil'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@layout': path.resolve(__dirname, 'src/layout'),
+      '@views': path.resolve(__dirname, 'src/views'),
+      '@common': path.resolve(__dirname, 'src/common'),
+      '@mocks': path.resolve(__dirname, 'src/mocks'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@type': path.resolve(__dirname, 'src/types'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
     },
   },
 });
