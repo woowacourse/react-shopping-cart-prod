9fe1ce55e1b2934174631d47336785142654d989
Gilpop8663 <wolfye@naver.com>
2023-05-27 20:15:55 +0900

    refactor: url 상수를 서버에 따라 반환하는 함수로 변경

--- src/constants/urlConstants.tsx
+++ src/constants/urlConstants.tsx
@@ -1,3 +1,21 @@
-export const BASE_URL = 'https://h3rb.shop';
-export const PRODUCT_PATH = 'products';
-export const CART_PATH = 'cart-items';
+export const SERVER_NAME = ["마코", "허브", "우가"] as const;
+
+export const SERVER = {
+  [SERVER_NAME[0]]: "https://m4co.shop",
+  [SERVER_NAME[1]]: "https://h3rb.shop",
+  [SERVER_NAME[2]]: "https://wuga.shop",
+} as const;
+
+export type ServerName = (typeof SERVER_NAME)[number];
+
+export const getProductPath = (serverName: ServerName) => {
+  return `${SERVER[serverName]}/products`;
+};
+
+export const getCartPath = (serverName: ServerName) => {
+  return `${SERVER[serverName]}/cart-items`;
+};
+
+export const getCouponPath = (serverName: ServerName) => {
+  return `${SERVER[serverName]}/coupons`;
+};
