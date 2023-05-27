25d01417dbe2c67eb964bc089286eaf484a8f27d
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-05-27 12:50:24 +0900

    feat: msw worker 실행 중지

--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
-import { worker } from './mocks/browser';
 import CartPage from './pages/CartPage';
 import ProductPage from './pages/ProductPage';
 import GlobalStyle from './styles/GlobalStyle';
-
-const main = async () => {
-  if (window.location.pathname === '/react-shopping-cart') {
-    window.location.pathname = '/react-shopping-cart/';
-    return;
-  }
-
-  await worker.start({
-    serviceWorker: {
-      url: '/react-shopping-cart/mockServiceWorker.js',
-    },
-  });
-};
 
 const router = createBrowserRouter(
   [
@@ -50,5 +36,3 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-main();
