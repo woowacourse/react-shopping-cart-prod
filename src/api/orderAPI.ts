093b621ec1b56a9e7cfd06f82b263987d8aa1d09
ashleysyheo <ashleysyheo@gmail.com>
2023-05-28 01:47:57 +0900

    fix: 장바구니 상품 주문 POST 요청 함수 리턴 타입 수정

--- src/api/orderAPI.ts
+++ src/api/orderAPI.ts
@@ -1,11 +1,8 @@
 import { API_ENDPOINT, AUTHORIZED_FETCH_OPTION_HEADERS } from '../constants/api';
-import { OrderCartItemsData, ProductItemData } from '../types';
+import { OrderCartItemsData } from '../types';
 import { fetchAPI } from './fetchAPI';
 
-const postOrder = async (
-  baseUrl: string,
-  cartItems: OrderCartItemsData[]
-): Promise<ProductItemData[]> => {
+const postOrder = async (baseUrl: string, cartItems: OrderCartItemsData[]): Promise<Response> => {
   const data = {
     cartItems,
   };
