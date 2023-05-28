089bda16df73610a5bb5b4b2a32da6f1651f075e
solo5star <solo_5@naver.com>
2023-05-29 06:32:52 +0900

    feat: API_URL은 origin 없는 절대 경로도 허용

--- src/config/servers.ts
+++ src/config/servers.ts
@@ -1,3 +1,6 @@
+import { joinPath } from '../api/utils/http';
+import { BASE_URL } from './environment';
+
 const API_URLS: string = import.meta.env.VITE_API_URLS;
 
 export type Server = {
@@ -6,18 +9,23 @@ export type Server = {
 };
 
 const parseAPIUrl = (apiUrl: string): Server => {
-  const matches = /^(?:\[(?<name>[가-힣\w\d_-]+)\])?(?<base>https?:\/\/.+)$/.exec(apiUrl);
+  const matches = /^(?:\[(?<name>[가-힣\w\d_-]+)\])?(?<base>(?<protocol>https?:\/\/)?.+)$/.exec(
+    apiUrl,
+  );
   if (matches === null || matches.groups === undefined)
     throw new Error(
-      `"${apiUrl}"은 올바르지 않은 API_URL입니다. [name]http:// 혹은 [name]https:// 형식으로 입력해야 합니다.`,
+      `"${apiUrl}"은 올바르지 않은 API_URL입니다. "[name]주소" 형식으로 입력해야 합니다.`,
     );
 
-  const { name, base } = matches.groups;
+  const { name, base, protocol } = matches.groups;
   if (!base) {
     throw new Error(`"${base}"은 올바르지 않은 URL입니다.`);
   }
 
-  return { name, base };
+  return {
+    name,
+    base: protocol ? base : joinPath(BASE_URL, base),
+  };
 };
 
 const servers: Server[] = API_URLS.split(',').map(parseAPIUrl);
