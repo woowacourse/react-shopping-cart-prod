ae1d5d7c31a24b56d553e4961584a87550114b37
TaeeunKim <xodms0309@naver.com>
2023-05-28 23:41:08 +0900

    refactor: 토스트 메시지 컴포넌트를  가장 바깥으로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import { RouterProvider } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
 import SvgSprite from './components/@common/Svg/SvgSprite';
+import { useToast } from 'components/@common/Toast/hooks/useToast';
 import router from './router';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 
 function App() {
+  const { renderToast } = useToast();
+
   return (
-    <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <RouterProvider router={router} />
-        <SvgSprite />
-      </ThemeProvider>
-    </RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+      {renderToast}
+      <SvgSprite />
+    </ThemeProvider>
   );
 }
 
--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,14 +1,10 @@
 import ContentLayout from 'components/@common/ContentLayout';
 import ProductItemList from 'components/Product/ProductItemList';
-import { useToast } from 'components/@common/Toast/hooks/useToast';
 
 const ProductList = () => {
-  const { renderToast } = useToast();
-
   return (
     <ContentLayout>
       <ProductItemList />
-      {renderToast}
     </ContentLayout>
   );
 };
