e142facb90dbbf9fd2ca00a37bea15c6ade7489f
solo5star <solo_5@naver.com>
2023-05-29 00:42:41 +0900

    feat: 서버 주소 설정 규칙 수정

---
+++ src/config/servers.ts
@@ -0,0 +1,25 @@
+const API_URLS: string = import.meta.env.VITE_API_URLS;
+
+export type Server = {
+  name: string;
+  base: string;
+};
+
+const parseAPIUrl = (apiUrl: string): Server => {
+  const matches = /^(?:\[(?<name>[가-힣\w\d_-]+)\])?(?<base>http?s:\/\/.+)$/.exec(apiUrl);
+  if (matches === null || matches.groups === undefined)
+    throw new Error(
+      `"${apiUrl}"은 올바르지 않은 API_URL입니다. [name]http:// 혹은 [name]https:// 형식으로 입력해야 합니다.`,
+    );
+
+  const { name, base } = matches.groups;
+  if (!base) {
+    throw new Error(`"${base}"은 올바르지 않은 URL입니다.`);
+  }
+
+  return { name, base };
+};
+
+const servers: Server[] = API_URLS.split(',').map(parseAPIUrl);
+
+export default servers;
