a2fc80f323ceff233955e62166d6f2d2452b4430
TaeeunKim <xodms0309@naver.com>
2023-05-28 23:30:09 +0900

    fix: 장바구니 수량을 바꾼 뒤 서버를 바꾸면 동기화되지 않는 오류 해결

--- src/components/@common/SelectSever/index.tsx
+++ src/components/@common/SelectSever/index.tsx
@@ -1,16 +1,32 @@
-import { useRecoilState } from 'recoil';
+import { useRecoilCallback, useRecoilValue } from 'recoil';
 import { serverAtom } from 'recoil/server';
+import { cartListAtom } from 'recoil/cartList';
 import { SERVERS } from 'utils/constants';
 import { ServerName } from 'types';
 import * as S from './SelectServer.styles';
+import { getCartList } from 'api/requests';
+import { useToast } from '../Toast/hooks/useToast';
 
 const SelectServer = () => {
-  const [server, setServer] = useRecoilState(serverAtom);
+  const server = useRecoilValue(serverAtom);
+  const { toast } = useToast();
 
-  const onChangeServer: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
-    const selectedServer = e.target.value as ServerName;
-    setServer(SERVERS[selectedServer]);
-  };
+  const onChangeServer: React.ChangeEventHandler<HTMLSelectElement> =
+    useRecoilCallback(
+      ({ set }) =>
+        async (event) => {
+          const serverName = event.target.value as ServerName;
+          set(serverAtom, SERVERS[serverName]);
+          try {
+            const cartList = await getCartList(SERVERS[serverName]);
+            set(cartListAtom, cartList);
+          } catch (error) {
+            if (!(error instanceof Error)) return;
+            toast.error('장바구니 목록을 가져오는데 실패했습니다.');
+          }
+        },
+      []
+    );
 
   return (
     <S.SelectBox onChange={onChangeServer}>
