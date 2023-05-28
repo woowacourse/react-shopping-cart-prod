59025231ea0840688b50cc3115df980d5754169d
solo5star <solo_5@naver.com>
2023-05-29 05:35:09 +0900

    fix: API_URL 잘못된 정규식 수정

--- src/config/servers.ts
+++ src/config/servers.ts
@@ -6,7 +6,7 @@ export type Server = {
 };
 
 const parseAPIUrl = (apiUrl: string): Server => {
-  const matches = /^(?:\[(?<name>[가-힣\w\d_-]+)\])?(?<base>http?s:\/\/.+)$/.exec(apiUrl);
+  const matches = /^(?:\[(?<name>[가-힣\w\d_-]+)\])?(?<base>https?:\/\/.+)$/.exec(apiUrl);
   if (matches === null || matches.groups === undefined)
     throw new Error(
       `"${apiUrl}"은 올바르지 않은 API_URL입니다. [name]http:// 혹은 [name]https:// 형식으로 입력해야 합니다.`,
