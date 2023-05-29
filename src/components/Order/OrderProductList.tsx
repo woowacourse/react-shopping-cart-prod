9ced673f8d8215a0122dc040b62caabba4570f83
hae-on <solbi2004@naver.com>
2023-05-29 17:26:36 +0900

    feat: OrderProductList 컴포넌트 구현

--- src/components/Order/OrderProductList.tsx
+++ src/components/Order/OrderProductList.tsx
@@ -1,23 +1,58 @@
+import styled from 'styled-components';
 import OrderProductItem from './OrderProductItem';
-import type { OrderProduct } from '../../types/product';
+import type { OrderDetails } from '../../types/product';
 
 interface OrderProductListProps {
-  orderProducts: OrderProduct[];
+  orderProducts: OrderDetails;
 }
 
 const OrderProductList = ({ orderProducts }: OrderProductListProps) => {
+  const { orderId, orderDateTime } = orderProducts;
+  const orderItems = orderProducts.orderItems;
+
   return (
-    <ul>
-      {orderProducts.map((orderProduct, index) => (
+    <OrderProductContainer>
+      <OrderProductListHeader>
+        <div>
+          <OrderId>주문번호 : {orderId}</OrderId>
+          <span>{orderDateTime}</span>
+        </div>
+        <OrderDetailsLink>상세보기 {'>'}</OrderDetailsLink>
+      </OrderProductListHeader>
+      {orderItems.map((orderItem, index) => (
         <li>
           <OrderProductItem
             key={`orderProducts-${index}`}
-            orderProduct={orderProduct}
+            orderProduct={orderItem}
           />
         </li>
       ))}
-    </ul>
+    </OrderProductContainer>
   );
 };
+
+const OrderProductContainer = styled.ul`
+  width: 1100px;
+  margin: 50px 0;
+  border: 1px solid ${({ theme }) => theme.colors.gray400};
+`;
+
+const OrderProductListHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 92px;
+  padding: 36px 39px;
+  background-color: ${({ theme }) => theme.colors.gray200};
+  font-size: 20px;
+`;
+
+const OrderId = styled.span`
+  display: inline-block;
+  margin-right: 50px;
+`;
+
+const OrderDetailsLink = styled.p`
+  cursor: pointer;
+`;
 
 export default OrderProductList;
