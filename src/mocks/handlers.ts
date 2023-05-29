b304aca00475b9619216ab491de880d625428c28
turtle601 <turtle601@naver.com>
2023-05-29 18:49:08 +0900

    feat(mock): mock 핸들러 추가(주문목록 리스트, 아이템)

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,6 +2,7 @@ import { rest } from 'msw';
 
 import { CART_LIST_LOCAL_STORAGE_KEY } from '../constants';
 import initialData from '../data/mockData.json';
+import initialOrderData from '../data/mockOrderData.json';
 import { CartItemType } from '../types';
 
 interface PostCartItemId {
@@ -13,6 +14,34 @@ const storeKey = CART_LIST_LOCAL_STORAGE_KEY;
 export const handlers = [
   rest.get('/products', (req, res, ctx) => {
     return res(ctx.status(200), ctx.delay(1500), ctx.json(initialData));
+  }),
+
+  rest.get('/order-items', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.delay(1500), ctx.json(initialOrderData));
+  }),
+
+  rest.get('/order-items/:id', (req, res, ctx) => {
+    const { id } = req.params;
+
+    const result = initialOrderData.find((order) => order.id === Number(id));
+
+    if (!result)
+      res(ctx.status(400), ctx.delay(1500), ctx.json('해당 주문 상품이 존재하지 않습니다.'));
+
+    return res(ctx.status(200), ctx.delay(1500), ctx.json(result));
+  }),
+
+  rest.get('/cart-items', (req, res, ctx) => {
+    const savedValue = localStorage.getItem(storeKey);
+
+    if (savedValue !== null) {
+      return res(ctx.status(200), ctx.delay(1000), ctx.json(JSON.parse(savedValue)));
+    }
+    return res(
+      ctx.status(500),
+      ctx.delay(1000),
+      ctx.json([{ error: '카트 목록이 존재하지 않습니다.' }])
+    );
   }),
 
   rest.get('/cart-items', (req, res, ctx) => {
