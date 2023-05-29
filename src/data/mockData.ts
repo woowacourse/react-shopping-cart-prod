ce2736bdfb04751ad0467dc60666715db01c6f71
hozzijeong <ghwls0905@naver.com>
2023-05-29 20:18:34 +0900

    chore: 주문 목록 mock데이터 추가

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -1,6 +1,6 @@
 import { LOCAL_STORAGE_KEY } from '../constants';
 import { getLocalStorage } from '../utils/localStorage';
-import type { CartItem, Product } from '../types';
+import type { CartItem, Order, Product } from '../types';
 
 export const cartItems: CartItem[] = getLocalStorage<CartItem[]>(LOCAL_STORAGE_KEY.CART_ITEM, []);
 
@@ -76,5 +76,63 @@ export const products: Product[] = [
     name: 'PET보틀-납작(260ml)',
     price: 64200,
     imageUrl: 'https://cdn-mart.baemin.com/sellergoods/main/d07bec18-ce84-41c2-8903-61cbd10712b6.jpg?h=300&w=300',
+  },
+];
+
+export const orderList: Order[] = [
+  {
+    id: 1,
+    orderTime: '2023-05-25T18:25:43.511Z',
+    productList: [
+      {
+        name: '차',
+        totalPrice: 190000,
+        quantity: 3,
+        imageUrl: '',
+      },
+      {
+        name: '자동차',
+        totalPrice: 210000,
+        quantity: 4,
+        imageUrl: '',
+      },
+    ],
+  },
+  {
+    id: 2,
+    orderTime: '2023-05-26T18:25:43.511Z',
+    productList: [
+      {
+        name: '차',
+        totalPrice: 190000,
+        quantity: 3,
+        imageUrl: '',
+      },
+      {
+        name: '자동차',
+        totalPrice: 210000,
+        quantity: 4,
+        imageUrl: '',
+      },
+    ],
+  },
+  {
+    id: 3,
+    orderTime: '2023-05-26T18:25:43.511Z',
+    productList: [
+      {
+        name: '뽀로로 튼튼한 성장기 어린이 음료 235mL',
+        totalPrice: 2550,
+        quantity: 3,
+        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrv1JEzSxNjrQgR2VcpDw5wUJV4_RiJEwRb-gn2-Q&s',
+      },
+      {
+        name: '데자와 민트초코 밀크티 ',
+        totalPrice: 50000,
+        quantity: 4,
+        imageUrl:
+          'https://image-cdn.hypb.st/https%3A%2F%2Fkr.hypebeast.com%2Ffiles%2F2020%2F11%2Ftejava-mint-chocolate-milk-tea-2020-release-info-1.jpg?q=90&w=1400&cbr=1&fit=max',
+      },
+    ],
   },
 ];
