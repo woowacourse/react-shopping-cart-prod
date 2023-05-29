e2586e2421d03ba49e81d6383269309df023ce8f
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-05-29 10:43:09 +0900

    refactor: useFetch 리팩터링

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -3,61 +3,47 @@ import { baseURLSelector } from '../store/server';
 import { AUTH } from '../constants/auth';
 import { fetchedCartListSelector } from '../store/asyncSelector';
 
+type FetchMethod = 'GET' | 'POST' | 'PATCH' | 'DELETE';
+
 const useFetch = () => {
   const baseURL = useRecoilValue(baseURLSelector);
   const refreshCartList = useRecoilRefresher_UNSTABLE(fetchedCartListSelector);
 
-  const addToCart = async (productId: number) => {
+  const handleCartItems = async (
+    method: FetchMethod,
+    body: {},
+    id?: number
+  ) => {
     try {
-      const response = await fetch(`${baseURL}/cart-items`, {
-        method: 'POST',
-        body: JSON.stringify({ productId }),
-        headers: {
-          'Content-Type': 'application/json',
-          authorization: `Basic ${AUTH}`,
-        },
-      });
+      const response = await fetch(
+        `${baseURL}/cart-items${id ? `/${id}` : ''}`,
+        {
+          method,
+          body: JSON.stringify(body),
+          headers: {
+            'Content-Type': 'application/json',
+            authorization: `Basic ${AUTH}`,
+          },
+        }
+      );
 
       if (!response.ok) throw new Error(`error code : ${response.status}`);
       refreshCartList();
     } catch (error) {
       alert(error);
     }
+  };
+
+  const addToCart = async (productId: number) => {
+    handleCartItems('POST', { productId });
   };
 
   const updateCartItem = async (id: number, quantity: number) => {
-    try {
-      const response = await fetch(`${baseURL}/cart-items/${id}`, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-          authorization: `Basic ${AUTH}`,
-        },
-        body: JSON.stringify({ quantity }),
-      });
-
-      if (!response.ok) throw new Error(`error code : ${response.status}`);
-      refreshCartList();
-    } catch (error) {
-      alert(error);
-    }
+    handleCartItems('PATCH', { quantity }, id);
   };
 
   const deleteCartItem = async (id: number) => {
-    try {
-      const response = await fetch(`${baseURL}/cart-items/${id}`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-          authorization: `Basic ${AUTH}`,
-        },
-      });
-
-      if (!response.ok) throw new Error(`error code : ${response.status}`);
-      refreshCartList();
-    } catch (error) {
-      alert(error);
-    }
+    handleCartItems('DELETE', {}, id);
   };
 
   return { addToCart, updateCartItem, deleteCartItem };
