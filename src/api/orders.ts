1def1e9e9ea78d1d22179c4218e9d6ffa255876a
chsua <csj1919@naver.com>
2023-05-29 15:50:23 +0900

    feat: 주문 명세 목록을 가지고 오는 fetch 구현

--- src/api/orders.ts
+++ src/api/orders.ts
@@ -1,21 +1,31 @@
 //실제 api와 연결할때 사용
 // import { ServerId } from "recoil/server";
 
-import { Order } from "types/domain";
+import { Order, EachOrderStatement } from "types/domain";
 
 // import { SERVER_LIST, USER_TOKEN } from "./constants";
 
-export const addOrder = async (orderList: Order[]): Promise<boolean> => {
+export const addOrder = async (orders: Order[]): Promise<boolean> => {
   const response = await fetch(`/orders`, {
     method: "POST",
     // headers: {
     //   Authorization: `Basic ${USER_TOKEN}`,
     //   "Content-Type": "application/json",
     // },
-    body: JSON.stringify(orderList),
+    body: JSON.stringify(orders),
   });
 
-  console.log(orderList);
-
   return response.status === 204;
+};
+
+export const getOrderStatement = async (): Promise<EachOrderStatement[]> => {
+  const response = await fetch(`/orders`, {
+    method: "GET",
+    // headers: {
+    //   Authorization: `Basic ${USER_TOKEN}`,
+    //   "Content-Type": "application/json",
+    // },
+  });
+
+  return response.status === 200 && response.json();
 };
