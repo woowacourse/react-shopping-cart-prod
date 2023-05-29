452abd6c55d4fe54bab55eadfd8000787ad01909
Dain Lee <feb.dain@gmail.com>
2023-05-29 13:09:42 +0900

    fix: 자동 배포 오류 개선
    `App`에서 안 쓰는 import 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import { ROUTE_PATH } from './constants';
 import CartPage from './pages/CartPage';
