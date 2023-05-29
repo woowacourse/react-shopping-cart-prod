07caf365c6892cb945440ce2e32e193f9d73e094
gilpop8663 <wolfye@naver.com>
2023-05-30 08:06:52 +0900

    feat: ErrorBoundary 임시 적용

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,15 @@
 import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import ProductList from '@components/home/ProductItemList';
 import SkeletonProduct from '@components/home/SkeletonProduct';
 
 function Home() {
   return (
-    <Suspense fallback={<SkeletonProduct />}>
-      <ProductList />
-    </Suspense>
+    <ErrorBoundary fallback={<p>에러입니다</p>}>
+      <Suspense fallback={<SkeletonProduct />}>
+        <ProductList />
+      </Suspense>
+    </ErrorBoundary>
   );
 }
 
