0d3ff702091cea80ab0383764de1b6efed84a751
Dain Lee <feb.dain@gmail.com>
2023-05-29 21:02:36 +0900

    feat: `OrderPage` 생성

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import { ROUTE_PATH } from './constants';
 import CartPage from './pages/CartPage';
 import GotLost from './pages/GotLost';
 import MainPage from './pages/MainPage';
+import OrderPage from './pages/OrderPage';
 
-const { MAIN_PAGE, CART_PAGE, ERROR_PAGE } = ROUTE_PATH;
+const { MAIN_PAGE, CART_PAGE, ERROR_PAGE, ORDER_LIST_PAGE } = ROUTE_PATH;
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route element={<Layout />}>
           <Route path={MAIN_PAGE} element={<MainPage />} />
           <Route path={CART_PAGE} element={<CartPage />} />
+          <Route path={ORDER_LIST_PAGE} element={<OrderPage />} />
           <Route path={ERROR_PAGE} element={<GotLost />} />
           <Route path='*' element={<Navigate replace to={ERROR_PAGE} />} />
         </Route>
