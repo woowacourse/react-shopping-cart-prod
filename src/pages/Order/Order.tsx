a5a7f1c9995c9d9556da19c62d565f2d9e183897
nangkyeonglim <nangkyeonglim@gmail.com>
2023-05-29 22:18:34 +0900

    feat: Order 페이지 구현

--- src/pages/Order/Order.tsx
+++ src/pages/Order/Order.tsx
@@ -1,5 +1,14 @@
+import { OrderList } from '../../components/OrderList';
+import { FatBorder, PageTitle } from '../../style/style';
+
 function Order() {
-  return <>주문목록</>;
+  return (
+    <>
+      <PageTitle>주문목록</PageTitle>
+      <FatBorder />
+      <OrderList />
+    </>
+  );
 }
 
 export default Order;
