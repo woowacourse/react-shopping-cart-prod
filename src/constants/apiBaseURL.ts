c577496ef044a591cb968cb562e43e8b7a66bcf1
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-29 20:32:36 +0900

    refactor: api baseUrl 정보 env 파일로 관리

--- src/constants/apiBaseURL.ts
+++ src/constants/apiBaseURL.ts
@@ -1,7 +1,7 @@
 const BASE_URL = {
-  헙크: 'https://hubcreator.shop',
-  다즐: 'https://dazzlebv.com',
-  베로: 'https://shop.vero.wiki',
+  헙크: `${process.env.REACT_APP_BASE_URL_HUBC}`,
+  다즐: `${process.env.REACT_APP_BASE_URL_DAZZLE}`,
+  베로: `${process.env.REACT_APP_BASE_URL_VERO}`,
 } as const;
 
 export default BASE_URL;
