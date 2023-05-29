6f64e5bf467cda9f22ba79b9641161cb9bd406be
ashleysyheo <ashleysyheo@gmail.com>
2023-05-29 23:41:09 +0900

    fix: Checkbox 컴포넌트 input 태그에 checked 고정 값 적은 부분 수정

--- src/components/common/Checkbox/Checkbox.tsx
+++ src/components/common/Checkbox/Checkbox.tsx
@@ -3,12 +3,12 @@ import { ComponentPropsWithoutRef } from 'react';
 import { CheckedIcon, UncheckedIcon } from '../../../assets/svg';
 import * as S from './Checkbox.styles';
 
-interface CheckboxProps extends ComponentPropsWithoutRef<'input'> {}
+type CheckboxProps = ComponentPropsWithoutRef<'input'>;
 
 const Checkbox = ({ id, checked, ...attributes }: CheckboxProps) => {
   return (
     <label htmlFor={id}>
-      <S.CheckboxInput id={id} checked {...attributes} />
+      <S.CheckboxInput id={id} checked={checked} {...attributes} />
       <S.CheckboxIconWrapper aria-label={id}>
         {checked ? <CheckedIcon /> : <UncheckedIcon />}
       </S.CheckboxIconWrapper>
