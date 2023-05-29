80305fa8c7a2e5ac7e337ec7916e81f06c236ba3
unknown <tkdrb15@gmail.com>
2023-05-29 21:10:55 +0900

    feat: OrderList 컴포넌트 추가

---
+++ src/pages/OrderList/style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Title = styled.h2`
+  font-weight: 700;
+  font-size: 32px;
+  text-align: center;
+  line-height: 37px;
+  color: #333333;
+
+  border-bottom: 2px solid #333333;
+  padding-bottom: 30px;
+`;
