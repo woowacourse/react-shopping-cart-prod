c88ab595f0c6cb020b3f0a40947676cace9d7821
hae-on <solbi2004@naver.com>
2023-05-29 21:06:59 +0900

    chore: 사용하지 않는 import 삭제

--- src/pages/CartProductsListPage.tsx
+++ src/pages/CartProductsListPage.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useState } from 'react';
+import { Suspense } from 'react';
 import styled from 'styled-components';
 
 import EstimatedPaymentBox from '../components/Cart/EstimatedPaymentBox';
