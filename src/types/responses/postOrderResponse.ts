cf83205f139230e73cf1d327ff6de9f0127f771b
sh981013s <sh981013s@gmail.com>
2023-05-30 00:13:57 +0900

    feat: OrderResponse type 선언

---
+++ src/types/responses/postOrderResponse.ts
@@ -0,0 +1,20 @@
+type Product = {
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+};
+
+type Item = {
+  product: Product;
+  quantity: number;
+};
+
+export type OrderResponse = {
+  orderId: number;
+  items: Item[];
+  productPrice: number;
+  discountPrice: number;
+  deliveryFee: number;
+  totalPrice: number;
+};
