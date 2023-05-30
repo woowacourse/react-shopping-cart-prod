d9d736221d6933405b6dfb6678286580fc162a07
ashleysyheo <ashleysyheo@gmail.com>
2023-05-30 15:55:22 +0900

    refactor: CartList 컴포넌트 스토리 mockCartListData 수정

--- src/stories/cart/CartList.stories.tsx
+++ src/stories/cart/CartList.stories.tsx
@@ -18,33 +18,39 @@ const mockCartListData = [
     id: 1684161725526,
     quantity: 7,
     product: {
-      id: 2,
-      name: '올인원 세트-물티수저',
-      price: 57600,
+      id: 1,
+      name: '종이용기(1100cc)-단골이 됐으면 좋겠다',
+      price: 61100,
+      discountRate: 0,
+      discountedPrice: 61100,
       imageUrl:
-        'https://cdn-mart.baemin.com/sellergoods/list/dcee7c3b-230f-482b-a549-ee0ee678222e.jpg?h=400&w=400',
+        'https://cdn-mart.baemin.com/sellergoods/list/cba783a5-b35d-4b9d-b58e-56801594351f.jpg?h=400&w=400',
     },
   },
   {
     id: 1684161727333,
     quantity: 1,
     product: {
-      id: 3,
-      name: '종이용기(900cc)-너무맛있겠다',
-      price: 46200,
+      id: 2,
+      name: '올인원 세트-물티수저',
+      price: 57600,
+      discountRate: 0,
+      discountedPrice: 57600,
       imageUrl:
-        'https://cdn-mart.baemin.com/sellergoods/list/4ece565d-7fbf-4af4-b4a0-e545c15eda10.jpg?h=400&w=400',
+        'https://cdn-mart.baemin.com/sellergoods/list/dcee7c3b-230f-482b-a549-ee0ee678222e.jpg?h=400&w=400',
     },
   },
   {
     id: 1684161728982,
     quantity: 1,
     product: {
-      id: 8,
-      name: '아이스팩-얼음',
-      price: 24400,
+      id: 3,
+      name: '종이용기(900cc)-너무맛있겠다',
+      price: 60000,
+      discountRate: 5,
+      discountedPrice: 57000,
       imageUrl:
-        'https://cdn-mart.baemin.com/sellergoods/list/d27ec68b-4ba0-442d-8233-e844edd794a1.jpg?h=400&w=400',
+        'https://cdn-mart.baemin.com/sellergoods/list/4ece565d-7fbf-4af4-b4a0-e545c15eda10.jpg?h=400&w=400',
     },
   },
 ];
@@ -53,6 +59,7 @@ export const Default: Story = {
   decorators: [
     (Story) => {
       saveToLocalStorage(CART_LIST_LOCAL_STORAGE_KEY, mockCartListData);
+
       return <Story />;
     },
   ],
