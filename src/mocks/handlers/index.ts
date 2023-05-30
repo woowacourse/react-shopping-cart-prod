01a5a8fc70061d35c7ab76c3cff90f3d986dc157
solo5star <solo_5@naver.com>
2023-05-30 15:15:49 +0900

    test: msw 핸들러 등록

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,6 +1,15 @@
 import { handlers as cartItemsHandlers } from './cart-items';
+import { handlers as cartPointsHandlers } from './cart-points';
+import { handlers as ordersHandlers } from './orders';
 import { handlers as productsHandlers } from './products';
+import { handlers as profileHandlers } from './profile';
 
-const handlers = [...productsHandlers, ...cartItemsHandlers];
+const handlers = [
+  ...profileHandlers,
+  ...ordersHandlers,
+  ...cartPointsHandlers,
+  ...productsHandlers,
+  ...cartItemsHandlers,
+];
 
 export default handlers;
