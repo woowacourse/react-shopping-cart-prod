5cac5ce921a669b50c6f0bee02852633c9ceb84e
inyeong-kang <tomatorkd@naver.com>
2023-05-30 12:04:01 +0900

    feat: API 명세서에 맞게 상품, 장바구니 상품, 주문 타입 반영, mock 데이터 반영

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,12 +1,27 @@
 export interface Product {
-  id: number;
+  productId: number;
   name: string;
   price: number;
   imageUrl: string;
+  stock: number;
 }
 
 export interface CartProduct {
-  id: number;
+  cartItemId: number;
   quantity: number;
   product: Product;
+}
+
+export interface Cart {
+  cartItems: CartProduct[];
+  userPoint: number;
+  minUsagePoints: number;
+}
+
+export type OrderProduct = Omit<CartProduct, 'cartItemId'>;
+export interface Order {
+  orderId: number;
+  orderDateTime: string;
+  orderItems: OrderProduct[];
+  totalPrice: number;
 }
