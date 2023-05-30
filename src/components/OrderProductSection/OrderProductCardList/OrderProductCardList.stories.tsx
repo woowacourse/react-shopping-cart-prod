8c7863aeea85169c5cd0cc6f1dc071947059e8b5
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-30 14:47:42 +0900

    test(OrderProductCardList): 스토리 레이아웃 추가

--- src/components/OrderProductSection/OrderProductCardList/OrderProductCardList.stories.tsx
+++ src/components/OrderProductSection/OrderProductCardList/OrderProductCardList.stories.tsx
@@ -1,9 +1,22 @@
 import { Meta, StoryObj } from '@storybook/react';
 import OrderProductCardList from './OrderProductCardList';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  position: relative;
+  margin-top: 80px;
+`;
 
 const meta = {
   component: OrderProductCardList,
   title: 'OrderProductCardList',
+  decorators: [
+    (Story) => (
+      <Container>
+        <Story />
+      </Container>
+    ),
+  ],
 } satisfies Meta<typeof OrderProductCardList>;
 
 export default meta;
