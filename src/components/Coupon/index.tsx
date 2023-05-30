663de508540d3b741f6301abebd67099816c2e0c
nlom0218 <nlom0218@naver.com>
2023-05-30 21:42:18 +0900

    feat: 사용할 쿠폰 선택하기

--- src/components/Coupon/index.tsx
+++ src/components/Coupon/index.tsx
@@ -1,8 +1,14 @@
+import { useModal } from 'noah-modal';
+import { useRecoilState } from 'recoil';
+
 import { CouponType } from '@Types/index';
+
+import selectedCouponIdState from '@Atoms/selectedCouponIdState';
 
 import * as S from './style';
 
 function Coupon({
+  id,
   name,
   description,
   isUsed = false,
@@ -10,8 +16,20 @@ function Coupon({
   type,
   isLoading = false,
 }: Partial<CouponType> & Partial<{ subMessage: string; type: 'issued' | 'use'; isLoading: boolean }>) {
-  const couponButton = type === 'issued' ? '⬇︎' : '➡︎';
+  const { closeModal } = useModal();
 
+  const [selectedCouponId, setSelectedCouponId] = useRecoilState(selectedCouponIdState);
+  console.log(selectedCouponId);
+
+  const IssuedOrUseCoupon = () => {
+    if (!id) return;
+    if (type === 'issued') return;
+
+    setSelectedCouponId(id);
+    closeModal('myCoupon');
+  };
+
+  const couponButton = type === 'issued' ? '⬇︎' : selectedCouponId === id ? '✓' : '➡︎';
   return (
     <S.Container isUsed={isUsed} isLoading={isLoading}>
       <S.CouponLayout>
@@ -19,7 +37,9 @@ function Coupon({
         <S.CouponName>{name}</S.CouponName>
         <S.CouponSubMessage>{subMessage}</S.CouponSubMessage>
       </S.CouponLayout>
-      <S.CouponButton isLoading={isLoading}>{couponButton}</S.CouponButton>
+      <S.CouponButton isLoading={isLoading} onClick={IssuedOrUseCoupon}>
+        {couponButton}
+      </S.CouponButton>
     </S.Container>
   );
 }
