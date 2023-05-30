f54e972e61150c0ea5031b6a73db8f323ffe734b
hafnium1923 <hafnium1923@gmail.com>
2023-05-30 13:52:41 +0900

    refactor: 장바구니 애니메이션 삭제

--- src/components/common/header/UserCartInfo.tsx
+++ src/components/common/header/UserCartInfo.tsx
@@ -1,7 +1,6 @@
 import styled from '@emotion/styled';
 import { Text } from '../Text/Text';
 import { useEffect, useRef, useState } from 'react';
-import { gsap } from 'gsap';
 import { Link } from 'react-router-dom';
 import { useCartFetch } from '../../../hooks/useCartFetch';
 
@@ -26,50 +25,33 @@ const UserCartInfo = () => {
       return;
     }
     if (cartTotalQuantity === 0 && isShown) {
-      setTimeout(() => {
-        setIsShown(false);
-      }, 200);
+      setIsShown(false);
     }
   }, [cartTotalQuantity]);
 
-  useEffect(() => {
-    if (cartTotalQuantity > 0 && !isShown) {
-      gsap.fromTo(
-        ref.current,
-        { opacity: 0, transform: 'translateX(-10px)', delay: 0.3, ease: 'ease' },
-        { opacity: 1, transform: 'translateX(0)' },
-      );
-    }
-    if (cartTotalQuantity === 0) {
-      gsap.fromTo(
-        ref.current,
-        { opacity: 1, transform: 'translateX(0)', delay: 0.3, ease: 'ease' },
-        { opacity: 0, transform: 'translateX(-10px)' },
-      );
-    }
-  }, [cartTotalQuantity, isShown]);
-
   return (
-    <CardCounterWrapper ref={ref} to="/cart">
-      {isShown && (
+    <CartCounterWrapper ref={ref} to="/cart">
+      <Text color="#ffffff" size="large" lineHeight="12px">
+        의 장바구니
+      </Text>
+      {isShown ? (
         <>
-          <Text color="#ffffff" size="large" lineHeight="12px">
-            의 장바구니
-          </Text>
           <CartCounter>
             <Text size="smallest" color="#ffffff">
               {cartTotalQuantity && cartTotalQuantity > 99 ? 99 : cartTotalQuantity}
             </Text>
           </CartCounter>
         </>
+      ) : (
+        <Wrapper />
       )}
-    </CardCounterWrapper>
+    </CartCounterWrapper>
   );
 };
 
 export default UserCartInfo;
 
-const CardCounterWrapper = styled(Link)`
+const CartCounterWrapper = styled(Link)`
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -82,4 +64,10 @@ const CartCounter = styled.div`
   border-radius: 100px;
   margin-left: 6px;
   text-align: center;
+`;
+
+const Wrapper = styled.div`
+  width: 26px;
+  height: 26px;
+  margin-left: 6px;
 `;
