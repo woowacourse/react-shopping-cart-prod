569a50a634f8273497b0e89abfbcf2e566e02fb3
nlom0218 <nlom0218@naver.com>
2023-05-30 16:05:20 +0900

    test: UsedCoupon 스토리 작성

--- src/components/Coupon/Coupon.stories.tsx
+++ src/components/Coupon/Coupon.stories.tsx
@@ -14,9 +14,24 @@ export default meta;
 
 type Story = StoryObj<typeof Coupon>;
 
+/**
+ * `DefaultCoupon`은 쿠폰의 기본적인 스토리입니다.
+ */
 export const DefaultCoupon: Story = {
   args: {
-    name: '3000원 할인',
+    name: '3,000원 할인',
+    description: '오픈맞이 특별 할인',
+    isUsed: false,
+    subMessage: '쿠폰으로 할인 받고 상품 구매하기',
+  },
+};
+
+/**
+ * `UsedCoupon`은 사용한 가격에 대한 스토리입니다.
+ */
+export const UsedCoupon: Story = {
+  args: {
+    name: '3,000원 할인',
     description: '오픈맞이 특별 할인',
     isUsed: true,
     subMessage: '쿠폰으로 할인 받고 상품 구매하기',
