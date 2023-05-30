caf9b12c3c5c307b5dafce812965ee4ca8d47fbb
ashleysyheo <ashleysyheo@gmail.com>
2023-05-30 13:52:34 +0900

    feat: 주문 내역이 없을 때 주문 내역이 없다는 메세지 보여주기

--- src/components/order/OrderList/OrderList.styles.ts
+++ src/components/order/OrderList/OrderList.styles.ts
@@ -1,15 +1,48 @@
 import styled from 'styled-components';
 
 import { SpinnerContainer } from '../../common/SpinnerContainer/SpinnerContainer.styles';
+import { Text } from '../../common/Text/Text.styles';
 
 const OrderListContainer = styled.ol`
   display: flex;
   flex-direction: column;
   gap: ${({ theme }) => theme.spacer.spacing6};
+
+  &.center {
+    position: relative;
+    top: calc(50% - 48px);
+    margin: 0;
+    padding: 72px 24px;
+    justify-content: center;
+    align-items: center;
+    gap: initial;
+    transform: translateY(-50%);
+
+    & button {
+      width: 200px;
+    }
+  }
+`;
+
+const OrderListEmptyImage = styled.img`
+  width: 150px;
+  height: 150px;
+  margin-bottom: ${({ theme }) => theme.spacer.spacing3};
+`;
+
+const OrderListEmptyMessage = styled(Text)`
+  margin-bottom: ${({ theme }) => theme.spacer.spacing4};
+  font-weight: 600;
+  text-align: center;
 `;
 
 const OrderListFallbackSpinnerContainer = styled(SpinnerContainer)`
   top: calc(50% - 48px);
 `;
 
-export { OrderListContainer, OrderListFallbackSpinnerContainer };
+export {
+  OrderListContainer,
+  OrderListEmptyImage,
+  OrderListEmptyMessage,
+  OrderListFallbackSpinnerContainer,
+};
--- src/components/order/OrderList/OrderList.tsx
+++ src/components/order/OrderList/OrderList.tsx
@@ -1,11 +1,28 @@
+import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 
+import EmptyImage from '../../../assets/png/empty-image.png';
+import { PATH } from '../../../constants/path';
 import { orderListState } from '../../../store/order';
+import Button from '../../common/Button/Button';
 import OrderItem from '../OrderItem/OrderItem';
 import * as S from './OrderList.styles';
 
 const OrderList = () => {
   const orderList = useRecoilValue(orderListState);
+  const navigate = useNavigate();
+
+  if (orderList.length === 0) {
+    return (
+      <S.OrderListContainer className="center">
+        <S.OrderListEmptyImage src={EmptyImage} alt="empty" />
+        <S.OrderListEmptyMessage size="large">주문내역이 없습니다.</S.OrderListEmptyMessage>
+        <Button variant="primary" onClick={() => navigate(PATH.ROOT)}>
+          홈으로 이동하기
+        </Button>
+      </S.OrderListContainer>
+    );
+  }
 
   return (
     <S.OrderListContainer>
