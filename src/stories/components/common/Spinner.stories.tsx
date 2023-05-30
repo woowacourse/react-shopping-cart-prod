42039cdc18cb46b28a68b73dcc452c2d22a4b2f6
Dain Lee <feb.dain@gmail.com>
2023-05-30 20:38:07 +0900

    refactor: 필요 없는 코드/파일 제거

--- src/stories/components/common/Spinner.stories.tsx
+++ src/stories/components/common/Spinner.stories.tsx
@@ -4,13 +4,8 @@ import SpinnerComponent from '../../../components/common/Spinner';
 const meta = {
   component: SpinnerComponent,
   title: 'Components/Spinner',
-  tags: ['autodocs'],
 } satisfies Meta<typeof SpinnerComponent>;
 
 export default meta;
 
-export const Spinner = () => (
-  <div style={{ margin: '80px' }}>
-    <SpinnerComponent />
-  </div>
-);
+export const Spinner = () => <SpinnerComponent />;
