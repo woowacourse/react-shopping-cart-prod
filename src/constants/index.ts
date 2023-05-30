5a6ad59b5b2d8366836fbaf463ac42deba7131aa
regularPark <srk99279@naver.com>
2023-05-30 15:13:34 +0900

    feat: 사용자 인증 정보 `constants`에 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,3 +5,7 @@ export const CART_ITEM_INDEX = 0;
 export const ONE_ITEM_IN_CART = 1;
 
 export const DELIVERY_FEE = 3000;
+
+export const email = 'a@a.com';
+export const password = '1234';
+export const base64 = btoa(`${email}:${password}`);
