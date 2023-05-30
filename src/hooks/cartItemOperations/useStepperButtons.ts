e260494f6427ba78195f19c932e4e74b686e0f85
sh981013s <sh981013s@gmail.com>
2023-05-30 21:47:02 +0900

    feat: stepperButton handling hook 고도화

--- src/hooks/cartItemOperations/useStepperButtons.ts
+++ src/hooks/cartItemOperations/useStepperButtons.ts
@@ -1,6 +1,8 @@
 import { useCallback } from 'react';
 import useCart from '../useCart.ts';
 import usePostUpdateCartItem from '../requests/usePostUpdateCartItem.ts';
+import { useToast } from '../useToast.ts';
+import toastMessages from '../../constants/toastMessages.ts';
 
 type StpperButtonsHookProps = {
   cartItemNumber: number | undefined;
@@ -14,19 +16,22 @@ type StpperButtonsHookProps = {
 const useStepperButtons = ({ cartItemNumber, id, name, price, imageUrl, refetchCartList }: StpperButtonsHookProps) => {
   const { updateCart } = useCart();
   const { updateCartItem } = usePostUpdateCartItem();
+  const showToast = useToast();
 
   const handleStepperIncreaseButton = useCallback(async (quantity: number) => {
+    if (!cartItemNumber) return;
     await updateCartItem({ param: cartItemNumber, body: { quantity: quantity + 1 } });
     await refetchCartList({});
-    if (!cartItemNumber) return;
     updateCart({ id: cartItemNumber, quantity: quantity + 1, product: { id, name, price, imageUrl }, isSelected: true });
   }, []);
 
   const handleStepperDecreaseButton = useCallback(async (quantity: number) => {
-    if (quantity === 1) return;
-    await updateCartItem({ param: cartItemNumber, body: { quantity: quantity - 1 } });
-    await refetchCartList({});
     if (!cartItemNumber) return;
+    await updateCartItem({ param: cartItemNumber, body: { quantity: quantity - 1 } });
+    if (quantity === 1) {
+      showToast(toastMessages.deleted);
+    }
+    await refetchCartList({});
     updateCart({ id: cartItemNumber, quantity: quantity - 1, product: { id, name, price, imageUrl }, isSelected: true });
   }, []);
 
--- src/hooks/cartItemOperations/useStepperInputChange.ts
+++ src/hooks/cartItemOperations/useStepperInputChange.ts
@@ -24,7 +24,7 @@ const useStepperInputChange = ({ cartItemNumber, id, name, price, imageUrl, refe
     async (e: React.ChangeEvent<HTMLInputElement>) => {
       let targetQuantity = parseInt(e.target.value, 10);
 
-      if (isNaN(targetQuantity) || targetQuantity < 1) {
+      if (isNaN(targetQuantity)) {
         targetQuantity = 1;
       } else if (targetQuantity > 99) {
         targetQuantity = 99;
