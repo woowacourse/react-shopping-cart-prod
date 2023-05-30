265b1858b82c1fe364f143722830434306129968
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-30 16:00:05 +0900

    refactor(OrderProductCardList): 아코디언 관련 로직 공용컴포넌트로 대체

--- src/components/OrderProductSection/OrderProductCardList/OrderProductCardList.tsx
+++ src/components/OrderProductSection/OrderProductCardList/OrderProductCardList.tsx
@@ -3,54 +3,45 @@ import { getCheckedCartProductsState } from 'state/checkedCartProductIds';
 import OrderProductCard from './OrderProductCard/OrderProductCard';
 import styled from 'styled-components';
 import Box from 'components/@common/Box';
-import upArrowIcon from 'assets/up-arrow-icon.svg';
-import { useState } from 'react';
+import Accordion from 'components/@common/Accordion';
 
 const OrderProductCardList = () => {
   const checkedCartProducts = useRecoilValue(getCheckedCartProductsState);
   const totalOrderCountText = `(총 ${checkedCartProducts.length}개)`;
-  const [isAccordionOpen, setIsAccordionOpen] = useState(true);
-  const accordionIconAlt = isAccordionOpen ? '상품목록 열림' : '상품목록 닫힘';
-
-  const toggleAccordion = () => {
-    setIsAccordionOpen((prev) => !prev);
-  };
 
   return (
-    <Box sizing={{ width: '100%' }} flex={{ flexDirection: 'column', align: 'flex-start' }} role="list">
-      <AccordionTrigger onClick={toggleAccordion}>
-        <Box sizing={{ width: '100%' }} flex={{ justify: 'flex-start' }}>
-          <Title>주문상품</Title>
-          <TotalOrderCount>{totalOrderCountText}</TotalOrderCount>
-        </Box>
-        <AccordionIcon src={upArrowIcon} alt={accordionIconAlt} isOpen={isAccordionOpen} />
-      </AccordionTrigger>
-      {isAccordionOpen && (
-        <Box sizing={{ width: '100%' }} flex={{ flexDirection: 'column' }}>
-          {checkedCartProducts.map((checkedCartProduct) => (
-            <OrderProductCard key={checkedCartProduct.product.id} checkedCartProduct={checkedCartProduct} />
-          ))}
-        </Box>
-      )}
+    <Box sizing={{ width: '100%' }} flex={{ flexDirection: 'column', align: 'flex-start' }}>
+      <Accordion.Root>
+        <Wrapper>
+          <Accordion.Trigger>
+            <Box sizing={{ width: '100%', height: '60px' }} flex={{ justify: 'flex-start' }}>
+              <Title>주문상품</Title>
+              <TotalOrderCount>{totalOrderCountText}</TotalOrderCount>
+            </Box>
+          </Accordion.Trigger>
+        </Wrapper>
+        <Accordion.Content>
+          <Box sizing={{ width: '100%' }} flex={{ flexDirection: 'column' }} role="list">
+            {checkedCartProducts.map((checkedCartProduct) => (
+              <OrderProductCard key={checkedCartProduct.product.id} checkedCartProduct={checkedCartProduct} />
+            ))}
+          </Box>
+        </Accordion.Content>
+      </Accordion.Root>
     </Box>
   );
 };
 
 export default OrderProductCardList;
 
-const AccordionTrigger = styled.button`
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+const Wrapper = styled.div`
   position: sticky;
   top: 80px;
   z-index: 10;
-  width: 100%;
-  height: 60px;
-  border: none;
-  border-bottom: 1px solid var(--color-grayscale-200);
   background-color: var(--color-pure-white);
+  width: 100%;
+  padding: 0 20px;
+  border-bottom: 1px solid var(--color-grayscale-200);
 `;
 
 const Title = styled.span`
@@ -63,11 +54,4 @@ const TotalOrderCount = styled.span`
   font-size: 18px;
   font-weight: 700;
   text-align: left;
-`;
-
-const AccordionIcon = styled.img<{ isOpen: boolean }>`
-  width: 20px;
-  height: 20px;
-
-  transform: ${({ isOpen }) => (isOpen ? '' : 'scaleY(-1)')};
 `;
