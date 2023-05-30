dd2a301fdf78376c36ccd023ac82eac140ba0f29
Dahye <06robin11@gmail.com>
2023-05-30 16:31:07 +0900

    feat(cartItem):개별 아이템 삭제 확인 모달 추가

--- src/components/Cart/CartItem/index.tsx
+++ src/components/Cart/CartItem/index.tsx
@@ -4,6 +4,8 @@ import Svg from 'components/@common/Svg';
 import { useCart } from 'components/Cart/hooks/useCart';
 import { useCheckedItemIds } from '../hooks/useCheckedItems';
 import { Cart } from 'types';
+import Modal from 'components/@common/Modal';
+import { useModal } from 'hooks/useModal';
 
 interface CartItemProps {
   cartItem: Cart;
@@ -12,6 +14,7 @@ interface CartItemProps {
 const CartItem = ({ cartItem }: CartItemProps) => {
   const { decreaseItemQuantity, increaseItemQuantity, deleteItem } = useCart();
   const { checkItem, checkedItemIds, unCheckItem } = useCheckedItemIds();
+  const { isModalOpen, openModal, closeModal } = useModal();
   const { product } = cartItem;
 
   const onCheckItem = () => {
@@ -21,6 +24,8 @@ const CartItem = ({ cartItem }: CartItemProps) => {
   const onDelete = () => {
     deleteItem(cartItem.id);
     unCheckItem(cartItem.id);
+
+    closeModal();
   };
 
   const increase = () => {
@@ -41,7 +46,7 @@ const CartItem = ({ cartItem }: CartItemProps) => {
       <S.CartItemImage src={product.imageUrl} alt={product.name} />
       <S.CartProductName>{product.name}</S.CartProductName>
       <S.CounterWrapper>
-        <button onClick={onDelete}>
+        <button onClick={openModal}>
           <Svg type="trash-can" width={24} height={24} />
         </button>
         <Counter
@@ -54,6 +59,12 @@ const CartItem = ({ cartItem }: CartItemProps) => {
           {product.price.toLocaleString('KR')}원
         </S.CartProductPrice>
       </S.CounterWrapper>
+      <Modal
+        message={`${product.name}을(를) 삭제하시겠습니까?`}
+        isOpen={isModalOpen}
+        onCloseModal={closeModal}
+        onClickYes={onDelete}
+      />
     </S.CartItemWrapper>
   );
 };
