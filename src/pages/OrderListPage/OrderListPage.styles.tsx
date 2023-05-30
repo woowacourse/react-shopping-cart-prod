d3615fae27b0ffe29c05609e771952438b132d32
sh981013s <sh981013s@gmail.com>
2023-05-30 16:53:22 +0900

    feat: OrderListPage 스타일 구현

---
+++ src/pages/OrderListPage/OrderListPage.styles.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { CartMainText, MainTextBorder } from '../CartPage/CartPage.styles.tsx';
+
+export const OrderListPageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 25px 0;
+`;
+
+export const OrderListPageContent = styled.div`
+  width: 60%;
+`;
+
+export const OrderListPageTitle = styled(CartMainText)``;
+
+export const OrderListPageTitleBorder = styled(MainTextBorder)``;
--- src/pages/OrderListPage/OrderListPage.tsx
+++ src/pages/OrderListPage/OrderListPage.tsx
@@ -1,16 +1,21 @@
 import useGetOrderList from '../../hooks/requests/useGetOrderList.ts';
 import OrderList from '../../components/@common/OrderList/OrderList.tsx';
+import * as Styled from './OrderListPage.styles.tsx';
 
 const OrderListPage = () => {
   const { data: orderListData } = useGetOrderList();
 
   return (
-    <>
-      {orderListData &&
-        orderListData.orders.map((orderData) => {
-          return <OrderList key={orderData.orderId} orderData={orderData} hasDetailNavigateButton />;
-        })}
-    </>
+    <Styled.OrderListPageWrapper>
+      <Styled.OrderListPageContent>
+        <Styled.OrderListPageTitle>주문목록</Styled.OrderListPageTitle>
+        <Styled.OrderListPageTitleBorder />
+        {orderListData &&
+          orderListData.orders.map((orderData) => {
+            return <OrderList key={orderData.orderId} orderData={orderData} hasDetailNavigateButton />;
+          })}
+      </Styled.OrderListPageContent>
+    </Styled.OrderListPageWrapper>
   );
 };
 
