2d64184a63c3ce5037a8e452b7929d0ab7412d93
sh981013s <sh981013s@gmail.com>
2023-05-30 15:44:50 +0900

    feat: useOrderItems hook 에 주문 시 recoil state 에서 주문 아이템 제거 로직 추가

--- src/hooks/cartItemOperations/useOrderItems.ts
+++ src/hooks/cartItemOperations/useOrderItems.ts
@@ -2,22 +2,29 @@ import useCart from '../useCart.ts';
 import { useNavigate } from 'react-router-dom';
 import usePostCreateOrder from '../requests/usePostCreateOrder.ts';
 import { useEffect } from 'react';
+import { useToast } from '../useToast.ts';
+import toastMessages from '../../constants/toastMessages.ts';
 
 const useOrderItems = () => {
   const navigate = useNavigate();
-  const { cartList } = useCart();
+  const { cartList, removeCartsWithId } = useCart();
   const { createOrderState, createOrder } = usePostCreateOrder();
+  const showToast = useToast();
 
   const handleOrderItems = async () => {
     if (!cartList) return;
 
     const orderItems = cartList.filter((item) => item.isSelected).map((item) => ({ id: item.product.id, quantity: item.quantity }));
 
-    createOrder({ body: { orderItems } });
+    const orderItemsWithId = cartList.filter((item) => item.isSelected).map((item) => item.id);
+
+    await createOrder({ body: { orderItems } });
+    removeCartsWithId(orderItemsWithId);
   };
 
   useEffect(() => {
     if (createOrderState.status === 'success' && createOrderState.data) {
+      showToast(toastMessages.ordered);
       navigate(`/order/${createOrderState.data.orderId}`, { state: { makeOrder: createOrderState.data } });
     }
   }, [createOrderState.status]);
