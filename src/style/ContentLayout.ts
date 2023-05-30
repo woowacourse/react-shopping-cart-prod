b2b030a0a6f56f2b9531a12f906b7b5dd0d216d5
regularPark <srk99279@naver.com>
2023-05-30 15:27:48 +0900

    design: `OrderListWrapper` 추가

--- src/style/ContentLayout.ts
+++ src/style/ContentLayout.ts
@@ -12,6 +12,18 @@ export const CartListWrapper = styled.div`
   }
 `;
 
+export const OrderListWrapper = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 80px;
+  gap: 80px;
+  @media all and (max-width: 479px) {
+    gap: 30px;
+  }
+`;
+
 export const ProductListWrapper = styled.div`
   width: 100vw;
   padding-top: 120px;
