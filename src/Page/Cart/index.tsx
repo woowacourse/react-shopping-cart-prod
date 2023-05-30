245afe468422378b23c1c954d76e5c8fef1dce5d
hozzijeong <ghwls0905@naver.com>
2023-05-30 17:36:01 +0900

    refactor: cart payments 조건부 렌더링 설정

--- src/Page/Cart/index.tsx
+++ src/Page/Cart/index.tsx
@@ -2,31 +2,22 @@ import { useRecoilValue } from 'recoil';
 import { ReactComponent as AlertBlank } from 'src/assets/baemin-alert-blank.svg';
 import CartProductItemList from 'src/components/CartProductItemList';
 import PaymentsView from 'src/components/PaymentsView';
-import { PARCEL_PRICE } from 'src/constants';
 import { $CartList, $CurrentServerUrl } from 'src/recoil/atom';
-import $CheckedCartTotalPrice from 'src/recoil/selector';
 import ContentLayout from 'src/components/Common/ContentLayout';
+import usePayments from 'src/hooks/usePayments';
 import styles from './index.module.scss';
 
 function Cart() {
   const currentServerUrl = useRecoilValue($CurrentServerUrl);
   const cartList = useRecoilValue($CartList(currentServerUrl));
-  const totalPrice = useRecoilValue($CheckedCartTotalPrice(currentServerUrl));
+
+  const { payments } = usePayments();
 
   const ResultComponent =
     cartList.length > 0 ? (
       <section className={styles['main-view']}>
         <CartProductItemList />
-        <PaymentsView
-          puschaseOption
-          paymentAmount={{
-            originalPrice: totalPrice,
-            deliveryFee: PARCEL_PRICE,
-            discountedPrice: totalPrice,
-            discounts: [],
-            finalPrice: totalPrice + PARCEL_PRICE,
-          }}
-        />
+        {payments && <PaymentsView puschaseOption paymentAmount={payments} />}
       </section>
     ) : (
       <section className={styles['main-view-blank']}>
