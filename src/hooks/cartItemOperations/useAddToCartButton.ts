0a9554c3ac3fa3e285c5cc6dc0f14ab9f68b6711
sh981013s <sh981013s@gmail.com>
2023-05-31 01:46:11 +0900

    refactor: 상품 등록 버튼 클릭 시 response 값 이용하여 updateCart 로직 작동

--- src/hooks/cartItemOperations/useAddToCartButton.ts
+++ src/hooks/cartItemOperations/useAddToCartButton.ts
@@ -2,7 +2,6 @@ import { useCallback } from 'react';
 import { Item } from '../../types/CartList.ts';
 import useCart from '../useCart.ts';
 import useCreateCartItem from '../requests/useCreateCartItem.ts';
-import useGetCartList from '../requests/useGetCartList.ts';
 import { useToast } from '../useToast.ts';
 import toastMessages from '../../constants/toastMessages.ts';
 
@@ -18,21 +17,16 @@ type AddToCartButtonProps = {
 const useAddToCartButton = ({ cartItemNumber, id, name, price, imageUrl, refetchCartList }: AddToCartButtonProps) => {
   const { updateCart } = useCart();
   const { createCartItemState, createCartItem } = useCreateCartItem();
-  const { status: cartListStatus, data: cartList, refetchCartList: getFreshCartList } = useGetCartList();
   const showToast = useToast();
 
   const handleAddToCartButton = useCallback(async () => {
     await createCartItem({ body: { productId: id } });
+    showToast(toastMessages.created);
     await refetchCartList({});
-    await getFreshCartList({});
 
-    if (cartListStatus === 'success' && cartList) {
-      const addedCartItem = cartList.find((item: Item) => item.product.id === id);
-
-      if (!addedCartItem) return;
-
-      showToast(toastMessages.created);
-      updateCart({ id: addedCartItem.id, quantity: 1, product: { id, name, price, imageUrl }, isSelected: true });
+    if (createCartItemState.status === 'success' && createCartItemState.data) {
+      const cartData: Item = createCartItemState.data;
+      updateCart({ id: cartData.id, quantity: 1, product: { id, name, price, imageUrl }, isSelected: true });
     }
   }, [cartItemNumber, id, name, price, imageUrl, refetchCartList, createCartItem, createCartItemState.error, updateCart]);
 
