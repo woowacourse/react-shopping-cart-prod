45f75d4c2549cf7bc1687270ea2461121da4cff0
nangkyeonglim <nangkyeonglim@gmail.com>
2023-05-30 19:03:21 +0900

    refactor: order 상수 컴포넌트 밖으로 분리

--- src/pages/OrderDetail/OrderDetail.tsx
+++ src/pages/OrderDetail/OrderDetail.tsx
@@ -2,34 +2,36 @@ import { useParams } from 'react-router-dom';
 import { OrderList } from '../../components/OrderList';
 import { FatBorder, PageTitle } from '../../style/style';
 import { OrderListWrapper } from '../Order/Order.style';
+import { PaymentInfoBox } from '../../components/PaymentInfoBox';
+
+const order = {
+  orderId: 0,
+  orderItems: [
+    {
+      id: 0,
+      productName: '바나나',
+      productPrice: 3500,
+      paymentPrice: 3500,
+      createdAt: 'string',
+      productQuantity: 3,
+      imageUrl:
+        'https://cdn.pixabay.com/photo/2016/01/03/17/59/bananas-1119790_1280.jpg',
+    },
+    {
+      id: 1,
+      productName: '딸기',
+      productPrice: 9900,
+      paymentPrice: 9900,
+      createdAt: 'string',
+      productQuantity: 2,
+      imageUrl:
+        'https://cdn.pixabay.com/photo/2018/04/29/11/54/strawberries-3359755_1280.jpg',
+    },
+  ],
+};
 
 function OrderDetail() {
   const params = useParams();
-  const order = {
-    orderId: 0,
-    orderItems: [
-      {
-        id: 0,
-        productName: '바나나',
-        productPrice: 3500,
-        paymentPrice: 3500,
-        createdAt: 'string',
-        productQuantity: 3,
-        imageUrl:
-          'https://cdn.pixabay.com/photo/2016/01/03/17/59/bananas-1119790_1280.jpg',
-      },
-      {
-        id: 1,
-        productName: '딸기',
-        productPrice: 9900,
-        paymentPrice: 9900,
-        createdAt: 'string',
-        productQuantity: 2,
-        imageUrl:
-          'https://cdn.pixabay.com/photo/2018/04/29/11/54/strawberries-3359755_1280.jpg',
-      },
-    ],
-  };
 
   return (
     <>
@@ -38,6 +40,7 @@ function OrderDetail() {
       <OrderListWrapper>
         <OrderList {...order} detail={false} />
       </OrderListWrapper>
+      <PaymentInfoBox />
     </>
   );
 }
