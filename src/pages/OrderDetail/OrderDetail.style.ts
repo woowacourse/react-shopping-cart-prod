deffa4db23cf98014d379fa161919d8e046ecc10
nangkyeonglim <nangkyeonglim@gmail.com>
2023-05-31 23:48:11 +0900

    refactor: 주문 상세 페이지 반응형 개선

--- src/pages/OrderDetail/OrderDetail.style.ts
+++ src/pages/OrderDetail/OrderDetail.style.ts
@@ -4,4 +4,7 @@ export const OrderInfoWrapper = styled.div`
   display: flex;
   gap: 40px;
   padding-bottom: 80px;
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.md}) {
+    flex-direction: column;
+  }
 `;
