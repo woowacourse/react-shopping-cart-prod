3532fab705fc77bcb71780eed88e3eb310882d05
Q Kim <qoli0703@gmail.com>
2023-05-31 21:28:52 +0900

    refactor: 불필요한 suspense 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import { Suspense } from 'react';
 import Header from './components/common/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<h1>서버 변경중..</h1>}>
-        <Header />
-        <Outlet />
-      </Suspense>
+      <Header />
+      <Outlet />
     </div>
   );
 }
