287060d1ba8742f4cd63fa764d81e4ceea445af2
Q Kim <qoli0703@gmail.com>
2023-05-31 20:51:45 +0900

    feat: 장바구니를 불러오는 selector

---
+++ src/globalState/selectors/fetchCartItems.ts
@@ -0,0 +1,27 @@
+import { selector } from 'recoil';
+import { USER_AUTH_TOKEN } from '../../constant';
+import ServerUtil from '../../utils/ServerUrl';
+import serverNameState from '../atoms/serverName';
+import { CartProduct } from '../../types/product';
+
+const fetchCartItems = selector<CartProduct[]>({
+  key: 'fetchCartItems',
+
+  get: async ({ get }) => {
+    const serverName = get(serverNameState);
+    const cartItemsUrl = ServerUtil.getCartItemsUrl(serverName);
+    console.log(serverName);
+
+    const response = await fetch(cartItemsUrl, {
+      headers: { Authorization: `Basic ${USER_AUTH_TOKEN}` },
+    });
+
+    if (response.status !== 200) throw new Error('서버에 장애가 발생했습니다.');
+
+    const cartItemList = await response.json();
+    console.log(cartItemList);
+    return cartItemList;
+  },
+});
+
+export default fetchCartItems;
