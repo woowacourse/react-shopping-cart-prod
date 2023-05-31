b7d02976528874fda8b45c4bbd9c15410e184e24
hozzijeong <ghwls0905@naver.com>
2023-05-31 17:49:35 +0900

    refactor: 주문 상세 상수 주소 표시 및 직접 이동가능하도록 변경

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -33,7 +33,7 @@ export const ROUTE_PATH = Object.freeze({
   DEFAULT: '/',
   CART: '/cart',
   ORDER: '/order',
-  ORDER_DETAIL: '/order/:id',
+  ORDER_DETAIL: (id?: string | null) => `/order/${id ?? ':id'}`,
 });
 
 export const PARCEL_PRICE = 3000;
