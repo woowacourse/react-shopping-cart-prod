005a8cd9aeaf28f8ff39d433a4ae26c3e7dddbe7
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-05-31 21:27:37 +0900

    feat: OrderDetail 페이지 생성

---
+++ src/pages/OrderDetailPage.tsx
@@ -0,0 +1,13 @@
+import OrderList from '../components/OrderList';
+import OrderPrice from '../components/OrderPrice';
+
+const OrderDetailPage = () => {
+  return (
+    <>
+      <OrderList />
+      <OrderPrice />
+    </>
+  );
+};
+
+export default OrderDetailPage;
