7478742a6a5ce44c5af6408a8536c4edc10dfe14
regularPark <srk99279@naver.com>
2023-05-31 20:27:29 +0900

    feat: 포인트 정책 위한 `PointInput` 생성

---
+++ src/components/cart/PointInput.tsx
@@ -0,0 +1,55 @@
+import { styled } from 'styled-components';
+
+const PointInput = () => {
+  return (
+    <S.Wrapper>
+      <S.PointLabel>사용 포인트</S.PointLabel>
+      <S.InputWrapper>
+        <S.Input type="text" placeholder="0원" />
+        <S.PointButton>전액사용</S.PointButton>
+      </S.InputWrapper>
+    </S.Wrapper>
+  );
+};
+
+const S = {
+  Wrapper: styled.section`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  `,
+
+  PointLabel: styled.span`
+    font-size: 18px;
+    font-weight: 700;
+  `,
+
+  InputWrapper: styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    gap: 10px;
+    width: 60%;
+  `,
+
+  Input: styled.input`
+    width: 70%;
+    text-align: end;
+    border: solid #aaa;
+    border-width: 0px 0px 1px 0px;
+  `,
+
+  PointButton: styled.button`
+    width: 70px;
+    height: 28px;
+    border: 1px solid #aaa;
+    border-radius: 7px;
+    padding: 2px;
+    font-weight: 400;
+    font-size: 16px;
+  `,
+};
+
+export default PointInput;
