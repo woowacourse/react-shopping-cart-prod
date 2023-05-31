52b1c672f48edc0bc6dfd92b2221b8c732c63212
Dahye <06robin11@gmail.com>
2023-05-31 16:24:46 +0900

    feat: 상품 할인 가격 필드 추가

--- src/components/Cart/PaymentDetail/PaymentDetail.styles.ts
+++ src/components/Cart/PaymentDetail/PaymentDetail.styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.section`
   width: 360px;
   padding: 24px;
   border: 1px solid ${(props) => props.theme.color.gray};
@@ -8,10 +8,8 @@ export const Container = styled.div`
 
 export const Title = styled.p`
   margin-bottom: 20px;
-  padding-bottom: 20px;
   font: ${(props) => props.theme.font.medium};
   text-align: center;
-  border-bottom: 1px solid ${(props) => props.theme.color.gray};
 `;
 
 export const Wrapper = styled.div`
@@ -32,4 +30,10 @@ export const OrderButton = styled.button`
   text-align: center;
   color: ${(props) => props.theme.color.white};
   cursor: pointer;
+`;
+
+export const Divider = styled.div`
+  width: 100%;
+  border-bottom: 1px solid ${(props) => props.theme.color.gray};
+  margin: 0 0 20px 0;
 `;
--- src/components/Cart/PaymentDetail/index.tsx
+++ src/components/Cart/PaymentDetail/index.tsx
@@ -7,9 +7,11 @@ import Modal from 'components/@common/Modal';
 import { useModal } from 'hooks/useModal';
 
 const PaymentDetail = () => {
-  const deliveryPrice = 3000;
   const totalPrice = useRecoilValue(totalPriceSelector);
-  const orderPrice = totalPrice === 0 ? 0 : totalPrice + deliveryPrice;
+  const deliveryPrice = totalPrice >= 30000 ? 0 : 3000;
+  const discountPrice = 5000;
+  const orderPrice =
+    totalPrice === 0 ? 0 : totalPrice + deliveryPrice - discountPrice;
   const moveTo = useNavigate();
   const { isModalOpen, openModal, closeModal } = useModal();
 
@@ -20,14 +22,20 @@ const PaymentDetail = () => {
   return (
     <S.Container>
       <S.Title>결제 예상 금액</S.Title>
+      <S.Divider />
       <S.Wrapper>
         <S.Text>총 상품 가격</S.Text>
         <S.Text>{totalPrice.toLocaleString('KR')}원</S.Text>
       </S.Wrapper>
       <S.Wrapper>
+        <S.Text>총 할인 가격</S.Text>
+        <S.Text>-{discountPrice.toLocaleString('KR')}원</S.Text>
+      </S.Wrapper>
+      <S.Wrapper>
         <S.Text>총 배송비</S.Text>
         <S.Text>{deliveryPrice.toLocaleString('KR')}원</S.Text>
       </S.Wrapper>
+      <S.Divider />
       <S.Wrapper>
         <S.Text>총 주문 금액</S.Text>
         <S.Text>{orderPrice.toLocaleString('KR')}원</S.Text>
