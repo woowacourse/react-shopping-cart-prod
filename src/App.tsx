38a4a91ab26181c6e2f5d5d0aa81645747fb7cf4
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-05-31 21:21:25 +0900

    style: themeprovider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { RouterProvider } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
 import router from './router';
 import GlobalStyle from './styles/GlobalStyle';
 import ResetStyle from './styles/ResetStyle';
+import { theme } from './styles/Theme';
 
 const App = () => {
   return (
     <RecoilRoot>
-      <ResetStyle />
-      <GlobalStyle />
-
-      <RouterProvider router={router} />
+      <ThemeProvider theme={theme}>
+        <ResetStyle />
+        <GlobalStyle />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </RecoilRoot>
   );
 };
