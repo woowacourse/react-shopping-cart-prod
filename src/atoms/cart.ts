182d262d0ffe7be238f8e5bf06e48d250c913479
2yunseong <dbsdltjd3701@naver.com>
2023-05-31 16:37:32 +0900

    refactor: 비구조화 할당 사용

--- src/atoms/cart.ts
+++ src/atoms/cart.ts
@@ -92,19 +92,15 @@ export const discountPrice = selector({
   key: 'discountPriceSelector',
   get: ({ get }) => {
     const cart = get(cartState);
-    const couponsState = get(coupons);
+    const { allCoupons, specificCoupons } = get(coupons);
     const selectedCoupons = get(selectedCouponsState);
     const totalPrice = get(totalPriceSelector);
 
     let totalDiscountPrice = 0;
     // 전체 쿠폰일 시의 로직
-    if (
-      couponsState.allCoupons.some((coupon) =>
-        selectedCoupons.includes(coupon.id)
-      )
-    ) {
-      const applyCoupon = couponsState.allCoupons.find(
-        (value) => value.id === selectedCoupons[0]
+    if (allCoupons.some((coupon) => selectedCoupons.includes(coupon.id))) {
+      const applyCoupon = allCoupons.find(
+        (coupon) => coupon.id === selectedCoupons[0]
       );
       if (!applyCoupon) return totalDiscountPrice;
       if (applyCoupon.discountType === 'RATE') {
@@ -121,8 +117,8 @@ export const discountPrice = selector({
 
     // 개별 주문시 할인 금액
     selectedCoupons.forEach((couponId) => {
-      const applyCoupon = couponsState.specificCoupons.find(
-        (value) => value.id === couponId
+      const applyCoupon = specificCoupons.find(
+        (coupon) => coupon.id === couponId
       );
 
       if (!applyCoupon) return totalDiscountPrice;
