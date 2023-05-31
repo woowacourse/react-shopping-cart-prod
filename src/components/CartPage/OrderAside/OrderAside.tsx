95290909be22f446afef2fab2b0f0246a38725f9
2yunseong <dbsdltjd3701@naver.com>
2023-05-31 15:19:00 +0900

    fix: 할인 정책으로 인한 배송비 제거 후 할인 금액 반영

--- src/components/CartPage/OrderAside/OrderAside.tsx
+++ src/components/CartPage/OrderAside/OrderAside.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { selectedItemsSelector } from '../../../atoms/cart';
 import { totalPriceSelector } from '../../../atoms/cart';
-import { DELIVERY_FEE } from '../../../constants/cart';
 import { useRefreshableRecoilValue } from '../../../hooks/common/useRefreshableAtom';
 import { useMutateOrder } from '../../../hooks/order/order';
 import * as S from './OrderAside.styles';
@@ -23,14 +22,12 @@ const OrderAside = () => {
         <S.Text>{totalPrice.toLocaleString()}원</S.Text>
       </S.TextWrapper>
       <S.TextWrapper>
-        <S.Text>총 배송비</S.Text>
-        <S.Text>{totalPrice ? DELIVERY_FEE.toLocaleString() : 0}원</S.Text>
+        <S.Text>할인 금액</S.Text>
+        <S.Text color='red'>0원</S.Text>
       </S.TextWrapper>
       <S.TextWrapper>
         <S.Text>총 주문금액</S.Text>
-        <S.Text>
-          {totalPrice ? (totalPrice + DELIVERY_FEE).toLocaleString() : 0}원
-        </S.Text>
+        <S.Text>{totalPrice ? totalPrice.toLocaleString() : 0}원</S.Text>
       </S.TextWrapper>
       <S.OrderButton size='L' view='black' onClick={onOrder}>
         주문하기
