22b617d03f4ce949b6fe6070415e4c14640ec7e1
2yunseong <dbsdltjd3701@naver.com>
2023-05-31 16:30:56 +0900

    feat: 할인 가격 측정 로직 구현

--- src/atoms/cart.ts
+++ src/atoms/cart.ts
@@ -1,6 +1,7 @@
 import { selector, atom, selectorFamily } from 'recoil';
 import { CartItem } from '../types/cart';
 import { fetchCart } from '../apis/cart';
+import { coupons, selectedCouponsState } from './coupons';
 
 export const cartState = atom({
   key: 'cart',
@@ -80,5 +81,67 @@ export const totalPriceSelector = selector({
         selectedItems.has(id) ? totalPrice + quantity * price : totalPrice,
       0
     );
+  },
+});
+
+const getDiscountPriceByRate = (base: number, rate: number) => {
+  return Math.floor(base * (rate / 100));
+};
+
+export const discountPrice = selector({
+  key: 'discountPriceSelector',
+  get: ({ get }) => {
+    const cart = get(cartState);
+    const couponsState = get(coupons);
+    const selectedCoupons = get(selectedCouponsState);
+    const totalPrice = get(totalPriceSelector);
+
+    let totalDiscountPrice = 0;
+    // 전체 쿠폰일 시의 로직
+    if (
+      couponsState.allCoupons.some((coupon) =>
+        selectedCoupons.includes(coupon.id)
+      )
+    ) {
+      const applyCoupon = couponsState.allCoupons.find(
+        (value) => value.id === selectedCoupons[0]
+      );
+      if (!applyCoupon) return totalDiscountPrice;
+      if (applyCoupon.discountType === 'RATE') {
+        totalDiscountPrice += getDiscountPriceByRate(
+          totalPrice,
+          applyCoupon.value
+        );
+      } else {
+        totalDiscountPrice += applyCoupon.value;
+      }
+
+      return totalDiscountPrice;
+    }
+
+    // 개별 주문시 할인 금액
+    selectedCoupons.forEach((couponId) => {
+      const applyCoupon = couponsState.specificCoupons.find(
+        (value) => value.id === couponId
+      );
+
+      if (!applyCoupon) return totalDiscountPrice;
+      const applyCartItem = cart.find(
+        (item) => item.product.id === applyCoupon.targetProductId
+      );
+
+      if (!applyCartItem) return totalDiscountPrice;
+
+      if (applyCoupon.discountType === 'RATE') {
+        totalDiscountPrice += getDiscountPriceByRate(
+          applyCartItem.product.price * applyCartItem.quantity,
+          applyCoupon.value
+        );
+      } else {
+        totalDiscountPrice += applyCoupon.value;
+      }
+    });
+
+    return totalDiscountPrice;
   },
 });
--- src/components/CartPage/OrderAside/OrderAside.tsx
+++ src/components/CartPage/OrderAside/OrderAside.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { selectedItemsSelector } from '../../../atoms/cart';
+import { discountPrice, selectedItemsSelector } from '../../../atoms/cart';
 import { totalPriceSelector } from '../../../atoms/cart';
 import { useRefreshableRecoilValue } from '../../../hooks/common/useRefreshableAtom';
 import { useMutateOrder } from '../../../hooks/order/order';
@@ -8,12 +8,15 @@ import * as S from './OrderAside.styles';
 const OrderAside = () => {
   const totalPrice = useRefreshableRecoilValue(totalPriceSelector);
   const selectedItems = useRefreshableRecoilValue(selectedItemsSelector);
+  const totalDiscountPrice = useRefreshableRecoilValue(discountPrice);
+
   const { postOrderMutation } = useMutateOrder();
   const navigate = useNavigate();
   const onOrder = async () => {
     await postOrderMutation({ cartItemIds: [...selectedItems] });
     navigate('/order');
   };
+
   return (
     <S.Root>
       <S.Title>결제예상금액</S.Title>
@@ -23,11 +26,11 @@ const OrderAside = () => {
       </S.TextWrapper>
       <S.TextWrapper>
         <S.Text>할인 금액</S.Text>
-        <S.Text color='red'>0원</S.Text>
+        <S.Text color='red'>{totalDiscountPrice.toLocaleString()}원</S.Text>
       </S.TextWrapper>
       <S.TextWrapper>
         <S.Text>총 주문금액</S.Text>
-        <S.Text>{totalPrice ? totalPrice.toLocaleString() : 0}원</S.Text>
+        <S.Text>{(totalPrice - totalDiscountPrice).toLocaleString()}원</S.Text>
       </S.TextWrapper>
       <S.OrderButton size='L' view='black' onClick={onOrder}>
         주문하기
