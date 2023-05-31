0257c5333e5f8bff9dbc3fcce9194567684538ad
guridaek <xylitol79@gmail.com>
2023-05-31 15:49:46 +0900

    feat: 주문 목록 페이지 Suspense 적용

--- src/pages/OrderList.tsx
+++ src/pages/OrderList.tsx
@@ -3,6 +3,7 @@ import Page from "components/common/Page";
 import React from "react";
 import Skeleton from "components/common/Skeleton";
 import OrderDetailList from "components/orderList/OrderDetailList";
+import LoadingSpinner from "components/common/LoadingSpinner";
 
 const OrderList = () => {
   return (
@@ -13,7 +14,9 @@ const OrderList = () => {
         <Header />
       </React.Suspense>
       <Page>
-        <OrderDetailList />
+        <React.Suspense fallback={<LoadingSpinner />}>
+          <OrderDetailList />
+        </React.Suspense>
       </Page>
     </>
   );
