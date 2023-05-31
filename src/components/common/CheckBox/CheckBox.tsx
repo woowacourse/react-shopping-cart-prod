2724f4dfbf2c4aadb5919f3d9b1f5320d2232929
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-05-31 17:24:59 +0900

    style : 체크박스 디자인 변경

--- src/components/common/CheckBox/CheckBox.tsx
+++ src/components/common/CheckBox/CheckBox.tsx
@@ -1,8 +1,22 @@
 import { styled } from 'styled-components';
+import UncheckedImage from '../../../assets/icon/unchecked-icon.svg';
+import CheckedImage from '../../../assets/icon/checked-icon.svg';
 
 const CheckBox = styled.input.attrs({ type: 'checkbox' })`
-  width: 28px;
-  height: 28px;
+  appearance: none;
+  border: 1px solid #dddddd;
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  background-image: url(${UncheckedImage});
+  background-size: cover;
+
+  &:checked {
+    border: 1px solid var(--main-bg-color);
+    background-color: var(--main-bg-color);
+    background-image: url(${CheckedImage});
+    background-size: cover;
+  }
 `;
 
 export default CheckBox;
