12b50ed51eb046e783267fdbd57a8d2ae0146ea9
Suyoung <jsy8492@gmail.com>
2023-05-31 11:22:38 +0900

    feat: Close svg icon 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -3,3 +3,4 @@ export { ReactComponent as SmallCartIcon } from './smallCart.svg';
 export { ReactComponent as PlusIcon } from './plus.svg';
 export { ReactComponent as MinusIcon } from './minus.svg';
 export { ReactComponent as TrashIcon } from './trash.svg';
+export { ReactComponent as CloseIcon } from './close.svg';
