5e01651c80cfb73aba8bc9406077c8077e7440bd
nlom0218 <nlom0218@naver.com>
2023-05-31 14:14:32 +0900

    fix: 오타 수정

--- src/components/Modal/MyCoupon/index.tsx
+++ src/components/Modal/MyCoupon/index.tsx
@@ -20,7 +20,7 @@ function MyCoupon() {
       <Suspense fallback={<CouponListSkeleton />}>
         <CouponList
           couponState={myCouponState}
-          noExistCouponText="쿠폰을 다 사용했요!"
+          noExistCouponText="쿠폰을 다 사용했어요!"
           noExistCouponSubText="쿠폰 발급하기에서 쿠폰을 발급해보세요."
           couponSubMessage="쿠폰으로 할인 받고 상품 구매하기"
           redirectMessage="쿠폰 발급 바로가기"
