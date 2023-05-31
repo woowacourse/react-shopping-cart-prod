85c4e9c7f903e3c0a861d2943d3491f4bcf70ce0
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-05-31 15:46:59 +0900

    feat: 포인트 GET요청 msw 핸들러 추가

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,5 +1,6 @@
 import { setupWorker } from 'msw';
 import { product } from 'mocks/handlers/product';
 import { cart } from 'mocks/handlers/cart';
+import { members } from './handlers/members';
 
-export const worker = setupWorker(...product, ...cart);
+export const worker = setupWorker(...product, ...cart, ...members);
---
+++ src/mocks/handlers/members.ts
@@ -0,0 +1,15 @@
+import { rest } from 'msw';
+import store from 'utils/storage';
+import point from 'mocks/fixtures/point.json';
+import { SERVER_OWNER } from 'constants/storeKey';
+import type { ServerOwner } from 'types/serverOwner';
+import BASE_URL from 'constants/apiBaseURL';
+
+const serverOwner = store.getStorage<ServerOwner>(SERVER_OWNER) ?? '다즐';
+const BASE_URL_BY_OWNER = BASE_URL[serverOwner];
+
+export const members = [
+  rest.get(`${BASE_URL_BY_OWNER}/members/point`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(point));
+  }),
+];
