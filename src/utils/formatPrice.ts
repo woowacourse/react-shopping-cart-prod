1cd00c6a19243cbd254ab15b7fefdd5025fd5710
wzrabbit <curious.wzrabbit@gmail.com>
2023-05-31 17:15:58 +0900

    feat: 원화를 붙이지 않고 포맷팅만 하는 함수 추가

--- src/utils/formatPrice.ts
+++ src/utils/formatPrice.ts
@@ -1,3 +1,7 @@
 export const formatPrice = (price: number) => {
   return `₩ ${price.toLocaleString('ko-KR')}`;
 };
+
+export const formatPriceWithoutWon = (price: number) => {
+  return `${price.toLocaleString('ko-KR')}`;
+};
