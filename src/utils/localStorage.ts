065771ed08e60e5a3e73ddd5545f97b754376275
inyeong-kang <tomatorkd@naver.com>
2023-05-31 16:12:03 +0900

    refactor: key값에 따라 localStorage에 mock 데이터 세팅하도록 수정

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,4 +1,5 @@
 import cart from '../mocks/data/cart.json';
+import order from '../mocks/data/order.json';
 
 export function updateData(key: string, data: unknown) {
   localStorage.setItem(key, JSON.stringify(data));
@@ -7,8 +8,9 @@ export function updateData(key: string, data: unknown) {
 export function getData(key: string) {
   const currentData = localStorage.getItem(key);
   if (currentData === '{}' || currentData === null) {
-    updateData('cart', cart);
-    return cart;
+    const mockData = key === 'cart' ? cart : order;
+    updateData(key, mockData);
+    return mockData;
   }
   return JSON.parse(currentData);
 }
