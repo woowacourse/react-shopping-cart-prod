772f5df8b9ad9416928bfd1d1cade1d9aaee8ac7
kangyeongmin <kym6050@hanyang.ac.kr>
2023-05-31 13:57:41 +0900

    feat: 주문내역상세 페이지의 결제금액정보 구현

--- src/pages/OrderDetail.tsx
+++ src/pages/OrderDetail.tsx
@@ -1,6 +1,5 @@
 import { styled } from "styled-components";
-import { Header, Page } from "../components";
-import { OrderHistoryList } from "../components/OrderHistoryList";
+import { Header, OrderHistory, Page } from "../components";
 
 const OrderDetail = () => {
   return (
@@ -8,7 +7,14 @@ const OrderDetail = () => {
       <Header />
       <Page>
         <TitleBox>주문 내역 상세</TitleBox>
-        <OrderHistoryList />
+        {/* <OrderHistory /> */}
+        <PaymentTitleBox>
+          <p>결제금액 정보</p>
+        </PaymentTitleBox>
+        <PaymentInfoBox>
+          <p>총 결제금액</p>
+          <p>324000원</p>
+        </PaymentInfoBox>
       </Page>
     </>
   );
@@ -23,6 +29,37 @@ const TitleBox = styled.div`
   font-size: 25px;
   text-align: center;
   border-bottom: 4px solid var(--dark-gray);
+`;
+
+const PaymentTitleBox = styled.div`
+  display: flex;
+  align-items: center;
+  align-self: flex-end;
+  width: 30%;
+  max-width: 560px;
+  height: 80px;
+
+  padding: 0 20px;
+  background: #f6f6f6;
+  border: 1px solid #aaaaaa;
+
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const PaymentInfoBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  align-self: flex-end;
+  width: 30%;
+  max-width: 560px;
+  height: 100px;
+
+  padding: 0 20px;
+  border: 1px solid #aaaaaa;
+  border-top: none;
+  font-weight: 600;
 `;
 
 export default OrderDetail;
