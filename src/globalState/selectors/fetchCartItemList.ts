ec3d133b36b3fbe4db6ab3a5f30984a4137a40d1
woo-jk <201702042@o.cnu.ac.kr>
2023-05-31 11:55:41 +0900

    refactor: fetchCartItemList selector 서버가 변경되면 자동으로 주소 변경되도록 변경

--- src/globalState/selectors/fetchCartItemList.ts
+++ src/globalState/selectors/fetchCartItemList.ts
@@ -1,22 +1,30 @@
 import { selector } from 'recoil';
 import { CartProduct } from '../../types/product';
 import ServerUtil from '../../utils/ServerUrl';
-import { DEFAULT_SERVER_NAME, USER_AUTH_TOKEN } from '../../constant';
+import { USER_AUTH_TOKEN } from '../../constant';
+import serverNameState from '../atoms/serverName';
 
 const fetchCartItemList = selector<CartProduct[]>({
   key: 'fetchCartItemList',
 
-  get: async () => {
-    const cartItemsUrl = ServerUtil.getCartItemsUrl(DEFAULT_SERVER_NAME);
+  get: async ({ get }) => {
+    const serverName = get(serverNameState);
+    const cartItemsUrl = ServerUtil.getCartItemsUrl(serverName);
 
     const response = await fetch(cartItemsUrl, {
       headers: { Authorization: `Basic ${USER_AUTH_TOKEN}` },
     });
 
-    if (response.status !== 200) throw new Error('서버에 장애가 발생했습니다.');
+    if (!response.ok) {
+      throw new Error(`장바구니 아이템을 불러오던 중 문제가 발생했습니다.`);
+    }
 
     const cartItemList = await response.json();
     return cartItemList;
+  },
+
+  cachePolicy_UNSTABLE: {
+    eviction: 'most-recent',
   },
 });
 
