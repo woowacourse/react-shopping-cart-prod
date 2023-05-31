210cd2903b4aa084ef4f126c5cf45744c8df9e41
Suyoung <jsy8492@gmail.com>
2023-05-31 19:52:25 +0900

    feat: 주문하기 관련 local storage key, base url 상수 추가

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,9 +1,11 @@
 import { ServerKeys } from '../types/api';
 
 export const CART_LOCAL_STORAGE_KEY = 'cart';
+export const ORDERS_LOCAL_STORAGE_KEY = 'orders';
 
 export const PRODUCTS_BASE_URL = '/products';
 export const CART_BASE_URL = '/cart-items';
+export const ORDERS_BASE_URL = '/orders';
 export const POINT_BASE_URL = '/points';
 
 export const BASE_URLS = {
