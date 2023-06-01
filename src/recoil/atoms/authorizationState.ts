5e4c6b9d48530c10e603b186b0dd5d7cc83b8da8
solo5star <solo_5@naver.com>
2023-06-01 17:22:58 +0900

    feat: 사용자는 기본적으로 로그아웃된 상태로 설정

--- src/recoil/atoms/authorizationState.ts
+++ src/recoil/atoms/authorizationState.ts
@@ -4,10 +4,7 @@ import type { Server } from '../../types/Server';
 
 const authorizationState = atomFamily<Authorization | null, Server>({
   key: 'authorizationState',
-  default: {
-    username: 'a@a.com',
-    password: '1234',
-  },
+  default: null,
 });
 
 export default authorizationState;
