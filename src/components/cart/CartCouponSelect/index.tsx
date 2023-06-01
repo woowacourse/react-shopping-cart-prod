85d1ffd25dd53885e6a2c0a456ad2ddadb454dd5
gilpop8663 <wolfye@naver.com>
2023-06-02 01:32:52 +0900

    feat: 쿠폰 선택 버튼이 있는 컴포넌트에 실제 사용 가능한 쿠폰 데이터를 적용

--- src/components/cart/CartCouponSelect/index.tsx
+++ src/components/cart/CartCouponSelect/index.tsx
@@ -1,18 +1,31 @@
 import Button from '@components/common/Button';
+import { CouponType } from '@type/couponType';
 import * as S from './CartCouponSelect.style';
 
 interface CartCouponSelectProps {
-  coupons: number;
+  availableCouponLength: number;
+  selectedCoupon?: CouponType;
 }
 
-function CartCouponSelect({ coupons }: CartCouponSelectProps) {
+function CartCouponSelect({ availableCouponLength, selectedCoupon }: CartCouponSelectProps) {
+  const getMessage = () => {
+    if (selectedCoupon) {
+      return `${selectedCoupon.name}이 적용되었습니다.`;
+    }
+
+    if (availableCouponLength > 0) {
+      return '사용 가능한 쿠폰이 있습니다. 쿠폰을 적용하시겠어요?';
+    }
+
+    return '현재 사용 가능한 쿠폰이 없습니다.';
+  };
+
   return (
     <div>
-      <S.Title>쿠폰 (사용 가능한 쿠폰 {coupons}개)</S.Title>
+      <S.Title>쿠폰 (사용 가능한 쿠폰 {availableCouponLength}개)</S.Title>
       <S.InformationWrapper>
-        {coupons === 0 && <S.Message>현재 사용 가능한 쿠폰이 없습니다.</S.Message>}
-        {coupons > 0 && <S.Message>사용 가능한 쿠폰이 있습니다. 쿠폰을 적용하시겠어요?</S.Message>}
-        <Button text="쿠폰 선택" disabled={coupons === 0} />
+        <S.Message>{getMessage()}</S.Message>
+        <Button text="쿠폰 선택" disabled={availableCouponLength === 0} />
       </S.InformationWrapper>
     </div>
   );
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,20 +1,52 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import serverState from '@recoil/server/serverState';
 import { useCheckCart } from '@hooks/recoil/cart/useCheckCart';
+import CartCouponSelect from '@components/cart/CartCouponSelect';
 import CartItemList from '@components/cart/CartItemList';
 import ExpectedPayment from '@components/cart/ExpectedPayment';
 import SkeletonCart from '@components/cart/SkeletonCartItemList';
 import Layout from '@components/layout/Layout';
+import { getAvailableCouponsByTotalPrice } from '@utils/coupon/coupon';
+import { getCoupon } from '@utils/coupon/fetchCoupon';
+import { CouponType } from '@type/couponType';
 import * as S from './Cart.style';
 
+const DELIVERY_FEE = 3000;
+
 function Cart() {
+  const serverName = useRecoilValue(serverState);
+  const [coupons, setCoupons] = useState<CouponType[]>([]);
   const { totalCartPrice } = useCheckCart();
+
+  useEffect(() => {
+    const fetchCoupon = async () => {
+      const coupons = await getCoupon(serverName);
+
+      setCoupons(coupons);
+    };
+
+    fetchCoupon();
+  }, [serverName]);
+
+  const availableCouponLength = getAvailableCouponsByTotalPrice({
+    coupons,
+    totalItemsPrice: totalCartPrice,
+  }).length;
 
   return (
     <Layout>
       <S.CartPageContainer>
         <Suspense fallback={<SkeletonCart />}>
           <CartItemList />
-          <ExpectedPayment totalItemsPrice={totalCartPrice} deliveryFee={3000} discountPrice={0} />
+          <div>
+            <CartCouponSelect availableCouponLength={availableCouponLength} />
+            <ExpectedPayment
+              totalItemsPrice={totalCartPrice}
+              deliveryFee={DELIVERY_FEE}
+              discountPrice={0}
+            />
+          </div>
         </Suspense>
       </S.CartPageContainer>
     </Layout>
