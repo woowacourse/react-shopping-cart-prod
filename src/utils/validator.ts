955b4a1282b8de28e404364b9ca2092717237b48
ashleysyheo <ashleysyheo@gmail.com>
2023-06-01 11:53:38 +0900

    feat: 두 객체가 같은 key-value를 가지고 있는지 검증하는 유틸 함수 생성

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -2,4 +2,15 @@ const isNumber = (inputValue: string) => {
   return /^[0-9]*$/.test(inputValue);
 };
 
-export { isNumber };
+const areObjectsEqual = <T extends Record<string, unknown>>(obj1: T, obj2: T): boolean => {
+  const keys1 = Object.keys(obj1) as Array<keyof T>;
+  const keys2 = Object.keys(obj2) as Array<keyof T>;
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  return keys1.every((key) => obj1[key] === obj2[key]);
+};
+
+export { isNumber, areObjectsEqual };
