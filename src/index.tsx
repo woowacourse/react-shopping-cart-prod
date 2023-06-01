b7f4a9a09476d80bb9bead04e60ab8c6c9417931
bassyu <yrkdgus@gmail.com>
2023-06-01 15:10:36 +0900

    chore(index.tsx): msw 실행

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { worker } from './mocks/worker';
 
 import '../public/assets/logoWhite.svg';
 import '../public/assets/logoBlack.svg';
@@ -12,6 +13,8 @@ import '../public/assets/whiteCheck.svg';
 import '../public/assets/404.svg';
 import '../public/assets/emptyProduct.svg';
 import '../public/assets/shoppingBag.svg';
+
+worker.start();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
