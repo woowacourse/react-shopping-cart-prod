466ebacbe0b32cbcada0b5eecc0cd4f1389f9d86
jeongwusi <jeongwusi@gmail.com>
2023-06-01 17:04:44 +0900

    feat: PaymentsInfo 컴포넌트 생성

---
+++ src/components/orderDetail/PaymentsInfo.tsx
@@ -0,0 +1,46 @@
+import { styled } from 'styled-components';
+
+interface Props {
+  orderTotalPrice: number;
+}
+
+const PaymentsInfo = ({ orderTotalPrice }: Props) => {
+  return (
+    <S.Wrapper>
+      <S.TitleWrapper>
+        <S.Title>결제금액 정보</S.Title>
+      </S.TitleWrapper>
+      <S.TotalPaymentAmountWrapper>
+        <S.Title>총 결제 금액</S.Title>
+        <S.TotalPaymentAmount>{orderTotalPrice}</S.TotalPaymentAmount>
+      </S.TotalPaymentAmountWrapper>
+    </S.Wrapper>
+  );
+};
+
+const S = {
+  Wrapper: styled.article`
+    border: solid 1px var(--gray-color);
+    width: 80%;
+  `,
+
+  TitleWrapper: styled.section`
+    display: flex;
+    align-items: center;
+    padding: 25px 20px;
+    background-color: var(--gray-color-300);
+    border-bottom: solid var(--black-color) 0.5px;
+  `,
+
+  Title: styled.h4``,
+
+  TotalPaymentAmountWrapper: styled.span`
+    display: flex;
+    justify-content: space-between;
+    padding: 25px 20px;
+  `,
+
+  TotalPaymentAmount: styled.span``,
+};
+
+export default PaymentsInfo;
