26764413b26b52a29e4f86a7c280fa5bee2605e3
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-06-01 19:26:39 +0900

    feat: 단일 주문 GET 요청 msw 핸들러 구현

--- src/mocks/handlers/orders.ts
+++ src/mocks/handlers/orders.ts
@@ -1,6 +1,7 @@
 import { rest } from 'msw';
 import store from 'utils/storage';
 import orderList from 'mocks/fixtures/orderList.json';
+import order from 'mocks/fixtures/order.json';
 import { SERVER_OWNER } from 'constants/storeKey';
 import type { ServerOwner } from 'types/serverOwner';
 import BASE_URL from 'constants/apiBaseURL';
@@ -11,5 +12,9 @@ const BASE_URL_BY_OWNER = BASE_URL[serverOwner];
 export const orders = [
   rest.get(`${BASE_URL_BY_OWNER}/orders`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(orderList));
+  }),
+
+  rest.get(`${BASE_URL_BY_OWNER}/orders/1`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(order));
   }),
 ];
