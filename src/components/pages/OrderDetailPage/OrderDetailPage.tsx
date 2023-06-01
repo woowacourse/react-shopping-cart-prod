7a37679ddaa92e625f8cc0184e3edc9459290fd1
Suyoung <jsy8492@gmail.com>
2023-06-01 16:23:41 +0900

    refactor: OrderDetailPage 스타일 수정 및 주문 데이터를 렌더링하도록 수정

--- src/components/pages/OrderDetailPage/OrderDetailPage.tsx
+++ src/components/pages/OrderDetailPage/OrderDetailPage.tsx
@@ -1,18 +1,36 @@
 import { styled } from 'styled-components';
+import { useRecoilValue } from 'recoil';
+import { useNavigate, useParams } from 'react-router-dom';
 import OrderList from '../../order/OrderList/OrderList';
+import { orderQuery } from '../../../recoil/selectors/order';
+import Spacer from '../../common/Spacer/Spacer';
+import { formatPrice } from '../../../utils/formatPrice';
 
 const OrderDetailPage = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  if (id === undefined) {
+    navigate('/error');
+
+    return <></>;
+  }
+
+  const order = useRecoilValue(orderQuery(Number(id)));
+  const { price } = order;
+
   return (
     <Container>
       <TitleWrapper>
         <Title>주문 내역 상세</Title>
       </TitleWrapper>
-      <OrderList />
+      <OrderList order={order} />
+      <Spacer height={30} />
       <OrderTotal>
         <OrderTotalHeader>결제금액 정보</OrderTotalHeader>
         <OrderTotalPrice>
           <span>총 결제금액</span>
-          <span>325,600원</span>
+          <Price>{formatPrice(price)}</Price>
         </OrderTotalPrice>
       </OrderTotal>
     </Container>
@@ -22,58 +40,63 @@ const OrderDetailPage = () => {
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  row-gap: 28px;
+  width: 60%;
 `;
 
 const TitleWrapper = styled.div`
   height: 67px;
-  border-bottom: 4px solid #333;
 `;
 
 const Title = styled.h2`
-  font-family: 'Noto Sans KR';
   font-weight: 700;
-  font-size: 32px;
+  font-size: 26px;
   line-height: 37px;
   text-align: center;
   letter-spacing: 0.5px;
-  color: #333;
+  color: ${(props) => props.theme.black};
 `;
 
 const OrderTotal = styled.div`
-  width: 560px;
-  height: 207px;
+  width: 360px;
+  height: 160px;
   margin-left: auto;
-  border: 1px solid #aaa;
+  border-radius: 8px;
+  border: 1px solid ${(props) => props.theme.color.gray300};
 `;
 
 const OrderTotalHeader = styled.div`
   display: flex;
   align-items: center;
-  height: 92px;
-  padding: 0 30px;
-  background: #f6f6f6;
-  border-bottom: 1px solid #aaa;
-  font-family: 'Noto Sans KR';
-  font-weight: 700;
-  font-size: 28px;
+  padding: 20px 30px;
+  height: 70px;
+  color: ${(props) => props.theme.color.black};
+  background-color: ${(props) => props.theme.color.white};
+  border-bottom: 2px solid ${(props) => props.theme.color.gray300};
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  font-weight: 600;
+  font-size: 20px;
   line-height: 28px;
   letter-spacing: 0.5px;
-  color: #333;
 `;
 
 const OrderTotalPrice = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 115px;
+  height: 90px;
   padding: 0 30px;
-  font-family: 'Noto Sans KR';
-  font-weight: 700;
-  font-size: 24px;
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+  font-weight: 600;
+  font-size: 22px;
   line-height: 24px;
   letter-spacing: 0.5px;
-  color: #333;
+  color: ${(props) => props.theme.color.black};
+`;
+
+const Price = styled.span`
+  color: ${(props) => props.theme.color.primary};
 `;
 
 export default OrderDetailPage;
