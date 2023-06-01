69dc4cf4313558d8948be104bf4a416ed994a11a
Q Kim <qoli0703@gmail.com>
2023-06-01 17:30:20 +0900

    test: 스토리북 코드 수정

--- src/components/common/Coupon/Coupon.stories.ts
+++ src/components/common/Coupon/Coupon.stories.ts
@@ -5,6 +5,11 @@ const meta: Meta<typeof Coupon> = {
   title: 'Coupon',
   component: Coupon,
   tags: ['autodocs'],
+  args: {
+    header: '우아한테크코스 레벨2',
+    couponName: '신나는 협업 미션!',
+    footer: '- 당장 코딩하러 가기',
+  },
 };
 
 export default meta;
@@ -13,15 +18,11 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    header: '우아한테크코스 레벨2',
-    couponName: '신나는 협업 미션!',
-    footer: '- 당장 코딩하러 가기',
+    onDownloadClick: undefined,
   },
 };
 
 export const WithDownloadButton: Story = {
-  ...Default,
-
   argTypes: {
     onDownloadClick: {
       action: '쿠폰 다운로드 요청 보냄!',
