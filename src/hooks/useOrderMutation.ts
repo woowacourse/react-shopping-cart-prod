22cefe0c739b60c05f51afcd4f3e8046dada7c1f
solo5star <solo_5@naver.com>
2023-06-01 09:54:57 +0900

    feat: 주문하는 mutation 훅 추가

---
+++ src/hooks/useOrderMutation.ts
@@ -0,0 +1,29 @@
+import { useRecoilValue } from 'recoil';
+import clientState from '../recoil/atoms/clientState';
+import userRemoteCartItemsState from '../recoil/user/userRemoteCartItemsState';
+import useMutation from './useMutation';
+
+type UseOrderMutationParams = {
+  usedPoints: number;
+};
+
+const useOrderMutation = () => {
+  const client = useRecoilValue(clientState);
+  const { cartItems } = useRecoilValue(userRemoteCartItemsState);
+
+  return useMutation(({ usedPoints }: UseOrderMutationParams) =>
+    client
+      .post('/orders', {
+        usedPoints,
+        cartItems: cartItems.map((cartItem) => ({
+          id: cartItem.id,
+          productId: cartItem.product.id,
+          quantity: cartItem.quantity,
+        })),
+      })
+      .acceptOrThrow(201)
+      .then((response) => Number(response.headers.location.split('/').pop())),
+  );
+};
+
+export default useOrderMutation;
