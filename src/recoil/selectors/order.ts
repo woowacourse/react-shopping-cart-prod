a72e72a509eebb74ebca99fc4c220ff7793ff762
Suyoung <jsy8492@gmail.com>
2023-06-01 16:43:31 +0900

    feat: 주문 목록 조회 selector 작성

--- src/recoil/selectors/order.ts
+++ src/recoil/selectors/order.ts
@@ -1,5 +1,5 @@
-import { selectorFamily } from 'recoil';
-import { fetchOrder } from '../../remotes/order';
+import { selector, selectorFamily } from 'recoil';
+import { fetchOrder, fetchOrders } from '../../remotes/order';
 import { ORDERS_BASE_URL } from '../../constants/api';
 import { serverOriginState } from '../atoms/common';
 import type { Order } from '../../types/order';
@@ -15,4 +15,15 @@ export const orderQuery = selectorFamily<Order, Order['id']>({
 
       return order;
     },
+});
+
+export const ordersQuery = selector<Order[]>({
+  key: 'orders',
+  get: async ({ get }) => {
+    const orders = await fetchOrders(
+      `${get(serverOriginState)}${ORDERS_BASE_URL}`,
+    );
+
+    return orders;
+  },
 });
