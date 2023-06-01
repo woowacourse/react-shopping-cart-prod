1d27c61c8d8afa9fa071d6e6ad2fdbfb8ff83ea6
Suyoung <jsy8492@gmail.com>
2023-06-01 14:54:11 +0900

    feat: 공통 모달 컴포넌트 구현

---
+++ src/components/common/Modal/Modal.tsx
@@ -0,0 +1,51 @@
+import { PropsWithChildren, useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { styled } from 'styled-components';
+import useModal from './useModal';
+
+const Modal = ({ children }: PropsWithChildren) => {
+  const { closeModal } = useModal();
+
+  const handleEscapeKeydown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      closeModal();
+    }
+  };
+
+  useEffect(() => {
+    document.body.addEventListener('keydown', handleEscapeKeydown);
+
+    return () =>
+      document.body.removeEventListener('keydown', handleEscapeKeydown);
+  }, []);
+
+  return createPortal(
+    <div role="dialog" aria-modal>
+      <BackDrop onClick={closeModal} />
+      <ModalContainer>{children}</ModalContainer>
+    </div>,
+    document.body,
+  );
+};
+
+const BackDrop = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.15);
+  z-index: ${(props) => props.theme.zIndex.modalBackdrop};
+`;
+
+const ModalContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: ${(props) => props.theme.color.white};
+  border-radius: 8px;
+  z-index: ${(props) => props.theme.zIndex.modalContainer};
+`;
+
+export default Modal;
---
+++ src/components/common/Modal/useModal.ts
@@ -0,0 +1,17 @@
+import { useRecoilState } from 'recoil';
+import { modalState } from '../../../recoil/atoms/common';
+
+const useModal = () => {
+  const [isModalOpen, setIsModalOpen] = useRecoilState(modalState);
+
+  const openModal = () => setIsModalOpen(true);
+
+  const closeModal = () => setIsModalOpen(false);
+
+  return {
+    isModalOpen,
+    openModal,
+    closeModal,
+  };
+};
+export default useModal;
