76ac234b3cc9dbe911d6f3fd7561ef4ad89ca803
inyeong-kang <tomatorkd@naver.com>
2023-06-01 17:29:14 +0900

    feat: 포인트 조회 API fetch시 인증 정보 추가

--- src/apis/point.ts
+++ src/apis/point.ts
@@ -2,14 +2,30 @@ import { servers } from '../constants/server';
 import type { Point } from '../types/point';
 import type { HostNameType } from '../types/server';
 
-export const getPoints = async (hostName: HostNameType) => {
-  const hostURL = servers[hostName];
-  const response = await fetch(`${hostURL}/points`);
+const email = process.env.REACT_APP_EMAIL;
+const password = process.env.REACT_APP_PASSWORD;
+const base64 = btoa(email + ':' + password);
 
-  if (!response.ok) {
-    throw new Error(response.status.toString());
-  }
+export const api = async (hostName: HostNameType) => {
+  const URL = `${servers[hostName]}/points`;
 
-  const data: Point = await response.json();
-  return data;
+  const getPoints = async () => {
+    const response = await fetch(URL, {
+      method: 'GET',
+      headers: {
+        Authorization: `Basic ${base64}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(response.status.toString());
+    }
+
+    const data: Point = await response.json();
+    return data;
+  };
+
+  return {
+    getPoints,
+  };
 };
