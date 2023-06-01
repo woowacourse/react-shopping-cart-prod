8b8fd5c017a04f24cb5249e499d5c134f45857f2
Q Kim <qoli0703@gmail.com>
2023-06-01 18:54:24 +0900

    refactor: 서버 요청 경로 상수화

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -8,10 +8,12 @@ export const BASE_URL: Record<ServerName, string | undefined> = {
   백엔드짱: process.env.REACT_APP_BACKEND_GOD_SERVER_URL,
 };
 
-export const DEFAULT_SERVER_NAME = Object.keys(BASE_URL)[0] as ServerName;
+export const DEFAULT_SERVER_NAME = Object.keys(BASE_URL)[3] as ServerName;
 
 export const PRODUCTS_PATH_NAME = `/products`;
 export const CART_ITEMS_PATH_NAME = `/cart-items`;
+export const ALL_COUPONS_PATH_NAME = '/coupons';
+export const COUPON_DOWNLOAD_PATH_NAME = '/members/coupon';
 
 export const CART_LIST_KEY = 'cart_list';
 
