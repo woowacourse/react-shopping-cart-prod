d0e526b75d2d4b73b058ab55b61fd07341a00bff
yogjin <youngjin988@gmail.com>
2023-06-01 21:43:36 +0900

    feat: `Member` 타입 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -34,4 +34,30 @@ interface OrderData {
   totalPrice: number;
 }
 
+export const RANK = {
+  normal: '일반',
+  silver: '실버',
+  gold: '골드',
+  platinum: '플래티넘',
+  diamond: '다이아몬드',
+} as const;
+
+export const DISCOUNT_RATE = {
+  [RANK.normal]: 0,
+  [RANK.silver]: 5,
+  [RANK.gold]: 10,
+  [RANK.platinum]: 15,
+  [RANK.diamond]: 20,
+} as const;
+
+export type RANK_VALUE = (typeof RANK)[keyof typeof RANK];
+
+export type RANK_DISCOUNT_RATE = (typeof DISCOUNT_RATE)[keyof typeof DISCOUNT_RATE];
+
+export interface Member {
+  id: string;
+  rank: RANK_VALUE;
+  discountRate: RANK_DISCOUNT_RATE;
+}
+
 export type { ProductItemData, CartItemData, PostCartItemRequestBody, OrderedItem, OrderData };
