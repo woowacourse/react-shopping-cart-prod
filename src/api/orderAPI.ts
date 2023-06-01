ce4e4c7971098fa10ca28169128467a631f2f490
yogjin <youngjin988@gmail.com>
2023-06-01 22:49:43 +0900

    feat: 특정 주문의 상세정보 확인 `/orders:orderId` get api 메서드 추가

--- src/api/orderAPI.ts
+++ src/api/orderAPI.ts
@@ -1,10 +1,17 @@
 import { API_ENDPOINT, CART_FETCH_OPTION_HEADERS } from '../constants/api';
-import { CartPriceData } from '../types';
+import { CartPriceData, OrderData } from '../types';
 import { fetchAPI } from './fetchAPI';
 
 export const getOrderAPI = (baseUrl: string) => {
   const getOrderList = async (): Promise<CartPriceData> => {
     return await fetchAPI(`${baseUrl}${API_ENDPOINT.ORDERS}`, {
+      method: 'GET',
+      headers: { ...CART_FETCH_OPTION_HEADERS },
+    });
+  };
+
+  const getOrder = async (orderId: OrderData['id']): Promise<Omit<OrderData, 'id'>> => {
+    return await fetchAPI(`${baseUrl}${API_ENDPOINT.ORDERS}/${orderId}`, {
       method: 'GET',
       headers: { ...CART_FETCH_OPTION_HEADERS },
     });
