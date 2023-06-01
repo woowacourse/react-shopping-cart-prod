0d987e6b26b52977f9f118227fb6762573dc6968
regularPark <srk99279@naver.com>
2023-06-01 11:23:15 +0900

    refactor: `CartList`의 관심사 분리

--- src/components/cart/CartList/index.tsx
+++ src/components/cart/CartList/index.tsx
@@ -1,24 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect } from 'react';
-import { cartState } from '../../../store/CartState';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { useCart } from '../../../hooks/useCart';
-import { CartItem, Product } from '../../../types';
+import { CartItem } from '../../../types';
 import CartListItem from '../CartListItem';
 import Checkbox from '../../@common/Checkbox';
 import TotalCheckbox from '../TotalCheckbox';
 import PriceWrapper from '../PriceWrapper';
 import { LoadingSpinner } from '../../@common/LoadingSpinner';
-import { CART_BASE_URL, PRODUCT_BASE_URL } from '../../../constants/url';
-import { serverState } from '../../../store/ServerState';
-import { productListState } from '../../../store/ProductListState';
-import useGet from '../../../hooks/useGet';
 import { S } from './CartList.styles';
 
-const CartList = () => {
-  const [cart, setCart] = useRecoilState(cartState);
-  const setProduct = useSetRecoilState(productListState);
-  const serverUrl = useRecoilValue(serverState);
+type Props = {
+  cart: CartItem[];
+  isLoading: boolean;
+};
+
+const CartList = ({ cart, isLoading }: Props) => {
   const {
     checkedItems,
     isChecked,
@@ -27,17 +22,7 @@ const CartList = () => {
     handleCheckAllItems,
     handleCheckedItem,
     handleRemoveCheckedItem,
-    initializeCheckItems,
   } = useCart();
-
-  const { data: fetchedCartList, isLoading } = useGet<CartItem[]>(`${serverUrl}${CART_BASE_URL}`);
-  const { data: fetchedProductList } = useGet<Product[]>(`${serverUrl}${PRODUCT_BASE_URL}`);
-
-  useEffect(() => {
-    if (fetchedCartList) setCart(fetchedCartList);
-    if (fetchedProductList) setProduct(fetchedProductList);
-    initializeCheckItems();
-  }, [serverUrl, fetchedProductList]);
 
   const cartList = cart.map((cartItem) => (
     <S.ItemWrapper key={cartItem.product.id}>
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,10 +1,34 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import CartList from '../components/cart/CartList';
 import { CartListWrapper } from '../style/ContentLayout';
+import { cartState } from '../store/CartState';
+import { productListState } from '../store/ProductListState';
+import { serverState } from '../store/ServerState';
+import useGet from '../hooks/useGet';
+import { CartItem, Product } from '../types';
+import { CART_BASE_URL, PRODUCT_BASE_URL } from '../constants/url';
+import { useEffect } from 'react';
+import { useCart } from '../hooks/useCart';
 
 const CartPage = () => {
+  const [cart, setCart] = useRecoilState(cartState);
+  const setProduct = useSetRecoilState(productListState);
+  const serverUrl = useRecoilValue(serverState);
+  const { initializeCheckItems } = useCart();
+
+  const { data: fetchedCartList, isLoading } = useGet<CartItem[]>(`${serverUrl}${CART_BASE_URL}`);
+  const { data: fetchedProductList } = useGet<Product[]>(`${serverUrl}${PRODUCT_BASE_URL}`);
+
+  useEffect(() => {
+    if (fetchedCartList) setCart(fetchedCartList);
+    if (fetchedProductList) setProduct(fetchedProductList);
+    initializeCheckItems();
+  }, [serverUrl, fetchedProductList]);
+
   return (
     <CartListWrapper>
-      <CartList />
+      <CartList cart={cart} isLoading={isLoading} />
     </CartListWrapper>
   );
 };
