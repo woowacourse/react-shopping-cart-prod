8595d5723b210eee081ec8d7659b9da63efee629
Suyoung <jsy8492@gmail.com>
2023-06-01 14:58:15 +0900

    feat: 주문 관련 api 요청 메서드 작성

---
+++ src/remotes/order.ts
@@ -0,0 +1,35 @@
+import { base64 } from '../constants/auth';
+import type { OrderPayload } from '../types/order';
+
+export const fetchOrder = async (url: string) => {
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Basic ${base64}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('주문 상세 내역을 불러올 수 없습니다.');
+  }
+
+  const order = await response.json();
+
+  return order;
+};
+
+export const postOrder = async (url: string, orderPayload: OrderPayload) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${base64}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(orderPayload),
+  });
+
+  if (!response.ok) {
+    throw new Error('주문에 실패했습니다.');
+  }
+
+  return response;
+};
