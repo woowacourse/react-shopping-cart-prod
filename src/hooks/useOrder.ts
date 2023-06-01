435c66634ccca330cec789afb257f18ca4876405
Suyoung <jsy8492@gmail.com>
2023-06-01 14:57:46 +0900

    feat: 주문 관련 로직을 담는 useOrder 커스텀 훅 작성

---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,32 @@
+import { useNavigate } from 'react-router-dom';
+import { ORDERS_BASE_URL } from '../constants/api';
+import { postOrder } from '../remotes/order';
+import useToast from '../components/common/Toast/useToast';
+import type { OrderPayload } from '../types/order';
+
+const useOrder = () => {
+  const navigate = useNavigate();
+  const { showToast } = useToast();
+
+  const sendOrder = async (orderPayload: OrderPayload) => {
+    try {
+      const response = await postOrder(ORDERS_BASE_URL, orderPayload);
+      const headers = Array.from(response.headers);
+      const locationHeader = headers.find(([name]) => name === 'location');
+
+      if (locationHeader === undefined) return;
+
+      const [, orderUrl] = locationHeader;
+      const orderId = orderUrl.replace(`${ORDERS_BASE_URL}/`, '');
+
+      navigate(`${ORDERS_BASE_URL}/complete/${orderId}`);
+    } catch (e) {
+      if (e instanceof Error) {
+        showToast('error', e.message);
+      }
+    }
+  };
+
+  return { sendOrder };
+};
+export default useOrder;
