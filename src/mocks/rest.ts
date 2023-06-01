0fbc44f538213af06ce9ccf04f8f9e2864bf193a
solo5star <solo_5@naver.com>
2023-06-01 14:54:23 +0900

    test: msw 에러 핸들링 추가

--- src/mocks/rest.ts
+++ src/mocks/rest.ts
@@ -3,6 +3,7 @@ import type { ExtractPathFromRestAPI, RestAPI } from '../api/rest/RestAPI';
 import type { ShoppingCartRestAPI } from '../api/rest/ShoppingCartRestAPI';
 import { joinPath } from '../api/utils/http';
 import { BASE_URL } from '../config/environment';
+import { MSWException } from './exceptions';
 import type { HigherResolver, Resolver } from './types';
 import { internalResponse } from './types';
 
@@ -12,23 +13,49 @@ type RestOptions = {
 class Rest<TRestAPI extends RestAPI> {
   constructor(private readonly options: RestOptions = {}) {}
 
-  handle<
+  private createResolver<
     Method extends TRestAPI['request']['method'],
     Path extends ExtractPathFromRestAPI<TRestAPI, Method>,
-  >(method: Method, path: Path, resolver: HigherResolver<TRestAPI, Method, Path>) {
-    const wrappedResolver: Resolver<TRestAPI, Method, Path> = (req, res, ctx) => {
+  >(higherResolver: HigherResolver<TRestAPI, Method, Path>): Resolver<TRestAPI, Method, Path> {
+    return (req, res, ctx) => {
       const wrappedRes = Object.assign(res, {
         response:
           internalResponse<
             Extract<TRestAPI, { request: { method: Method; path: Path } }>['response']
           >(),
       });
-      return resolver(req, wrappedRes, ctx);
+
+      const handleThrown = (thrown: unknown): any => {
+        if (thrown instanceof Error) {
+          return res(ctx.status(500), ctx.json({ message: thrown.message }));
+        }
+        if (thrown instanceof MSWException) {
+          return res(ctx.status(thrown.status), ctx.json({ message: thrown.message }));
+        }
+        return thrown;
+      };
+
+      try {
+        const resolverReturn = higherResolver(req, wrappedRes, ctx);
+        if (resolverReturn instanceof Promise) {
+          resolverReturn.then(handleThrown);
+        }
+        return resolverReturn;
+      } catch (error) {
+        return handleThrown(error);
+      }
     };
+  }
+
+  handle<
+    Method extends TRestAPI['request']['method'],
+    Path extends ExtractPathFromRestAPI<TRestAPI, Method>,
+  >(method: Method, path: Path, higherResolver: HigherResolver<TRestAPI, Method, Path>) {
+    const resolver = this.createResolver(higherResolver);
 
     return mswRest[method.toLowerCase() as Lowercase<Method>](
       joinPath(this.options.baseUrl, path),
-      wrappedResolver,
+      resolver,
     );
   }
 
