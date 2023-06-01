f4a7e9c3a5ed0995a50f76e0f971f3d043862c14
Gilpop8663 <wolfye@naver.com>
2023-06-01 11:29:28 +0900

    feat: 주문 내역 리스트 컴포넌트 디자인 구현 및 스토리북 적용

--- src/components/orders/OrderItemList/index.tsx
+++ src/components/orders/OrderItemList/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { OrderType } from '@type/orderType';
+import OrderItem from './OrderItem';
+import * as S from './OrderItemList.style';
 
-function OrderItemList() {
-  return <div>OrderItemList</div>;
+interface OrderItemListProps {
+  orders: OrderType[];
+}
+
+function OrderItemList({ orders }: OrderItemListProps) {
+  return (
+    <S.Container>
+      {orders.map((order) => (
+        <OrderItem order={order} />
+      ))}
+    </S.Container>
+  );
 }
 
 export default OrderItemList;
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -149,14 +149,16 @@ export const MOCK_ORDER_LIST: OrderType[] = [
         orderItemId: 3,
         name: '치킨',
         price: 10000,
-        imageUrl: 'http://example.com/chicken.jpg',
+        imageUrl:
+          'https://images.unsplash.com/photo-1578874557108-9fc2cfb1121e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
         quantity: 2,
       },
       {
         orderItemId: 5,
         name: '치킨',
         price: 10000,
-        imageUrl: 'http://example.com/chicken.jpg',
+        imageUrl:
+          'https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1776&q=80',
         quantity: 1,
       },
     ],
