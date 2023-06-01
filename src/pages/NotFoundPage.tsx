403cdf4856156bce97339d9d74bad96611164edd
inyeong-kang <tomatorkd@naver.com>
2023-06-01 12:19:59 +0900

    refactor: props 값 변경

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -3,7 +3,7 @@ import Message from '../components/Common/Message';
 const NotFoundPage = () => {
   return (
     <main>
-      <Message type='notFound' link />
+      <Message type='notFound' link='/' />
     </main>
   );
 };
