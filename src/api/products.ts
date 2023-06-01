8529dbdef32ddb4ad41e6f1a991f065dafe5439c
chsua <csj1919@naver.com>
2023-06-01 16:04:07 +0900

    feat: 판매 물품 api를 실제 서버로 연결하기

--- src/api/products.ts
+++ src/api/products.ts
@@ -4,19 +4,13 @@ import { SERVER_LIST, USER_TOKEN } from "./constants";
 import { ERROR_MESSAGE } from "constants/errorType";
 
 export const getProducts = async (serverId: ServerId): Promise<Product[]> => {
-  const response = await fetch(`/products`, {
+  const response = await fetch(`${SERVER_LIST[serverId]}/products`, {
     method: "GET",
     headers: {
       Authorization: `Basic ${USER_TOKEN}`,
     },
   });
 
-  // const response = await fetch(`${SERVER_LIST[serverId]}/products`, {
-  //   method: "GET",
-  //   headers: {
-  //     Authorization: `Basic ${USER_TOKEN}`,
-  //   },
-  // });
   const errorMessage = ERROR_MESSAGE[response.status] ?? ERROR_MESSAGE[0];
 
   if (!response.ok) throw new Error(errorMessage);
