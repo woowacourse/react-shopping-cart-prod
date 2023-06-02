a2f645f99c0dfeb69422fd563176a4e80bbfa358
inyeong-kang <tomatorkd@naver.com>
2023-06-02 13:52:46 +0900

    feat: 상품 재고에 따라 경고 메시지, 품절 라벨 보여주는 기능 구현

--- src/components/Product/ProductItem.tsx
+++ src/components/Product/ProductItem.tsx
@@ -16,6 +16,8 @@ const ProductItem = ({ product }: ProductItemProps) => {
   const { imageUrl, name, price, stock } = product;
   const { target, addProduct, addCount, subtractCount } = useCart(product);
   const isProductExistInCart = target && target.quantity > 0;
+  const isSoldOut = stock === 0;
+  const isSoldSoon = stock > 0 && stock <= 5;
 
   return (
     <ProductContainer>
@@ -25,37 +27,51 @@ const ProductItem = ({ product }: ProductItemProps) => {
           <ProductName>{name}</ProductName>
           <ProductPrice>{price.toLocaleString('ko-KR')} 원</ProductPrice>
         </dl>
-        {isProductExistInCart ? (
-          <AmountCounter
-            designType='main'
-            count={target.quantity}
-            addCount={addCount}
-            subtractCount={subtractCount}
-          />
+        {isSoldOut ? (
+          <SoldOutLabel>품절</SoldOutLabel>
         ) : (
-          <ProductCartBtn
-            type='button'
-            data-testid='product-cart-btn'
-            onClick={addProduct}
-          >
-            <CartIcon width={25} height={22} color='gray400' />
-          </ProductCartBtn>
+          <>
+            {isProductExistInCart ? (
+              <AmountCounter
+                designType='main'
+                count={target.quantity}
+                addCount={addCount}
+                subtractCount={subtractCount}
+              />
+            ) : (
+              <ProductCartBtn
+                type='button'
+                data-testid='product-cart-btn'
+                onClick={addProduct}
+              >
+                <CartIcon width={25} height={22} color='gray400' />
+              </ProductCartBtn>
+            )}
+          </>
         )}
       </ProductInfoContainer>
+      <SoldOutWarning>
+        {isSoldSoon && (
+          <>
+            <p>품절 임박! {stock}개 남았어요</p>
+          </>
+        )}
+      </SoldOutWarning>
     </ProductContainer>
   );
 };
 
 const ProductContainer = styled.div`
   width: 282px;
+}
 `;
 
 const ProductInfoContainer = styled.div`
   position: relative;
   display: flex;
   justify-content: space-between;
-  margin-top: 18px;
-  padding: 0 14px;
+  margin-top: 14px;
+  padding: 0 14px 6px 14px;
 `;
 
 const ProductName = styled.dt`
@@ -65,6 +81,40 @@ const ProductName = styled.dt`
 const ProductPrice = styled.dd`
   font-size: 20px;
   line-height: 26.67px;
+`;
+
+const SoldOutWarning = styled.p`
+  margin-left: 14px;
+  color: red;
+
+  font-weight: 800;
+
+  animation: blink 2s infinite;
+
+  @keyframes blink {
+    0% {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`;
+
+const SoldOutLabel = styled.div`
+  width: 80px;
+  height: 40px;
+  text-align: center;
+  color: red;
+  transform: rotate(-20deg);
+
+  font-size: 20px;
+  font-weight: 600;
+  padding-top: 5px;
+  border: 2px solid red;
 `;
 
 const ProductCartBtn = styled.button`
