52047cff19b2662c2f432ca1e7b802140449366a
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-02 14:54:22 +0900

    feat: 서버 변경 시 로그아웃을 묻는 기능 구현

--- src/recoil/serverAtom.ts
+++ src/recoil/serverAtom.ts
@@ -1,20 +1,46 @@
-import { AtomEffect, atom } from "recoil";
+import { AtomEffect, atom, selector } from "recoil";
 import { getLocalStorage, setLocalStorage } from "../utils/localStorage";
+import { useState } from "react";
+import { userRepository, userState } from "./userAtom.ts";
 
 const localStorageEffect: <T>(key: string) => AtomEffect<T> =
   (key: string) =>
-    ({ setSelf, onSet }) => {
-      const savedValue = getLocalStorage(key, null);
+  ({ setSelf, onSet }) => {
+    const savedValue = getLocalStorage(key, null);
 
-      if (savedValue) setSelf(savedValue);
+    if (savedValue) setSelf(savedValue);
 
-      onSet((newValue) => {
-        setLocalStorage(key, newValue);
-      });
-    };
+    onSet((newValue) => {
+      setLocalStorage(key, newValue);
+    });
+  };
 
 export const serverState = atom<string>({
   key: "serverState",
   default: "테스트",
-  effects: [localStorageEffect<string>('server')],
+  effects: [localStorageEffect<string>("server")],
+});
+
+export const serverRepository = selector({
+  key: "serverRepository",
+  get: ({ get, getCallback }) => {
+    const changeServer = getCallback(
+      ({ set, snapshot }) =>
+        async (newServer: string) => {
+          const user = await snapshot.getPromise(userState);
+          const { logout } = get(userRepository);
+
+          if (user) {
+            if (confirm("로그아웃 하시겠습니까?")) {
+              logout();
+              set(serverState, newServer);
+            }
+          } else {
+            set(serverState, newServer);
+          }
+          return;
+        }
+    );
+    return { changeServer };
+  },
 });
