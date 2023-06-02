c95f4a8bcc03a44e80615cb73d6d372d61bcecf2
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-06-02 19:45:33 +0900

    fix: 상품금액이 10만원 일때 할인이 들어가지 않는 오류 수정

--- src/util/getDiscountAmount.ts
+++ src/util/getDiscountAmount.ts
@@ -1,7 +1,7 @@
 import { DISCOUNT_BOUNDARY, DISCOUNT_PERCENT } from '../constants/policy';
 
 const getDiscountAmount = (totalProductAmount: number) => {
-  if (totalProductAmount > DISCOUNT_BOUNDARY)
+  if (totalProductAmount >= DISCOUNT_BOUNDARY)
     return totalProductAmount * (DISCOUNT_PERCENT / 100);
   return 0;
 };
