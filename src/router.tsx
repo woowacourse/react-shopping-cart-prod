2ea22545fdad718614e2a63bb42d0b5117c65f50
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-06-02 11:45:28 +0900

    feat: 주문 상세 페이지 경로 변경

--- src/router.tsx
+++ src/router.tsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter(
           element: <OrderListPage />,
         },
         {
-          path: 'order-detail',
+          path: 'order-detail/:orderId',
           element: <OrderDetailPage />,
         },
       ],
