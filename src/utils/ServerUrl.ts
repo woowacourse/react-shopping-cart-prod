f377d666a40b10c04134b19305f9de55b1691053
woo-jk <201702042@o.cnu.ac.kr>
2023-06-02 17:59:09 +0900

    feat: ServerUtil 쿠폰 관련 url getter 추가

--- src/utils/ServerUrl.ts
+++ src/utils/ServerUrl.ts
@@ -1,6 +1,9 @@
 import {
   BASE_URL,
   CART_ITEMS_PATH_NAME,
+  COUPONS_PATH_NAME,
+  MEMBERS_COUPONS_PATH_NAME,
+  MEMBERS_COUPON_PATH_NAME,
   PRODUCTS_PATH_NAME,
 } from '../constant';
 import { ServerName } from '../types/server';
@@ -11,6 +14,15 @@ const ServerUtil = {
 
   getCartItemsUrl: (serverName: ServerName) =>
     BASE_URL[serverName] + CART_ITEMS_PATH_NAME,
+
+  getCouponsUrl: (serverName: ServerName) =>
+    BASE_URL[serverName] + COUPONS_PATH_NAME,
+
+  getMembersCouponsUrl: (serverName: ServerName) =>
+    BASE_URL[serverName] + MEMBERS_COUPONS_PATH_NAME,
+
+  getMemberCouponUrl: (serverName: ServerName) =>
+    BASE_URL[serverName] + MEMBERS_COUPON_PATH_NAME,
 };
 
 export default ServerUtil;
