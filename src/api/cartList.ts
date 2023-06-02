e8e2d909913ff2e4343355b489059d83107c0cce
Jeremy <102432453+shackstack@users.noreply.github.com>
2023-06-02 21:36:57 +0900

    fix: 불필요한 코드 제거

--- src/api/cartList.ts
+++ src/api/cartList.ts
@@ -1,21 +1,7 @@
 import { AUTH } from '../constants/auth';
-import { BASE_URL } from '../constants/baseURL';
 
 export const fetchCartList = async <T>(baseURL: string): Promise<T> => {
   const response = await fetch(`${baseURL}/cart-items`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      authorization: `Basic ${AUTH}`,
-    },
-  });
-  const data = await response.json();
-
-  return data;
-};
-
-export const fetchCartItem = async <T>(id: number): Promise<T> => {
-  const response = await fetch(`${BASE_URL.SPLIT}/cart-items/${id}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
