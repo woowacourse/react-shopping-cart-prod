06943656ecf716bdc90d8fba18159f02bad1f04a
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-06-03 01:37:47 +0900

    feat: msw 쿠폰, 주문 기능 구현

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,8 +1,10 @@
 import { rest } from 'msw';
 
-import { CART_STORAGE_ID } from '../constants/storage';
 import products from './data/products.json';
-import cats from './data/cats.json';
+import coupons from './data/coupons.json';
+
+import { CART_STORAGE_ID } from '../constants/storage';
+
 import {
   addTargetProduct,
   deleteTargetProduct,
@@ -10,13 +12,17 @@ import {
   updateTargetQuantity,
 } from '../states/cartProducts/util';
 import type { CartProduct } from '../types/product';
+import { Order, OrderDetail } from '../types/order';
+
+const orders: Order[] = [];
+const orderDetails: OrderDetail[] = [];
 
 export const handlers = [
   rest.get('/products', (_, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.set('Content-Type', 'application/json'),
-      ctx.json(cats)
+      ctx.json(products)
     );
   }),
 
@@ -38,6 +44,25 @@ export const handlers = [
       ctx.set('Content-Type', 'application/json'),
       ctx.json(JSON.parse(localStorage.getItem(CART_STORAGE_ID) ?? '[]'))
     );
+  }),
+
+  rest.get('/coupons', (_, res, ctx) => {
+    return res(ctx.status(200), ctx.json(coupons));
+  }),
+
+  rest.get('/orders', (_, res, ctx) => {
+    return res(ctx.status(200), ctx.json(orders));
+  }),
+
+  rest.get('/orders/:orderId', (req, res, ctx) => {
+    const { orderId } = req.params;
+    const orderDetail = orderDetails.find(
+      orderDetail => orderDetail.order.orderId === Number(orderId)
+    );
+
+    if (!orderDetail) return res(ctx.status(400));
+
+    return res(ctx.status(200), ctx.json(orderDetail));
   }),
 
   rest.post<{ productId: number }>('/cart-items', (req, res, ctx) => {
@@ -63,6 +88,39 @@ export const handlers = [
     );
 
     return res(ctx.status(201), ctx.set('location', `/${product.id}`));
+  }),
+
+  rest.post<{
+    cartItemIds: number[];
+    totalPrice: number;
+    couponId: number;
+  }>('/orders', (req, res, ctx) => {
+    const reqOrder = req.body;
+
+    if (!Object.keys(reqOrder).every(key => key in reqOrder)) {
+      return res(ctx.status(400));
+    }
+
+    const cartItems = JSON.parse(localStorage.getItem(CART_STORAGE_ID) ?? '[]');
+    const orderItems = reqOrder.cartItemIds.map(cartItemId => {
+      return cartItems.find(
+        (cartItem: CartProduct) => cartItem.id === cartItemId
+      );
+    });
+
+    const order = {
+      orderId: orders.length + 1,
+      orderItems: orderItems,
+    };
+    orders.push(order);
+
+    const orderDetail = {
+      order: order,
+      totalPrice: reqOrder.totalPrice,
+    };
+    orderDetails.push(orderDetail);
+
+    return res(ctx.status(200));
   }),
 
   rest.patch<{ quantity: number }>(
