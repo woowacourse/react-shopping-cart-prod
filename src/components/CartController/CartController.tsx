d37a55411059992708a815af102b6808f17c4e49
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-02 15:11:37 +0900

    fix: 버튼으로 수량 조절 시 수량이 100개 이상으로 나갈 수 있는 문제 수정

--- src/components/CartController/CartController.tsx
+++ src/components/CartController/CartController.tsx
@@ -18,17 +18,34 @@ interface CartControllerProps {
 }
 
 function CartController({ product }: CartControllerProps) {
-  const { addCartItem, updateCartItemQuantity } = useRecoilValue(cartRepository);
-
+  const { addCartItem, updateCartItemQuantity } =
+    useRecoilValue(cartRepository);
   const quantity = useRecoilValue(quantityByProductIdSelector(product.id));
 
-  const handleChangeQuantity = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChangeQuantityByInput = (
+    event: ChangeEvent<HTMLInputElement>
+  ) => {
     const quantityInputValue = Number(
       event.target.value.replaceAll("/", "").replace(/\D/g, "")
     );
     const newQuantity = quantityInputValue > 100 ? 100 : quantityInputValue;
 
     updateCartItemQuantity(product, newQuantity);
+  };
+
+  const handleChangeQuantityByButton = (type: "increase" | "decrease") => {
+    switch (type) {
+      case "increase":
+        if (quantity < 100) {
+          updateCartItemQuantity(product, quantity + 1);
+        }
+        break;
+      case "decrease":
+        if (quantity > 0) {
+          updateCartItemQuantity(product, quantity - 1);
+        }
+        break;
+    }
   };
 
   return (
@@ -38,15 +55,18 @@ function CartController({ product }: CartControllerProps) {
           <CartBox>
             <QuantityControlButton
               onClick={() => {
-                updateCartItemQuantity(product, quantity - 1);
+                handleChangeQuantityByButton("decrease");
               }}
             >
               -
             </QuantityControlButton>
-            <QuantityInput value={quantity} onChange={handleChangeQuantity} />
+            <QuantityInput
+              value={quantity}
+              onChange={handleChangeQuantityByInput}
+            />
             <QuantityControlButton
               onClick={() => {
-                updateCartItemQuantity(product, quantity + 1);
+                handleChangeQuantityByButton("increase");
               }}
             >
               +
