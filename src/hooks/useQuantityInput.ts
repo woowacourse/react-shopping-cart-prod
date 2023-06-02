bdbfba1942d7a0991184fbe87d702aec6ca05df5
bassyu <yrkdgus@gmail.com>
2023-06-02 15:49:49 +0900

    feat(useQuantityInput): API나 이벤트에 로그인 토큰 적용

--- src/hooks/useQuantityInput.ts
+++ src/hooks/useQuantityInput.ts
@@ -3,55 +3,55 @@ import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 import * as api from '../api';
 import useToast from './useToast';
-import { cartState, serverNameState } from '../recoil/state';
+import { cartState, serverNameState, tokenState } from '../recoil/state';
 import { API_ERROR_MESSAGE, API_SUCCESS_MESSAGE } from '../constants';
 
 const useQuantityInput = (cartItemId: number) => {
-  const [input, setInput] = useState('');
-  const setCart = useSetRecoilState(cartState);
   const serverName = useRecoilValue(serverNameState);
+  const token = useRecoilValue(tokenState);
+  const setCart = useSetRecoilState(cartState);
+
+  const [input, setInput] = useState('');
   const { showToast } = useToast();
 
-  const getCart = async () => {
+  const getCart = async (token: string) => {
     try {
-      const cart = await api.getCart(serverName);
+      const cart = await api.getCart(serverName, token);
       setCart(cart);
     } catch {
       showToast('error', API_ERROR_MESSAGE.getCart);
     }
   };
 
-  const deleteCartItem = async () => {
+  const deleteCartItem = async (token: string) => {
     try {
-      await api.deleteCartItem(serverName, cartItemId);
+      await api.deleteCartItem(serverName, token, cartItemId);
       showToast('info', API_SUCCESS_MESSAGE.deleteCartItem);
     } catch {
       showToast('error', API_ERROR_MESSAGE.deleteCartItem);
-      return;
     }
-
-    getCart();
   };
 
-  const patchCartItemQuantity = async (quantity: number) => {
+  const patchCartItemQuantity = async (token: string, quantity: number) => {
     try {
-      await api.patchCartItemQuantity(serverName, cartItemId, quantity);
+      await api.patchCartItemQuantity(serverName, token, cartItemId, quantity);
+      setInput(quantity.toString());
       showToast('info', API_SUCCESS_MESSAGE.patchCartItemQuantity);
     } catch {
       showToast('error', API_ERROR_MESSAGE.postCartItem);
     }
-
-    getCart();
   };
 
   const setInputWithRequest = async (quantity: number) => {
+    if (token === null) return;
+
     if (quantity === 0) {
-      await deleteCartItem();
+      await deleteCartItem(token);
     } else {
-      await patchCartItemQuantity(quantity);
+      await patchCartItemQuantity(token, quantity);
     }
 
-    setInput(quantity.toString());
+    getCart(token);
   };
 
   return { input, setInput, setInputWithRequest };
