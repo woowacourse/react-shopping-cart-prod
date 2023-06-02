a6d12c20b0df1f7c723d022db5651e699e7b9d96
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-06-02 22:46:46 +0900

    refactor(CheckOutPriceSection): 금액 표시 분기 로직 추가 및 개선

--- src/components/CheckOutPriceSection/CheckOutPriceSection.tsx
+++ src/components/CheckOutPriceSection/CheckOutPriceSection.tsx
@@ -10,12 +10,11 @@ import styled from 'styled-components';
 import { createOrder } from 'apis/orders';
 
 const CheckOutPriceSection = () => {
-  const { checkedCartProductIds } = useCartCheckBox();
-  const { pointCost } = useCheckOutPointCostContext();
   const resetCartProducts = useResetRecoilState(cartProductsState);
-  const cartTotalPrice = useRecoilValue(checkedCartProductsTotalPrice(checkedCartProductIds));
-
   const navigate = useNavigate();
+  const { checkedCartProductIds } = useCartCheckBox();
+  const cartTotalPrice = useRecoilValue(checkedCartProductsTotalPrice(checkedCartProductIds));
+  const { pointCost } = useCheckOutPointCostContext();
 
   const isCheckedProductsExist = checkedCartProductIds.size > 0;
   const shippingFee = cartTotalPrice < SHIPPING_FEE_THRESHOLD ? BASE_SHIPPING_FEE : 0;
@@ -23,13 +22,13 @@ const CheckOutPriceSection = () => {
   const earnPoints = Math.ceil(paymentAmount / PERCENTAGE_OF_EARN_POINTS);
 
   const productTotalPriceText = `${cartTotalPrice.toLocaleString('ko-KR')}원`;
-  const shippingFeeText = `${shippingFee.toLocaleString('ko-KR')}원`;
-  const usedPointsText = `-${pointCost.toLocaleString('ko-KR')}원`;
+  const shippingFeeText = isCheckedProductsExist ? `${shippingFee.toLocaleString('ko-KR')}원` : '0원';
+  const usedPointsText = pointCost > 0 ? `-${pointCost.toLocaleString('ko-KR')}원` : '0원';
   const paymentAmountText = isCheckedProductsExist ? `${paymentAmount.toLocaleString('ko-KR')}원` : '0원';
   const orderConfirmButtonText = isCheckedProductsExist
     ? `총 ${checkedCartProductIds.size}건 주문하기(${paymentAmount.toLocaleString('ko-KR')}원)`
     : '주문하기';
-  const earnPointsText = `${earnPoints.toLocaleString('ko-KR')}P 적립 예정`;
+  const earnPointsText = isCheckedProductsExist ? `${earnPoints.toLocaleString('ko-KR')}P 적립 예정` : '';
 
   const addOrder = async () => {
     try {
