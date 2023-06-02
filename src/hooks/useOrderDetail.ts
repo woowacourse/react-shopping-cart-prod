f34bf73b0c9968c46dc8d80258112c26de5480ec
jeonjeunghoon <jeonjeunghoon@gmail.com>
2023-06-02 19:23:44 +0900

    fix: 주문 상세 정보 페이지 새로고침 시 데이터 불러오도록 수정

--- src/hooks/useOrderDetail.ts
+++ src/hooks/useOrderDetail.ts
@@ -7,6 +7,7 @@ import { OrderDetail } from '../types/order';
 import fetchApis from '../apis/fetchApis';
 import { TOAST_STATE } from '../constants/toast';
 import { selectedOrderIdState } from '../states/order';
+import { useLocation } from 'react-router-dom';
 
 export const useOrderDetail = () => {
   const [orderDetail, setOrderDetail] = useState<OrderDetail>();
@@ -14,10 +15,17 @@ export const useOrderDetail = () => {
   const serverName = useRecoilValue(serverNameState);
   const { getData } = fetchApis(serverName);
   const setToastState = useSetRecoilState(toastState);
+  const pathname = useLocation().pathname;
+
+  const findSelectedOrderId = () => {
+    return pathname.split('/').pop();
+  };
 
   const getOrders = async () => {
     try {
-      const data = await getData<OrderDetail>(`/orders/${selectedOrderId}`);
+      const data = await getData<OrderDetail>(
+        `/orders/${selectedOrderId ?? findSelectedOrderId()}`
+      );
       setOrderDetail(data);
     } catch {
       setToastState(TOAST_STATE.failedGetOrder);
