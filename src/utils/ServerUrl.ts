46c62327c9c9950d3098f78f8c5ba7a4af810f17
woo-jk <201702042@o.cnu.ac.kr>
2023-06-02 20:28:24 +0900

    refactor: ServerUtil 쿠폰 url path name 변경

--- src/utils/ServerUrl.ts
+++ src/utils/ServerUrl.ts
@@ -2,9 +2,8 @@ import {
   BASE_URL,
   CART_ITEMS_PATH_NAME,
   COUPONS_PATH_NAME,
-  MEMBERS_COUPONS_PATH_NAME,
-  MEMBERS_COUPON_PATH_NAME,
   PRODUCTS_PATH_NAME,
+  USERS_COUPON_PATH_NAME,
 } from '../constant';
 import { ServerName } from '../types/server';
 
@@ -18,11 +17,8 @@ const ServerUtil = {
   getCouponsUrl: (serverName: ServerName) =>
     BASE_URL[serverName] + COUPONS_PATH_NAME,
 
-  getMembersCouponsUrl: (serverName: ServerName) =>
-    BASE_URL[serverName] + MEMBERS_COUPONS_PATH_NAME,
-
-  getMemberCouponUrl: (serverName: ServerName) =>
-    BASE_URL[serverName] + MEMBERS_COUPON_PATH_NAME,
+  getUsersCouponUrl: (serverName: ServerName) =>
+    BASE_URL[serverName] + USERS_COUPON_PATH_NAME,
 };
 
 export default ServerUtil;
