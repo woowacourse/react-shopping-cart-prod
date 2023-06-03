621c096d7a39123707132cb935e9e1463bcbe4ff
solo5star <solo_5@naver.com>
2023-06-04 01:47:07 +0900

    feat: `ErrorBoundary` 동작 방식 변경 및 JSDoc 추가

--- src/components/utils/ErrorBoundary.tsx
+++ src/components/utils/ErrorBoundary.tsx
@@ -6,10 +6,28 @@ type ErrorBoundaryState<TError> = {
 };
 
 type ErrorBoundaryProps<TError> = PropsWithChildren<
-  ({ catches: (error: unknown) => unknown } | { catchesInstanceof: new () => TError }) &
-    ({ fallback?: React.ReactNode } | { fallbackRenderer?: (error: TError) => React.ReactNode })
+  {
+    catchesInstanceof?: new () => TError;
+    catches?: (error: unknown) => boolean;
+  } & ({ fallback?: React.ReactNode } | { fallbackRenderer?: (error: TError) => React.ReactNode })
 >;
 
+/**
+ * 컴포넌트 렌더링 중 throw된 값을 catch합니다. 이는 대체로 {@link Error} 입니다.
+ *
+ * `catches` 혹은 `catchesInstanceof` prop을 사용하면 특정 값만 catch할 수 있습니다.
+ * 만약 `catches`와 `catchesInstanceof` 둘 다 명시하지 않는다면 모든 값을 catch합니다.
+ *
+ * `catches`와 `catchInstanceof` 둘 다 사용한다면 `catchInstanceof` 를 먼저 검사한 뒤 `catches` 로 검사합니다.
+ *
+ * @example
+ * <ErrorBoundary
+ *   catches={(thrown) => thrown instanceof MyError}
+ *   fallback={<div>오류가 발생하였습니다!</div>}
+ * >
+ *   <UserInfo />
+ * </ErrorBoundary>
+ */
 class ErrorBoundary<TError> extends React.Component<
   ErrorBoundaryProps<TError>,
   ErrorBoundaryState<TError>
@@ -22,15 +40,12 @@ class ErrorBoundary<TError> extends React.Component<
   override componentDidCatch(thrown: unknown, thrownInfo: unknown) {
     const { props } = this;
 
-    if ('catchesInstanceof' in props) {
-      if (thrown instanceof props.catchesInstanceof) return;
-
-      throw thrown;
+    if ('catchesInstanceof' in props && typeof props.catchesInstanceof === 'function') {
+      if (!(thrown instanceof props.catchesInstanceof)) throw thrown;
     }
-    if ('catches' in props) {
-      if (props.catches(thrown)) return;
+    if ('catches' in props && typeof props.catches === 'function') {
+      if (!props.catches(thrown)) throw thrown;
     }
-    throw thrown;
   }
 
   static getDerivedStateFromError(error: unknown) {
--- src/components/utils/ResponseErrorBoundary.tsx
+++ src/components/utils/ResponseErrorBoundary.tsx
@@ -3,22 +3,26 @@ import RestClientResponse from '../../api/RestClientResponse';
 import type { HttpResponse } from '../../api/rest/RestAPI';
 import ErrorBoundary from './ErrorBoundary';
 
-type ResponseErrorBoundaryProps<TResponse extends HttpResponse> = PropsWithChildren<{
-  catches: (response: RestClientResponse<TResponse>) => TResponse | null;
-  fallback?: React.ReactNode | ((response: TResponse) => React.ReactNode);
-}>;
+type ResponseErrorBoundaryProps<TResponse extends HttpResponse> = PropsWithChildren<
+  {
+    catches: (response: RestClientResponse<TResponse>) => TResponse | null;
+  } & (
+    | { fallback?: React.ReactNode }
+    | { fallbackRenderer?: (response: TResponse) => React.ReactNode }
+  )
+>;
 
 const ResponseErrorBoundary = <TResponse extends HttpResponse = HttpResponse>(
   props: ResponseErrorBoundaryProps<TResponse>,
 ) => {
-  const { catches, fallback, children } = props;
+  const { catches, children, ...errorBoundaryProps } = props;
 
   return (
     <ErrorBoundary
       catches={(thrown) =>
         thrown instanceof RestClientResponse<TResponse> && catches(thrown) !== null
       }
-      fallback={fallback}
+      {...errorBoundaryProps}
     >
       {children}
     </ErrorBoundary>
