5b97366268b2f1f0c1afd99da53ce6c6e8a87a0c
solo5star <solo_5@naver.com>
2023-06-04 02:25:23 +0900

    feat: 401 오류 시 `LoginPage`를 띄우도록 구현

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -1,6 +1,10 @@
-import { Outlet, useNavigate } from 'react-router-dom';
+import type { PropsWithChildren } from 'react';
+import { useState } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
 import AppBar from '../components/page/AppBar';
+import ResponseErrorBoundary from '../components/utils/ResponseErrorBoundary';
+import LoginPage from './LoginPage';
 
 const Content = styled.main`
   margin: 0 auto;
@@ -10,14 +14,36 @@ const Content = styled.main`
   max-width: 1300px;
 `;
 
+type UnauthorizedErrorBoundaryProps = PropsWithChildren;
+
+const UnauthorizedErrorBoundary = (props: UnauthorizedErrorBoundaryProps) => {
+  const { children } = props;
+  const location = useLocation();
+  const [key, setKey] = useState<number>(0);
+  const rerender = () => setKey(Date.now());
+
+  return (
+    <ResponseErrorBoundary
+      key={location.pathname + key}
+      catches={(response) => response.accept(401)}
+      fallback={<LoginPage onLoginSuccess={rerender} />}
+    >
+      {children}
+    </ResponseErrorBoundary>
+  );
+};
+
 const RootPage = () => {
   const navigate = useNavigate();
 
   return (
     <>
       <AppBar onNavigate={navigate} />
+
       <Content>
-        <Outlet />
+        <UnauthorizedErrorBoundary>
+          <Outlet />
+        </UnauthorizedErrorBoundary>
       </Content>
     </>
   );
