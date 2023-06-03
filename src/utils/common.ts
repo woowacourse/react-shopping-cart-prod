ddfc7a3d729aded94aaca302f4c91cfce440d34f
gilpop8663 <wolfye@naver.com>
2023-06-03 16:48:43 +0900

    feat: 총 숫자와 퍼센트를 입력받아 해당되는 퍼센트가 숫자 몇인지 반환하는 함수 구현

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -41,3 +41,27 @@ export const getFormattedWon = (money: number) => {
 export const getFormattedPrice = (price: number) => {
   return price.toLocaleString('ko-KR');
 };
+
+interface GetPercentageNumberParams {
+  total: number;
+  percent: number;
+}
+
+export const getPercentageNumber = ({ total, percent }: GetPercentageNumberParams) => {
+  if (total < 0) {
+    console.error('총 가격이 음수일땐 안됩니다.');
+    return null;
+  }
+
+  if (percent < 0) {
+    console.error('퍼센트가 음수일땐 안됩니다.');
+    return null;
+  }
+
+  if (percent >100 ) {
+    console.error('퍼센트가 100 초과일땐 안됩니다.');
+    return null;
+  }
+
+  return total * (percent / 100);
+};
