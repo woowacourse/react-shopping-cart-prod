7f615c202189737b207c20ca1bf5ce82d23fc45f
jeongwusi <jeongwusi@gmail.com>
2023-06-03 19:36:06 +0900

    feat: 주문 상세 내역의 데이터를 받아오는 로직 생성

--- src/components/orderDetail/OrderDetailList.tsx
+++ src/components/orderDetail/OrderDetailList.tsx
@@ -1,40 +1,58 @@
 import { styled } from 'styled-components';
-import OrderDetailItems from './OrderDetailItems';
-import { orderDetails } from '../../data/mockData';
+import { useEffect } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { serverState } from '../../store/ServerState';
+import { OrderItemDetails } from '../../types';
+import { useFetchData } from '../../hooks/useFetchData';
+import { useParams } from 'react-router-dom';
+import { ORDER_DETAIL_BASE_URL } from '../../constants/url';
+import OrderDetailItem from './OrderDetailItem';
+import { orderDetailState } from '../../store/OrderDetailState';
 
 const OrderDetailList = () => {
+  const { orderId } = useParams();
+  const [orderDetailList, setOrderDetailList] = useRecoilState(orderDetailState);
+  const serverUrl = useRecoilValue(serverState);
+
+  const { api } = useFetchData<OrderItemDetails | undefined>(setOrderDetailList);
+
+  useEffect(() => {
+    api.get(`${serverUrl}${ORDER_DETAIL_BASE_URL.replace(':orderId', String(orderId))}`);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [serverUrl]);
+
   return (
     <>
       <S.Title>주문 내역 상세</S.Title>
-      {orderDetails.map((items) => (
-        <OrderDetailItems key={items.orderId} items={items} />
-      ))}
+      <S.Wrapper>
+        <S.OrderInfoWrapper>
+          <S.OrderNumber>주문번호: {orderDetailList?.orderId}</S.OrderNumber>
+        </S.OrderInfoWrapper>
+        {orderDetailList?.orderProducts.map((item) => (
+          <OrderDetailItem key={item.productId} item={item} />
+        ))}
+      </S.Wrapper>
     </>
   );
 };
 
 const S = {
   Wrapper: styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    width: 90%;
-    margin-bottom: 50%;
-
-    @media all and (max-width: 479px) {
-      & > :first-child {
-        font-size: 20px;
-        margin-top: 30px;
-        input {
-          width: 24px;
-        }
-
-        button {
-          width: fit-content;
-        }
-      }
-    }
+    border: solid 1px var(--gray-color);
+    border-bottom: none;
+    width: 80%;
   `,
+
+  OrderInfoWrapper: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 25px 20px;
+    background-color: var(--gray-color-300);
+    border-bottom: solid var(--black-color) 0.5px;
+  `,
+
+  OrderNumber: styled.h4``,
 
   Title: styled.h1`
     width: 80%;
