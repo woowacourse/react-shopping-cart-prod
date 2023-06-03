8bf03b6d63678e1eb2ffffda83dafd3b1a586918
TaeeunKim <xodms0309@naver.com>
2023-06-03 19:33:31 +0900

    refactor: 상품 목록 fetching을 selector로 변경 및 Suspense 적용

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,10 +1,19 @@
 import ContentLayout from 'components/@common/ContentLayout';
+import LoadingSkeleton from 'components/Product/ProductItem/LoadingSkeleton';
 import ProductItemList from 'components/Product/ProductItemList';
+import { ProductListWrapper } from 'components/Product/ProductItemList/ProductItemList.styles';
+import { Suspense } from 'react';
 
 const ProductList = () => {
+  const Loading = Array.from({ length: 16 }).map((_, idx) => (
+    <LoadingSkeleton key={idx} />
+  ));
+
   return (
     <ContentLayout>
-      <ProductItemList />
+      <Suspense fallback={<ProductListWrapper>{Loading}</ProductListWrapper>}>
+        <ProductItemList />
+      </Suspense>
     </ContentLayout>
   );
 };
