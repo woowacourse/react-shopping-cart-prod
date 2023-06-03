dfbcb6fd12dc69b33c43945da2287b6be334365b
Gilpop8663 <wolfye@naver.com>
2023-06-03 11:04:14 +0900

    refactor: 다음날 날짜를 가져오는 함수 테스트를 test.each로 변경

--- src/__test__/common.test.ts
+++ src/__test__/common.test.ts
@@ -1,11 +1,15 @@
 import { getFormattedNextDay } from '@utils/common';
 
 describe('공통 유틸 함수가 올바르게 작동하는 지 확인한다.', () => {
-  test('다음날 날짜를 가져오는 함수를 실행시키면 다음날의 날짜를 가져온다. ', () => {
-    const today = 1685607102432; // 목요일  6/01 2023
+  test.each([
+    [1685607102432, '금요일 6/2'],
+    [1685757671523, '일요일 6/4'],
+  ])(
+    '(%i일때 %s)다음날 날짜를 가져오는 함수를 실행시키면 다음날의 날짜를 가져온다.',
+    (today: number, expectedResult: string) => {
+      const result = getFormattedNextDay(today);
 
-    const result = getFormattedNextDay(today);
-
-    expect(result).toBe('금요일 6/2');
-  });
+      expect(result).toBe(expectedResult);
+    }
+  );
 });
