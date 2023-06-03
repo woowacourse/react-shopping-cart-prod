2629bd454ae4e7c7568b1d0b20a1d0a3d7921003
Jungwoo <evencoding@gmail.com>
2023-06-03 16:10:26 +0900

    feat: size 프로퍼티 추가

--- src/components/common/Spinner/Spinner.tsx
+++ src/components/common/Spinner/Spinner.tsx
@@ -9,15 +9,38 @@ const spinAnimation = keyframes`
   }
 `;
 
-const SpinnerContainer = styled.div`
-  border: 4px solid #f3f3f3;
+const Size = {
+  sm: 18,
+  m: 24,
+  lg: 36,
+};
+
+// const SpinnerContainer = styled.div`
+//   /* display: flex;
+//   justify-content: center;
+
+//   height: fit-content;
+//   background-color: red; */
+// `;
+
+const SpinnerContainer = styled.div<SpinnerProps>`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  border: 4px solid var(--grey-200);
   border-top: 4px solid var(--primary-color);
   border-radius: 50%;
-  width: 18px;
-  height: 18px;
+  width: ${(props) => Size[props.size]}px;
+  height: ${(props) => Size[props.size]}px;
   animation: ${spinAnimation} 1s linear infinite;
 `;
 
-export const Spinner = () => {
-  return <SpinnerContainer />;
+interface SpinnerProps {
+  size: 'sm' | 'm' | 'lg';
+}
+
+export const Spinner = ({ size }: SpinnerProps) => {
+  return <SpinnerContainer size={size} />;
 };
