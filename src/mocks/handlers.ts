883cbbe10746f1d9339d637556fb02b243c7a402
Dahye <06robin11@gmail.com>
2023-06-03 21:11:07 +0900

    chore: mock data 생성

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -5,6 +5,9 @@ import orderDetail from './orderDetail.json';
 import coupons from './coupons.json';
 import deliveryPolicy from './deliveryPolicy.json';
 import resultPrice from './resultPrice.json';
+import resultPrice2 from './resultPrice2.json';
+import resultPrice3 from './resultPrice3.json';
+import resultPrice4 from './resultPrice4.json';
 import cartItems from './cartItems.json';
 
 export const handlers = [
@@ -32,15 +35,23 @@ export const handlers = [
     return res(ctx.status(200), ctx.json(orderDetail));
   }),
 
-  rest.get('api/cart-items/coupon', (req, res, ctx) => {
+  rest.get('api/cart-items/coupon/', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(resultPrice));
   }),
 
   rest.get('api/cart-items/coupon/2', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(resultPrice));
+    return res(ctx.status(200), ctx.json(resultPrice4));
   }),
 
   rest.get('api/cart-items/coupon/1', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(resultPrice));
+    return res(ctx.status(200), ctx.json(resultPrice2));
+  }),
+
+  rest.get('api/cart-items/coupon/1,2', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(resultPrice3));
+  }),
+
+  rest.get('api/cart-items/coupon/2,1', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(resultPrice3));
   }),
 ];
