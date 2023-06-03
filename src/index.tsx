a1205ada443f406aa07d1354ed63c40ab5330ff1
yeopto <gunyeop530@gmail.com>
2023-06-03 15:49:51 +0900

    feat: msw 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "./style/GlobalStyle";
+import { worker } from "./mocks/workers";
+
+const main = async () => {
+  if (window.location.pathname === "/react-shopping-cart-prod") {
+    window.location.pathname = "/react-shopping-cart-prod/";
+    return;
+  }
+
+  await worker.start({
+    serviceWorker: {
+      url: "/react-shopping-cart-prod/mockServiceWorker.js",
+    },
+  });
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -13,3 +27,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+main();
