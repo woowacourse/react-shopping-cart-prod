f8923488456fa93e0381c43822e3d197749a3d5f
solo5star <solo_5@naver.com>
2023-06-04 01:16:45 +0900

    feat: `AwaitFuture`, `AwaitRecoilState`에서 내장된 `ErrorBoundary` 제거

--- src/components/utils/AwaitFuture.tsx
+++ src/components/utils/AwaitFuture.tsx
@@ -1,7 +1,6 @@
 import type { PropsWithChildren } from 'react';
 import { Suspense, useMemo } from 'react';
 import type Future from '../../utils/Future';
-import ErrorBoundary from './ErrorBoundary';
 
 type AwaitFutureLoaderProps<TData> = {
   future: Future<TData>;
@@ -20,25 +19,22 @@ const AwaitFutureLoader = <TData,>(props: AwaitFutureLoaderProps<TData>) => {
 type AwaitFutureProps<TData> = {
   future?: Future<TData> | null;
   loadingElement?: React.ReactNode;
-  errorElement?: React.ReactNode;
   children?: (
     data: TData,
   ) => PropsWithChildren extends { children?: infer Children } ? Children : never;
 };
 
 const AwaitFuture = <TData,>(props: AwaitFutureProps<TData>) => {
-  const { future, loadingElement, errorElement, children } = props;
+  const { future, loadingElement, children } = props;
 
   // state가 변경되었을 때 새로 렌더링하기 위함
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const key = useMemo(() => Date.now(), [future]);
 
   return (
-    <ErrorBoundary key={key} catchesInstanceof={Error} fallback={errorElement}>
-      <Suspense fallback={loadingElement}>
-        {future && <AwaitFutureLoader future={future}>{children}</AwaitFutureLoader>}
-      </Suspense>
-    </ErrorBoundary>
+    <Suspense key={key} fallback={loadingElement}>
+      {future && <AwaitFutureLoader future={future}>{children}</AwaitFutureLoader>}
+    </Suspense>
   );
 };
 
--- src/components/utils/AwaitRecoilState.tsx
+++ src/components/utils/AwaitRecoilState.tsx
@@ -1,9 +1,7 @@
 import type { PropsWithChildren } from 'react';
 import { Suspense, useMemo } from 'react';
 import type { RecoilValueReadOnly } from 'recoil';
-import { selector, useRecoilValue, useRecoilValueLoadable } from 'recoil';
-
-import ErrorBoundary from './ErrorBoundary';
+import { useRecoilValue } from 'recoil';
 
 type AwaitRecoilStateLoaderProps<T> = {
   state: RecoilValueReadOnly<T>;
@@ -22,29 +20,22 @@ const AwaitRecoilStateLoader = <T,>(props: AwaitRecoilStateLoaderProps<T>) => {
 type AwaitRecoilStateProps<T> = {
   state?: RecoilValueReadOnly<T>;
   loadingElement?: React.ReactNode;
-  errorElement?: React.ReactNode;
   children?: (
     data: T,
   ) => PropsWithChildren extends { children?: infer Children } ? Children : never;
 };
 
-const nullState = selector({ key: 'nullState', get: () => null });
-
 const AwaitRecoilState = <T,>(props: AwaitRecoilStateProps<T>) => {
-  const { errorElement, loadingElement, state, children } = props;
+  const { loadingElement, state, children } = props;
 
-  const loadable = useRecoilValueLoadable(state ?? nullState);
-
-  // state 혹은 loadable이 변경되었을 때 새로 렌더링하기 위함
+  // state가 변경되었을 때 새로 렌더링하기 위함
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const key = useMemo(() => Date.now(), [state, loadable]);
+  const key = useMemo(() => Date.now(), [state]);
 
   return (
-    <ErrorBoundary key={key} catchesInstanceof={Error} fallback={errorElement}>
-      <Suspense fallback={loadingElement}>
-        {state && <AwaitRecoilStateLoader state={state}>{children}</AwaitRecoilStateLoader>}
-      </Suspense>
-    </ErrorBoundary>
+    <Suspense key={key} fallback={loadingElement}>
+      {state && <AwaitRecoilStateLoader state={state}>{children}</AwaitRecoilStateLoader>}
+    </Suspense>
   );
 };
 
