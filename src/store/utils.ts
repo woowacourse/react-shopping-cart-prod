452ddf5ff5eee809798f8b41fe16f226b0855a54
yogjin <youngjin988@gmail.com>
2023-06-04 23:00:06 +0900

    feat: 주문 상품 리스트를 받아서 가격 정보를 구하는 유틸함수 추가

---
+++ src/store/utils.ts
@@ -0,0 +1,49 @@
+import { CartItemData, CartPriceData } from '../types';
+
+const getTotalItemDiscountAmount = (orderedItemList: CartItemData[]) => {
+  return orderedItemList.reduce((prev, curr) => {
+    if (curr.product.discountRate > 0) {
+      return prev + curr.quantity * (curr.product.price * (curr.product.discountRate / 100));
+    }
+    return prev;
+  }, 0);
+};
+
+const getTotalMemberDiscountAmount = (orderedItemList: CartItemData[]) => {
+  return (
+    getTotalItemPrice(orderedItemList) -
+    getDiscountedTotalItemPrice(orderedItemList) -
+    getTotalItemDiscountAmount(orderedItemList)
+  );
+};
+
+const getTotalItemPrice = (orderedItemList: CartItemData[]) => {
+  return orderedItemList.reduce((prev, curr) => {
+    return prev + +curr.quantity * curr.product.price;
+  }, 0);
+};
+
+const getDiscountedTotalItemPrice = (orderedItemList: CartItemData[]) => {
+  return orderedItemList.reduce((prev, curr) => {
+    return prev + +curr.quantity * curr.product.discountedPrice;
+  }, 0);
+};
+
+const getShippingFee = (orderedItemList: CartItemData[]) => {
+  return getDiscountedTotalItemPrice(orderedItemList) >= 50000 ? 0 : 3000;
+};
+
+const getTotalPrice = (orderedItemList: CartItemData[]) => {
+  return getDiscountedTotalItemPrice(orderedItemList) + getShippingFee(orderedItemList);
+};
+
+export const getCartPriceInformation = (orderedItemList: CartItemData[]): CartPriceData => {
+  return {
+    totalItemDiscountAmount: getTotalItemDiscountAmount(orderedItemList),
+    totalMemberDiscountAmount: getTotalMemberDiscountAmount(orderedItemList),
+    totalItemPrice: getTotalItemPrice(orderedItemList),
+    discountedTotalItemPrice: getDiscountedTotalItemPrice(orderedItemList),
+    shippingFee: getShippingFee(orderedItemList),
+    totalPrice: getTotalPrice(orderedItemList),
+  };
+};
