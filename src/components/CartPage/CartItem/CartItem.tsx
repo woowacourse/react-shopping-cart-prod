ee4b26e920be6d109e00556e5496df5497227ab6
n0eyes <doll4586633@naver.com>
2023-06-04 21:58:08 +0900

    feat: 장바구니 아이템 쿠폰 적용 기능 추가

--- src/components/CartPage/CartItem/CartItem.tsx
+++ src/components/CartPage/CartItem/CartItem.tsx
@@ -1,8 +1,12 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { productCouponsSelector } from '../../../atoms/coupon';
 import { DELETE_CART_ITEM } from '../../../constants/cart';
 import { useCartSelector, useMutateCart } from '../../../hooks/cart/cart';
+import { useRefreshableRecoilValue } from '../../../hooks/common/useRefreshableAtom';
 import { CartItem as CartItemType } from '../../../types/cart';
+import { SpecificCoupon } from '../../../types/coupon';
 import { debounce } from '../../../utils/debounce';
+import { getDiscountInfo } from '../../../utils/discount';
 import Flex from '../../common/Flex';
 import QuantityStepper from '../../common/QuantityStepper/QuantityStepper';
 import * as S from './CartItem.styles';
@@ -13,11 +17,34 @@ const CartItem: React.FC<CartItemProps> = (props) => {
   const {
     id,
     quantity,
-    product: { imageUrl, name, price },
+    product: { id: productId, imageUrl, name, price },
   } = props;
+
+  const [selectedCoupon, setSelectedCoupon] = useState<SpecificCoupon | null>(
+    null
+  );
+
   const quantityRef = useRef<HTMLInputElement>(null);
   const { selectedItems, selectItem } = useCartSelector();
   const { updateCartItemMutation, deleteCartItemMutation } = useMutateCart();
+  const productCoupons = useRefreshableRecoilValue(
+    productCouponsSelector(productId)
+  );
+
+  const discountInfo = selectedCoupon
+    ? getDiscountInfo(price, {
+        discountType: selectedCoupon.discountType,
+        value: selectedCoupon.value,
+      })
+    : null;
+
+  const itemPrice = (discountInfo?.discountedPrice ?? price).toLocaleString();
+
+  const selectCoupon = (id: SpecificCoupon['id']) => {
+    const targetCoupon = productCoupons?.find((coupon) => coupon.id === id);
+
+    setSelectedCoupon(targetCoupon ?? null);
+  };
 
   const updateQuantity = debounce(() =>
     updateCartItemMutation({
@@ -43,6 +70,16 @@ const CartItem: React.FC<CartItemProps> = (props) => {
           <S.Name>{name}</S.Name>
           <S.Info dir="column" justify="space-between" align="end">
             <S.DeleteButton onClick={deleteCartItem}>X</S.DeleteButton>
+            <select
+              defaultValue={-1}
+              onChange={({ target: { value } }) => selectCoupon(Number(value))}>
+              <option value={-1}>선택 없음</option>
+              {productCoupons?.map(({ id, name }) => (
+                <option key={id} value={id}>
+                  {name}
+                </option>
+              ))}
+            </select>
             <QuantityStepper
               max={100}
               min={1}
@@ -51,7 +88,12 @@ const CartItem: React.FC<CartItemProps> = (props) => {
               onIncrease={updateQuantity}
               onDecrease={updateQuantity}
             />
-            <S.Price>{price.toLocaleString()} 원</S.Price>
+            {selectedCoupon && (
+              <S.Price>
+                {discountInfo?.dcMsg} <del>{price.toLocaleString()} 원</del>
+              </S.Price>
+            )}
+            <S.Price>{itemPrice} 원</S.Price>
           </S.Info>
         </S.ProductContainer>
       </Flex>
