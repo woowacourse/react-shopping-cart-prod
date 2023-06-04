b8a7bdedb2f54f3f0b9025b9cfaa5a93eb34f2b4
cruelladevil <dev.timetravel@gmail.com>
2023-06-04 17:13:23 +0900

    fix: msw url 수정

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -2,7 +2,7 @@ export const SERVER = {
   도이: 'https://wooteco.store',
   우르: 'https://course-shopping.store',
   준팍: 'https://www.woowacourse.com',
-  MSW: 'http://hello.world',
+  MSW: process.env.PUBLIC_URL,
 } as const;
 
 export const PRODUCT_LIST_URL = '/products';
