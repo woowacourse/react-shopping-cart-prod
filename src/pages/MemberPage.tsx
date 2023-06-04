327d3abda582f8111bcd21b4e9d2c6b374590d8f
jeongwusi <jeongwusi@gmail.com>
2023-06-04 15:17:34 +0900

    feat: MemberPage 컴포넌트 생성

---
+++ src/pages/MemberPage.tsx
@@ -0,0 +1,12 @@
+import Member from '../components/member/Member';
+import { Wrapper } from '../style/ContentLayout';
+
+const MemberPage = () => {
+  return (
+    <Wrapper>
+      <Member />
+    </Wrapper>
+  );
+};
+
+export default MemberPage;
