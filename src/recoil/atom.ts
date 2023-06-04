587913a2a4dc2b80172171502f9e1270a7c73efc
yeopto <gunyeop530@gmail.com>
2023-06-05 03:34:42 +0900

    refactor: atom 셀렉터 로직 제거

--- src/recoil/atom.ts
+++ src/recoil/atom.ts
@@ -1,26 +1,9 @@
-import { atom, selector } from "recoil";
-import { LocalProductType, ProductType } from "../types/domain";
-import { fetchProducts } from "../api";
-import { makeLocalProducts } from "../utils/domain";
-
-export const productsState = atom<ProductType[]>({
-  key: "products",
-  default: selector<ProductType[]>({
-    key: "initialProducts/default",
-    get: async () => {
-      const response = await fetchProducts();
-      if (!response.ok) throw new Error(response.status.toString());
-      return await response.json();
-    },
-  }),
-});
+import { atom } from "recoil";
+import { LocalProductType } from "../types/domain";
 
 export const localProductsState = atom<LocalProductType[]>({
   key: "localProducts",
-  default: selector<LocalProductType[]>({
-    key: "products/default",
-    get: () => makeLocalProducts(),
-  }),
+  default: [],
 });
 
 export const selectedProductsState = atom<LocalProductType[]>({
