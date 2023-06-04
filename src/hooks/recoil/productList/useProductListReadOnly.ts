0748f6759acb094387cb85ab5356f94039860208
gilpop8663 <wolfye@naver.com>
2023-06-05 04:11:57 +0900

    chore: 사용하지 않는 훅 삭제 및 테스트 삭제

--- src/hooks/recoil/productList/useProductListReadOnly.ts
+++
@@ -1,6 +0,0 @@
-import { useRecoilValue } from 'recoil';
-import { productListState } from '@recoil/product/productListState';
-
-export const useProductListReadOnly = () => {
-  return useRecoilValue(productListState);
-};
