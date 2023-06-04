e1885c52438bea4349800cd9987f474ad7a8b5f6
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-06-04 16:07:24 +0900

    test(EmptyDataCard): props 추가로 인한 description 추가

--- src/components/EmptyDataCard/EmptyDataCard.stories.tsx
+++ src/components/EmptyDataCard/EmptyDataCard.stories.tsx
@@ -4,11 +4,16 @@ import EmptyDataCard from './EmptyDataCard';
 const meta = {
   component: EmptyDataCard,
   title: 'EmptyCartSection',
+  argTypes: {
+    children: {
+      description: '데이터가 비어있음을 설명하는 메세지입니다.',
+    },
+  },
 } satisfies Meta<typeof EmptyDataCard>;
 
 export default meta;
 type Story = StoryObj<typeof EmptyDataCard>;
 
 export const Default: Story = {
-  render: () => <EmptyDataCard />,
+  render: () => <EmptyDataCard>특정 데이터가 비어있어요</EmptyDataCard>,
 };
