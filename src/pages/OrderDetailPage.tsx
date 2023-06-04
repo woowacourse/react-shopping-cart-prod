2520dfecf228f1ae6ce423097067f064455f2162
solo5star <solo_5@naver.com>
2023-06-05 04:12:18 +0900

    feat: 주문 상세 페이지 수정

--- src/pages/OrderDetailPage.tsx
+++ src/pages/OrderDetailPage.tsx
@@ -1,7 +1,6 @@
 import { useParams } from 'react-router-dom';
 import OrderCartItem from '../components/OrderCartItem';
 import OrderListItem from '../components/OrderListItem';
-import OrderPaymentDetails from '../components/OrderPaymentDetails';
 import LoadingPlaceholder from '../components/common/LoadingPlaceholder';
 import PageHeader from '../components/page/PageHeader';
 import AwaitRecoilState from '../components/utils/AwaitRecoilState';
@@ -20,27 +19,16 @@ const OrderDetailPage = () => {
         loadingElement={<LoadingPlaceholder title="주문 상세 정보를 불러오는 중입니다 ..." />}
       >
         {(order) => (
-          <>
-            <OrderListItem order={order}>
-              {order.cartItems.map((orderCartItem) => (
-                <OrderCartItem
-                  productName={orderCartItem.name}
-                  productPrice={orderCartItem.price}
-                  quantity={orderCartItem.quantity}
-                  imageUrl={orderCartItem.imageUrl}
-                />
-              ))}
-            </OrderListItem>
-
-            <OrderPaymentDetails
-              points={order.usedPoints}
-              savingRate={order.savingRate}
-              price={order.cartItems.reduce(
-                (price, orderCartItem) => price + orderCartItem.price * orderCartItem.quantity,
-                0,
-              )}
-            />
-          </>
+          <OrderListItem order={order}>
+            {order.cartItems.map((orderCartItem) => (
+              <OrderCartItem
+                productName={orderCartItem.name}
+                productPrice={orderCartItem.price}
+                quantity={orderCartItem.quantity}
+                imageUrl={orderCartItem.imageUrl}
+              />
+            ))}
+          </OrderListItem>
         )}
       </AwaitRecoilState>
     </>
