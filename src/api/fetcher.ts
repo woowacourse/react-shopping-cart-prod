9a8a358dfa0ffca0b6e31bb9b976032baae6fac1
nangkyeonglim <nangkyeonglim@gmail.com>
2023-06-04 21:12:01 +0900

    feat: 쿠폰 get, 주문 post API fetcher 구현

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -1,3 +1,4 @@
+import { OrderRequest } from '../types/types';
 import { url } from './url';
 
 export const fetchMemberList = ({ server }: { server: string }) => {
@@ -25,5 +26,32 @@ export const fetchOrder = ({ server, auth, orderId }: { server: string; auth: st
     headers: {
       Authorization: `Basic ${auth}`,
     },
+  });
+};
+
+export const fetchCouponList = ({ server, auth }: { server: string; auth: string }) => {
+  return fetch(`${url[server]}/coupons`, {
+    headers: {
+      Authorization: `Basic ${auth}`,
+    },
+  });
+};
+
+export const fetchPostOrder = ({
+  server,
+  auth,
+  bodyData,
+}: {
+  server: string;
+  auth: string;
+  bodyData: OrderRequest;
+}) => {
+  return fetch(`${url[server]}/orders`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${auth}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(bodyData),
   });
 };
