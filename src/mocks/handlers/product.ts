1f574b7ba9d2a90f27985e0c517f77359a5d5a1e
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-06-05 07:05:04 +0900

    feat: mockData 삭제

--- src/mocks/handlers/product.ts
+++
@@ -1,8 +0,0 @@
-import { rest } from 'msw';
-import products from 'mocks/fixtures/products.json';
-
-export const product = [
-  rest.get('/products', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(products));
-  }),
-];
