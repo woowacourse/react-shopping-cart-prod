aeaf7cb155f08941f1f54d1d87222668d363428d
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-06-04 11:53:54 +0900

    refactor(OrderCardList): 로딩 에러 카드 적용

--- src/components/OrderCardList/OrderCardList.tsx
+++ src/components/OrderCardList/OrderCardList.tsx
@@ -3,12 +3,15 @@ import Box from 'components/@common/Box';
 import useFetch from 'hooks/useFetch';
 import { Order } from 'types/order';
 import OrderCard from './OrderCard/OrderCard';
+import LoadingErrorCard from 'components/LoadingErrorCard/LoadingErrorCard';
 
 const OrderCardList = () => {
-  const { data: orderList, isLoading, errorState } = useFetch<Order[]>(getOrderList);
+  const { data: orderList, isLoading, errorState, fetchData } = useFetch<Order[]>(getOrderList);
 
   if (isLoading) return <div>주문목록 로딩중</div>;
-  if (errorState?.isError) return <div>주문목록 로딩 실패</div>;
+  if (errorState?.isError) {
+    return <LoadingErrorCard onClickRetryButton={fetchData}>{errorState.error.message}</LoadingErrorCard>;
+  }
 
   return (
     <Box sizing={{ width: '100%' }} flex={{ flexDirection: 'column', gap: '60px' }} role="list">
