3e4b7c266bf5488c053745c227c330e0e9478abb
gyeongza <gyeongza@naver.com>
2023-06-04 15:48:25 +0900

    [step2] refactor: 서버 변경시 홈으로 돌아가도록 변경

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -31,6 +31,8 @@ export const Header = () => {
                   setCartItems(cartItems);
                 });
 
+                navigate('/');
+
                 return newApiEndPoint;
               });
             }}
