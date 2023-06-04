08ea4375f13185b4f96c81f08a55a6d3a6b147d1
nangkyeonglim <nangkyeonglim@gmail.com>
2023-06-04 21:02:42 +0900

    feat: 서버 mocking을 위한 MSW handler 작성

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,18 +1,18 @@
 // src/mocks/handlers.js
 import { rest } from 'msw';
 import { getLocalStorage, setLocalStorage } from '../utils/localStorage';
-import type { CartItem, Order, OrdersResponses, OrderRequest, Point } from '../types/types';
+import type { CartItem, Order, OrdersResponses, OrderRequest, Point, CartItemResponses } from '../types/types';
 import mockProductList from '../mocks/mockProductListData.json';
 import mockMemberList from '../mocks/mockMemberListData.json';
 import mockCouponList from '../mocks/mockCouponListData.json';
 import { ProductItem } from '../types/types';
-import { Coupon } from '../types/types';
 
+let couponList = [...mockCouponList];
 const LOCAL_STORAGE_KEY = {
-  CART_LIST: 'cartList',
-  MEMBER_LIST: 'memberList',
-  POINT: 'point',
-  ORDER_LIST: 'orderList',
+  CART_LIST: 'mockCartList',
+  MEMBER_LIST: 'mockMemberList',
+  POINT: 'mockPoint',
+  ORDER_LIST: 'mockOrder',
 };
 
 export const handlers = [
@@ -21,7 +21,7 @@ export const handlers = [
   }),
 
   rest.get('/cart-items', (_, res, ctx) => {
-    const cartItems = getLocalStorage(LOCAL_STORAGE_KEY.CART_LIST, []);
+    const cartItems = getLocalStorage<CartItemResponses>(LOCAL_STORAGE_KEY.CART_LIST, { cartItems: [], totalPrice: 0 });
 
     return res(ctx.delay(100), ctx.status(200), ctx.json(cartItems));
   }),
@@ -29,7 +29,7 @@ export const handlers = [
   rest.post('/cart-items', async (req, res, ctx) => {
     const { productId } = await req.json();
 
-    const cartItems = getLocalStorage(LOCAL_STORAGE_KEY.CART_LIST, []);
+    const cartItems = getLocalStorage<CartItemResponses>(LOCAL_STORAGE_KEY.CART_LIST, { cartItems: [], totalPrice: 0 });
 
     const newItemId = Date.now();
 
@@ -39,17 +39,17 @@ export const handlers = [
       product: mockProductList.find((product) => product.id === productId),
     };
 
-    setLocalStorage(LOCAL_STORAGE_KEY.CART_LIST, [...cartItems, newItem]);
+    setLocalStorage(LOCAL_STORAGE_KEY.CART_LIST, { cartItems: [...cartItems.cartItems, newItem], totalPrice: 0 });
     return res(ctx.delay(100), ctx.status(201), ctx.json(true));
   }),
 
   rest.delete('/cart-items/:cartItemId', (req, res, ctx) => {
     const { cartItemId } = req.params;
-    const cartItems = getLocalStorage<CartItem[]>(LOCAL_STORAGE_KEY.CART_LIST, []);
+    const cartItems = getLocalStorage<CartItemResponses>(LOCAL_STORAGE_KEY.CART_LIST, { cartItems: [], totalPrice: 0 });
 
     setLocalStorage(
       LOCAL_STORAGE_KEY.CART_LIST,
-      cartItems.filter((item) => item.id !== Number(cartItemId))
+      cartItems.cartItems.filter((item) => item.id !== Number(cartItemId))
     );
     return res(ctx.delay(100), ctx.status(204));
   }),
@@ -58,9 +58,10 @@ export const handlers = [
     const { cartItemId } = req.params;
     const { quantity } = await req.json();
 
-    const cartItems = getLocalStorage<CartItem[]>(LOCAL_STORAGE_KEY.CART_LIST, []);
-    const cartItemIndex = cartItems.findIndex((item) => item.id === Number(cartItemId));
-    cartItems[cartItemIndex].quantity = quantity;
+    const cartItems = getLocalStorage<CartItemResponses>(LOCAL_STORAGE_KEY.CART_LIST, { cartItems: [], totalPrice: 0 });
+
+    const cartItemIndex = cartItems.cartItems.findIndex((item) => item.id === Number(cartItemId));
+    cartItems.cartItems[cartItemIndex].quantity = quantity;
 
     setLocalStorage(LOCAL_STORAGE_KEY.CART_LIST, cartItems);
 
@@ -74,13 +75,14 @@ export const handlers = [
 
   // 쿠폰 조회 GET
   rest.get('/coupons', (_, res, ctx) => {
-    return res(ctx.delay(1000), ctx.status(200), ctx.json(mockCouponList));
+    return res(ctx.delay(1000), ctx.status(200), ctx.json(couponList));
   }),
 
   // 포인트 조회 GET(로컬 스토리지 반영)
   rest.get('/point', (_, res, ctx) => {
-    const point = getLocalStorage(LOCAL_STORAGE_KEY.POINT, { pointHistories: [], totalPoint: 0 });
+    const point = getLocalStorage(LOCAL_STORAGE_KEY.POINT, { pointHistories: [], totalPoint: 10000000000 });
 
+    setLocalStorage(LOCAL_STORAGE_KEY.POINT, point);
     return res(ctx.delay(100), ctx.status(200), ctx.json(point));
   }),
 
@@ -92,12 +94,12 @@ export const handlers = [
   }),
 
   // 주문 상세 정보 GET
-  rest.patch('/orders/:orderId', (req, res, ctx) => {
+  rest.get('/orders/:orderId', (req, res, ctx) => {
     const { orderId } = req.params;
 
     const orderList = getLocalStorage<OrdersResponses>(LOCAL_STORAGE_KEY.ORDER_LIST, { orderResponses: [] });
     const orderItem = orderList.orderResponses.find((order: Order) => order.orderId === Number(orderId));
-
+    console.log(orderItem);
     return res(ctx.delay(100), ctx.status(200), ctx.json(orderItem));
   }),
 
@@ -120,13 +122,16 @@ export const handlers = [
       }
     }
      */
-    const { orderInfo }: { orderInfo: OrderRequest } = await req.json();
+
+    const orderInfo = (await req.json()) as OrderRequest;
     const uniqueId = Date.now();
     const orderList = getLocalStorage<OrdersResponses>(LOCAL_STORAGE_KEY.ORDER_LIST, { orderResponses: [] });
     const userPoint = getLocalStorage<Point>(LOCAL_STORAGE_KEY.POINT, { pointHistories: [], totalPoint: 0 });
-    const currentCouponId = orderInfo.orderDiscounts.couponIds[0];
-    const currentCoupon = mockCouponList.find((coupon) => coupon.id === currentCouponId) as Coupon;
     const totalPrice: number[] = [];
+    const orderItemId: number[] = [];
+    const currentCouponId =
+      orderInfo.orderDiscounts.couponIds.length === 0 ? -1 : orderInfo.orderDiscounts.couponIds[0];
+    const currentCoupon = couponList.find((coupon) => coupon.id === currentCouponId);
     let couponDiscountAmount = 0;
 
     const newOrderItem = {
@@ -134,12 +139,13 @@ export const handlers = [
       orderItems: orderInfo.orderItems.map((orderItem) => {
         const productItem = mockProductList.find((product) => product.id === orderItem.productId) as ProductItem;
         const paymentPrice =
-          currentCoupon.discountAmount === 0
+          currentCoupon?.discountAmount === 0
             ? productItem.price - Math.floor(productItem.price * (currentCoupon.discountPercent / 100))
             : productItem.price;
 
         totalPrice.push(paymentPrice);
-        if (currentCoupon.discountPercent === 0) couponDiscountAmount = currentCoupon.discountAmount;
+        orderItemId.push(orderItem.cartItemId);
+        if (currentCoupon?.discountPercent === 0) couponDiscountAmount = currentCoupon.discountAmount;
 
         return {
           id: orderItem.productId,
@@ -149,29 +155,34 @@ export const handlers = [
           imageUrl: productItem?.imageUrl,
         };
       }),
-      usedCoupons: [currentCoupon],
+      usedCoupons: currentCoupon ? [currentCoupon] : [],
       usedPoint: orderInfo.orderDiscounts.point,
       paymentPrice:
         totalPrice.reduce((acc, cur) => acc + cur, 0) - orderInfo.orderDiscounts.point - couponDiscountAmount,
       createdAt: new Date(uniqueId + 9 * 60 * 60 * 1000).toISOString().replace('T', ' ').slice(0, -5),
-      // 2021-08-05 09:51:31
     };
 
     const newOrderList: OrdersResponses = {
       orderResponses: [...orderList.orderResponses, newOrderItem],
     };
 
-    setLocalStorage(LOCAL_STORAGE_KEY.CART_LIST, newOrderList);
+    setLocalStorage(LOCAL_STORAGE_KEY.ORDER_LIST, newOrderList);
 
-    setLocalStorage(
-      LOCAL_STORAGE_KEY.POINT,
-      userPoint.totalPoint - orderInfo.orderDiscounts.point + newOrderItem.paymentPrice * 0.1
-    );
+    setLocalStorage(LOCAL_STORAGE_KEY.POINT, {
+      totalPoint: userPoint.totalPoint - orderInfo.orderDiscounts.point + Math.floor(newOrderItem.paymentPrice * 0.01),
+      pointHistories: [],
+    });
 
-    mockCouponList.filter((mockCoupon) => mockCoupon.id !== currentCoupon.id);
+    const cartList = getLocalStorage<CartItemResponses>(LOCAL_STORAGE_KEY.CART_LIST, { cartItems: [], totalPrice: 0 });
+    const newCartList = cartList.cartItems.filter((items) => !orderItemId.includes(items.id));
+
+    setLocalStorage(LOCAL_STORAGE_KEY.CART_LIST, {
+      cartItems: newCartList,
+      totalPrice: 0,
+    });
+
+    if (currentCoupon) couponList = couponList.filter((mockCoupon) => mockCoupon.id !== currentCoupon.id);
 
     return res(ctx.delay(100), ctx.status(201), ctx.json(true));
   }),
-
-  // 결제 정보 POST(카트, 포인트, 쿠폰 넘겨주면 결제 금액 알려줌)
 ];
