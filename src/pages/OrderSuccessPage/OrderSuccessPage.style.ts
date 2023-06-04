6ea9e9497570a366d612dff9b691198212be1698
yogjin <youngjin988@gmail.com>
2023-06-05 06:47:28 +0900

    feat: 주문 성공 페이지 추가

---
+++ src/pages/OrderSuccessPage/OrderSuccessPage.style.ts
@@ -0,0 +1,32 @@
+import { styled } from 'styled-components';
+
+import { Text } from '../../components/common/Text/Text.styles';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  transform: translateY(40%);
+  width: 320px;
+  gap: 14px;
+`;
+
+export const CheckedIconWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const OrderSuccessTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const OrderSuccessText = styled(Text).attrs({ size: 'xLarge' })``;
+
+export const OrderPriceTextContainer = styled.div``;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+`;
---
+++ src/pages/OrderSuccessPage/OrderSuccessPage.tsx
@@ -0,0 +1,49 @@
+import { generatePath, useNavigate, useParams, useSearchParams } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+
+import { TickCircleCheckedIcon } from '../../assets';
+import { Button } from '../../components/common/Button/Button.styles';
+import { Divider } from '../../components/common/Divider/Divider.styles';
+import { Text } from '../../components/common/Text/Text.styles';
+import { PATH } from '../../constants/path';
+import { memberInformationState } from '../../store/member';
+import { priceFormatter } from '../../utils/formatter';
+import * as S from './OrderSuccessPage.style';
+
+type Props = {};
+
+export const OrderSuccessPage = ({}: Props) => {
+  const navigate = useNavigate();
+  const { id: memberId } = useRecoilValue(memberInformationState);
+  const [searchParams] = useSearchParams();
+
+  const orderId = String(searchParams).split('=')[1];
+
+  return (
+    <S.Container>
+      <S.CheckedIconWrapper>
+        <TickCircleCheckedIcon width="60px" />
+      </S.CheckedIconWrapper>
+      <S.OrderSuccessTextContainer>
+        <S.OrderSuccessText>{memberId}님의 주문이 완료되었습니다.</S.OrderSuccessText>
+        <S.OrderSuccessText>내일 아침에 만나요!</S.OrderSuccessText>
+      </S.OrderSuccessTextContainer>
+      <Divider />
+      <S.OrderPriceTextContainer>
+        <Text>결제 금액</Text>
+        <Text size="xLarge">{priceFormatter(100000)}</Text>
+      </S.OrderPriceTextContainer>
+      <S.ButtonContainer>
+        <Button variant="primary" onClick={() => navigate(PATH.ROOT)}>
+          홈으로 이동하기
+        </Button>
+        <Button
+          variant="secondary"
+          onClick={() => navigate(generatePath(PATH.ORDERS_OrderId, { orderId }))}
+        >
+          주문내역 상세보기
+        </Button>
+      </S.ButtonContainer>
+    </S.Container>
+  );
+};
