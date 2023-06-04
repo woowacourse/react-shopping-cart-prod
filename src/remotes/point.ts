33f91dfc2fcbe221ed8dbc118c92d0e803a115d8
Suyoung <jsy8492@gmail.com>
2023-06-04 15:28:53 +0900

    feat: 특정 주문으로 적립된 포인트 조회 API 메서드 작성

--- src/remotes/point.ts
+++ src/remotes/point.ts
@@ -11,7 +11,23 @@ export const fetchPoint = async (url: string) => {
     throw new Error('보유 중인 포인트 정보를 불러올 수 없습니다.');
   }
 
-  const point = await response.json();
+  const { points } = await response.json();
 
-  return point;
+  return points;
+};
+
+export const fetchSavedPointByOrder = async (url: string) => {
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Basic ${base64}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('해당 주문으로 적립된 포인트 정보를 불러올 수 없습니다.');
+  }
+
+  const { points_saved } = await response.json();
+
+  return points_saved;
 };
