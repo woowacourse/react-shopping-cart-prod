a5da77543ff9744f44f0ca9ae6b16b26a669f42c
hozzijeong <ghwls0905@naver.com>
2023-06-04 17:13:09 +0900

    feat: 품목 삭제시에 alert로 의사 물어보는 기능 추가

--- src/components/CartProductItem/index.tsx
+++ src/components/CartProductItem/index.tsx
@@ -1,9 +1,11 @@
-// import { ReactComponent as TrashBox } from 'src/assets/trash-box.svg';
 import CountButton from 'src/components/Common/CountButton';
 import type { CartItem } from 'src/types';
 import { RiDeleteBinLine } from 'react-icons/ri';
-import styles from './index.module.scss';
+import useModal from 'src/hooks/useModal';
+import Modal from '../Common/Modal';
 import CheckBox from '../Common/CheckBox';
+import ModalNotification from '../Common/ModalNotification';
+import styles from './index.module.scss';
 
 interface CartProductItemProps {
   cartItem: CartItem;
@@ -18,12 +20,16 @@ function CartProductItem({ cartItem, toggleCheck, checked, mutateQuantity, delet
   const { id, product, quantity } = cartItem;
   const { name, imageUrl, price } = product;
 
+  const { isModalOpen, openModal, closeModal } = useModal();
+
   const handleDeleteButton = () => {
-    deleteCartItem(id);
+    openModal();
   };
+
   const handleUpButton = () => {
     mutateQuantity(id, quantity + 1);
   };
+
   const handleDownButton = () => {
     if (quantity > 1) {
       mutateQuantity(id, quantity - 1);
@@ -49,6 +55,15 @@ function CartProductItem({ cartItem, toggleCheck, checked, mutateQuantity, delet
           <div className={styles['product-price']}>{(price * quantity).toLocaleString()} 원</div>
         </div>
       </div>
+      {isModalOpen && (
+        <Modal closeEvent={closeModal} direction="center">
+          <ModalNotification
+            message={{ title: '정말 삭제하시겠습니까?' }}
+            cancelCallback={closeModal}
+            assignCallback={() => deleteCartItem(id)}
+          />
+        </Modal>
+      )}
     </div>
   );
 }
--- src/components/CartProductItemList/index.tsx
+++ src/components/CartProductItemList/index.tsx
@@ -4,6 +4,9 @@ import useCart from 'src/hooks/useCart';
 import { $CheckedCartIdList, $CurrentServerUrl } from 'src/recoil/atom';
 import CartProductItem from 'src/components/CartProductItem';
 import type { CartItem } from 'src/types';
+import useModal from 'src/hooks/useModal';
+import ModalNotification from '../Common/ModalNotification';
+import Modal from '../Common/Modal';
 import CheckBox from '../Common/CheckBox';
 import styles from './index.module.scss';
 
@@ -11,6 +14,8 @@ function CartProductItemList() {
   const currentServerUrl = useRecoilValue($CurrentServerUrl);
   const [checkedCartIdList, setCheckedCartIdList] = useRecoilState($CheckedCartIdList(currentServerUrl));
   const { cartList, deleteCartItem, mutateQuantity } = useCart();
+
+  const { isModalOpen, openModal, closeModal } = useModal();
 
   const checkAllCartItem: React.ChangeEventHandler<HTMLInputElement> = ({ target: { checked } }) => {
     if (checked) {
@@ -41,7 +46,7 @@ function CartProductItemList() {
           size="large"
         />
         <div>전체 선택 ({`${checkedCartIdList.length}/${cartList.length}`})</div>
-        <button type="button" onClick={deleteCheckedCartItem}>
+        <button type="button" onClick={openModal}>
           선택 삭제
         </button>
       </div>
@@ -57,6 +62,17 @@ function CartProductItemList() {
           />
         ))}
       </section>
+      {isModalOpen && (
+        <Modal direction="center" closeEvent={closeModal}>
+          <ModalNotification
+            message={{
+              title: `${checkedCartIdList.length}개의 품목을 삭제하시겠습니까? \n삭제한다면 되돌릴 수 없습니다.`,
+            }}
+            cancelCallback={closeModal}
+            assignCallback={deleteCheckedCartItem}
+          />
+        </Modal>
+      )}
     </div>
   );
 }
