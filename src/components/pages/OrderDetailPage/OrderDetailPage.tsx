3fd9a0677bbeb4dfb78c1233e719a54988109135
Suyoung <jsy8492@gmail.com>
2023-06-04 18:26:09 +0900

    feat: 주문 상세 페이지에 적립된 포인트 항목 추가

--- src/components/pages/OrderDetailPage/OrderDetailPage.tsx
+++ src/components/pages/OrderDetailPage/OrderDetailPage.tsx
@@ -5,6 +5,7 @@ import OrderList from '../../order/OrderList/OrderList';
 import { orderQuery } from '../../../recoil/selectors/order';
 import Spacer from '../../common/Spacer/Spacer';
 import { formatPrice } from '../../../utils/formatPrice';
+import { savedPointByOrderQuery } from '../../../recoil/selectors/point';
 
 const OrderDetailPage = () => {
   const { id } = useParams();
@@ -16,8 +17,10 @@ const OrderDetailPage = () => {
     return <></>;
   }
 
-  const order = useRecoilValue(orderQuery(Number(id)));
+  const orderId = Number(id);
+  const order = useRecoilValue(orderQuery(orderId));
   const { price } = order;
+  const savedPointByOrder = useRecoilValue(savedPointByOrderQuery(orderId));
 
   return (
     <Container>
@@ -29,10 +32,16 @@ const OrderDetailPage = () => {
       <Spacer height={30} />
       <OrderTotal>
         <OrderTotalHeader>결제금액 정보</OrderTotalHeader>
-        <OrderTotalPrice>
-          <span>총 결제금액</span>
-          <Price>{formatPrice(price)}</Price>
-        </OrderTotalPrice>
+        <Detail>
+          <DetailInner>
+            <dt>총 결제금액</dt>
+            <dd>{formatPrice(price)}</dd>
+          </DetailInner>
+          <DetailInner>
+            <dt>적립된 포인트</dt>
+            <dd>+ {formatPrice(savedPointByOrder)}</dd>
+          </DetailInner>
+        </Detail>
       </OrderTotal>
     </Container>
   );
@@ -57,15 +66,15 @@ const Title = styled.h2`
   color: ${(props) => props.theme.black};
 `;
 
-const OrderTotal = styled.div`
+const OrderTotal = styled.dl`
   width: 360px;
-  height: 160px;
+  height: 200px;
   margin-left: auto;
   border-radius: 8px;
   border: 1px solid ${(props) => props.theme.color.gray300};
 `;
 
-const OrderTotalHeader = styled.div`
+const OrderTotalHeader = styled.p`
   display: flex;
   align-items: center;
   padding: 20px 30px;
@@ -81,12 +90,12 @@ const OrderTotalHeader = styled.div`
   letter-spacing: 0.5px;
 `;
 
-const OrderTotalPrice = styled.div`
+const Detail = styled.div`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 90px;
-  padding: 0 30px;
+  flex-direction: column;
+  justify-content: center;
+  row-gap: 30px;
+  padding: 25px 30px;
   border-bottom-left-radius: 8px;
   border-bottom-right-radius: 8px;
   font-weight: 600;
@@ -94,10 +103,29 @@ const OrderTotalPrice = styled.div`
   line-height: 24px;
   letter-spacing: 0.5px;
   color: ${(props) => props.theme.color.black};
+
+  & > div:last-child > * {
+    font-size: 18px;
+  }
 `;
 
-const Price = styled.span`
-  color: ${(props) => props.theme.color.primary};
+const DetailInner = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  & > dd {
+    color: ${(props) => props.theme.color.primary};
+  }
+`;
+
+const Point = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 5px;
+
+  & > dd {
+    color: ${(props) => props.theme.color.primary};
+  }
 `;
 
 export default OrderDetailPage;
