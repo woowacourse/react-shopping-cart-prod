e515aeca7828a0756ccecb296554084b65bb12e4
turtle601 <turtle601@naver.com>
2023-06-04 19:41:51 +0900

    feat(Product): react-query 사용으로 인한 Product 관련 컴포넌트 리팩토링

--- src/components/ProductAddition/ProductAddition.tsx
+++ src/components/ProductAddition/ProductAddition.tsx
@@ -1,39 +1,83 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useState } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
+import { cartItemState, cartListState } from '../../store/cart';
+import { originState } from '../../store/origin';
 import { ProductItemType } from '../../types';
 import { priceFormatter } from '../../utils/formatter';
 import StepperButton from '../StepperButton/StepperButton';
 import styles from './style.module.css';
 
 interface ProductAdditionProps {
-  productInformation: ProductItemType;
+  product: ProductItemType;
   closeModalByClick: () => void;
-  submitEvent: (quantity: number) => void;
 }
 
-const ProductAddition = ({
-  productInformation,
-  closeModalByClick,
-  submitEvent,
-}: ProductAdditionProps) => {
+type PostDataType = {
+  productId: number;
+  quantity: number;
+};
+
+const ProductAddition = ({ product, closeModalByClick }: ProductAdditionProps) => {
   const [quantity, setQuantity] = useState(1);
+  const [cartList, setCartList] = useRecoilState(cartListState);
+  const cartItem = useRecoilValue(cartItemState(product.id));
+  const origin = useRecoilValue(originState);
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: async (data: PostDataType) => {
+      await fetch(`${origin}cart-items`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+      });
+    },
+    onSuccess: (data, variable, context) => {
+      if (cartItem) {
+        const newCartList = cartList.map((cartItem) => {
+          if (cartItem.product.id === variable.productId) {
+            return {
+              ...cartItem,
+              quantity: cartItem.quantity + variable.quantity,
+            };
+          }
+          return cartItem;
+        });
+
+        setCartList(newCartList);
+        return;
+      }
+
+      setCartList([
+        ...cartList,
+        {
+          id: cartList.length + 1,
+          quantity: variable.quantity,
+          product,
+          isChecked: false,
+        },
+      ]);
+
+      queryClient.invalidateQueries({ queryKey: ['cart-items'] });
+    },
+  });
 
   return (
     <div className={styles.container}>
       <h4 className={styles.header}>장바구니 담기</h4>
       <div className={styles.informationContainer}>
-        <img src={productInformation.imageUrl} alt={productInformation.name} />
+        <img src={product.imageUrl} alt={product.name} />
         <div className={styles.informationContainertwo}>
           <div>
-            <h4 className={styles.productName}>{productInformation.name}</h4>
-            <h4 className={styles.productPrice}>{priceFormatter(productInformation.price)}원</h4>
+            <h4 className={styles.productName}>{product.name}</h4>
+            <h4 className={styles.productPrice}>{priceFormatter(product.price)}원</h4>
           </div>
           <StepperButton count={quantity} setCount={setQuantity} />
         </div>
       </div>
       <div className={styles.totalPriceContainer}>
         <h5>합계</h5>
-        <h3>{priceFormatter(productInformation.price * quantity)} 원</h3>
+        <h3>{priceFormatter(product.price * quantity)} 원</h3>
       </div>
       <div className={styles.buttonContainer}>
         <button
@@ -47,7 +91,10 @@ const ProductAddition = ({
           className={styles.addButton}
           aria-label="add item"
           onClick={() => {
-            submitEvent(quantity);
+            mutation.mutate({
+              productId: product.id,
+              quantity: quantity,
+            });
           }}
         >
           장바구니 담기
--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,31 +1,15 @@
-import { useEffect } from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
-
-import { useFetch } from '../../hooks/useFetch';
-import { originState } from '../../store/origin';
-import productListState from '../../store/product';
-import { ProductItemType } from '../../types';
-import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
+import { useGetProductList } from '../../hooks/useFetchUrl';
 import ProductItem from '../ProductItem/ProductItem';
-
 import styles from './styles.module.css';
 
 const ProductList = () => {
-  const [, setList] = useRecoilState(productListState);
-
-  const { data, fetchApi, isLoading } = useFetch<ProductItemType[]>(setList);
-  const origin = useRecoilValue(originState);
-  useEffect(() => {
-    fetchApi.get(`${origin}products`);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [origin]);
+  const productList = useGetProductList();
 
   return (
     <div className={styles.container}>
-      {isLoading && <LoadingSpinner />}
-      {data?.map((productItem) => (
-        <ProductItem key={productItem.id} information={productItem} />
-      ))}
+      {productList?.map((productItem) => {
+        return <ProductItem key={productItem.id} product={productItem} />;
+      })}
     </div>
   );
 };
