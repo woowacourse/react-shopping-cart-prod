f040c450a969df67985cad8734797678bf86f23d
cruelladevil <dev.timetravel@gmail.com>
2023-06-04 14:09:37 +0900

    refactor: get의 Header를 api로 추상화, 변수명 변경

--- src/components/main/ProductList.tsx
+++ src/components/main/ProductList.tsx
@@ -2,13 +2,13 @@ import { useEffect } from 'react';
 import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { styled } from 'styled-components';
 import { CART_URL, PRODUCT_LIST_URL } from '../../constants/url';
-import { useFetchData } from '../../hooks/useFetchData';
-import { cartState, productListState, serverState } from '../../recoil';
+import useFetchData from '../../hooks/useFetchData';
+import { cartState, productsState, serverState } from '../../recoil';
 import Spinner from '../Spinner';
 import ProductItem from './ProductItem';
 
 const ProductList = () => {
-  const [productList, setProductList] = useRecoilState(productListState);
+  const [products, setProducts] = useRecoilState(productsState);
   const server = useRecoilValue(serverState);
   const setCart = useSetRecoilState(cartState);
 
@@ -16,59 +16,44 @@ const ProductList = () => {
 
   useEffect(() => {
     api.get(`${server}${PRODUCT_LIST_URL}`).then((data) => {
-      setProductList(data);
+      setProducts(data);
     });
 
-    api
-      .get(`${server}${CART_URL}`, {
-        Authorization: 'Basic YUBhLmNvbToxMjM0',
-        'Content-Type': 'application/json',
-      })
-      .then((data) => {
-        setCart(data);
-      });
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    api.get(`${server}${CART_URL}`).then((data) => {
+      setCart(data);
+    });
   }, [server]);
 
   if (isLoading) return <Spinner />;
 
   return (
-    <S.Wrapper>
-      {productList.map((product) => (
-        <ProductItem
-          key={product.id}
-          id={product.id}
-          name={product.name}
-          price={product.price}
-          imageUrl={product.imageUrl}
-        />
+    <Grid>
+      {products.map((product) => (
+        <ProductItem key={product.id} {...product} />
       ))}
-    </S.Wrapper>
+    </Grid>
   );
 };
 
-const S = {
-  Wrapper: styled.main`
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: 86px 4%;
-    max-width: 1270px;
-    margin: 0 auto;
-    padding: 0 20px 120px;
+const Grid = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 86px 4%;
+  max-width: 1270px;
+  margin: 0 auto;
+  padding: 0 20px 120px;
 
-    @media (max-width: 1270px) {
-      grid-template-columns: repeat(3, 1fr);
-    }
+  @media (max-width: 1270px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
 
-    @media (max-width: 768px) {
-      grid-template-columns: repeat(2, 1fr);
-    }
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 
-    @media (max-width: 480px) {
-      grid-template-columns: repeat(1, 1fr);
-    }
-  `,
-};
+  @media (max-width: 480px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
 
 export default ProductList;
