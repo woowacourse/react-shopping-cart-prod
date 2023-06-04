e6f01520db3cfb6a37479065fd29144221384e55
Jungwoo <evencoding@gmail.com>
2023-06-04 17:53:05 +0900

    refactor: App과 router 분리, react-error-boundary 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import { Suspense, useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
 import GlobalStyle from './GlobalStyle';
 
-import { Layout } from '@components/common/Layout/Layout';
-import { Home } from '@components/pages/Home/Home';
-import { CartPage } from '@components/pages/CartPage/CartPage';
-import { OrdersPage } from '@components/pages/OrdersPage/OrdersPage';
-import { OrderDetailPage } from '@components/pages/OrderDetailPage/OrderDetailPage';
-
-import { PATH } from '@constants/index';
-
-import { Spinner } from '@components/common/Spinner/Spinner';
-
 import { useCartRepository } from '@recoils/cartAtoms';
+import { Router } from './router';
 
 export const App = () => {
   const { fetchCart } = useCartRepository();
@@ -24,48 +14,7 @@ export const App = () => {
   return (
     <>
       <GlobalStyle />
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route
-            path={PATH.HOME}
-            Component={() => (
-              <Layout>
-                <Home />
-              </Layout>
-            )}
-          />
-          <Route
-            path={PATH.CART}
-            Component={() => (
-              <Layout pageTitle="장바구니">
-                <Suspense fallback={<Spinner size="lg" />}>
-                  <CartPage />
-                </Suspense>
-              </Layout>
-            )}
-          />
-          <Route
-            path={PATH.ORDERS}
-            Component={() => (
-              <Layout pageTitle="주문 목록">
-                <Suspense fallback={<Spinner size="lg" />}>
-                  <OrdersPage />
-                </Suspense>
-              </Layout>
-            )}
-          />
-          <Route
-            path={`${PATH.ORDERS}/:id`}
-            Component={() => (
-              <Layout pageTitle="주문 내역 상세">
-                <Suspense fallback={<Spinner size="lg" />}>
-                  <OrderDetailPage />
-                </Suspense>
-              </Layout>
-            )}
-          />
-        </Routes>
-      </BrowserRouter>
+      <Router />
     </>
   );
 };
---
+++ src/router.tsx
@@ -0,0 +1,66 @@
+import { Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { Layout } from '@components/common/Layout/Layout';
+import { Spinner } from '@components/common/Spinner/Spinner';
+import { CartPage } from '@components/pages/CartPage/CartPage';
+import { Home } from '@components/pages/Home/Home';
+import { OrderDetailPage } from '@components/pages/OrderDetailPage/OrderDetailPage';
+import { OrdersPage } from '@components/pages/OrdersPage/OrdersPage';
+
+import { PATH } from './constants';
+
+export const Router = () => {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route
+          path={PATH.HOME}
+          Component={() => (
+            <Layout>
+              <Home />
+            </Layout>
+          )}
+        />
+        <Route
+          path={PATH.CART}
+          Component={() => (
+            <Layout pageTitle="장바구니">
+              <Suspense fallback={<Spinner size="lg" />}>
+                <CartPage />
+              </Suspense>
+            </Layout>
+          )}
+        />
+        <Route
+          path={PATH.ORDERS}
+          Component={() => (
+            <Layout pageTitle="주문 목록">
+              <Suspense fallback={<Spinner size="lg" />}>
+                <OrdersPage />
+              </Suspense>
+            </Layout>
+          )}
+        />
+        <Route
+          path={`${PATH.ORDERS}/:id`}
+          Component={() => (
+            <Layout pageTitle="주문 내역 상세">
+              <Suspense fallback={<Spinner size="lg" />}>
+                <OrderDetailPage />
+              </Suspense>
+            </Layout>
+          )}
+        />
+        <Route
+          path={'/:notExist'}
+          Component={() => (
+            <Layout>
+              <div>존재하지 않는 페이지 입니다</div>
+            </Layout>
+          )}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
