f833c389f6e65960ea4d93944a509ead287f8e5c
regularPark <srk99279@naver.com>
2023-06-04 17:45:59 +0900

    feat: member 정보 불러오기 및 set하기

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,23 @@
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import ProductList from '../components/main/ProductList';
 import { ProductListWrapper } from '../style/ContentLayout';
+import memberState from '../store/MemberState';
+import { serverState } from '../store/ServerState';
+import useGet from '../hooks/useGet';
+import { MEMBER_BASE_URL } from '../constants/url';
+import { useEffect } from 'react';
+import { MemberInfo } from '../types';
 
 const MainPage = () => {
+  const setMember = useSetRecoilState(memberState);
+  const serverUrl = useRecoilValue(serverState);
+
+  const { data: memberData } = useGet<MemberInfo>(`${serverUrl}${MEMBER_BASE_URL}`);
+
+  useEffect(() => {
+    if (memberData) setMember(memberData);
+  }, [memberData, setMember]);
+
   return (
     <ProductListWrapper>
       <ProductList />
