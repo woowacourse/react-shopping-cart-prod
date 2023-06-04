a9f3aa93e044e32033c5ca55b8e3718c2fd279f1
solo5star <solo_5@naver.com>
2023-06-05 03:47:47 +0900

    fix: 주문 후 상태가 새로고침되지 않는 오류 수정

--- src/hooks/useOrderMutation.ts
+++ src/hooks/useOrderMutation.ts
@@ -1,5 +1,7 @@
-import { useRecoilValue } from 'recoil';
+import { useRecoilRefresher_UNSTABLE, useRecoilValue } from 'recoil';
 import clientState from '../recoil/atoms/clientState';
+import ordersQuery from '../recoil/queries/ordersQuery';
+import profileQuery from '../recoil/queries/profileQuery';
 import userCartItemsRepository from '../recoil/user/userCartItemsRepository';
 import userRemoteCartItemsState from '../recoil/user/userRemoteCartItemsState';
 import useMutation from './useMutation';
@@ -13,19 +15,30 @@ const useOrderMutation = () => {
   const { cartItems } = useRecoilValue(userRemoteCartItemsState);
   const { doDownstreamSync } = useRecoilValue(userCartItemsRepository);
 
+  const refreshProfile = useRecoilRefresher_UNSTABLE(profileQuery({ client }));
+  const refreshOrders = useRecoilRefresher_UNSTABLE(ordersQuery({ client }));
+
+  const refreshStates = () => {
+    refreshProfile();
+    refreshOrders();
+    doDownstreamSync();
+  };
+
   const { mutate: order, ...mutationParams } = useMutation(
     ({ usedPoints }: UseOrderMutationParams) =>
       client
         .post('/orders', {
           usedPoints,
-          cartItems: cartItems.map((cartItem) => ({
-            id: cartItem.id,
-            productId: cartItem.product.id,
-            quantity: cartItem.quantity,
-          })),
+          cartItems: cartItems
+            .filter((cartItem) => cartItem.checked)
+            .map((cartItem) => ({
+              id: cartItem.id,
+              productId: cartItem.product.id,
+              quantity: cartItem.quantity,
+            })),
         })
         .then((response) => Number(response.acceptOrThrow(201).headers.location.split('/').pop()))
-        .finally(doDownstreamSync),
+        .finally(refreshStates),
   );
 
   return { order, ...mutationParams };
