18d688d823e0113a61e5f1e66e115bbc435d8701
yogjin <youngjin988@gmail.com>
2023-06-05 01:26:26 +0900

    refactor: 주문 내역 상세 페이지에서 `DetailOrderedItem` 컴포넌트 사용하도록 수정

--- src/pages/OrderDetailPage/OrderDetailPage.tsx
+++ src/pages/OrderDetailPage/OrderDetailPage.tsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 
 import SpinnerContainer from '../../components/common/SpinnerContainer/SpinnerContainer';
-import { OrderedItem } from '../../components/order/OrderedItem/OrderedItem';
+import { DetailOrderedItem } from '../../components/order/OrderedItem/DetailOrderedItem';
 import { OrderedItemDetailInformation } from '../../components/order/OrderedItemDetailInformation/OrderedItemDetailInformation';
 import { detailOrderState } from '../../store/order';
 import * as S from './OrderDetailPage.style';
@@ -19,7 +19,7 @@ export const OrderDetailPage = ({}: Props) => {
       <S.OrderPageHeading size="small">주문 내역 상세</S.OrderPageHeading>
       <Suspense fallback={<SpinnerContainer />}>
         <S.Container>
-          <OrderedItem {...orderedItem} />
+          <DetailOrderedItem {...orderedItem} />
           <OrderedItemDetailInformation {...orderedItem} />
         </S.Container>
       </Suspense>
