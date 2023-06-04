e071cf78153c0de8f7e369e03c951e3217db4a0a
yogjin <youngjin988@gmail.com>
2023-06-04 23:02:17 +0900

    refactor: 주문 상품 리스트를 받아서 가격 정보를 구하는 (msw mock 전용)유틸함수 추가

--- src/mocks/utils.ts
+++ src/mocks/utils.ts
@@ -1,15 +1,15 @@
-import { OrderedItem } from '../types';
+import { CartItemData, CartPriceData, OrderedItemData } from '../types';
 
-export const getTotalItemDiscountAmount = (orderedItemList: OrderedItem[]) => {
+export const getTotalItemDiscountAmount = (orderedItemList: OrderedItemData[]) => {
   return orderedItemList.reduce((prev, curr) => {
-    if (curr.product.discountRate > 0) {
-      return prev + curr.quantity * (curr.product.price * (curr.product.discountRate / 100));
+    if (curr.discountRate > 0) {
+      return prev + curr.quantity * (curr.price * (curr.discountRate / 100));
     }
     return prev;
   }, 0);
 };
 
-export const getTotalMemberDiscountAmount = (orderedItemList: OrderedItem[]) => {
+export const getTotalMemberDiscountAmount = (orderedItemList: OrderedItemData[]) => {
   return (
     getTotalItemPrice(orderedItemList) -
     getDiscountedTotalItemPrice(orderedItemList) -
@@ -17,22 +17,33 @@ export const getTotalMemberDiscountAmount = (orderedItemList: OrderedItem[]) => 
   );
 };
 
-export const getTotalItemPrice = (orderedItemList: OrderedItem[]) => {
+export const getTotalItemPrice = (orderedItemList: OrderedItemData[]) => {
   return orderedItemList.reduce((prev, curr) => {
-    return prev + curr.product.price;
+    return prev + curr.price;
   }, 0);
 };
 
-export const getDiscountedTotalItemPrice = (orderedItemList: OrderedItem[]) => {
+export const getDiscountedTotalItemPrice = (orderedItemList: OrderedItemData[]) => {
   return orderedItemList.reduce((prev, curr) => {
-    return prev + curr.product.discountedPrice;
+    return prev + curr.discountedPrice;
   }, 0);
 };
 
-export const getShippingFee = (orderedItemList: OrderedItem[]) => {
+export const getShippingFee = (orderedItemList: OrderedItemData[]) => {
   return getDiscountedTotalItemPrice(orderedItemList) >= 50000 ? 0 : 3000;
 };
 
-export const getTotalPrice = (orderedItemList: OrderedItem[]) => {
+export const getTotalPrice = (orderedItemList: OrderedItemData[]) => {
   return getDiscountedTotalItemPrice(orderedItemList) + getShippingFee(orderedItemList);
+};
+
+export const getCartPriceInformation = (orderedItemList: OrderedItemData[]): CartPriceData => {
+  return {
+    totalItemDiscountAmount: getTotalItemDiscountAmount(orderedItemList),
+    totalMemberDiscountAmount: getTotalMemberDiscountAmount(orderedItemList),
+    totalItemPrice: getTotalItemPrice(orderedItemList),
+    discountedTotalItemPrice: getDiscountedTotalItemPrice(orderedItemList),
+    shippingFee: getShippingFee(orderedItemList),
+    totalPrice: getTotalPrice(orderedItemList),
+  };
 };
