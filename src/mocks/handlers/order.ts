75bd91678ca72b11a86913f98164b0a75902a453
Suyoung <jsy8492@gmail.com>
2023-06-04 15:38:30 +0900

    feat: 주문하기 API 요청 handler 작성

---
+++ src/mocks/handlers/order.ts
@@ -0,0 +1,87 @@
+import { rest } from 'msw';
+import {
+  CART_BASE_URL,
+  ORDERS_BASE_URL,
+  ORDERS_LOCAL_STORAGE_KEY,
+} from '../../constants/api';
+import { fetchCartItems } from '../../remotes/cart';
+import { setLocalStorage } from '../../utils/localStorage';
+import type { Order } from '../../types/order';
+import type { CartItem } from '../../types/cart';
+
+const localStorageOrders = localStorage.getItem(ORDERS_LOCAL_STORAGE_KEY);
+// eslint-disable-next-line prefer-const
+let orders: Order[] = localStorageOrders ? JSON.parse(localStorageOrders) : [];
+let point = 0;
+
+export const orderHandlers = [
+  // 전체 주문 목록 조회
+  rest.get(ORDERS_BASE_URL, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(orders));
+  }),
+
+  // 단일 주문 조회
+  rest.get(`${ORDERS_BASE_URL}/:id`, async (req, res, ctx) => {
+    const orderId = Number(req.params.id);
+    const order = orders.find((order) => order.id === orderId);
+
+    if (order === undefined) {
+      return res(ctx.status(404));
+    }
+
+    return res(ctx.status(200), ctx.json(order));
+  }),
+
+  // 주문하기
+  rest.post(ORDERS_BASE_URL, async (req, res, ctx) => {
+    const { cartItemIds, usePoint } = await req.json();
+    const cart: CartItem[] = await fetchCartItems(CART_BASE_URL);
+
+    const orderItems = cart.filter((cartItem) =>
+      cartItemIds.includes(cartItem.id),
+    );
+
+    const _orders = orderItems.map((orderItem) => {
+      const { id, quantity, product } = orderItem;
+      const { price, name, imageUrl } = product;
+
+      return {
+        id,
+        quantity,
+        price,
+        name,
+        imageUrl,
+      };
+    });
+
+    const totalPrice = orderItems.reduce((prev, orderItem) => {
+      const { quantity, product } = orderItem;
+      const { price } = product;
+
+      return prev + quantity * price;
+    }, 0);
+
+    const newOrder = {
+      id: Date.now(),
+      price: totalPrice,
+      orderDate: new Date().toISOString(),
+      orders: _orders,
+    } satisfies Order;
+
+    const addedPoint = Math.floor(totalPrice * 0.025);
+
+    orders = [...orders, newOrder];
+    point += addedPoint;
+
+    setLocalStorage(ORDERS_LOCAL_STORAGE_KEY, orders);
+
+    return res(
+      ctx.status(201),
+      ctx.json({
+        addedPoint: Math.floor(totalPrice * 0.025),
+        remainPoint: point,
+      }),
+      ctx.set('Location', `/orders/${orders.at(-1)?.id}`),
+    );
+  }),
+];
