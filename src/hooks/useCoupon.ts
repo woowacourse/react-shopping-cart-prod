d582ff997a19d5ac468a3349fcbe3e1499bee374
yeopto <gunyeop530@gmail.com>
2023-06-04 13:42:51 +0900

    feat: useCoupon 훅 생성

--- src/hooks/useCoupon.ts
+++ src/hooks/useCoupon.ts
@@ -2,34 +2,34 @@ import { useRecoilValue } from "recoil";
 import { selectedProductsState } from "../recoil/atom";
 import { useEffect, useState } from "react";
 import { fetchCoupons } from "../api";
-import { parseExpiredDate } from "../utils";
+import { parseExpiredDate } from "../utils/domain";
 import { Coupon } from "../types/domain";
 
 export const useCoupon = () => {
   const checkedCartList = useRecoilValue(selectedProductsState);
   const [coupons, setCoupons] = useState<Coupon[]>([]);
 
+  const getCoupon = async () => {
+    try {
+      const response = await fetchCoupons(
+        checkedCartList.map((checkedCartItem) => checkedCartItem.id)
+      );
+
+      if (!response.ok) throw new Error(response.status.toString());
+
+      const couponsData = await response.json();
+
+      const parsedCoupon = parseExpiredDate(couponsData.coupons);
+
+      setCoupons(parsedCoupon);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
-    const getCoupon = async () => {
-      try {
-        const response = await fetchCoupons(
-          checkedCartList.map((checkedCartItem) => checkedCartItem.id)
-        );
-        
-        if (!response.ok) throw new Error(response.status.toString());
-
-        const couponsData = await response.json();
-
-        const parsedCoupon = parseExpiredDate(couponsData);
-
-        setCoupons(parsedCoupon);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
     getCoupon();
-  }, [checkedCartList]);
+  }, []);
 
   return { coupons };
 };
