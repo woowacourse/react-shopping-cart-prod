cc6dba11ca147c5a95f874ee065a17c7ea3db451
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-06-04 17:25:48 +0900

    test(Modal): props description 추가

--- src/components/@common/Modal/Modal.stories.tsx
+++ src/components/@common/Modal/Modal.stories.tsx
@@ -1,10 +1,38 @@
 import { Meta, StoryObj } from '@storybook/react';
 import Modal from './Modal';
 import useModal from './hooks/useModal';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  position: relative;
+  width: 500px;
+  height: 500px;
+`;
 
 const meta = {
   component: Modal,
   title: 'Common/Modal',
+  decorators: [
+    (Story) => (
+      <Container>
+        <Story />
+      </Container>
+    ),
+  ],
+
+  argTypes: {
+    children: {
+      description: '모달에 표시할 내용물 입니다.',
+    },
+
+    isOpen: {
+      description: '모달의 열고 닫힘 상태입니다.',
+    },
+
+    closeModal: {
+      description: '모달을 닫는 함수입니다.',
+    },
+  },
 } satisfies Meta<typeof Modal>;
 
 export default meta;
