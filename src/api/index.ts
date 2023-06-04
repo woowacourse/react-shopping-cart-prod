9d417fea2f76e6e7308b51c4f5d42f6100926917
TaeeunKim <xodms0309@naver.com>
2023-06-05 01:20:09 +0900

    feat: response status에 따른 에러 메시지 작성

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,12 @@
+import { handleHTTPError } from './handleHTTPError';
+
 export const fetchApi = async (url: string, options: RequestInit) => {
   if (!navigator.onLine) {
     throw new Error('네트워크 오프라인이 감지되었습니다');
   }
   const response = await fetch(url, options);
   if (!response.ok) {
-    throw new Error('api 요청 중에 오류가 발생했습니다.');
+    handleHTTPError(response.status);
   }
   const contentType = response.headers.get('content-type');
   if (contentType && contentType.includes('application/json')) {
