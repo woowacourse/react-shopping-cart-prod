c19d35ea006d746943a6eac3b9a1f7f5c2ac3ac3
TaeeunKim <xodms0309@naver.com>
2023-06-04 19:27:14 +0900

    feat: 서버에서 받아온 데이터로 가격 변경

--- src/components/Cart/PaymentDetail/index.tsx
+++ src/components/Cart/PaymentDetail/index.tsx
@@ -1,16 +1,64 @@
+import { getCouponApplied } from 'api/requests';
+import { useGet } from 'hooks/useGet';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
-import { totalPriceSelector } from 'recoil/cartList';
+import { cartListAtom, checkedItemsAtom, couponIdAtom } from 'recoil/cartList';
+import { CouponAppliedPriceResponse } from 'types/api';
+import { formatPrice } from 'utils';
 import * as S from './PaymentDetail.styles';
 
 const PaymentDetail = () => {
-  const deliveryPrice = 3000;
-  const totalPrice = useRecoilValue(totalPriceSelector);
-  const orderPrice = totalPrice === 0 ? 0 : totalPrice + deliveryPrice;
+  const dependency = useRecoilValue(couponIdAtom);
+  const checkedItems = useRecoilValue(checkedItemsAtom);
+  const cartList = useRecoilValue(cartListAtom);
+
+  const { data } = useGet<CouponAppliedPriceResponse>(
+    getCouponApplied(),
+    dependency
+  );
+
   const navigate = useNavigate();
 
   const onOrderButtonClick = () => {
     navigate('/order');
+  };
+
+  const calculateDeliveryPrice = () => {
+    if (!data) return 0;
+    return data.deliveryPrice.originalPrice - data.deliveryPrice.discountPrice;
+  };
+
+  const calculateTotalPrice = () => {
+    if (!data) return 0;
+
+    return data.cartItemsPrice.reduce((acc, cur) => {
+      const quantity =
+        cartList.find((item) => item.id === cur.cartItemId)?.quantity ?? 0;
+      return acc + cur.originalPrice * quantity;
+    }, 0);
+  };
+
+  const calculateTotalDiscountPrice = () => {
+    if (!data) return 0;
+
+    const discountPrice = data.cartItemsPrice.reduce((acc, cur) => {
+      if (!checkedItems.includes(cur.cartItemId)) return acc;
+      const quantity =
+        cartList.find((item) => item.id === cur.cartItemId)?.quantity ?? 0;
+      return acc + cur.discountPrice * quantity;
+    }, 0);
+
+    const couponDiscountPrice = data.discountFromTotalPrice.discountPrice;
+    return discountPrice + couponDiscountPrice;
+  };
+
+  const calculateOrderPrice = () => {
+    if (!data) return 0;
+    return (
+      calculateDeliveryPrice() +
+      calculateTotalPrice() -
+      calculateTotalDiscountPrice()
+    );
   };
 
   return (
@@ -18,15 +66,19 @@ const PaymentDetail = () => {
       <S.Title>결제 예상 금액</S.Title>
       <S.Wrapper>
         <S.Text>총 상품 가격</S.Text>
-        <S.Text>{totalPrice.toLocaleString('KR')}원</S.Text>
+        <S.Text>{formatPrice(calculateTotalPrice())}원</S.Text>
       </S.Wrapper>
       <S.Wrapper>
         <S.Text>총 배송비</S.Text>
-        <S.Text>{deliveryPrice.toLocaleString('KR')}원</S.Text>
+        <S.Text>{formatPrice(calculateDeliveryPrice())}원</S.Text>
+      </S.Wrapper>
+      <S.Wrapper>
+        <S.Text>총 할인 금액</S.Text>
+        <S.Text>-{formatPrice(calculateTotalDiscountPrice())}원</S.Text>
       </S.Wrapper>
       <S.Wrapper>
         <S.Text>총 주문 금액</S.Text>
-        <S.Text>{orderPrice.toLocaleString('KR')}원</S.Text>
+        <S.Text>{formatPrice(calculateOrderPrice())}원</S.Text>
       </S.Wrapper>
       <S.OrderButton onClick={onOrderButtonClick}>주문하기</S.OrderButton>
     </S.Container>
