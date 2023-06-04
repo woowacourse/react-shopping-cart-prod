a262b0e42ef3200fdca2076e69e63ae23cb9c9ef
Suyoung <jsy8492@gmail.com>
2023-06-04 17:29:24 +0900

    test: 특정 주문으로 적립된 포인트 조회 API handler 작성

--- src/mocks/handlers/point.ts
+++ src/mocks/handlers/point.ts
@@ -1,12 +1,31 @@
 import { rest } from 'msw';
-import { POINT_BASE_URL } from '../../constants/api';
+import { ORDERS_BASE_URL, POINT_BASE_URL } from '../../constants/api';
+import { fetchOrder } from '../../remotes/order';
 
 const POINT = {
   points: 20_000,
 };
 
 export const pointHandlers = [
+  // 보유 포인트 조회
   rest.get(POINT_BASE_URL, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(POINT));
+  }),
+
+  // 특정 주문으로 적립된 포인트 조회
+  rest.get(`${ORDERS_BASE_URL}/:id${POINT_BASE_URL}`, async (req, res, ctx) => {
+    const orderId = Number(req.params.id);
+    const order = await fetchOrder(`${ORDERS_BASE_URL}/${orderId}`);
+    const { price } = order;
+
+    if (order === undefined) {
+      return res(ctx.status(404));
+    }
+
+    const savedPoint = {
+      points_saved: Math.floor(price * 0.025),
+    };
+
+    return res(ctx.status(200), ctx.json(savedPoint));
   }),
 ];
