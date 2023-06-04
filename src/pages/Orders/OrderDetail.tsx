f24c729bb216cd163c8be42bda574b73f597a0ba
gilpop8663 <wolfye@naver.com>
2023-06-05 04:10:45 +0900

    feat: 주문 상세 내역을 서버와 연동되도록 컴포넌트에 코드 적용

--- src/pages/Orders/OrderDetail.tsx
+++ src/pages/Orders/OrderDetail.tsx
@@ -1,30 +1,19 @@
-import { useNavigate, useParams } from 'react-router-dom';
-import { ROUTER_PATH } from '@router/router';
+import { Suspense } from 'react';
+import { useParams } from 'react-router-dom';
+import SkeletonCart from '@components/cart/SkeletonCartItemList';
 import Layout from '@components/layout/Layout';
+import OrderDetailArea from '@components/orders/OrderDetailArea';
 import OrderHeader from '@components/orders/OrderHeader';
-import OrderItem from '@components/orders/OrderItemList/OrderItem';
-import OrderPaymentAmount from '@components/orders/OrderPaymentAmount';
-import { MOCK_ORDER_LIST } from '@mocks/handlers';
-import * as S from './OrderDetail.style';
 
 function OrderDetail() {
   const { orderId } = useParams();
-  const navigate = useNavigate();
-  const order = MOCK_ORDER_LIST.find((orderItem) => orderItem.id === Number(orderId));
-
-  if (!order) {
-    navigate(ROUTER_PATH.HOME);
-    return <div>존재하지 않는 주문 내역입니다.</div>;
-  }
 
   return (
     <Layout>
       <OrderHeader text="주문 내역 상세" />
-      <OrderItem order={order} isDetail={false} />
-      <S.OrderPaymentAmountWrapper>
-        <div />
-        <OrderPaymentAmount {...order} />
-      </S.OrderPaymentAmountWrapper>
+      <Suspense fallback={<SkeletonCart />}>
+        <OrderDetailArea orderId={Number(orderId)} />
+      </Suspense>
     </Layout>
   );
 }
