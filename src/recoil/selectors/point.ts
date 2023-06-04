4dc7b5b418741e8a1fd446326813b44238cc5eab
Suyoung <jsy8492@gmail.com>
2023-06-04 15:29:11 +0900

    feat: 적립 포인트 데이터를 저장하는 recoil atom, selector 작성

--- src/recoil/selectors/point.ts
+++ src/recoil/selectors/point.ts
@@ -1,7 +1,9 @@
-import { selector } from 'recoil';
-import { fetchPoint } from '../../remotes/point';
+import { selector, selectorFamily } from 'recoil';
+import { fetchPoint, fetchSavedPointByOrder } from '../../remotes/point';
 import { serverOriginState } from '../atoms/common';
 import { POINT_BASE_URL } from '../../constants/api';
+import { ORDERS_BASE_URL } from '../../constants/api';
+import type { Order } from '../../types/order';
 
 export const pointQuery = selector<number>({
   key: 'point',
@@ -12,4 +14,19 @@ export const pointQuery = selector<number>({
 
     return point;
   },
+});
+
+export const savedPointByOrderQuery = selectorFamily<number, Order['id']>({
+  key: 'savedPointByOrder',
+  get:
+    (orderId: Order['id']) =>
+    async ({ get }) => {
+      const savedPointByOrder = await fetchSavedPointByOrder(
+        `${get(
+          serverOriginState,
+        )}${ORDERS_BASE_URL}/${orderId}${POINT_BASE_URL}`,
+      );
+
+      return savedPointByOrder;
+    },
 });
