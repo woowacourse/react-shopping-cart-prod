b767d917fe47cefd194da152f64b006cb9e9b8e8
nangkyeonglim <nangkyeonglim@gmail.com>
2023-06-04 21:13:25 +0900

    feat: 주문 완료 후 주문내역 페이지 이동 시, 장바구니와 point정보 불리오기

--- src/pages/Order/Order.tsx
+++ src/pages/Order/Order.tsx
@@ -1,19 +1,37 @@
-import { useRecoilValue } from 'recoil';
-import { fetchOrderList } from '../../api/fetcher';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { fetchMemberPoint, fetchOrderList } from '../../api/fetcher';
 import { OrderList } from '../../components/Order/OrderList';
 import useGetQuery from '../../hooks/useGetQuery';
 import { FatBorder, PageTitle } from '../../style/style';
-import { OrdersResponses } from '../../types/types';
+import { OrdersResponses, Point } from '../../types/types';
 import * as S from './Order.style';
-import { memberAuthorization } from '../../recoil/userAtoms';
+import { memberAuthorization, memberPointState } from '../../recoil/userAtoms';
 import { serverState } from '../../recoil/serverAtom';
+import { fetchCartList } from '../../api/api';
+import { useEffect } from 'react';
+import { cartState } from '../../recoil/cartAtoms';
 
 function Order() {
   const memberAuth = useRecoilValue(memberAuthorization);
   const server = useRecoilValue(serverState);
+  const setCartList = useSetRecoilState(cartState);
+  const setMemberPoint = useSetRecoilState(memberPointState);
   const { data: orders } = useGetQuery<OrdersResponses>({
     fetcher: fetchOrderList({ server, auth: memberAuth }),
   });
+  useGetQuery<Point>({
+    fetcher: fetchMemberPoint({ server, auth: memberAuth }),
+    onSuccess: (point) => setMemberPoint(point),
+  });
+
+  const loadCartList = async () => {
+    const checkedCartItems = await fetchCartList(server, memberAuth);
+    setCartList(checkedCartItems.cartItems);
+  };
+
+  useEffect(() => {
+    loadCartList();
+  }, [server, memberAuth]);
 
   return (
     <>
