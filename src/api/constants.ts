37a216d2b37315cb2cf39fdbe80155b5b14209ec
guridaek <xylitol79@gmail.com>
2023-06-04 14:06:56 +0900

    chore: 서버 주소 변경

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -4,7 +4,7 @@ const password = "1234";
 export const USER_TOKEN = btoa(username + ":" + password);
 
 export const SERVER_LIST = {
-  "power-server": "https://power.better-than-coupang.kro.kr",
+  "power-server": "https://power2.better-than-coupang.kro.kr",
   "ttaengchil-server": "https://0chil.better-than-coupang.kro.kr",
   "ori-server": "https://duck.better-than-coupang.kro.kr",
 } as const;
