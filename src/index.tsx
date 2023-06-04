567220fc8e98731231a6ac9503cdd725001ee843
TaeeunKim <xodms0309@naver.com>
2023-06-04 21:11:16 +0900

    fix: msw worker url 수정

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,14 @@ import App from './App';
 import { worker } from './mocks/browser';
 
 const main = async () => {
-  if (window.location.pathname === '/react-shopping-cart') {
-    window.location.pathname = '/react-shopping-cart/';
+  if (window.location.pathname === '/react-shopping-cart-prod') {
+    window.location.pathname = '/react-shopping-cart-prod/';
     return;
   }
 
   await worker.start({
     serviceWorker: {
-      url: '/react-shopping-cart/mockServiceWorker.js',
+      url: '/react-shopping-cart-prod/mockServiceWorker.js',
     },
   });
 };
