c742ddc60d207c2e1cae9babbd48327bc5758b5b
regularPark <srk99279@naver.com>
2023-06-04 17:45:01 +0900

    design: ProductListWrapper 생성

--- src/style/ContentLayout.ts
+++ src/style/ContentLayout.ts
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
 
+export const ProductListWrapper = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-top: 120px;
+  gap: 80px;
+`;
+
 export const CartListWrapper = styled.div`
   width: 100vw;
   display: flex;
@@ -17,8 +27,8 @@ export const OrderListWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 80px;
-  gap: 80px;
+  padding: 120px 0;
+  gap: 40px;
   @media all and (max-width: 479px) {
     gap: 30px;
   }
@@ -28,11 +38,16 @@ export const OrderWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 80%;
+  width: 100%;
 `;
 
-export const ProductListWrapper = styled.div`
-  width: 100vw;
-  padding-top: 120px;
-  gap: 80px;
+export const OrderDetailWrapper = styled.div`
+  width: 80vw;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 40px;
+  @media all and (max-width: 479px) {
+    gap: 30px;
+  }
 `;
