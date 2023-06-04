ce70449f4f5c4bcb562da02be84a6975fd6cfca2
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2023-06-04 22:56:47 +0900

    style: 불필요한 코드 제거

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -9,24 +9,7 @@ import useCartOrder from '../hooks/useCartOrder';
 import cartItemsState from '../recoil/atoms/cartItemsState';
 import type { CartItem } from '../type';
 
-const Header = styled.header`
-  padding-bottom: 32px;
-
-  border-bottom: 4px solid #000000;
-
-  text-align: center;
-  font-size: 32px;
-  font-weight: 700;
-`;
-
 const CartLayout = styled.article`
-  display: grid;
-  grid-template-columns: 1fr;
-  align-items: flex-start;
-  justify-content: space-between;
-  row-gap: 48px;
-  column-gap: 24px;
-
   margin-top: 32px;
 
   @media screen and (min-width: 992px) {
@@ -34,17 +17,11 @@ const CartLayout = styled.article`
   }
 `;
 
-const CartItemListSection = styled.section``;
-
-const CartItemListCaption = styled.h2`
-  margin-bottom: 16px;
-
-  font-size: 20px;
+const CartItemListSection = styled.section`
+  border-bottom: 7px solid ${({ theme }) => theme.colors.gray300};
 `;
 
 const CartItemList = styled.ul`
-  border-top: 4px solid #aaaaaa;
-
   & > * + * {
     border-top: 1.5px solid #cccccc;
   }
@@ -59,19 +36,27 @@ const CartItemListItemContainer = styled.li`
 
 const CartItemListController = styled.div`
   display: flex;
+  justify-content: space-between;
   align-items: center;
   gap: 16px;
+
+  border-bottom: 7px solid ${({ theme }) => theme.colors.gray300};
+
+  & span {
+    display: flex;
+  }
 `;
 
 const CartItemSelected = styled.p`
   font-size: 16px;
+
+  margin-left: 10px;
 `;
 
 const DeleteSelectedButton = styled.button`
-  padding: 6px;
+  margin-right: 30px;
 
-  border: 1px solid #bbbbbb;
-  font-size: 16px;
+  ${({ theme }) => theme.fonts.description}
 `;
 
 type CartPageContentProps = {
@@ -102,7 +87,15 @@ const CartPageContent = (props: CartPageContentProps) => {
   return (
     <CartLayout>
       <CartItemListSection>
-        <CartItemListCaption>배송 상품 ({cartItems.length}개)</CartItemListCaption>
+        <CartItemListController>
+          <span>
+            <Checkbox value={allSelected} onChange={handleEnableAll(cartItems)} />
+            <CartItemSelected>전체{cartItems.length}개</CartItemSelected>
+          </span>
+          <DeleteSelectedButton onClick={handleDeleteSelected(cartItems)}>
+            선택삭제
+          </DeleteSelectedButton>
+        </CartItemListController>
         <CartItemList>
           {cartItems.map((cartItem) => (
             <CartItemListItemContainer>
@@ -118,38 +111,24 @@ const CartPageContent = (props: CartPageContentProps) => {
             </CartItemListItemContainer>
           ))}
         </CartItemList>
-
-        <CartItemListController>
-          <Checkbox value={allSelected} onChange={handleEnableAll(cartItems)} />
-          <CartItemSelected>
-            전체선택 ({selectedCount}/{cartItems.length}개)
-          </CartItemSelected>
-          <DeleteSelectedButton onClick={handleDeleteSelected(cartItems)}>
-            선택삭제
-          </DeleteSelectedButton>
-        </CartItemListController>
       </CartItemListSection>
 
-      <CartOrder isCartEmpty={selectedCount === 0} />
+      <CartOrder selectedCount={selectedCount} />
     </CartLayout>
   );
 };
 
 const CartPage = () => {
   return (
-    <>
-      <Header>장바구니</Header>
-
-      <AwaitRecoilState state={cartItemsState}>
-        {(cartItems) =>
-          cartItems.length === 0 ? (
-            <CartEmptyPlaceholder />
-          ) : (
-            <CartPageContent cartItems={cartItems} />
-          )
-        }
-      </AwaitRecoilState>
-    </>
+    <AwaitRecoilState state={cartItemsState}>
+      {(cartItems) =>
+        cartItems.length === 0 ? (
+          <CartEmptyPlaceholder />
+        ) : (
+          <CartPageContent cartItems={cartItems} />
+        )
+      }
+    </AwaitRecoilState>
   );
 };
 
