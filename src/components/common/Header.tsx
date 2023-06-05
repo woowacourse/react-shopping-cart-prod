88e37a49cec611708c9fb5c3eda84df2ea37c653
bassyu <yrkdgus@gmail.com>
2023-06-05 10:26:24 +0900

    chore(Header): 사용하지 않는 import 삭제

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,5 @@
 import type { ServerNameType } from '../../types';
 
-import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import styled from 'styled-components';
