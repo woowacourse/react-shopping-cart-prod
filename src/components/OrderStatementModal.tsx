4d2888ad7d3ee7930d8fa7aa0b061270b4bbdad3
chsua <csj1919@naver.com>
2023-06-05 14:43:54 +0900

    feat: 주문 상세 내역 구조 변경
    - 주문 정보 형식 수정
    - 주문 금액, 결제 금액 등 금액 상세화

--- src/components/OrderStatementModal.tsx
+++ src/components/OrderStatementModal.tsx
@@ -19,32 +19,47 @@ const OrderStatementModal = ({
     <Modal closeEvent={closeModal}>
       <PageHeader>주문 상세 내역</PageHeader>
       <OrderInfo>
-        <span>주문번호: {orderId}</span>
-        <span>총 결제금액: {total.toLocaleString()}원</span>
-        <span></span>
-        <span>배송비: {deliveryFee.toLocaleString()}원</span>
+        <span>주문번호</span>
+        <span>주문 금액</span>
+        <span>배송비</span>
+        <span>총 결제금액</span>
+        <span>{orderId}</span>
+        <span>{total.toLocaleString()}원</span>
+        <span>{deliveryFee.toLocaleString()}원</span>
+        <span>{(total + deliveryFee).toLocaleString()}원</span>
       </OrderInfo>
       <OrderList>
         {orderItems.map((item) => {
+          const { product, quantity, total, coupons } = item;
           return (
             <OrderBox key={`order-item-${item.product.id}`}>
               <OrderItem item={item} />
               <OrderDetail>
                 <span>상품 금액 (1개)</span>
-                <span> {item.product.price.toLocaleString()}원</span>
+                <span> {product.price.toLocaleString()}원</span>
                 <span>주문 수량</span>
-                <span> {item.quantity.toLocaleString()}개</span>
+                <span> {quantity.toLocaleString()}개</span>
                 <span>결제 금액</span>
-                <span> {item.total.toLocaleString()}원</span>
+                <span> {total.toLocaleString()}원</span>
+                {coupons.length !== 0 && (
+                  <>
+                    <span> ㄴ 주문 금액</span>
+                    <span>{(product.price * quantity).toLocaleString()}원</span>
+                    <span> ㄴ 할인 금액</span>
+                    <span>
+                      {(product.price * quantity - total).toLocaleString()}원
+                    </span>
+                  </>
+                )}
               </OrderDetail>
-              {item.coupons.length !== 0 && (
+              {coupons.length !== 0 && (
                 <OrderDetail>
                   <span>적용 쿠폰</span>
-                  <span> {item.coupons[0].name}</span>
+                  <span> {coupons[0].name}</span>
                   <span>할인 금액</span>
                   <span>
-                    {item.coupons[0].discount.amount}
-                    {item.coupons[0].discount.type === "price" ? "원" : "%"}
+                    {coupons[0].discount.amount}
+                    {coupons[0].discount.type === "price" ? "원" : "%"}
                   </span>
                 </OrderDetail>
               )}
@@ -58,14 +73,16 @@ const OrderStatementModal = ({
 
 const OrderInfo = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
   gap: 10px 0;
 
-  font-size: 18px;
+  font-size: 16px;
   margin: 5px 0;
 
-  & :nth-child(even) {
-    text-align: right;
+  text-align: center;
+
+  & > * {
+    // border: 1px solid var(--gray-color);
   }
 `;
 
@@ -74,7 +91,7 @@ const OrderList = styled.div`
 `;
 
 const OrderBox = styled.div`
-  border: 1px solid var(--primary-blue-color);
+  border: 1px solid var(--gray-color);
   border-radius: 5px;
 
   margin: 13px 0;
@@ -87,11 +104,13 @@ const OrderDetail = styled.div`
 
   font-size: 15.5px;
 
-  border: 1px solid var(--primary-blue-color);
+  border: 1px solid var(--gray-color);
   border-radius: 5px;
 
   margin: 5px 10px 10px 10px;
   padding: 10px 15px;
+
+  background-color: var(--light-color);
 
   & :nth-child(even) {
     text-align: right;
