b27af82e692f57fb16df67a191068f924acdb574
Suyoung <jsy8492@gmail.com>
2023-06-05 13:26:32 +0900

    feat: 사용자 전환 아이콘 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -7,3 +7,4 @@ export { ReactComponent as CloseIcon } from './close.svg';
 export { ReactComponent as RewardIcon } from './reward.svg';
 export { ReactComponent as OrderIcon } from './order.svg';
 export { ReactComponent as UserIcon } from './user.svg';
+export { ReactComponent as UserChangeIcon } from './userChange.svg';
