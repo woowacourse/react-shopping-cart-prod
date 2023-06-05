35c27181979ac7f74a9c68237c2b97eb77c79b52
jeongwusi <jeongwusi@gmail.com>
2023-06-05 15:24:17 +0900

    refactor: Order API의 명세 수정으로 인한 코드 수정

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -3,15 +3,20 @@ import { useFetchData } from './useFetchData';
 import { serverState } from '../store/ServerState';
 import { ORDER_BASE_URL } from '../constants/url';
 import { checkedItemsState } from '../store/CheckedItemsState';
+import { inputPointValueState } from '../store/InputPointValueState';
 
 export const useOrder = () => {
   const { api } = useFetchData();
   const serverUrl = useRecoilValue(serverState);
   const checkedItems = useRecoilValue(checkedItemsState);
-  // const inputPointValue = useRecoilValue(inputPointValueState); 서버 완료되면 point 바꿔야함
+  const inputPointValue = useRecoilValue(inputPointValueState);
 
   const orderToItems = () => {
-    api.post(`${serverUrl}${ORDER_BASE_URL}`, { cartIds: checkedItems, point: 0 }, ORDER_BASE_URL);
+    api.post(
+      `${serverUrl}${ORDER_BASE_URL}`,
+      { cartIds: checkedItems, point: inputPointValue, deliveryFee: 3000 },
+      ORDER_BASE_URL,
+    );
   };
 
   return { orderToItems };
