7a9ceb088ed8c2af350c550171d5ebe63b641dfb
wzrabbit <curious.wzrabbit@gmail.com>
2023-06-05 16:03:33 +0900

    feat: 더미 데이터 제거, Loadable를 적용해 로딩화면/오류화면이 나타나도록 구현

--- src/components/pages/OrderDetailPage/OrderDetailPage.tsx
+++ src/components/pages/OrderDetailPage/OrderDetailPage.tsx
@@ -1,50 +1,40 @@
 import { styled } from 'styled-components';
-import OrderTableList from '../../order/OrderTableList/OrderTableList';
-
-const orderInfos = [
-  {
-    id: 123,
-    price: 40000,
-    orderDate: '2023-10-20T23:01:59',
-    orders: [
-      {
-        id: 3942,
-        quantity: 2,
-        product: {
-          id: 2543,
-          name: '우코우 피자',
-          price: 15000,
-          imageUrl:
-            'https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_1280.jpg',
-        },
-      },
-      {
-        id: 1029,
-        quantity: 5,
-        product: {
-          id: 2543,
-          name: '도우밥 만두',
-          price: 3000,
-          imageUrl:
-            'https://cdn.pixabay.com/photo/2016/02/17/10/41/dumplings-1204814_1280.jpg',
-        },
-      },
-    ],
-  },
-];
+import OrderTable from '../../order/OrderTable/OrderTable';
+import FullWidthTitle from '../../common/FullWidthTitle/FullWidthTitle';
+import colors from '../../../colors';
+import { useRecoilValueLoadable } from 'recoil';
+import { orderDetailQuery } from '../../../recoil/selectors';
+import { useParams } from 'react-router-dom';
+import { formatPrice } from '../../../utils/formatPrice';
+import ErrorComponent from '../../common/Error/ErrorComponent';
+import Spinner from '../../common/Spinner/Spinner';
 
 const OrderDetailPage = () => {
+  const { orderId } = useParams();
+
+  if (orderId === undefined) {
+    throw new Error('주소가 잘못되었습니다.');
+  }
+
+  const orderInfo = useRecoilValueLoadable(orderDetailQuery(orderId));
+
+  if (orderInfo.state === 'loading') {
+    return <Spinner />;
+  }
+
+  if (orderInfo.state === 'hasError') {
+    return <ErrorComponent>{orderInfo.contents.message}</ErrorComponent>;
+  }
+
   return (
     <Container>
-      <TitleWrapper>
-        <Title>주문 내역 상세</Title>
-      </TitleWrapper>
-      <OrderTableList orderInfos={orderInfos} />
+      <FullWidthTitle>주문 내역 상세</FullWidthTitle>
+      <OrderTable orderInfo={orderInfo.contents} />
       <OrderTotal>
         <OrderTotalHeader>결제금액 정보</OrderTotalHeader>
         <OrderTotalPrice>
           <span>총 결제금액</span>
-          <span>325,600원</span>
+          <span>{formatPrice(orderInfo.contents.price)}</span>
         </OrderTotalPrice>
       </OrderTotal>
     </Container>
@@ -57,26 +47,11 @@ const Container = styled.div`
   row-gap: 28px;
 `;
 
-const TitleWrapper = styled.div`
-  height: 67px;
-  border-bottom: 4px solid #333;
-`;
-
-const Title = styled.h2`
-  font-family: 'Noto Sans KR';
-  font-weight: 700;
-  font-size: 32px;
-  line-height: 37px;
-  text-align: center;
-  letter-spacing: 0.5px;
-  color: #333;
-`;
-
 const OrderTotal = styled.div`
   width: 560px;
   height: 207px;
   margin-left: auto;
-  border: 1px solid #aaa;
+  border: 1px solid ${colors.transparentGold};
 `;
 
 const OrderTotalHeader = styled.div`
@@ -84,28 +59,32 @@ const OrderTotalHeader = styled.div`
   align-items: center;
   height: 92px;
   padding: 0 30px;
-  background: #f6f6f6;
-  border-bottom: 1px solid #aaa;
-  font-family: 'Noto Sans KR';
+  background: ${colors.pureBlack};
+  border-bottom: 1px solid ${colors.transparentGold};
   font-weight: 700;
   font-size: 28px;
   line-height: 28px;
   letter-spacing: 0.5px;
-  color: #333;
+  color: ${colors.gold};
 `;
 
 const OrderTotalPrice = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 115px;
+  height: 114px;
   padding: 0 30px;
-  font-family: 'Noto Sans KR';
   font-weight: 700;
   font-size: 24px;
   line-height: 24px;
   letter-spacing: 0.5px;
-  color: #333;
+  color: ${colors.gold};
+  background-color: ${colors.pureBlack};
+  border-bottom: ${colors.transparentGold};
+
+  & span {
+    color: ${colors.lightGold};
+  }
 `;
 
 export default OrderDetailPage;
