93c62ee0e5d50958d9a20792d42c3f3fd1952978
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-06-05 16:22:08 +0900

    refactor: 에러 바운더리 내부 에러 분기 추가

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -5,6 +5,7 @@ interface ErrorBoundaryState {
   hasError: boolean;
   isCorsError: boolean;
   isAuthError: boolean;
+  isNetworkError: boolean;
 }
 
 interface ErrorBoundaryProps {
@@ -14,18 +15,21 @@ interface ErrorBoundaryProps {
 export class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
   constructor(props: ErrorBoundaryProps) {
     super(props);
-    this.state = { hasError: false, isCorsError: false, isAuthError: false };
+    this.state = { hasError: false, isCorsError: false, isAuthError: false, isNetworkError: false };
   }
 
   static getDerivedStateFromError(error: Error): ErrorBoundaryState {
     if (error.message === 'CORS Error: Unable to make the request due to CORS restrictions.') {
-      return { hasError: true, isCorsError: true, isAuthError: false };
+      return { hasError: true, isCorsError: true, isAuthError: false, isNetworkError: false };
+    }
+    if (error.message?.includes('서버문제로 HTTP 통신에 실패했습니다.')) {
+      return { hasError: true, isCorsError: false, isAuthError: false, isNetworkError: true };
     }
     if (error.message?.includes('해당 멤버가 존재하지 않습니다.')) {
-      return { hasError: true, isCorsError: false, isAuthError: true };
+      return { hasError: true, isCorsError: false, isAuthError: true, isNetworkError: false };
     }
     // Update state to show fallback UI for other errors
-    return { hasError: true, isCorsError: false, isAuthError: false };
+    return { hasError: true, isCorsError: false, isAuthError: false, isNetworkError: false };
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
@@ -41,6 +45,10 @@ export class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoun
       if (this.state.isAuthError) {
         window.alert('해당 멤버가 존재하지 않습니다.');
         return;
+      }
+      if (this.state.isNetworkError) {
+        window.alert('서버문제로 HTTP 통신에 실패했습니다.');
+        return <LoadingErrorCard onClickRetryButton={() => (window.location.href = '/')} />;
       } else {
         console.log(this.state);
         // Render a generic fallback UI component for other errors
