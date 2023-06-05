39b4ae7f8730bb46042e0919260602f1ceb2a48e
chsua <csj1919@naver.com>
2023-06-05 14:51:44 +0900

    feat: msw 워커 업데이트
    - 쿠폰 불러오기
    - 주문 목록 불러오기
    - 주문하기

--- src/mocks/worker.ts
+++ src/mocks/worker.ts
@@ -6,18 +6,64 @@ import couponList from "./data/mockCouponList.json";
 import orderList from "./data/mockOrderStatementList.json";
 
 export const handlers = [
-  // 쿠폰 전체 조회
+  rest.get("/products", (req, res, ctx) => {
+    return res(
+      ctx.delay(1000),
+      ctx.status(201),
+      ctx.set("Content-Type", "application/json"),
+      ctx.json(products)
+    );
+  }),
+
+  rest.get("/cart-items", (req, res, ctx) => {
+    return res(
+      ctx.delay(5000),
+      ctx.status(200),
+      ctx.set("Content-Type", "application/json"),
+      ctx.json(getCart())
+    );
+  }),
+
+  rest.post("/cart-items", async (req, res, ctx) => {
+    const { productId } = await req.json();
+
+    const cartItemId = addCartItem(productId);
+
+    return res(
+      ctx.delay(100),
+      ctx.status(201),
+      ctx.set("Location", `/cart-items/${cartItemId}`)
+    );
+  }),
+
+  rest.patch("/cart-items/:cartItemId", async (req, res, ctx) => {
+    const { cartItemId } = req.params;
+    const { quantity } = await req.json();
+
+    setCartItem(Number(cartItemId), quantity);
+
+    return res(ctx.delay(100), ctx.status(200));
+  }),
+
+  rest.delete("/cart-items/:cartItemId", async (req, res, ctx) => {
+    const { cartItemId } = req.params;
+
+    setCartItem(Number(cartItemId), 0);
+
+    return res(ctx.delay(100), ctx.status(204));
+  }),
+
   rest.get("/coupons", (req, res, ctx) => {
     return res(
       ctx.delay(1000),
-      ctx.status(200),
+      ctx.status(400),
       ctx.set("Content-Type", "application/json"),
       ctx.json(couponList)
     );
   }),
 
   rest.post("/orders", (req, res, ctx) => {
-    return res(ctx.delay(500), ctx.status(204));
+    return res(ctx.delay(200), ctx.status(204));
   }),
 
   rest.get("/orders", (req, res, ctx) => {
@@ -28,33 +74,4 @@ export const handlers = [
       ctx.json(orderList)
     );
   }),
-
-  // rest.post("/cart-items", async (req, res, ctx) => {
-  //   const { productId } = await req.json();
-
-  //   const cartItemId = addCartItem(productId);
-
-  //   return res(
-  //     ctx.delay(100),
-  //     ctx.status(201),
-  //     ctx.set("Location", `/cart-items/${cartItemId}`)
-  //   );
-  // }),
-
-  // rest.patch("/cart-items/:cartItemId", async (req, res, ctx) => {
-  //   const { cartItemId } = req.params;
-  //   const { quantity } = await req.json();
-
-  //   setCartItem(Number(cartItemId), quantity);
-
-  //   return res(ctx.delay(100), ctx.status(200));
-  // }),
-
-  // rest.delete("/cart-items/:cartItemId", async (req, res, ctx) => {
-  //   const { cartItemId } = req.params;
-
-  //   setCartItem(Number(cartItemId), 0);
-
-  //   return res(ctx.delay(100), ctx.status(204));
-  // }),
 ];
