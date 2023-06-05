590dc6f884a2dd8aaf9d219ecec50f65cfbb2dea
chsua <csj1919@naver.com>
2023-06-05 14:52:04 +0900

    refactor: 사용하지 않는 msw 워커 삭제하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { RecoilRoot } from "recoil";
 import { PageRouterProvider } from "./router";
-import { worker } from "mocks/browser";
-
-// if (process.env.NODE_ENV === "development") {
-//   worker.start();
-// }
 
 const App = () => {
   return (
