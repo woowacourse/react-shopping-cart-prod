98c1866d9c0be375956dc5a35fb68355d2df446a
yogjin <youngjin988@gmail.com>
2023-06-05 17:06:14 +0900

    feat: App 에 Suspense 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Error from './components/common/Error/Error';
 import ErrorBoundary from './components/common/ErrorBoundary/ErrorBoundary';
 import ErrorModal from './components/common/ErrorModal/ErrorModal';
 import Header from './components/common/Header/Header';
+import SpinnerContainer from './components/common/SpinnerContainer/SpinnerContainer';
 import { useErrorReset } from './hooks/common/useErrorReset';
 
 const App = () => {
   const { handleErrorReset } = useErrorReset();
 
   return (
-    <ErrorBoundary Fallback={Error} onReset={handleErrorReset}>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <ErrorModal />
-    </ErrorBoundary>
+    <Suspense fallback={<SpinnerContainer />}>
+      <ErrorBoundary Fallback={Error} onReset={handleErrorReset}>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <ErrorModal />
+      </ErrorBoundary>
+    </Suspense>
   );
 };
 
