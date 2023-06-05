dafba30b5d6b9169292ea05d2b4f76339f74c8c2
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-06-05 09:21:10 +0900

    test: 스토리 표시 오류 수정

--- src/components/CheckOutProductSection/CheckOutProductSection.stories.tsx
+++ src/components/CheckOutProductSection/CheckOutProductSection.stories.tsx
@@ -1,9 +1,21 @@
 import { Meta, StoryObj } from '@storybook/react';
 import CheckOutProductSection from './CheckOutProductSection';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin-top: 80px;
+`;
 
 const meta = {
   component: CheckOutProductSection,
   title: 'checkout-page/CheckOutProductSection',
+  decorators: [
+    (Story) => (
+      <Container>
+        <Story />
+      </Container>
+    ),
+  ],
 } satisfies Meta<typeof CheckOutProductSection>;
 
 export default meta;
