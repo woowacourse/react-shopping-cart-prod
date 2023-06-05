d337b31789b3a80e42fc759712af203750b993b4
chex <chex1004@gmail.com>
2023-06-05 16:28:10 +0900

    coment: 불필요한 주석 제거

--- src/api/APIHandler.ts
+++ src/api/APIHandler.ts
@@ -5,17 +5,13 @@ const defaultToken = btoa('b@b.com:1234');
 
 const APIHandler = {
   get: async <T>(url: string, token: string = defaultToken): Promise<ResponseResult<T>> => {
-    // console.log('get 요청을 보냈습니다.');
-
     const options: RequestInit = { method: 'GET', headers: { Authorization: `Basic ${token}` } };
     const responseResult = await fetchAPI<T>(url, options);
-    // console.log('get에 대한 응답을 받았습니다.', responseResult.statusCode);
 
     return responseResult;
   },
 
   post: async <T>(url: string, body: T, token: string = defaultToken) => {
-    // console.log('post 요청을 보냈습니다.');
     const options: RequestInit = {
       method: 'POST',
       headers: {
@@ -25,13 +21,7 @@ const APIHandler = {
       body: JSON.stringify(body),
     };
 
-    // console.log('post요청 options', options);
-    // console.log('post요청 options.header', options.headers);
-    // console.log('post요청 options.body', options.body);
-
     const responseResult = await fetchAPI<T>(url, options);
-    // console.log('post에 대한 응답을 받았습니다.', responseResult.statusCode);
-    // console.log('post에 대한 응답을 받았습니다.', responseResult.errorMessage);
 
     return responseResult;
   },
