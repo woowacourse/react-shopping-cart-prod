5342f97715d86fa1c0539776983d3b3d8167b9a9
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-06-05 15:47:30 +0900

    refactor: Header 스타일 수정

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,20 +1,27 @@
 import styled from '@emotion/styled';
-import { Text } from '../Text/Text';
-import { CartIcon } from '../../../assets';
 import UserCartInfo from './UserCartInfo';
 import { Link } from 'react-router-dom';
 import Selector from '../Selector/Selector';
+import { Text } from '../Text/Text';
 
 const Header = () => {
   return (
     <HeaderWrapper>
       <HeaderInner>
-        <LogoWrapper to="/">
-          <CartIcon fill="#FFFFFF" style={{ marginRight: '20px' }} />
-          <Text color="#FFFFFF" size="largest" weight="bold" lineHeight="58px">
-            SHOP
-          </Text>
-        </LogoWrapper>
+        <Nav>
+          <LogoWrapper to="/">
+            <HeaderLogo
+              src="https://cdn-mart.baemin.com/front-end/assets-static/bmmart_logo_2021@3x.png"
+              alt="logo"
+            />
+          </LogoWrapper>
+          <Tab to="/event">
+            <Text size="smaller" weight="normal">
+              이벤트
+            </Text>
+          </Tab>
+        </Nav>
+
         <CartWrapper>
           <Selector />
           <UserCartInfo />
@@ -26,20 +33,35 @@ const Header = () => {
 
 export default Header;
 
+const Nav = styled.div`
+  display: flex;
+`;
+
+const Tab = styled(Link)`
+  margin-left: 30px;
+  display: flex;
+  align-items: center;
+`;
+
+const HeaderLogo = styled.img`
+  width: 120px;
+`;
+
 const HeaderWrapper = styled.div`
   width: 100%;
   height: 80px;
-  background-color: #333333;
+  background-color: rgba(255, 255, 255);
   display: flex;
   align-items: center;
   justify-content: center;
   position: fixed;
   top: 0;
   z-index: 1000;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 `;
 
 const HeaderInner = styled.div`
-  width: 1280px;
+  width: 1140px;
   display: flex;
   flex-direction: row;
   align-items: center;
