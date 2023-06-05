eb856f56dfd8b03c27b65edb5370026287bd5010
Suyoung <jsy8492@gmail.com>
2023-06-05 13:27:08 +0900

    refactor: 사용자 인증 정보 상수화

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,3 +1,14 @@
-const username = 'a@a.com';
+import type { UserName } from '../types/user';
+
+export const DEFAULT_USER_NAME = '유스';
+export const PAIR_USER_NAME = '요술토끼';
+
+const userNameMapper = {
+  유스: 'a@a.com',
+  요술토끼: 'b@b.com',
+} as const satisfies Record<UserName, string>;
 const password = '1234';
-export const base64 = btoa(username + ':' + password);
+
+export const getBase64 = (userName: UserName) => {
+  return btoa(userNameMapper[userName] + ':' + password);
+};
