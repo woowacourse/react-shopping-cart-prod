732464a91294c3f4455d6c1954af744b199da0f9
gilpop8663 <wolfye@naver.com>
2023-06-05 11:27:51 +0900

    fix: 장바구니 선택 삭제 기능에서 Recoil만 변경되는 것이 아니라 서버와 통신하도록 수정

--- src/hooks/recoil/cart/useCheckCart.ts
+++ src/hooks/recoil/cart/useCheckCart.ts
@@ -1,11 +1,20 @@
 import { useRecoilState, useRecoilValue } from 'recoil';
 import cartState from '@recoil/cart/cartState';
+import withCartTotalPrice from '@recoil/cart/selector/withCartTotalPrice';
 import withTotalCheckedCount from '@recoil/cart/selector/withTotalCheckedCount';
-import { allSelectCartItem, removeSelectedCartItem } from '@utils/cart/cart';
+import serverState from '@recoil/server/serverState';
+import userState from '@recoil/user/userState';
+import { useRecoilCart } from '@hooks/useRecoilCart';
+import { allSelectCartItem, cartItemSelectedById, removeSelectedCartItem } from '@utils/cart/cart';
+import { removeCartItemApi } from '@utils/cart/fetchCart';
 
 export const useCheckCart = () => {
+  const serverName = useRecoilValue(serverState);
+  const userInfo = useRecoilValue(userState);
   const [cart, setCart] = useRecoilState(cartState);
+  const { cartFetchData } = useRecoilCart();
   const checkedCount = useRecoilValue(withTotalCheckedCount);
+  const totalCartPrice = useRecoilValue(withCartTotalPrice);
 
   const isAllChecked = () => {
     return cart.length === checkedCount;
@@ -17,14 +26,26 @@ export const useCheckCart = () => {
     setCart(updatedCart);
   };
 
-  const deleteCheckedItems = () => {
+  const deleteCheckedItems = async () => {
     const updatedCart = removeSelectedCartItem(cart);
 
     setCart(updatedCart);
+
+    const selectedCartIds = cartItemSelectedById(cart);
+
+    for (const cartId of selectedCartIds) {
+      try {
+        await removeCartItemApi({ cartId, serverName, userInfo });
+      } catch (error) {
+        console.error(error);
+        cartFetchData();
+      }
+    }
   };
 
   return {
     checkedCount,
+    totalCartPrice,
     isAllChecked,
     toggleAllCartItem,
     deleteCheckedItems,
