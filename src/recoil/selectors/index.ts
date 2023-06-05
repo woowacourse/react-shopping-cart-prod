f2453e4cab06669537827463be4df9ed93e20dd9
wzrabbit <curious.wzrabbit@gmail.com>
2023-06-05 16:25:34 +0900

    feat: 주문 목록 / 주문 상세 정보 / 포인트 정보를 얻어낼 수 있도록 selector 구현

--- src/recoil/selectors/index.ts
+++ src/recoil/selectors/index.ts
@@ -1,34 +1,65 @@
-import { selector } from 'recoil';
+import { selector, selectorFamily } from 'recoil';
 import {
   serverOriginState,
   checkedItemIdsState,
   cartState,
   pointsState,
 } from '../atoms';
-import { CART_BASE_URL, PRODUCTS_BASE_URL } from '../../constants/api';
-import { fetchCartItems } from '../../remotes/cart';
+import {
+  PRODUCTS_BASE_URL,
+  ORDERS_BASE_URL,
+  POINTS_BASE_URL,
+} from '../../constants/api';
 import { fetchProducts } from '../../remotes/product';
-import type { CartItem, Product } from '../../types/product';
+import type { Product } from '../../types/product';
+import { Order } from '../../types/order';
+import { fetchOrders, fetchOrderDetail } from '../../remotes/order';
+import { fetchSavedPoints, fetchCurrentPoints } from '../../remotes/points';
 
 export const productsQuery = selector<Product[]>({
   key: 'products',
   get: async ({ get }) => {
-    const products = await fetchProducts(
-      `${get(serverOriginState)}${PRODUCTS_BASE_URL}`,
-    );
-
-    return products;
+    return await fetchProducts(`${get(serverOriginState)}${PRODUCTS_BASE_URL}`);
   },
 });
 
-export const cartItemsQuery = selector<CartItem[]>({
-  key: 'cartItems',
+export const ordersQuery = selector<Order[]>({
+  key: 'orderDetail',
   get: async ({ get }) => {
-    const cartItems = await fetchCartItems(
-      `${get(serverOriginState)}${CART_BASE_URL}`,
-    );
+    return await fetchOrders(`${get(serverOriginState)}${ORDERS_BASE_URL}`);
+  },
+});
 
-    return cartItems;
+export const orderDetailQuery = selectorFamily<Order, string>({
+  key: 'orderDetail',
+  get:
+    (orderId) =>
+    async ({ get }) => {
+      return await fetchOrderDetail(
+        `${get(serverOriginState)}${ORDERS_BASE_URL}/${orderId}`,
+      );
+    },
+});
+
+export const savedPointsQuery = selectorFamily<number, string>({
+  key: 'savedPoints',
+  get:
+    (orderId) =>
+    async ({ get }) => {
+      return await fetchSavedPoints(
+        `${get(
+          serverOriginState,
+        )}${ORDERS_BASE_URL}/${orderId}${POINTS_BASE_URL}`,
+      );
+    },
+});
+
+export const currentPointsQuery = selector<number>({
+  key: 'currentPoints',
+  get: async ({ get }) => {
+    return await fetchCurrentPoints(
+      `${get(serverOriginState)}${POINTS_BASE_URL}`,
+    );
   },
 });
 
