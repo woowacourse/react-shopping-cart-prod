0751319c5bc9024aab6e2adae86bd4b2b88a132f
yogjin <youngjin988@gmail.com>
2023-06-05 17:08:39 +0900

    feat: 주문 post 성공(onSuccess) 시 로직 추가

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -7,37 +7,49 @@ import { getCartAPI } from '../api/cartAPI';
 import { getOrderAPI } from '../api/orderAPI';
 import { TOAST_SHOW_DURATION } from '../constants';
 import { CART_API_ERROR_MESSAGE, HTTP_STATUS_CODE } from '../constants/api';
+import { PATH } from '../constants/path';
 import { CART_LIST_CHECKBOX_KEY } from '../constants/store';
 import { cartItemQuantityState, cartListState, checkedCartItemListState } from '../store/cart';
 import { checkedListState } from '../store/checkbox';
 import { errorModalMessageState } from '../store/error';
+import { memberInformationState } from '../store/member';
+import { orderListState } from '../store/order';
 import { currentServerState } from '../store/server';
-import { getCartPriceInformation } from '../store/utils';
-import { CartItemData, ProductItemData } from '../types';
 import { APIErrorMessage, PostOrdersRequestBody } from '../types/api';
 import { useMutationFetch } from './common/useMutationFetch';
+import { useNavigateParams } from './common/useNavigateParams';
 
 const useOrder = () => {
   const currentServer = useRecoilValue(currentServerState);
   const orderAPI = useMemo(() => getOrderAPI(currentServer), [currentServer]);
+  const cartAPI = useMemo(() => getCartAPI(currentServer), [currentServer]);
   const setErrorModalMessage = useSetRecoilState(errorModalMessageState);
-  const setCartListState = useSetRecoilState(cartListState);
-  const navigate = useNavigate();
+  const navigate = useNavigateParams();
 
-  const { mutate: orderCheckedCartItems } = useMutationFetch<{ id: number }, PostOrdersRequestBody>(
+  const { mutate: orderCheckedCartItems } = useMutationFetch<string, PostOrdersRequestBody>(
     useRecoilCallback(
       () => async (cartItemsForOrder) => {
         const response = await orderAPI.postOrderList(cartItemsForOrder);
-        const orderId = response.headers.get('Location')?.split('/').pop();
+        const orderId = response.headers.get('Location')?.split('/').pop()!;
 
-        return { id: Number(orderId) };
+        return orderId;
       },
       [orderAPI]
     ),
     {
-      onSuccess: (cartItem) => {
-        navigate(''); // orderedId와 함께 주문 완료 페이지로 이동
-      },
+      onSuccess: useRecoilCallback(
+        ({ refresh, set }) =>
+          async (orderId) => {
+            refresh(memberInformationState);
+            refresh(orderListState);
+
+            const updatedCartList = await cartAPI.getCartList();
+            set(cartListState, updatedCartList);
+
+            navigate(PATH.ORDERS_SUCCESS, { orderId }); // orderId와 함께 주문 완료 페이지로 이동
+          },
+        [navigate]
+      ),
       onError(error) {
         // handleCartError(error, CART_API_ERROR_MESSAGE.ADD);
       },
