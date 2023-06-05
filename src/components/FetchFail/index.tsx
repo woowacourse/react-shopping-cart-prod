1aada560d123415927ec56b7f5c4e60b70187465
hozzijeong <ghwls0905@naver.com>
2023-06-05 11:10:18 +0900

    feat: 실패시에 홈으로 돌아가는 버튼 추가

--- src/components/FetchFail/index.tsx
+++ src/components/FetchFail/index.tsx
@@ -1,22 +1,34 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ErrorBoundaryContext } from 'src/context/ErrorBoundaryContext';
 import useError from 'src/hooks/useError';
 import styles from './index.module.scss';
 
 function FetchFail() {
+  const navigation = useNavigate();
   const context = useContext(ErrorBoundaryContext);
   const { resetBoundary } = useError();
 
   if (!context) return null;
+
+  const moveHome = () => {
+    navigation('/');
+    resetBoundary();
+  };
 
   const { error } = context;
 
   return (
     <div className={styles['error-container']}>
       <h1 className={styles.message}>{error.message}</h1>
-      <button type="button" onClick={resetBoundary}>
-        다시 시도하기
-      </button>
+      <div>
+        <button type="button" onClick={resetBoundary}>
+          다시 시도하기
+        </button>
+        <button type="button" onClick={moveHome}>
+          홈으로 돌아가기
+        </button>
+      </div>
     </div>
   );
 }
