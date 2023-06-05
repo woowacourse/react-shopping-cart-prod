2ca1b53d6339b01189272ed6a874b54dfdac3a45
wzrabbit <curious.wzrabbit@gmail.com>
2023-06-05 15:55:59 +0900

    feat: 주문 상품에 대한 타입 추가

--- src/types/order.ts
+++ src/types/order.ts
@@ -1,10 +1,16 @@
-import { CartItem } from './product';
-
 interface Order {
   id: number;
   price: number;
   orderDate: string;
-  orders: CartItem[];
+  orders: OrderItem[];
+}
+
+export interface OrderItem {
+  id: number;
+  quantity: number;
+  price: number;
+  name: string;
+  imageUrl: string;
 }
 
 export type { Order };
