b3866b71d5f7b473fa0df66554197a568212b647
ashleysyheo <ashleysyheo@gmail.com>
2023-06-07 02:23:10 +0900

    refactor: 불필요한 fragment 제거

--- src/components/orderDetail/OrderDetailItem/OrderDetailItem.tsx
+++ src/components/orderDetail/OrderDetailItem/OrderDetailItem.tsx
@@ -6,26 +6,24 @@ type OrderDetailItemProps = OrderedItemData;
 
 const OrderDetailItem = ({ ...information }: OrderDetailItemProps) => {
   return (
-    <>
-      <S.OrderDetailItemContainer>
-        <S.OrderDetailItemImage src={information.imageUrl} alt={information.name} />
-        <S.OrderDetailItemInformation>
-          <S.OrderDetailItemName>{information.name}</S.OrderDetailItemName>
-          <S.OrderDetailItemPriceContainer>
-            <S.OrderDetailItemConsumerPrice as="span">
-              {priceFormatter(information.discountedPrice)}원
-            </S.OrderDetailItemConsumerPrice>
-            {information.discountRate > 0 && (
-              <S.OrderDetailItemOriginalPrice>
-                {priceFormatter(information.price)}원
-              </S.OrderDetailItemOriginalPrice>
-            )}
-            <S.VerticalLine />
-            <S.OrderDetailItemQuantity>{information.quantity}개</S.OrderDetailItemQuantity>
-          </S.OrderDetailItemPriceContainer>
-        </S.OrderDetailItemInformation>
-      </S.OrderDetailItemContainer>
-    </>
+    <S.OrderDetailItemContainer>
+      <S.OrderDetailItemImage src={information.imageUrl} alt={information.name} />
+      <S.OrderDetailItemInformation>
+        <S.OrderDetailItemName>{information.name}</S.OrderDetailItemName>
+        <S.OrderDetailItemPriceContainer>
+          <S.OrderDetailItemConsumerPrice as="span">
+            {priceFormatter(information.discountedPrice)}원
+          </S.OrderDetailItemConsumerPrice>
+          {information.discountRate > 0 && (
+            <S.OrderDetailItemOriginalPrice>
+              {priceFormatter(information.price)}원
+            </S.OrderDetailItemOriginalPrice>
+          )}
+          <S.VerticalLine />
+          <S.OrderDetailItemQuantity>{information.quantity}개</S.OrderDetailItemQuantity>
+        </S.OrderDetailItemPriceContainer>
+      </S.OrderDetailItemInformation>
+    </S.OrderDetailItemContainer>
   );
 };
 
