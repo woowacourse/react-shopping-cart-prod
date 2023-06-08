e474cc14e4dcf280cf446f643e6fa32d106fea4f
kangyeongmin <kym6050@hanyang.ac.kr>
2023-06-08 19:08:35 +0900

    refactor: timeoutId useRef로 수정

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -1,21 +1,21 @@
+import { useRef } from "react";
 import { useSetRecoilState } from "recoil";
 import { toastState } from "../recoil/atom";
 import { ToastType } from "../types/domain";
 
-let timeoutID: null | number = null;
-
 export const useToast = () => {
   const setToast = useSetRecoilState(toastState);
+  const timeoutID = useRef<number | null>(null);
 
   const showToast = (
     type: ToastType["type"],
     message: ToastType["message"]
   ) => {
-    if (timeoutID !== null) clearTimeout(timeoutID);
+    if (timeoutID.current) clearTimeout(timeoutID.current);
 
     setToast({ isShown: true, type, message });
 
-    timeoutID = window.setTimeout(() => {
+    timeoutID.current = window.setTimeout(() => {
       setToast({ isShown: false, type, message: "" });
     }, 2000);
   };
