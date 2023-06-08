5ff96d5e11fe4513905ea6821c25230310ea304a
inyeong-kang <tomatorkd@naver.com>
2023-06-08 23:21:05 +0900

    refactor: 함수 파라미터 타입 확장

--- src/utils/timeFormat.ts
+++ src/utils/timeFormat.ts
@@ -1,6 +1,17 @@
-export function formatDateTime(dateTime: string) {
-  const [date, time] = dateTime.split(' ');
-  const [year, month, day] = date.split('-');
+export function formatDateTime(dateTime: string | Date) {
+  let parsedDate: Date;
+
+  if (typeof dateTime === 'string') {
+    parsedDate = new Date(dateTime);
+  } else if (dateTime instanceof Date) {
+    parsedDate = dateTime;
+  } else {
+    throw new Error('Invalid date format');
+  }
+
+  const year = parsedDate.getFullYear();
+  const month = String(parsedDate.getMonth() + 1).padStart(2, '0');
+  const day = String(parsedDate.getDate()).padStart(2, '0');
 
   return `${year}.${month}.${day}`;
 }
