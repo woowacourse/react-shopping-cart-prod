707c7883c72d636411a3b4e2f6d595376a58d59b
regularPark <srk99279@naver.com>
2023-06-09 06:46:18 +0900

    refactor: 의미를 명확하게 하기 위해 과도한 삼항연산자 구문 분리

--- src/pages/OrderCompletePage.tsx
+++ src/pages/OrderCompletePage.tsx
@@ -25,17 +25,20 @@ const OrderCompletePage = () => {
   const currentOrder = useRecoilValue(currentOrderSelector);
   const id = currentOrder ? currentOrder.orderId : null;
 
-  return isLoading ? (
-    <LoadingSpinner />
-  ) : currentOrder && id ? (
-    <OrderListWrapper>
-      <OrderCompleteItem order={currentOrder} id={id} />
-      <S.ButtonWrapper>
-        <S.HomeButton onClick={goHome}>홈으로 가기</S.HomeButton>
-        <S.DetailButton onClick={() => goOrderDetail(id)}>주문 상세 내역 보기</S.DetailButton>
-      </S.ButtonWrapper>
-    </OrderListWrapper>
-  ) : null;
+  if (isLoading) return <LoadingSpinner />;
+
+  if (currentOrder && id)
+    return (
+      <OrderListWrapper>
+        <OrderCompleteItem order={currentOrder} id={id} />
+        <S.ButtonWrapper>
+          <S.HomeButton onClick={goHome}>홈으로 가기</S.HomeButton>
+          <S.DetailButton onClick={() => goOrderDetail(id)}>주문 상세 내역 보기</S.DetailButton>
+        </S.ButtonWrapper>
+      </OrderListWrapper>
+    );
+
+  return null;
 };
 
 export default OrderCompletePage;
