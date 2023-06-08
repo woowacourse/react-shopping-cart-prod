191779921b9f27c5e483ca26b4227cc8fe1c36c2
TaeeunKim <xodms0309@naver.com>
2023-06-08 20:36:35 +0900

    refactor: cartList를 가져오는 곳을 recoil selector에서 useGet hook으로 변경

--- src/components/Cart/hooks/useFetchCart.ts
+++ src/components/Cart/hooks/useFetchCart.ts
@@ -1,16 +1,20 @@
 import { useEffect } from 'react';
-import { useRecoilState, useRecoilValueLoadable } from 'recoil';
-import { cartListAtom, fetchCartListSelector } from 'recoil/cartList';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { cartListAtom, checkedItemsAtom } from 'recoil/cartList';
+import { getCartList } from 'api/requests';
+import { useGet } from 'hooks/useGet';
+import { Cart } from 'types';
 
 export const useFetchCart = () => {
   const [cartList, setCartList] = useRecoilState(cartListAtom);
-  const cartListResponse = useRecoilValueLoadable(fetchCartListSelector);
+  const setCheckedItems = useSetRecoilState(checkedItemsAtom);
+  const { data } = useGet<Cart[]>(getCartList);
 
   useEffect(() => {
-    if (cartListResponse.state === 'hasValue') {
-      setCartList(cartListResponse.contents);
-    }
-  }, []);
+    if (!data) return;
+    setCartList(data);
+    setCheckedItems(data.map((item) => item.id));
+  }, [data]);
 
-  return { cartList, setCartList };
+  return { cartList };
 };
--- src/recoil/cartList.ts
+++ src/recoil/cartList.ts
@@ -1,21 +1,9 @@
-import { getCartList } from 'api/requests';
 import { atom, selector } from 'recoil';
 import { Cart } from 'types';
-import { serverAtom } from './server';
 
 export const cartListAtom = atom<Cart[]>({
   key: 'cartList',
   default: [],
-});
-
-export const fetchCartListSelector = selector<Cart[]>({
-  key: 'fetchCartListSelector',
-  get: async ({ get }) => {
-    if (get(cartListAtom).length) return get(cartListAtom);
-    const server = get(serverAtom);
-    const data = await getCartList(server);
-    return data;
-  },
 });
 
 export const checkedItemsAtom = atom<number[]>({
