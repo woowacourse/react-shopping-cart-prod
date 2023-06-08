50980d75bfc98471b6a3a9c2f0ad3446d5c37852
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-09 01:29:45 +0900

    refactor: 장바구니 목록 요청을 useFetch가 하도록 개선

--- src/components/OrderList/OrderList.tsx
+++ src/components/OrderList/OrderList.tsx
@@ -1,41 +1,36 @@
 import OrderItem from "../OrderItem/OrderItem.tsx";
-import { useEffect, useState } from "react";
-import { OrderedGroup } from "../../types/types.ts";
-import { fetchOrderedList } from "../../app/api/api.ts";
 import { useRecoilValue } from "recoil";
 import { serverState } from "../../app/recoil/serverAtom.ts";
-import { NoneOrderHistory } from "./OrderList.style.ts";
 import BigAlert from "../BigAlert/BigAlert.tsx";
+
+import { url } from "../../app/api/url.ts";
+import useFetch from "../../hooks/useFetch.ts";
+import type { ResponseOrdered } from "../../types/types.ts";
 
 function OrderList() {
   const server = useRecoilValue(serverState);
-  const [orderList, setOrderList] = useState<OrderedGroup[]>([]);
-  const [isError, setError] = useState(false);
+  const { data: response, isLoading, error } = useFetch<ResponseOrdered>('GET', `${url[server]}/orders`);
 
-  const loadOrderedList = async () => {
-    try {
-      const data = await fetchOrderedList(server);
-      setOrderList(data.orderResponses);
-    } catch (error) {
-      setError(true);
-      throw new Error();
-    }
-  };
+  console.log(response, isLoading, error);
 
-  useEffect(() => {
-    loadOrderedList();
-  }, []);
+  if (isLoading) {
+    return <BigAlert title="⌛" message="로딩중..." />;
+  }
 
-  if (isError) {
-    throw new Error();
+  if (error) {
+    throw new Error(error?.message);
+  }
+
+  if (response === null) {
+    return null;
   }
 
   return (
-    <>
+    <div>
       {
-        orderList.length > 0
+        response.orderResponses.length > 0
           ?
-          [...orderList.sort((a, b) => b.orderId - a.orderId)].map(
+          [...response.orderResponses.sort((a, b) => b.orderId - a.orderId)].map(
             (orderItem, i) => (
               <OrderItem key={i} orderItem={orderItem} goDetail />
             )
@@ -43,8 +38,10 @@ function OrderList() {
           :
           <BigAlert title="텅" message="주문 이력이 없습니다." goHome />
       }
-    </>
+    </div>
   );
 }
 
 export default OrderList;
+
+
