56d2e9317d66c0c2d926447aea65d07cecb4873b
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-06-09 09:40:41 +0900

    refactor: 분리된 2개의 분기 삼항식 적용

--- src/components/OrderCardList/OrderCardList.tsx
+++ src/components/OrderCardList/OrderCardList.tsx
@@ -7,7 +7,7 @@ import LoadingErrorCard from 'components/LoadingErrorCard/LoadingErrorCard';
 import EmptyDataCard from 'components/EmptyDataCard/EmptyDataCard';
 
 const OrderCardList = () => {
-  const { data: orderList, isLoading, errorState, fetchData } = useFetch<Order[]>(getOrderList);
+  const { data: orderList = [], isLoading, errorState, fetchData } = useFetch<Order[]>(getOrderList);
 
   if (isLoading) return <div>주문목록 로딩중</div>;
   if (errorState?.isError) {
@@ -16,8 +16,15 @@ const OrderCardList = () => {
 
   return (
     <Box sizing={{ width: '100%' }} flex={{ flexDirection: 'column', gap: '60px' }} role="list">
-      {orderList && orderList.map((order) => <OrderCard key={order.orderId} order={order} />)}
-      {orderList?.length === 0 && <EmptyDataCard>주문 목록이 비어있어요</EmptyDataCard>}
+      {orderList && orderList.length > 0 ? (
+        <>
+          {orderList.map((order) => (
+            <OrderCard key={order.orderId} order={order} />
+          ))}
+        </>
+      ) : (
+        <EmptyDataCard>주문 목록이 비어있어요</EmptyDataCard>
+      )}
     </Box>
   );
 };
