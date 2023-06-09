01be84c94c88f36a0370e6bec17bbc3d871f030d
Dahye <06robin11@gmail.com>
2023-06-09 10:27:03 +0900

    chore: 서버 상수 type명시

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
+import { Server, ServerName } from 'types/server';
+
 export const ROUTES = {
   PRODUCT_LIST: '/',
   CART_LIST: '/cart',
@@ -5,7 +7,7 @@ export const ROUTES = {
   ORDERED_DETAIL: '/order_detail',
 } as const;
 
-export const SERVERS = {
+export const SERVERS: Readonly<Record<ServerName, Server>> = {
   여우: 'https://www.backfoxxx.shop',
   프론트: 'api',
   루쿠: 'https://www.woowacourse.shop',
