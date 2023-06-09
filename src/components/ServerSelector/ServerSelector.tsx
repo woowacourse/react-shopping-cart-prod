ad34fd253f8614f56b20c5748f8e9a99b5a4afe5
WaiNaat <qoli0703@gmail.com>
2023-06-10 02:10:00 +0900

    refactor: 서버 변경 시 주문 상세 정보 상태 초기화

--- src/components/ServerSelector/ServerSelector.tsx
+++ src/components/ServerSelector/ServerSelector.tsx
@@ -6,14 +6,17 @@ import { BASE_URL } from '../../constant';
 import cartState from '../../globalState/atoms/cartState';
 import serverNameState from '../../globalState/atoms/serverName';
 import { isProperServerName } from '../../types/server';
+import orderDetailState from '../../globalState/atoms/orderDetail';
 
 const ServerSelector = () => {
   const [serverName, setServerName] = useRecoilState(serverNameState);
   const resetCart = useResetRecoilState(cartState);
+  const resetOrderDetail = useResetRecoilState(orderDetailState);
   const navigate = useNavigate();
 
   useEffect(() => {
     resetCart();
+    resetOrderDetail();
     navigate('/', { replace: true });
   }, [serverName]);
 
